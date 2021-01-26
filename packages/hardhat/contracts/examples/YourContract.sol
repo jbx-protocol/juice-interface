4ac1a4417419c3de911fe118af7776fe28cbca8b
jovritt <ritterjv@protonmail.com>
2021-01-25 21:42:05 -0500

    fountain -> juice

--- packages/hardhat/contracts/examples/YourContract.sol
+++ packages/hardhat/contracts/examples/YourContract.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import "../abstract/JuiceAdmin.sol";
 
-/// @dev This contract is an example of how you can use Fountain to fund your own project.
+/// @dev This contract is an example of how you can use Juice to fund your own project.
 contract YourContract is JuiceAdmin {
     constructor(
         IJuicer _controller,

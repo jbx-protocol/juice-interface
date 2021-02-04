1981fe96e612cc923093af9d9eef9db442ed5680
jovritt <ritterjv@protonmail.com>
2021-02-04 00:01:19 -0500

    Tickets distributed regardless of surplus

--- packages/hardhat/contracts/TicketStore.sol
+++ packages/hardhat/contracts/TicketStore.sol
@@ -136,35 +136,6 @@ contract TicketStore is Store, ITicketStore {
     }
 
     /**
-        @notice Gets the amount of an owner's tickets that a holder has.
-        @param _issuer The issuer of the Ticket to get a value for.
-        @param _holder The ticket holder to get a value for.
-        @return _value The value.
-    */
-    function getTicketBalance(address _issuer, address _holder)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        return tickets[_issuer].balanceOf(_holder);
-    }
-
-    /**
-        @notice Gets the total circulating supply of an owner's tickets.
-        @param _issuer The issuer of the Ticket to get a value for.
-        @return _value The value.
-    */
-    function getTicketSupply(address _issuer)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        return tickets[_issuer].totalSupply();
-    }
-
-    /**
         @notice Gets the reward token for the provided issuer's Tickets.
         @param _issuer The issuer of the Ticket to get a value for.
         @return _value The value.

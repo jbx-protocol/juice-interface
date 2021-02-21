dce82beab804ce3f3f5dc74eea21af995a6416b2
ty.h <tylerxhayes@gmail.com>
2021-02-21 13:32:10 -0500

    local provider fix

--- packages/app/src/constants/local-provider.ts
+++ packages/app/src/constants/local-provider.ts
@@ -3,16 +3,11 @@ import { JsonRpcProvider } from '@ethersproject/providers'
 import { NetworkName } from '../models/network-name'
 import { NETWORKS } from './networks'
 
-const targetNetwork =
-  NETWORKS[
-    (process.env.REACT_APP_INFURA_NETWORK as NetworkName) ||
+const networkName: NetworkName =
+  process.env.NODE_ENV === 'production'
+    ? (process.env.REACT_APP_INFURA_NETWORK as NetworkName) ||
+      NetworkName.mainnet
+    : (process.env.REACT_APP_INFURA_DEV_NETWORK as NetworkName) ||
       NetworkName.localhost
-  ]
 
-const localProviderUrl = targetNetwork.rpcUrl
-
-const localProviderUrlFromEnv = process.env.REACT_APP_PROVIDER
-  ? process.env.REACT_APP_PROVIDER
-  : localProviderUrl
-
-export const localProvider = new JsonRpcProvider(localProviderUrlFromEnv)
+export const localProvider = new JsonRpcProvider(NETWORKS[networkName].rpcUrl)

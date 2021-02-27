83205ebdfa9b24d193d8eb519d729bc50222e1c7
jovritt <ritterjv@protonmail.com>
2021-02-27 17:16:44 -0500

    copy edits

--- packages/app/src/components/Faq.tsx
+++ packages/app/src/components/Faq.tsx
@@ -12,7 +12,7 @@ const QAs: {
     q: 'Who funds Juice projects?',
     a: [
       `Users fund your project by paying to use your app or service, or as an investor by making a payment directly to your project's smart contract (like on juice.work).`,
-      `For users paying through your app, you should route those funds through Juice to ensure they receive tickets in exchange. *Link: Using Juice composibly.*`,
+      `For users paying through your app, you should route those funds through the Juice smart contracts to ensure they receive tickets in exchange. *Link: Using Juice composibly.*`,
     ],
   },
   {
@@ -23,15 +23,33 @@ const QAs: {
     ],
   },
   {
-    q: `Why should I invest in a project's tickets?`,
+    q: 'What is a discount rate?',
     a: [
-      `Tickets can be redeemed for a portion of a project's overflow. If the amount of overflow a project has increases over time, the value of its tickets will also go up.`,
+      "Juice projects are created with an optional discount rate that determines how many tickets are distributed in exchange for money contributed.",
+      "For example, with a discount rate of 97%, 100 DAI paid to a project today might provide you 100 tickets, but 100 DAI during the next budgeting period would only provide you 97 tickets.",
+      "This incentivize people to fund projects earlier.",
+    ],
+  },
+  {
+    q: `Why should I want a project's tickets?`,
+    a: [
+      "Tickets can be redeemed for a portion of a project's overflow.",
+      "If you earned a project's tickets at a discounted rate a while ago, the tickets will accumulate value over time as the project's overflow increases",
     ],
   },
   {
     q: 'What is a discount rate?',
     a: [
-      `Juice projects are created with an optional discount rate that allows tickets to be earned and redeemed on a bonding curve. This helps incentivize investors to fund projects earlier, and hodl tickets longer.`,
+      'Juice projects are created with an optional discount rate that determines how many tickets are distributed when payments are made to the project.',
+      'For example, with a discount rate of 97%, 100 DAI paid to a project today might give you 100 tickets, but 100 DAI during the next budgeting period would only give you 97 tickets.', 
+      'This incentivize people to fund projects earlier.',
+    ],
+  },
+  {
+    q: 'What is a bonding curve?',
+    a: [
+      "A bonding curve keeps tickets from being redeemable exploitatively.",
+      "Those who wait longer to redeem tickets compared to others will benefit the most from a project's overflow"
     ],
   },
   {
@@ -49,12 +67,13 @@ const QAs: {
     q: `Does eliminating profits remove incentive for project creators to innovate?`,
     a: [
       `If a maintainer team neglects a project or moves it in a direction that doesn’t work for the market, it could be forked by the community or simply given up by its users. Either of these would compromise paychecks for the project.`,
+      "On the other hand, projects with a clear drive and sense of direction can propose ambitious budgets with their community's backing that allows them to charge forward, in the name of greater overflow for everyone later."
     ],
   },
   {
     q: `Can’t I still rake in unlimited profit if I set my budget target to a bajillion?`,
     a: [
-      `Sure. Although if the market prefers to support projects with more realistic budgets, there’s a good chance yours will be forked or outcompeted.`,
+      `Sure, although open source projects can always be forked. Or if the market prefers to support projects with more realistic budgets, yours might get outcompeted.`,
     ],
   },
 ]

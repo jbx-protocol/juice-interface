9196f4677f8598350d00a0ffef9cf20125eacab8
jovritt <ritterjv@protonmail.com>
2021-02-28 10:38:34 -0500

    copy edits

--- packages/app/src/components/configure-budget-steps/contractStep.tsx
+++ packages/app/src/components/configure-budget-steps/contractStep.tsx
@@ -26,7 +26,7 @@ export function contractStep({
       ></BudgetForm>
     ),
     info: [
-      "Your contract will create a budgeting time frame that'll begin accepting payments right away, and up until the time frame runs out.",
+      "Your contract will create a budget that'll begin accepting payments right away, and up until the time frame runs out.",
       'A new budgeting time frame will be created automatically once the current one expires so that you can continue collecting money.',
       "You can make changes to your contract's specs for future budgeting time frames under a certain condition – more on this in step 2.",
     ],

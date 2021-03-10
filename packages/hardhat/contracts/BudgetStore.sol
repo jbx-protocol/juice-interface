f122bd8625a880162c43b53db31eb6bab2689537
mejango <mejango@protonmail.com>
2021-03-09 21:23:56 -0500

    fixed weight

--- packages/hardhat/contracts/BudgetStore.sol
+++ packages/hardhat/contracts/BudgetStore.sol
@@ -25,7 +25,7 @@ contract BudgetStore is Store, IBudgetStore {
     // --- public properties --- //
 
     /// @notice A big number to base ticket issuance off of.
-    uint256 public constant BUDGET_BASE_WEIGHT = 1E25;
+    uint256 public constant BUDGET_BASE_WEIGHT = 10E25;
 
     /// @notice The latest Budget ID for each project address.
     mapping(address => uint256) public override latestBudgetId;

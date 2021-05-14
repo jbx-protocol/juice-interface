e111a37dd3dbe45ec66649e1d7c92d730cba40f1
mejango <mejango@protonmail.com>
2021-05-14 10:51:15 -0400

    addMod and removeMod => setMods

--- packages/hardhat/contracts/ModStore.sol
+++ packages/hardhat/contracts/ModStore.sol
@@ -15,9 +15,6 @@ contract ModStore is IModStore {
     /// @notice A contract storing operator assignments.
     IOperatorStore public immutable override operatorStore;
 
-    /// @notice the total number of mods.
-    uint256 public override count = 0;
-
     /** 
       @param _projects A Projects contract which mints ERC-721's that represent project ownership and transfers.
       @param _operatorStore A contract storing operator assignments.
@@ -44,15 +41,15 @@ contract ModStore is IModStore {
     /** 
       @notice Adds a mod to the list.
       @param _projectId The project to add a mod to.
-      @param _beneficiary The address being funded from your tapped amount.
-      @param _amount The amount to send to the beneficiary of this mod. If 0, the percent will be used instead.
-      @param _percent The percent of your target amount to send to the beneficiary of this mod. Out of 1000.
+      @param _beneficiaries The addresses being funded from your tapped amount.
+      @param _amounts The amounts to send to the beneficiary of this mod. If 0, the percent will be used instead.
+      @param _percents The percents of your target amount to send to the beneficiary of this mod. Out of 1000.
     */
-    function addMod(
+    function setMods(
         uint256 _projectId,
-        address payable _beneficiary,
-        uint256 _amount,
-        uint256 _percent
+        address payable[] memory _beneficiaries,
+        uint256[] memory _amounts,
+        uint256[] memory _percents
     ) external override {
         // Get a reference to the project owner.
         address _owner = projects.ownerOf(_projectId);
@@ -62,54 +59,34 @@ contract ModStore is IModStore {
             msg.sender == _owner ||
                 operatorStore.operatorLevel(_owner, _projectId, msg.sender) >=
                 2,
-            "Juicer::addMod: UNAUTHORIZED"
+            "Juicer::setMods: UNAUTHORIZED"
         );
 
-        // Either the amount or the percent must be specified.
+        // The params must be of equal lengths.
         require(
-            _amount > 0 || _percent > 0,
-            "ModStore::addMod: UNSPECIFIED_PORTION"
+            _beneficiaries.length == _amounts.length &&
+                _beneficiaries.length == _percents.length,
+            "ModStore::setMods: BAD_ARGS"
         );
 
-        // The percent should be less than 1000.
-        require(_percent <= 1000, "ModStore::addMod: BAD_PERCENT");
-
-        // Increment the count.
-        count++;
-
-        // Push the new mod into the project's list of mods.
-        mods[_projectId].push(
-            Mod(_beneficiary, uint16(_percent), _amount, count)
-        );
-
-        emit AddMod(_projectId, count, _beneficiary, _amount, _percent);
-    }
-
-    /** 
-      @notice Removes a mod from the list.
-      @param _projectId The project to remove a mod from.
-      @param _id The id of the mod to remove.
-    */
-    function removeMod(uint256 _projectId, uint256 _id) external override {
-        // Get a reference to the project owner.
-        address _owner = projects.ownerOf(_projectId);
-
-        // Only the project owner, or a delegated operator of level 2 or greater, can remove a mod.
-        require(
-            msg.sender == _owner ||
-                operatorStore.operatorLevel(_owner, _projectId, msg.sender) >=
-                2,
-            "Juicer::removeMod: UNAUTHORIZED"
-        );
-
-        // Get all of the project's mods into memory.
-        Mod[] memory _mods = mods[_projectId];
-        // Delete the storage value.
         delete mods[_projectId];
-        // Add the non deleted mods into storage.
-        for (uint256 _i = 0; _i < _mods.length; _i++)
-            if (_mods[_i].id != _id) mods[_projectId].push(_mods[_i]);
 
-        emit RemoveMod(_projectId, _id);
+        for (uint256 _i = 0; _i < _beneficiaries.length; _i++) {
+            // Either the amount or the percent must be specified.
+            require(
+                _amounts[_i] > 0 || _percents[_i] > 0,
+                "ModStore::setMods: UNSPECIFIED_PORTION"
+            );
+
+            // The percent should be less than 1000.
+            require(_percents[_i] <= 1000, "ModStore::setMods: BAD_PERCENT");
+
+            // Push the new mod into the project's list of mods.
+            mods[_projectId].push(
+                Mod(_beneficiaries[_i], uint16(_percents[_i]), _amounts[_i])
+            );
+        }
+
+        emit SetMods(_projectId, _beneficiaries, _amounts, _percents);
     }
 }
--- packages/hardhat/contracts/interfaces/IModStore.sol
+++ packages/hardhat/contracts/interfaces/IModStore.sol
@@ -10,34 +10,26 @@ struct Mod {
     // Optional. Either specify amount or percent (out of 1000).
     uint16 percent;
     uint256 amount;
-    uint256 id;
 }
 
 interface IModStore {
-    event AddMod(
+    event SetMods(
         uint256 indexed projectId,
-        uint256 indexed id,
-        address indexed beneficiary,
-        uint256 amount,
-        uint256 percent
+        address payable[] _beneficiaries,
+        uint256[] _amounts,
+        uint256[] _percents
     );
-
-    event RemoveMod(uint256 indexed projectId, uint256 indexed id);
 
     function projects() external view returns (IProjects);
 
     function operatorStore() external view returns (IOperatorStore);
 
-    function count() external view returns (uint256);
-
     function allMods(uint256 _projectId) external view returns (Mod[] memory);
 
-    function addMod(
+    function setMods(
         uint256 _projectId,
-        address payable _beneficiary,
-        uint256 _amount,
-        uint256 _percent
+        address payable[] memory _beneficiaries,
+        uint256[] memory _amounts,
+        uint256[] memory _percents
     ) external;
-
-    function removeMod(uint256 _projectId, uint256 _id) external;
 }

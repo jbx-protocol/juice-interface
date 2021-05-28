02eaaf1fc14608578d0c9e38b8e5a45773535369
mejango <mejango@protonmail.com>
2021-05-28 15:26:40 -0400

    Tests design down

--- packages/hardhat/contracts/Projects.sol
+++ packages/hardhat/contracts/Projects.sol
@@ -2,14 +2,12 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "./abstract/Administered.sol";
 import "./interfaces/IProjects.sol";
 import "./libraries/Operations.sol";
 
 // Stores project ownership and identifying information.
-// ERC721URIStorage,
 contract Projects is ERC721Enumerable, IProjects, Administered {
     // --- private properties --- //
 
--- packages/hardhat/test/behaviors/operator_store/index.js
+++ packages/hardhat/test/behaviors/operator_store/index.js
@@ -1,8 +1,16 @@
+const { ethers } = require("hardhat");
 const shouldBehaveLike = require("./behaviors");
 
-module.exports = () => {
-  describe("Set operator", shouldBehaveLike.setOperator);
-  describe("Set operators", shouldBehaveLike.setOperators);
+const contractName = "OperatorStore";
+
+module.exports = function() {
+  before(async function() {
+    const contractArtifacts = await ethers.getContractFactory(contractName);
+    this.contract = await contractArtifacts.deploy();
+    await this.contract.deployTransaction.wait();
+  });
+  // describe("Set operator", shouldBehaveLike.setOperator);
+  // describe("Set operators", shouldBehaveLike.setOperators);
   describe("Has permission", shouldBehaveLike.hasPermission);
-  describe("Has permissions", shouldBehaveLike.hasPermissions);
+  // describe("Has permissions", shouldBehaveLike.hasPermissions);
 };

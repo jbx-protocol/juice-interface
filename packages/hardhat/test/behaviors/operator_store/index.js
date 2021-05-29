46b272ba8c10dd5454f86cc38e3ff2fd7a87e53f
mejango <mejango@protonmail.com>
2021-05-28 23:27:59 -0400

    A few more comments to tests

--- packages/hardhat/test/behaviors/operator_store/index.js
+++ packages/hardhat/test/behaviors/operator_store/index.js
@@ -4,11 +4,13 @@ const shouldBehaveLike = require("./behaviors");
 const contractName = "OperatorStore";
 
 module.exports = function() {
+  // Before the tests, deploy the contract.
   before(async function() {
     const contractArtifacts = await ethers.getContractFactory(contractName);
     this.contract = await contractArtifacts.deploy();
-    await this.contract.deployTransaction.wait();
   });
+
+  // Test each function.
   describe("setOperator(...)", shouldBehaveLike.setOperator);
   describe("setOperators(...)", shouldBehaveLike.setOperators);
   describe("hasPermission(...)", shouldBehaveLike.hasPermission);
--- packages/hardhat/test/behaviors/prices/index.js
+++ packages/hardhat/test/behaviors/prices/index.js
@@ -4,11 +4,13 @@ const shouldBehaveLike = require("./behaviors");
 const contractName = "Prices";
 
 module.exports = function() {
+  // Before the tests, deploy the contract.
   before(async function() {
     const contractArtifacts = await ethers.getContractFactory(contractName);
     this.contract = await contractArtifacts.deploy();
-    await this.contract.deployTransaction.wait();
   });
+
+  // Test each function.
   describe("addFeed(...)", shouldBehaveLike.addFeed);
   // describe("getETHPrice(...)", shouldBehaveLike.getETHPrice);
 };

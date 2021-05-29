28d33631d351ea6b8a47de69a010a60a61b9ad88
mejango <mejango@protonmail.com>
2021-05-28 23:27:59 -0400

    A few more comments to tests

--- packages/hardhat/test/index.js
+++ packages/hardhat/test/index.js
@@ -7,14 +7,21 @@ const snapshot = () => ethers.provider.send("evm_snapshot", []);
 const restore = id => ethers.provider.send("evm_revert", [id]);
 
 describe("Juice", async function() {
+  // Bind a reference to the deployer address, and an array of other addresses to `this`.
   before(async function() {
     [this.deployer, ...this.addrs] = await ethers.getSigners();
   });
+
+  // Before each test, take a snapshot of the contract state.
   beforeEach(async function() {
     snapshotId = await snapshot();
   });
+
+  // Test each contract.
   describe("OperatorStore", shouldBehaveLike.operatorStore);
   describe("Prices", shouldBehaveLike.prices);
+
+  // After each test, restore the contract state.
   afterEach(async function() {
     await restore(snapshotId);
   });

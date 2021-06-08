5beb373ade25e341b50600ac275175740dbd9bed
mejango <mejango@protonmail.com>
2021-06-08 00:16:25 -0400

    finished tests for currentBallotState

--- packages/hardhat/test/behaviors/funding_cycles/behaviors/index.js
+++ packages/hardhat/test/behaviors/funding_cycles/behaviors/index.js
@@ -2,10 +2,12 @@ const configure = require("./configure");
 const tap = require("./tap");
 const getCurrent = require("./get_current");
 const getQueued = require("./get_queued");
+const currentBallotState = require("./current_ballot_state");
 
 module.exports = {
   configure,
   tap,
   getCurrent,
-  getQueued
+  getQueued,
+  currentBallotState
 };

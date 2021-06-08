aae4f05ace3b66a6bcc9631a1e62eb7035ab6c85
mejango <mejango@protonmail.com>
2021-06-08 15:50:27 -0400

    moved time test helpers into funding cycles test dir

--- packages/hardhat/test/behaviors/funding_cycles/index.js
+++ packages/hardhat/test/behaviors/funding_cycles/index.js
@@ -1,6 +1,40 @@
+const { ethers } = require("hardhat");
 const shouldBehaveLike = require("./behaviors");
 
 const contractName = "FundingCycles";
+
+/** 
+  These tests rely on time manipulation quite a bit, which as far as i understand is hard to do precisely. 
+  Ideally, the tests could mock the block.timestamp to preset numbers, but instead 
+  they rely on 'fastforwarding' the time between operations. Fastforwarding creates a
+  high probability that the subsequent operation will fall on a block with the intended timestamp,
+  but there's a small chance that there's an off-by-one error. 
+
+  If anyone has ideas on how to mitigate this, please let me know.
+*/
+
+let timeMark;
+const getTimestamp = async block => {
+  return ethers.BigNumber.from(
+    (await ethers.provider.getBlock(block || "latest")).timestamp
+  );
+};
+const setTimeMark = async blockNumber => {
+  timeMark = await getTimestamp(blockNumber);
+};
+const fastforward = async seconds => {
+  // eslint-disable-next-line no-await-in-loop
+  const now = await getTimestamp();
+  const timeSinceTimemark = now.sub(timeMark);
+  timeMark = now;
+
+  // Subtract away any time that has already passed between the start of the test,
+  // or from the last fastforward, from the provided value.
+  await ethers.provider.send("evm_increaseTime", [
+    seconds.toNumber() - timeSinceTimemark
+  ]);
+  await ethers.provider.send("evm_mine");
+};
 
 module.exports = function() {
   // Before the tests, deploy mocked dependencies and the contract.
@@ -10,6 +44,15 @@ module.exports = function() {
 
     // Deploy the contract.
     this.contract = await this.deployContract(contractName);
+
+    // Bind the ability to manipulate time to `this`.
+    this.fastforward = fastforward;
+    this.getTimestamp = getTimestamp;
+    this.setTimeMark = setTimeMark;
+  });
+  beforeEach(async function() {
+    // Mark the start time of each test.
+    timeMark = await getTimestamp();
   });
 
   // Test each function.
--- packages/hardhat/test/index.js
+++ packages/hardhat/test/index.js
@@ -1,5 +1,4 @@
-const { ethers } = require("hardhat");
-const bre = require("hardhat");
+const { ethers, config } = require("hardhat");
 const fs = require("fs");
 
 const { deployMockContract } = require("@ethereum-waffle/mock-contract");
@@ -7,43 +6,14 @@ const { deployMockContract } = require("@ethereum-waffle/mock-contract");
 const shouldBehaveLike = require("./behaviors");
 
 let snapshotId;
-let timeMark;
 
 const snapshot = () => ethers.provider.send("evm_snapshot", []);
 const restore = id => ethers.provider.send("evm_revert", [id]);
-const getTimestamp = async block => {
-  return ethers.BigNumber.from(
-    (await ethers.provider.getBlock(block || "latest")).timestamp
-  );
-};
-const mineBlock = () => ethers.provider.send("evm_mine");
-const setTimeMark = async blockNumber => {
-  timeMark = await getTimestamp(blockNumber);
-};
-const fastforward = async seconds => {
-  // eslint-disable-next-line no-await-in-loop
-  const now = await getTimestamp();
-  const timeSinceTimemark = now.sub(timeMark);
-  timeMark = now;
-
-  // Subtract away any time that has already passed between the start of the test,
-  // or from the last fastforward, from the provided value.
-  await ethers.provider.send("evm_increaseTime", [
-    seconds.toNumber() - timeSinceTimemark
-  ]);
-  await ethers.provider.send("evm_mine");
-};
 
 describe("Juice", async function() {
   before(async function() {
     // Bind a reference to the deployer address and an array of other addresses to `this`.
     [this.deployer, ...this.addrs] = await ethers.getSigners();
-
-    // Bind the ability to manipulate time to `this`.
-    this.fastforward = fastforward;
-    this.getTimestamp = getTimestamp;
-    this.mineBlock = mineBlock;
-    this.setTimeMark = setTimeMark;
 
     // Bind a reference to a function that can deploy mock contracts from an abi.
     this.deployMockContract = abi => deployMockContract(this.deployer, abi);
@@ -53,7 +23,7 @@ describe("Juice", async function() {
       // Deploy mock contracts.
       const mockArtifacts = fs
         .readFileSync(
-          `${bre.config.paths.artifacts}/contracts/${mockContractName}.sol/${mockContractName}.json`
+          `${config.paths.artifacts}/contracts/${mockContractName}.sol/${mockContractName}.json`
         )
         .toString();
 
@@ -70,9 +40,6 @@ describe("Juice", async function() {
   // Before each test, take a snapshot of the contract state.
   beforeEach(async function() {
     snapshotId = await snapshot();
-
-    // Mark the start time of each test.
-    timeMark = await getTimestamp();
   });
 
   // Test each contract.

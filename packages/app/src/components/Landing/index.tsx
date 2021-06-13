2f542f6b963d78da8a2a94a516c9433a9c9672a3
mejango <mejango@protonmail.com>
2021-06-13 16:56:51 -0400

    header

--- packages/app/src/components/Landing/index.tsx
+++ packages/app/src/components/Landing/index.tsx
@@ -82,7 +82,7 @@ export default function Landing() {
                   paddingBottom: 60,
                 }}
               >
-                {bigHeader('A business model for the open internet')}
+                {bigHeader('A business model for the people')}
                 <div>
                   <p style={{ fontWeight: 500, fontSize: '1.14rem' }}>
                     On Juice, people say up front how much cashflow they need in order

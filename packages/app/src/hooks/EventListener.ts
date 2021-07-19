e80fea316cc945091e8a52fe0da1fb73b069e996
Teddy Wilson <teddywilson@google.com>
2021-07-19 18:18:23 -0400

    Effect dependencies

--- packages/app/src/hooks/EventListener.ts
+++ packages/app/src/hooks/EventListener.ts
@@ -66,7 +66,7 @@ export default function useEventListener<E>({
         setShouldGetHistory(false)
       })
     }
-  }, [shouldGetHistory, contract, filter])
+  }, [shouldGetHistory, filter, contract])
 
   // Setup listener for future events
   useEffect(() => {

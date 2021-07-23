5737a616279dfdb72909d28208be31462c814e5c
peripheralist <peripheralist@gmail.com>
2021-07-22 23:15:31 -0400

    subgraph endpoint

--- packages/app/src/constants/subgraphs.ts
+++ packages/app/src/constants/subgraphs.ts
@@ -1,2 +1,4 @@
 export const subgraphUrl =
-  'https://api.studio.thegraph.com/query/2231/juicebox/0.0.15'
+  process.env.NODE_ENV === 'development'
+    ? 'https://api.studio.thegraph.com/query/2231/juicebox/0.0.15'
+    : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`

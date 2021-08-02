85743f927d4b71c3e9b8144c743598e3c081fb06
peripheralist <peripheralist@gmail.com>
2021-08-02 10:29:14 -0400

    update subgraph query url

--- packages/app/src/constants/subgraphs.ts
+++ packages/app/src/constants/subgraphs.ts
@@ -1,7 +1,4 @@
 export const subgraphUrl =
-  'https://api.studio.thegraph.com/query/2231/juicebox/0.1.0'
-
-// export const subgraphUrl =
-//   process.env.NODE_ENV === 'development'
-//     ? 'https://api.studio.thegraph.com/query/2231/juicebox/0.1.0'
-//     : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`
+  process.env.NODE_ENV === 'development'
+    ? 'https://api.studio.thegraph.com/query/2231/juicebox/0.1.0'
+    : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`

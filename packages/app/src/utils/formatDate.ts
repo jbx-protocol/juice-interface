756541fd029372997c71c56fd3ebbd93a5d2344e
odd-amphora <odd_amphora@protonmail.com>
2021-08-09 18:42:00 -0400

    Style consistency

--- packages/app/src/utils/formatDate.ts
+++ packages/app/src/utils/formatDate.ts
@@ -4,6 +4,5 @@ import moment from 'moment'
 export const formatDate = (dateMillis: BigNumberish, format = 'M-D-YY h:mma') =>
   moment(BigNumber.from(dateMillis).toNumber()).format(format)
 
-export const formatHistoricalDate = (dateMillis: BigNumberish) => {
-  return `${moment(BigNumber.from(dateMillis).toNumber()).fromNow(true)} ago`
-}
+export const formatHistoricalDate = (dateMillis: BigNumberish) =>
+  `${moment(BigNumber.from(dateMillis).toNumber()).fromNow(true)} ago`

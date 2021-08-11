f17426e007b69e1f8d67d1d2e149431cf748e95c
peripheralist <peripheralist@gmail.com>
2021-08-11 13:08:42 -0400

    show owner balance in spending block

--- packages/app/src/components/Navbar/Balance.tsx
+++ packages/app/src/components/Navbar/Balance.tsx
@@ -2,54 +2,54 @@ import { BigNumber } from '@ethersproject/bignumber'
 import EthPrice from 'components/Dashboard/EthPrice'
 import { NetworkContext } from 'contexts/networkContext'
 import { ThemeContext } from 'contexts/themeContext'
-import { useEtherPrice } from 'hooks/EtherPrice'
 import { usePoller } from 'hooks/Poller'
 import { useContext, useState } from 'react'
 import { formatWad } from 'utils/formatNumber'
 
-export default function Balance({ userAddress }: { userAddress: string }) {
-  const { signingProvider } = useContext(NetworkContext)
-  const [dollarMode, setDollarMode] = useState(false)
+import CurrencySymbol from '../shared/CurrencySymbol'
+import { readProvider } from '../../constants/readProvider'
+
+export default function Balance({
+  address,
+  showEthPrice,
+}: {
+  address: string | undefined
+  showEthPrice?: boolean
+}) {
   const [balance, setBalance] = useState<BigNumber>()
   const {
     theme: { colors },
   } = useContext(ThemeContext)
 
-  const usdPerEth = useEtherPrice()
-
   // get updated balance
   usePoller(
     () => {
-      if (!userAddress || !signingProvider) return
+      if (!address) return
 
       try {
-        signingProvider.getBalance(userAddress).then(setBalance)
+        readProvider.getBalance(address).then(setBalance)
       } catch (e) {
         console.log('Error getting balance', e)
       }
     },
-    [userAddress, signingProvider],
+    [address],
     30000,
   )
-
-  const displayBalance =
-    usdPerEth && dollarMode
-      ? `$${balance ? formatWad(balance.mul(usdPerEth).toString()) : '--'}`
-      : `${parseFloat(formatWad(balance) ?? '0').toFixed(4)}ETH`
 
   return (
     <div
       style={{
         verticalAlign: 'middle',
-        cursor: 'pointer',
         lineHeight: 1,
       }}
-      onClick={() => setDollarMode(!dollarMode)}
     >
-      {displayBalance}
-      <div style={{ color: colors.text.tertiary }}>
-        <EthPrice />
-      </div>
+      <CurrencySymbol currency={0} />
+      {formatWad(balance) ?? '--'}
+      {showEthPrice && (
+        <div style={{ color: colors.text.tertiary }}>
+          <EthPrice />
+        </div>
+      )}
     </div>
   )
 }

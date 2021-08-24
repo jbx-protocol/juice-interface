158fbcae9cdc70d4a8d7c28ccffc4c7722b8d901
odd-amphora <odd_amphora@protonmail.com>
2021-08-24 10:35:33 -0400

    More configuration stuff

--- packages/app/src/services.ts
+++ packages/app/src/services.ts
@@ -3,6 +3,7 @@ import Onboard from 'bnc-onboard'
 import { readNetwork } from 'constants/networks'
 
 const networkId = readNetwork.chainId;
+// TODO(odd-amphora): rpc url is wrong, re: rinkeby.
 const rpcUrl = `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`
 const dappId = process.env.REACT_APP_BLOCKNATIVE_API_KEY;
 
@@ -28,16 +29,16 @@ export function initOnboard(subscriptions) {
         },
         {
           walletName: 'walletConnect',
-          infuraKey: 'cea9deb6467748b0b81b920b005c10c1'
+          infuraKey: `${process.env.REACT_APP_INFURA_ID}`
         },
-        { walletName: 'cobovault', appName: 'React Demo', rpcUrl },
-        { walletName: 'keystone', appName: 'React Demo', rpcUrl },
-        { walletName: 'keepkey', rpcUrl },
-        {
-          walletName: 'lattice',
-          appName: 'Onboard Demo',
-          rpcUrl
-        },
+        // { walletName: 'cobovault', appName: 'React Demo', rpcUrl },
+        // { walletName: 'keystone', appName: 'React Demo', rpcUrl },
+        // { walletName: 'keepkey', rpcUrl },
+        // {
+        //   walletName: 'lattice',
+        //   appName: 'Onboard Demo',
+        //   rpcUrl
+        // },
         { walletName: 'coinbase' },
         { walletName: 'status' },
         { walletName: 'walletLink', rpcUrl },
@@ -45,30 +46,30 @@ export function initOnboard(subscriptions) {
           walletName: 'portis',
           apiKey: 'b2b7586f-2b1e-4c30-a7fb-c2d1533b153b'
         },
-        { walletName: 'fortmatic', apiKey: 'pk_test_886ADCAB855632AA' },
-        { walletName: 'torus' },
-        { walletName: 'trust', rpcUrl },
-        { walletName: 'opera' },
-        { walletName: 'operaTouch' },
-        { walletName: 'imToken', rpcUrl },
-        { walletName: 'meetone' },
-        { walletName: 'mykey', rpcUrl },
-        { walletName: 'wallet.io', rpcUrl },
-        { walletName: 'huobiwallet', rpcUrl },
-        { walletName: 'alphawallet', rpcUrl },
-        { walletName: 'hyperpay' },
-        { walletName: 'atoken' },
-        { walletName: 'liquality' },
-        { walletName: 'frame' },
-        { walletName: 'tokenpocket', rpcUrl },
-        { walletName: 'authereum', disableNotifications: true },
-        { walletName: 'ownbit' },
+        // { walletName: 'fortmatic', apiKey: 'pk_test_886ADCAB855632AA' },
+        // { walletName: 'torus' },
+        // { walletName: 'trust', rpcUrl },
+        // { walletName: 'opera' },
+        // { walletName: 'operaTouch' },
+        // { walletName: 'imToken', rpcUrl },
+        // { walletName: 'meetone' },
+        // { walletName: 'mykey', rpcUrl },
+        // { walletName: 'wallet.io', rpcUrl },
+        // { walletName: 'huobiwallet', rpcUrl },
+        // { walletName: 'alphawallet', rpcUrl },
+        // { walletName: 'hyperpay' },
+        // { walletName: 'atoken' },
+        // { walletName: 'liquality' },
+        // { walletName: 'frame' },
+        // { walletName: 'tokenpocket', rpcUrl },
+        // { walletName: 'authereum', disableNotifications: true },
+        // { walletName: 'ownbit' },
         { walletName: 'gnosis' },
-        { walletName: 'dcent' },
-        { walletName: 'bitpie' },
-        { walletName: 'xdefi' },
-        { walletName: 'binance' },
-        { walletName: 'tp' },
+        // { walletName: 'dcent' },
+        // { walletName: 'bitpie' },
+        // { walletName: 'xdefi' },
+        // { walletName: 'binance' },
+        // { walletName: 'tp' },
       ]
     },
     walletCheck: [

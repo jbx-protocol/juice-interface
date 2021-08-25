498e48ca19d256fee421358b62084c3fdbda8b7e
odd-amphora <odd_amphora@protonmail.com>
2021-08-25 09:48:08 -0400

    Use more idiomatic way of expressing darkmode and clean up Network.tsx

--- packages/app/src/contexts/networkContext.ts
+++ packages/app/src/contexts/networkContext.ts
@@ -1,7 +1,4 @@
 import { Web3Provider } from '@ethersproject/providers'
-import { Account } from 'bnc-notify'
-import { Wallet } from 'bnc-onboard/dist/src/interfaces'
-import Web3 from 'web3'
 
 import { NetworkName } from 'models/network-name'
 import { createContext } from 'react'

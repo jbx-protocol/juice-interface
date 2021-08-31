19655d7c2a58932f879bfc77409a5349f0001249
peripheralist <peripheralist@gmail.com>
2021-08-31 13:48:51 -0500

    funding cycle history

--- packages/app/src/components/Dashboard/FundingHistory.tsx
+++ packages/app/src/components/Dashboard/FundingHistory.tsx
@@ -1,23 +1,27 @@
+import { CaretRightOutlined } from '@ant-design/icons'
 import { BigNumber } from '@ethersproject/bignumber'
 import { Space } from 'antd'
+import Modal from 'antd/lib/modal/Modal'
 import CurrencySymbol from 'components/shared/CurrencySymbol'
 import Loading from 'components/shared/Loading'
 import { ThemeContext } from 'contexts/themeContext'
 import useContractReader from 'hooks/ContractReader'
 import { ContractName } from 'models/contract-name'
+import { CurrencyOption } from 'models/currency-option'
 import { FundingCycle } from 'models/funding-cycle'
 import { useCallback, useContext, useState } from 'react'
 import { deepEqFundingCycles } from 'utils/deepEqFundingCycles'
+import { formatHistoricalDate } from 'utils/formatDate'
 import { formatWad } from 'utils/formatNumber'
 
 import FundingCycleDetails from '../FundingCycle/FundingCycleDetails'
-import { CurrencyOption } from 'models/currency-option'
 
 export default function FundingHistory({
   startId,
 }: {
   startId: BigNumber | undefined
 }) {
+  const [selectedIndex, setSelectedIndex] = useState<number>()
   const [fundingCycles, setFundingCycles] = useState<FundingCycle[]>([])
   const [cycleNumbers, setCycleNumbers] = useState<BigNumber[]>([])
   const {
@@ -30,6 +34,8 @@ export default function FundingHistory({
   const cycleNumber = allCyclesLoaded
     ? undefined
     : cycleNumbers[cycleNumbers.length - 1]
+  const selectedFC =
+    selectedIndex !== undefined ? fundingCycles[selectedIndex] : undefined
 
   useContractReader<FundingCycle>({
     contract: ContractName.FundingCycles,
@@ -62,35 +68,39 @@ export default function FundingHistory({
         fundingCycles.map((cycle, i) => (
           <div
             key={cycle.id.toString()}
-            style={
-              i < fundingCycles.length - 1
+            onClick={() => setSelectedIndex(i)}
+            style={{
+              display: 'flex',
+              alignItems: 'baseline',
+              justifyContent: 'space-between',
+              cursor: 'pointer',
+              ...(i < fundingCycles.length - 1
                 ? {
                     paddingBottom: 20,
                     borderBottom: '1px solid ' + colors.stroke.tertiary,
                   }
-                : {}
-            }
+                : {}),
+            }}
           >
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'baseline',
-                justifyContent: 'space-between',
-              }}
-            >
+            <Space align="baseline">
               <h3>#{cycle.number.toString()}</h3>
-              <div>
-                <span style={{ fontSize: '.8rem' }}>
-                  <CurrencySymbol
-                    currency={cycle.currency.toNumber() as CurrencyOption}
-                  />
-                  {formatWad(cycle.tapped)}/{formatWad(cycle.target)}
-                </span>{' '}
-                withdrawn
-              </div>
-            </div>
 
-            <FundingCycleDetails fundingCycle={cycle} />
+              <div style={{ fontSize: '.8rem', marginLeft: 10 }}>
+                <CurrencySymbol
+                  currency={cycle.currency.toNumber() as CurrencyOption}
+                />
+                {formatWad(cycle.tapped)}/{formatWad(cycle.target)} withdrawn
+              </div>
+            </Space>
+
+            <div style={{ flex: 1 }}></div>
+
+            <Space align="baseline" style={{ fontSize: '.8rem' }}>
+              {formatHistoricalDate(
+                cycle.start.add(cycle.duration).mul(1000).toNumber(),
+              )}
+              <CaretRightOutlined />
+            </Space>
           </div>
         ))
       ) : (
@@ -104,6 +114,20 @@ export default function FundingHistory({
       {fundingCycleElems}
 
       {allCyclesLoaded ? null : <Loading />}
+
+      {selectedFC && (
+        <Modal
+          visible={!!selectedFC}
+          width={600}
+          title={`Cycle #${selectedFC.number.toString()}`}
+          onCancel={() => setSelectedIndex(undefined)}
+          onOk={() => setSelectedIndex(undefined)}
+          cancelButtonProps={{ hidden: true }}
+          okText="Done"
+        >
+          <FundingCycleDetails fundingCycle={selectedFC} />
+        </Modal>
+      )}
     </div>
   )
 }

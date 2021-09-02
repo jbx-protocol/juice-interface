7faccf3711f4a72d8618f8be65df995d34776339
odd-amphora <odd_amphora@protonmail.com>
2021-09-02 10:05:24 -0400

    Clean up

--- packages/app/src/Network.tsx
+++ packages/app/src/Network.tsx
@@ -3,7 +3,7 @@ import { NETWORKS } from 'constants/networks'
 import { NetworkContext } from 'contexts/networkContext'
 import { ChildElems } from 'models/child-elems'
 import { NetworkName } from 'models/network-name'
-import { useContext, useEffect, useState, useRef } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { readNetwork } from 'constants/networks'
 import { initOnboard } from 'utils/onboard'
 import { API, Subscriptions, Wallet } from 'bnc-onboard/dist/src/interfaces'
@@ -16,13 +16,12 @@ export default function Network({ children }: { children: ChildElems }) {
   const { isDarkMode } = useContext(ThemeContext);
 
   const [signingProvider, setSigningProvider] = useState<Web3Provider>()
-  const [network, setNetwork] = useState<NetworkName>()  
+  const [network, setNetwork] = useState<NetworkName>()
   const [account, setAccount] = useState<string>()
   const [onboard, setOnboard] = useState<API>()
   const previousAccount = usePrevious(account);
 
   const resetWallet = () => {
-    console.log('reset');
     onboard?.walletReset()
     setSigningProvider(undefined);
     window.localStorage.setItem(KEY_SELECTED_WALLET, "")
@@ -59,11 +58,9 @@ export default function Network({ children }: { children: ChildElems }) {
 
     const selectWallet = async (newWallet: Wallet) => {
       if (newWallet.provider) {
-        console.log('hi');
         setSigningProvider(new Web3Provider(newWallet.provider))
         window.localStorage.setItem(KEY_SELECTED_WALLET, newWallet.name || "")
       } else {
-        console.log('bye');
         resetWallet();
       }
     }

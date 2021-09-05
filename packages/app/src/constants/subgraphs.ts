e6fd43cbe20054c12fe09fc74b70be8e27e9c3d9
peripheralist <peripheralist@gmail.com>
2021-09-05 14:41:56 -0500

    temp dev subgraph url config

--- packages/app/src/constants/subgraphs.ts
+++ packages/app/src/constants/subgraphs.ts
@@ -1,11 +1,6 @@
-const deploymentID = 'Qmdv9KXrQR4hb9xeaoAmBDGNWNC7HD8ojPRDUEDqXvwtwe'
+const deploymentID = 'Qma55qUEASRbpSAGupaKSd9XZxz6Fk9hNjFoTRY3XSV8fF'
 
 export const subgraphUrl =
-  process.env.NODE_ENV === 'development'
+  true
     ? 'https://api.studio.thegraph.com/query/2231/juicebox/0.3.0'
     : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/deployments/id/${deploymentID}`
-
-// export const subgraphUrl =
-//   process.env.NODE_ENV === 'development'
-//     ? 'https://api.studio.thegraph.com/query/2231/juicebox/0.1.24'
-//     : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`

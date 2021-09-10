d2d0da6f2e85086be71b49bf6e0bbdce1065af72
peripheralist <peripheralist@gmail.com>
2021-09-10 16:46:09 -0500

    use dev subgraph query url

--- packages/app/src/constants/subgraphs.ts
+++ packages/app/src/constants/subgraphs.ts
@@ -1,4 +1,7 @@
-export const subgraphUrl = `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`
+// export const subgraphUrl = `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`
+
+export const subgraphUrl =
+  'https://api.studio.thegraph.com/query/2231/juicebox/0.3.0'
 
 // export const subgraphUrl =
 //   process.env.NODE_ENV === 'development'

dfc6b69e2f8cc97156fa65075a1313671de5d9cc
odd-amphora <odd_amphora@protonmail.com>
2021-09-20 18:26:47 -0400

    Update subgraph URL for rinkeby

--- packages/app/src/constants/subgraphs.ts
+++ packages/app/src/constants/subgraphs.ts
@@ -1,6 +1,5 @@
-export const subgraphUrl = `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`
-
-// export const subgraphUrl =
-//   process.env.NODE_ENV === 'development'
-//     ? 'https://api.studio.thegraph.com/query/2231/juicebox/0.3.0'
-//     : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`
+// TODO: support local dev.
+export const subgraphUrl =
+  process.env.REACT_APP_INFURA_NETWORK === `rinkeby`
+    ? `https://api.studio.thegraph.com/query/9534/juicebox-rinkeby/v0.0.1`
+    : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`

752f8c2432def26265f6622723ee2993355cee33
peripheralist <peripheralist@gmail.com>
2021-09-28 15:09:37 -0500

    subgraph url

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -1,5 +1,7 @@
+const deploymentID = 'Qma55qUEASRbpSAGupaKSd9XZxz6Fk9hNjFoTRY3XSV8fF'
+
 // TODO: support local dev.
 export const subgraphUrl =
   process.env.REACT_APP_INFURA_NETWORK === `rinkeby`
     ? `https://api.studio.thegraph.com/query/9534/juicebox-rinkeby/v0.0.1`
-    : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/subgraphs/id/0x63a2368f4b509438ca90186cb1c15156713d5834-0`
+    : `https://gateway.thegraph.com/api/${process.env.REACT_APP_SUBGRAPH_API_KEY}/deployments/id/${deploymentID}`

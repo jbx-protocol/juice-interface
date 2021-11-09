27f72f616929485c8682663f5eb7c6ea2b8718c1
peripheralist <peripheralist@gmail.com>
2021-11-09 13:04:40 -0500

    project page tabs

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,40 +1,83 @@
-import { Button, Select } from 'antd'
+import { Button, Select, Space } from 'antd'
 import Loading from 'components/shared/Loading'
 import ProjectsGrid from 'components/shared/ProjectsGrid'
 import { layouts } from 'constants/styles/layouts'
+import { ThemeContext } from 'contexts/themeContext'
 import { useProjects } from 'hooks/Projects'
-import { useState } from 'react'
+import { ProjectState } from 'models/project-visibility'
+import { useContext, useState } from 'react'
 
 type OrderByOption = 'createdAt' | 'totalPaid'
 
 export default function Projects() {
+  const [selectedTab, setSelectedTab] = useState<ProjectState>('active')
   const [orderBy, setOrderBy] = useState<OrderByOption>('totalPaid')
+
+  const {
+    theme: { colors },
+  } = useContext(ThemeContext)
 
   const projects = useProjects({
     orderBy,
     orderDirection: 'desc',
+    filter: selectedTab,
   })
+
+  const tab = (tab: ProjectState) => (
+    <div
+      style={{
+        textTransform: 'uppercase',
+        cursor: 'pointer',
+        ...(tab === selectedTab
+          ? {
+              color: colors.text.primary,
+              fontWeight: 500,
+            }
+          : {
+              color: colors.text.secondary,
+            }),
+      }}
+      onClick={() => setSelectedTab(tab)}
+    >
+      {tab}
+    </div>
+  )
 
   return (
     <div style={{ ...layouts.maxWidth }}>
+      <h1>Projects on Juicebox</h1>
+
       <div
         style={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'baseline',
+          flexWrap: 'wrap',
+          marginBottom: 40,
         }}
       >
-        <h1>Projects on Juicebox</h1>
-        <Select value={orderBy} onChange={setOrderBy} style={{ width: 180 }}>
-          <Select.Option value="totalPaid">Total earned</Select.Option>
-          <Select.Option value="createdAt">Last created</Select.Option>
-        </Select>
-      </div>
+        <div style={{ height: 40 }}>
+          <Space direction="horizontal" size="large">
+            {tab('active')}
+            {tab('archived')}
+          </Space>
+        </div>
 
-      <div style={{ marginBottom: 40 }}>
-        <a href="/#/create">
-          <Button size="small">Create a project</Button>
-        </a>
+        <div>
+          <Space direction="horizontal">
+            <Select
+              value={orderBy}
+              onChange={setOrderBy}
+              style={{ width: 180 }}
+            >
+              <Select.Option value="totalPaid">Total earned</Select.Option>
+              <Select.Option value="createdAt">Last created</Select.Option>
+            </Select>
+            <a href="/#/create" style={{ marginLeft: 10 }}>
+              <Button>New project</Button>
+            </a>
+          </Space>
+        </div>
       </div>
 
       {projects ? <ProjectsGrid projects={projects} /> : <Loading />}

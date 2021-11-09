f573c61adf447aee406fc98fedada12d0bfb7668
peripheralist <peripheralist@gmail.com>
2021-11-09 16:27:41 -0500

    archived project ids by networkName

--- src/constants/archived-projects.ts
+++ src/constants/archived-projects.ts
@@ -1,5 +1,10 @@
-export const archivedProjectIds = [
-  11, // @pxdao
-  27, // @svspool002
-  32, // @fidenza420
-]
+import { NetworkName } from 'models/network-name'
+
+export const archivedProjectIds: Partial<Record<NetworkName, number[]>> = {
+  [NetworkName.mainnet]: [
+    11, // @pxdao
+    27, // @svspool002
+    32, // @fidenza420
+  ],
+  [NetworkName.rinkeby]: [],
+}

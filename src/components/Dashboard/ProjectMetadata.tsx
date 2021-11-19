eae0bba6791c7f763d6a89f18625b906e6213df5
Nikita Kudryavtsev <kud.nikita@gmail.com>
2021-11-19 19:11:51 +0300

    fix(metadata): removed default metadata from index and only consider optionality fields in project metas

--- src/components/Dashboard/ProjectMetadata.tsx
+++ src/components/Dashboard/ProjectMetadata.tsx
@@ -14,12 +14,14 @@ const ProjectMetadata = ({
 }: {
   metadata?: ProjectMetadataV3
 }) => {
+  const isDefault = metadata.name === DEFAULT_METADATA.name
+
   return (
     <Helmet>
       <title>{metadata.name}</title>
       <meta
         property="og:url"
-        content={metadata ? window.location.href : 'https://juicebox.money'}
+        content={!isDefault ? window.location.href : 'https://juicebox.money'}
       />
       <meta property="og:title" content={metadata.name} />
       <meta name="description" content={metadata.description} />
@@ -29,20 +31,20 @@ const ProjectMetadata = ({
         href="/opensearch.xml"
         title="Juicebox"
       ></link>
-      <meta name="twitter:site" content={`@${metadata.twitter}`} />
-      <meta name="twitter:creator" content={`@${metadata.twitter}`} />
-      <meta name="twitter:card" content="summary_large_image" />
+      {metadata.twitter && <meta name="twitter:site" content={`@${metadata.twitter}`} />}
+      {metadata.twitter && <meta name="twitter:creator" content={`@${metadata.twitter}`} />}
+      {metadata.twitter && <meta name="twitter:card" content="summary_large_image" />}
       <meta name="twitter:title" content={metadata.name} />
-      <meta name="twitter:description" content={metadata.description} />
-      <meta property="og:image" content={metadata.logoUri} />
-      <meta property="og:image:alt" content={metadata.description} />
-      <meta property="og:image:width" content={metadata ? '1020' : '2870'} />
-      <meta property="og:image:height" content={metadata ? '1020' : '1245'} />
+      {metadata.description && <meta name="twitter:description" content={metadata.description} />}
+      {metadata.logoUri && <meta property="og:image" content={metadata.logoUri} />}
+      {metadata.description && <meta property="og:image:alt" content={metadata.description} />}
+      <meta property="og:image:width" content={!isDefault ? '1020' : '2870'} />
+      <meta property="og:image:height" content={!isDefault ? '1020' : '1245'} />
       <meta property="og:site_name" content="Juicebox" />
       <meta property="og:type" content="object" />
       <meta property="og:title" content={metadata.name} />
-      <meta property="og:description" content={metadata.description} />
-      <meta name="twitter:image" content={metadata.logoUri} />
+      {metadata.description && <meta property="og:description" content={metadata.description} />}
+      {<meta name="twitter:image" content={metadata.logoUri} />}
     </Helmet>
   )
 }

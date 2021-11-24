ee8b9543ce9b511c7cee709d61381d356c6676c5
peripheralist <peripheralist@gmail.com>
2021-11-23 20:26:14 -0500

    image stretching fix

--- src/components/shared/RichImgPreview.tsx
+++ src/components/shared/RichImgPreview.tsx
@@ -15,6 +15,9 @@ export default function RichImgPreview({
 }) {
   const contentType = useContentType(src)
 
+  const w = width ?? 100
+  const h = height ?? 100
+
   if (
     contentType === 'image/jpeg' ||
     contentType === 'image/jpg' ||
@@ -26,8 +29,10 @@ export default function RichImgPreview({
       <Image
         src={src}
         style={{
-          maxWidth: width ?? 100,
-          maxHeight: height ?? 100,
+          maxWidth: w,
+          maxHeight: h,
+          width: w,
+          height: h,
           ...style,
         }}
       />

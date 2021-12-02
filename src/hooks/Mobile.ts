91d67cd7aa1fe106e4ea834de720ddbc239712a6
Nick Allevato <nicholas.allevato@gmail.com>
2021-12-01 20:58:44 -0500

    Left align addresses and include a new useMobile hook for determining mobile client reliably, even when the browser size changes.

---
+++ src/hooks/Mobile.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+// Copied from react-use, so if we ever decide to include that library,
+// replace this implementation with the react-use version.
+function useMedia(query: string, defaultState: boolean = false) {
+  const [state, setState] = useState(() =>
+    typeof window === 'undefined'
+      ? defaultState
+      : window.matchMedia(query).matches,
+  )
+
+  useEffect(() => {
+    let mounted = true
+    const queryList = window.matchMedia(query)
+    const onChange = () => {
+      if (!mounted) {
+        return
+      }
+      setState(queryList.matches)
+    }
+
+    queryList.addEventListener('change', onChange)
+    setState(queryList.matches)
+
+    return () => {
+      mounted = false
+      queryList.removeEventListener('change', onChange)
+    }
+  }, [query])
+
+  return state
+}
+
+export default function useMobile() {
+  return useMedia('(max-width: 767px)')
+}

4d6cf99340b633d28d802b0acaabbfd42860c506
Jeff Noh <jeff@jnoh.net>
2021-12-04 00:27:49 -0800

    Fix handle

--- src/pages/p/[handle].tsx
+++ src/pages/p/[handle].tsx
@@ -16,6 +16,29 @@ function ProjectPage() {
   return <Dashboard />
 }
 
-export async function getServerSideProps() {}
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch(`https://.../data`)
+//   const data = await res.json()
+
+//   querySubgraph(
+//     {
+//       entity: 'project',
+//       keys: ['createdAt', 'totalPaid'],
+//       where: {
+//         key: 'id',
+//         value: projectId.toString(),
+//       },
+//     },
+//     res => {
+//       if (!res?.projects) return
+//       const project = parseProjectJson(res.projects[0])
+//       setCreatedAt(project.createdAt)
+//       setEarned(project.totalPaid)
+//     },
+//   )
+
+//   return { props: { data } }
+// }
 
 export default ProjectPage

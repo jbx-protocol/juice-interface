6fb0d2ad159aa4fc5c2924c3ca04b7208b7b5a1d
Exekias <odd_amphora@protonmail.com>
2021-12-10 10:02:50 -0500

    Use new v1.1 dependency

--- src/models/contract-name.ts
+++ src/models/contract-name.ts
@@ -1,6 +1,7 @@
 export enum ContractName {
   FundingCycles = 'FundingCycles',
   TerminalV1 = 'TerminalV1',
+  TerminalV1_1 = 'TerminalV1_1',
   ModStore = 'ModStore',
   OperatorStore = 'OperatorStore',
   Prices = 'Prices',

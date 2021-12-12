499bbdc99f8a91066dd6c7614d328cd8f2422bd8
aeolianeth <aeolus.dev@protonmail.com>
2021-12-12 21:12:14 +0930

    Wip: make app test relevant

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,48 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import ProvideReactQuery from '../ReactQuery'
+import store from '../redux/store'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+jest.mock('i18n')
+jest.mock('@lingui/react', () => {
+  return {
+    Trans({ id }: { id: string }) {
+      return id
+    },
+  }
+})
+jest.mock('@lingui/core', () => {
+  return {
+    plural: jest.fn(),
+    i18n: {
+      _: (str: string) => str,
+    },
+  }
+})
+
+test('renders without crashing', () => {
+  global.matchMedia =
+    global.matchMedia ||
+    function () {
+      return {
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      }
+    }
+
+  act(() => {
+    render(
+      <ProvideReactQuery>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ProvideReactQuery>,
+    )
+  })
+
+  expect(
+    screen.getByText('Community funding for people and projects'),
+  ).toBeInTheDocument()
 })

268874586f64bb6453013aaa8703f2c742893ac2
peripheralist <peripheralist@gmail.com>
2021-12-12 20:39:18 -0500

    adminFeePercent -> terminalFee hook

--- src/contexts/userContext.ts
+++ src/contexts/userContext.ts
@@ -1,4 +1,3 @@
-import { BigNumber } from '@ethersproject/bignumber'
 import { Transactor } from 'hooks/Transactor'
 import { Contracts } from 'models/contracts'
 import { createContext } from 'react'
@@ -6,5 +5,4 @@ import { createContext } from 'react'
 export const UserContext: React.Context<{
   contracts?: Contracts
   transactor?: Transactor
-  adminFeePercent?: BigNumber
 }> = createContext({})

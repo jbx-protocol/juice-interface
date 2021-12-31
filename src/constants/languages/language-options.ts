c8f59d88887c757b58530e705ce2be06cd4865e2
Johnny D <johnnyd.eth@protonmail.com>
2021-12-31 12:48:51 +1000

    selector only includes languages with implemented translation (russian and chinese) and updated docs for adding language

--- src/constants/languages/language-options.ts
+++ src/constants/languages/language-options.ts
@@ -5,6 +5,6 @@ export const Languages: Language = {
   en: { code: 'en', name: 'english', short: 'EN', long: 'English' },
   zh: { code: 'zh', name: 'chinese', short: '中文', long: '中文' },
   ru: { code: 'ru', name: 'russian', short: 'RU', long: 'Pусский' },
-  es: { code: 'es', name: 'spanish', short: 'ES', long: 'Español' },
-  tr: { code: 'tr', name: 'turkish', short: 'TR', long: 'Türkçe' },
+  // es: { code: 'es', name: 'spanish', short: 'ES', long: 'Español' },
+  // tr: { code: 'tr', name: 'turkish', short: 'TR', long: 'Türkçe' },
 }

3198e4661bb46ea919d4e08c5e6868724d48ce43
0xzachvai <94148670+0xzachvai@users.noreply.github.com>
2022-01-01 02:01:38 -0500

    graph query tests and formatting tweaks

---
+++ src/utils/__tests__/graph.test.ts
@@ -0,0 +1,79 @@
+import { formatGraphQuery } from '../graph'
+
+test('format basic queries', () => {
+  const query = formatGraphQuery({
+    entity: 'project',
+    keys: ['id', 'creator', 'uri'],
+  })
+
+  expect(query).toBe('{ projects { id creator uri } }')
+})
+
+test('includes id key by default', () => {
+  const query = formatGraphQuery({
+    entity: 'project',
+    keys: [],
+  })
+
+  expect(query).toBe('{ projects { id } }')
+})
+
+test('format with first & skip', () => {
+  const query = formatGraphQuery({
+    entity: 'project',
+    keys: ['id', 'creator', 'uri'],
+    first: 100,
+    skip: 10,
+  })
+
+  expect(query).toBe('{ projects(first: 100, skip: 10) { id creator uri } }')
+})
+
+test('format with single where', () => {
+  const query = formatGraphQuery({
+    entity: 'project',
+    keys: ['id', 'creator', 'uri'],
+    where: {
+      key: 'id',
+      value: '1',
+    },
+  })
+
+  expect(query).toBe('{ projects(where: { id: "1" }) { id creator uri } }')
+})
+
+test('format with multiple where', () => {
+  const query = formatGraphQuery({
+    entity: 'project',
+    keys: ['id', 'creator', 'uri'],
+    where: [
+      {
+        key: 'id',
+        value: '1',
+      },
+      {
+        key: 'creator',
+        value: '123',
+      },
+    ],
+  })
+
+  expect(query).toBe(
+    '{ projects(where: { id: "1", creator: "123" }) { id creator uri } }',
+  )
+})
+
+test('format nested entitites', () => {
+  const query = formatGraphQuery({
+    entity: 'project',
+    keys: [
+      'id',
+      {
+        entity: 'participant',
+        keys: ['id'],
+      },
+    ],
+  })
+
+  expect(query).toBe('{ projects { id participant { id } } }')
+})

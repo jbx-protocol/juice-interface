6a9d309e3173271d25fe8cda50ae8f7175286c57
0xzachvai <94148670+0xzachvai@users.noreply.github.com>
2022-01-05 22:12:15 -0500

    add my projects tab

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -5,19 +5,21 @@ import ProjectsGrid from 'components/shared/ProjectsGrid'
 import { Tab, TabGroup } from 'components/shared/Tab'
 
 import { ThemeContext } from 'contexts/themeContext'
-import { useInfiniteProjectsQuery } from 'hooks/Projects'
-import { ProjectState } from 'models/project-visibility'
+import { NetworkContext } from 'contexts/networkContext'
+import { useInfiniteProjectsQuery, useParticipantsQuery } from 'hooks/Projects'
 import { useContext, useEffect, useRef, useState } from 'react'
 import { Trans, t } from '@lingui/macro'
 
 import { layouts } from 'constants/styles/layouts'
+
+type TabOption = 'active' | 'my projects' | 'archived'
 
 type OrderByOption = 'createdAt' | 'totalPaid'
 
 const pageSize = 20
 
 export default function Projects() {
-  const [selectedTab, setSelectedTab] = useState<ProjectState>('active')
+  const [selectedTab, setSelectedTab] = useState<TabOption>('active')
   const [orderBy, setOrderBy] = useState<OrderByOption>('totalPaid')
 
   const loadMoreContainerRef = useRef<HTMLDivElement>(null)
@@ -25,6 +27,19 @@ export default function Projects() {
   const {
     theme: { colors },
   } = useContext(ThemeContext)
+
+  // To get a user's projects, we get the participants matching their wallet,
+  // and from those filter projects on id.
+  const { userAddress } = useContext(NetworkContext)
+  const { data: participants, isLoading: isLoadingIds } = useParticipantsQuery(
+    userAddress!,
+  )
+  const myProjectIds = participants && participants!.map(p => p.project!)
+
+  const projectStatusFilter =
+    selectedTab === 'active' || selectedTab === 'archived'
+      ? selectedTab
+      : undefined
 
   const {
     data: pages,
@@ -35,8 +50,9 @@ export default function Projects() {
   } = useInfiniteProjectsQuery({
     orderBy,
     pageSize,
+    projectIds: selectedTab === 'my projects' ? myProjectIds : undefined,
     orderDirection: 'desc',
-    filter: selectedTab,
+    filter: projectStatusFilter,
   })
 
   // When we scroll within 200px of our loadMoreContainerRef, fetch the next page.
@@ -89,7 +105,8 @@ export default function Projects() {
       >
         <div style={{ height: 40 }}>
           <TabGroup value={{ selectedTab, setSelectedTab }}>
-            <Tab value="active" text="all" />
+            <Tab value="active" />
+            {userAddress && <Tab value="my projects" />}
             <Tab value="archived" />
           </TabGroup>
         </div>
@@ -110,6 +127,21 @@ export default function Projects() {
           </Space>
         </div>
       </div>
+
+      {selectedTab === 'my projects' && (
+        <p style={{ marginBottom: 40, maxWidth: 800 }}>
+          <Trans>
+            <InfoCircleOutlined /> Juicebox projects that you've contributed to.{' '}
+          </Trans>
+          <span
+            style={{
+              color: colors.text.action.primary,
+              fontWeight: 500,
+              cursor: 'default',
+            }}
+          ></span>
+        </p>
+      )}
 
       {selectedTab === 'archived' && (
         <p style={{ marginBottom: 40, maxWidth: 800 }}>
@@ -135,7 +167,9 @@ export default function Projects() {
       )}
 
       {concatenatedPages && <ProjectsGrid projects={concatenatedPages} />}
-      {(isLoading || isFetchingNextPage) && <Loading />}
+      {(isLoading ||
+        isFetchingNextPage ||
+        (selectedTab === 'my projects' && isLoadingIds)) && <Loading />}
 
       {/* Place a div below the grid that we can connect to an intersection observer */}
       <div ref={loadMoreContainerRef} />

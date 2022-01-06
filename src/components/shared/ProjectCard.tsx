0b4501232b88b2f6f55ee64c985e4ac852ecacb2
0xzachvai <94148670+0xzachvai@users.noreply.github.com>
2022-01-05 21:45:29 -0500

    add archived tag

--- src/components/shared/ProjectCard.tsx
+++ src/components/shared/ProjectCard.tsx
@@ -7,15 +7,17 @@ import { Project } from 'models/subgraph-entities/project'
 import { useContext } from 'react'
 import { formatDate } from 'utils/formatDate'
 import { formatWad } from 'utils/formatNumber'
+import { InfoCircleOutlined } from '@ant-design/icons'
 
 import CurrencySymbol from './CurrencySymbol'
 import Loading from './Loading'
 import ProjectLogo from './ProjectLogo'
+import { archivedProjectIds } from 'constants/archived-projects'
 
 export default function ProjectCard({
   project,
 }: {
-  project: Pick<Project, 'handle' | 'uri' | 'totalPaid' | 'createdAt'>
+  project: Pick<Project, 'id' | 'handle' | 'uri' | 'totalPaid' | 'createdAt'>
 }) {
   const {
     theme: { colors, radii },
@@ -57,17 +59,34 @@ export default function ProjectCard({
             />
           </div>
 
-          <div style={{ minWidth: 0 }}>
-            <h2
+          <div style={{ minWidth: 0, flexGrow: 1 }}>
+            <div
               style={{
-                color: colors.text.primary,
-                margin: 0,
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
+                display: 'flex',
+                justifyContent: 'space-between',
               }}
             >
-              {metadata.name}
-            </h2>
+              <h2
+                style={{
+                  color: colors.text.primary,
+                  margin: 0,
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                {metadata.name}
+              </h2>
+
+              {archivedProjectIds.includes(project.id!.toNumber()) && (
+                <Tooltip title="This project has been archived and cannot be paid.">
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h3>
+                      <InfoCircleOutlined /> Archived
+                    </h3>
+                  </div>
+                </Tooltip>
+              )}
+            </div>
 
             <div style={{ color: colors.text.tertiary }}>
               <span style={{ color: colors.text.primary, fontWeight: 500 }}>

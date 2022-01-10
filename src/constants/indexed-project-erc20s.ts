1b5ab2659b3242e04a36fbbf9f174d713ae05791
peripheralist <peripheralist@gmail.com>
2022-01-09 20:07:50 -0500

    rinkeby indexed erc20s

--- src/constants/indexed-project-erc20s.ts
+++ src/constants/indexed-project-erc20s.ts
@@ -2,4 +2,5 @@ import { NetworkName } from 'models/network-name'
 
 export const indexedProjectERC20s: Partial<Record<NetworkName, number[]>> = {
   [NetworkName.mainnet]: [1, 2, 7, 8],
+  [NetworkName.rinkeby]: [1, 4, 12, 20],
 }

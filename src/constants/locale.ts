5602b00cbc6ccdeeab096721e40c148c613ba710
Johnny D <johnnyd.eth@protonmail.com>
2022-01-12 12:06:47 +1000

    configures the site to handle the new turkish translations (adds to language selector,etc.)

--- src/constants/locale.ts
+++ src/constants/locale.ts
@@ -1,4 +1,4 @@
-export const SUPPORTED_LOCALES = ['en', 'zh', 'ru']
+export const SUPPORTED_LOCALES = ['en', 'zh', 'ru', 'tr']
 export type SupportedLocale = typeof SUPPORTED_LOCALES[number]
 
 export const DEFAULT_LOCALE: SupportedLocale = 'en'

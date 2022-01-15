795bd050cfbe7994b07c45b1dbf2487708b35232
aeolian <aeolus.dev@protonmail.com>
2022-01-15 14:12:30 +0930

    Reorganise files
    - create providers/ directory and move relevant components there
    - move App.tsx to src/
    - remove CatchallRedirect file, move it's
    component definition to Router.tsx

--- src/components/CatchallRedirect.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect, useParams } from 'react-router'
-
-export default function CatchallRedirect() {
-  const route = useParams<{ route: string }>()['route']
-  return <Redirect to={'/p/' + route} />
-}

1fd32d54542c26de9ec515ccdde64e91477d05ed
aeolian <aeolus.dev@protonmail.com>
2022-01-16 13:05:46 +0930

    Update provider export names

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -4,11 +4,11 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
-const ProvideReactQuery: React.FC = ({ children }) => (
+const ReactQueryProvider: React.FC = ({ children }) => (
   <QueryClientProvider client={queryClient}>
     {children}
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
 
-export default ProvideReactQuery
+export default ReactQueryProvider

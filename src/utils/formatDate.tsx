c21344ac58dcf633d750abe02d91a5bf10bbf35d
Johnny D <johnnyd.eth@protonmail.com>
2022-01-17 19:23:10 +1000

    add tooltip with timestamp to historial dates

--- src/utils/formatDate.tsx
+++ src/utils/formatDate.tsx
@@ -1,10 +1,17 @@
 import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
 import { t } from '@lingui/macro'
 
+import { Tooltip } from 'antd'
+
 import moment from 'moment'
 
 export const formatDate = (dateMillis: BigNumberish, format = 'M-D-YY h:mma') =>
   moment(BigNumber.from(dateMillis).toNumber()).format(format)
 
-export const formatHistoricalDate = (dateMillis: BigNumberish) =>
-  t`${moment(BigNumber.from(dateMillis).toNumber()).fromNow(true)} ago`
+export function formatHistoricalDate(dateMillis: BigNumberish) {
+  return (
+    <Tooltip title={formatDate(dateMillis)}>
+      {t`${moment(BigNumber.from(dateMillis).toNumber()).fromNow(true)} ago`}
+    </Tooltip>
+  )
+}

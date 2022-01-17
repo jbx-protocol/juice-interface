9d78274edcb22183504cfb4d7bd13d34279e2ead
Johnny D <johnnyd.eth@protonmail.com>
2022-01-17 12:26:34 +1000

    remove unnecessary Trans tag

--- src/components/Landing/Faq.tsx
+++ src/components/Landing/Faq.tsx
@@ -1,6 +1,5 @@
 import { Collapse } from 'antd'
 import CollapsePanel from 'antd/lib/collapse/CollapsePanel'
-import { Trans } from '@lingui/macro'
 
 import QAs from './QAs'
 
@@ -9,12 +8,7 @@ export default function Faq() {
     <Collapse defaultActiveKey={QAs.length ? 0 : undefined} accordion>
       {QAs().map((qa, i) => (
         <CollapsePanel header={qa.q} key={i}>
-          {qa.a &&
-            qa.a.map((p, j) => (
-              <p key={j}>
-                <Trans>{p}</Trans>
-              </p>
-            ))}
+          {qa.a && qa.a.map((p, j) => <p key={j}>{p}</p>)}
           {qa.img && <img src={qa.img.src} alt={qa.img.alt} />}
         </CollapsePanel>
       ))}

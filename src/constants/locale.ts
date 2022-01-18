03feaf8ec418c6e70e36da987361660295e43eb0
Johnny D <johnnyd.eth@protonmail.com>
2022-01-19 05:54:25 +1000

    address reviewer feedback

--- src/constants/locale.ts
+++ src/constants/locale.ts
@@ -1,4 +1,4 @@
-export const SUPPORTED_LOCALES = ['en', 'zh', 'ru', 'tr', 'pt']
+export const SUPPORTED_LOCALES = ['en', 'zh', 'ru', 'tr', 'es']
 export type SupportedLocale = typeof SUPPORTED_LOCALES[number]
 
 export const DEFAULT_LOCALE: SupportedLocale = 'en'

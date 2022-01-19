36d4225e624826787f569dccbaaafcf579aab1c7
Johnny D <johnnyd.eth@protonmail.com>
2022-01-19 16:38:02 +1000

    convert wrapping \<div/\> to \<a\>

--- src/components/shared/ProjectCard.tsx
+++ src/components/shared/ProjectCard.tsx
@@ -39,14 +39,14 @@ export default function ProjectCard({
   const terminalVersion = getTerminalVersion(project.terminal)
 
   return (
-    <div
+    <a
       style={{
         borderRadius: radii.lg,
         cursor: 'pointer',
         overflow: 'hidden',
       }}
-      className="clickable-border"
       key={project?.handle}
+      href={`/p/${project.handle}`}
       onClick={() => (window.location.hash = '/p/' + project.handle)}
     >
       {metadata ? (
@@ -58,6 +58,7 @@ export default function ProjectCard({
             overflow: 'hidden',
             padding: 20,
           }}
+          className="clickable-border"
         >
           <div style={{ marginRight: 20 }}>
             <ProjectLogo
@@ -71,6 +72,7 @@ export default function ProjectCard({
             style={{
               flex: 1,
               minWidth: 0,
+              fontWeight: 400,
             }}
           >
             <h2
@@ -138,6 +140,6 @@ export default function ProjectCard({
           {project.handle} <Loading />
         </div>
       )}
-    </div>
+    </a>
   )
 }

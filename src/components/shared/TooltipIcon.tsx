db2a2ee6c156e1c8cffedb75e3a6f8eb34da598b
peripheralist <peripheralist@gmail.com>
2022-01-31 19:36:46 -0500

    change TooltipIcon icon
    small improvements to TooltipIcon implementation

--- src/components/shared/TooltipIcon.tsx
+++ src/components/shared/TooltipIcon.tsx
@@ -1,16 +1,24 @@
-import { InfoCircleOutlined } from '@ant-design/icons'
+import { QuestionCircleOutlined } from '@ant-design/icons'
 import { Tooltip, TooltipProps } from 'antd'
+import { CSSProperties } from 'react'
 
 export default function TooltipIcon({
   tip,
   placement,
+  style,
 }: {
   tip?: string | JSX.Element
   placement?: TooltipProps['placement']
+  style?: CSSProperties
 }) {
   return (
-    <Tooltip title={tip} placement={placement} trigger={['hover', 'click']}>
-      <InfoCircleOutlined />
+    <Tooltip
+      title={tip}
+      placement={placement}
+      trigger={['hover', 'click']}
+      style={style}
+    >
+      <QuestionCircleOutlined />
     </Tooltip>
   )
 }

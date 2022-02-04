197a8b55e845b66d60662c8b644844d7d9877782
Anton Lobovkin <anton@lobovkin.ru>
2022-02-05 00:42:45 +0100

    Test PR for project-specific metadata
    Changes Router to use "/p/projectname" urls.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,13 @@
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { useEffect } from 'react'
+import {
+  HashRouter,
+  Router as ReactRouter,
+  Route,
+  Switch,
+  useLocation,
+} from 'react-router-dom'
 import { Redirect, useParams } from 'react-router'
+import { createBrowserHistory } from 'history'
 import Dashboard from 'components/Dashboard'
 import Landing from 'components/Landing'
 import Create from 'components/Create'
@@ -10,29 +18,48 @@ function CatchallRedirect() {
   return <Redirect to={'/p/' + route} />
 }
 
+function BrowserRedirect() {
+  const location = useLocation()
+  useEffect(() => history.replace(location.pathname), [location.pathname]) //escape from HashRouter
+  return null
+}
+
+const history = createBrowserHistory()
+
 export default function Router() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-        <Route path="/projects/:owner">
-          <Projects />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/p/:handle">
-          <Dashboard />
-        </Route>
-        <Route path="/:route">
-          <CatchallRedirect />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <>
+      <ReactRouter history={history}>
+        <Switch>
+          <Route path="/p/:handle">
+            <Dashboard />
+          </Route>
+          <Route exact path="/">
+            <HashRouter>
+              <Switch>
+                <Route exact path="/">
+                  <Landing />
+                </Route>
+                <Route path="/create">
+                  <Create />
+                </Route>
+                <Route path="/projects/:owner">
+                  <Projects />
+                </Route>
+                <Route path="/projects">
+                  <Projects />
+                </Route>
+                <Route path="/p/:handle">
+                  <BrowserRedirect />
+                </Route>
+              </Switch>
+            </HashRouter>
+          </Route>
+          <Route path="/:route">
+            <CatchallRedirect />
+          </Route>
+        </Switch>
+      </ReactRouter>
+    </>
   )
 }

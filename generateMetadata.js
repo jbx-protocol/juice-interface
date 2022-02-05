c239bdd2f5c505cc2f0f94438a1736e3507d9f7a
Anton Lobovkin <anton@lobovkin.ru>
2022-02-05 02:57:31 +0100

    Added image size to display a larger image

--- generateMetadata.js
+++ generateMetadata.js
@@ -10,6 +10,8 @@ const template = `<!DOCTYPE html>
     <meta property="og:description" content="%description%" />
     <meta property="og:type" content="website" />
     <meta property="og:image" content="%logoUri%" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="1200" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@%twitter%" />
     <meta name="twitter:creator" content="@%twitter%" />

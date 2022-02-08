69051ec5c9065701feb95935fe2b365036585d2b
aeolian <aeolus.dev@protonmail.com>
2022-02-08 11:15:54 +0930

    Use Plural component in trending projects card

--- src/utils/pluralize.ts
+++
@@ -1,8 +0,0 @@
-import { i18n } from '@lingui/core'
-
-// Uses i18n plural
-export default function pluralize<S, P>(count: number, singular: S, plural: P) {
-  return i18n._(`{count, plural, one {# ${singular}} other {# ${plural}}}`, {
-    count,
-  })
-}

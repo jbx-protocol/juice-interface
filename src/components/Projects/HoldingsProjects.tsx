895e8b907428cc19fedbcc0813d99851fd36e7b3
peripheralist <peripheralist@gmail.com>
2022-02-07 19:25:44 -0500

    better loading handling for trending + holdings hooks

--- src/components/Projects/HoldingsProjects.tsx
+++ src/components/Projects/HoldingsProjects.tsx
@@ -4,14 +4,14 @@ import Loading from 'components/shared/Loading'
 import ProjectCard from 'components/shared/ProjectCard'
 import { NetworkContext } from 'contexts/networkContext'
 import { ThemeContext } from 'contexts/themeContext'
-import { useContributedProjectsQuery } from 'hooks/v1/Projects'
+import { useHoldingsProjectsQuery } from 'hooks/v1/Projects'
 import React, { useContext } from 'react'
 import { InfoCircleOutlined } from '@ant-design/icons'
 
 export default function HoldingsProjects() {
   const { userAddress } = useContext(NetworkContext)
 
-  const { data: projects, isLoading } = useContributedProjectsQuery(userAddress)
+  const { data: projects, isLoading } = useHoldingsProjectsQuery(userAddress)
 
   const {
     theme: { colors },
@@ -19,26 +19,19 @@ export default function HoldingsProjects() {
 
   return (
     <React.Fragment>
-      {projects ? (
-        projects.length ? (
-          <React.Fragment>
-            <Grid>
-              {projects?.map(p => (
-                <ProjectCard project={p} />
-              ))}
-            </Grid>
-            <div
-              style={{
-                textAlign: 'center',
-                color: colors.text.disabled,
-                padding: 20,
-              }}
-            >
-              {projects?.length}{' '}
-              {projects?.length === 1 ? t`project` : t`projects`}{' '}
-            </div>
-          </React.Fragment>
-        ) : (
+      {projects && projects.length > 0 && (
+        <React.Fragment>
+          <Grid>
+            {projects.map(p => (
+              <ProjectCard project={p} />
+            ))}
+          </Grid>
+        </React.Fragment>
+      )}
+
+      {!isLoading &&
+        projects &&
+        (projects.length === 0 ? (
           <div
             style={{
               textAlign: 'center',
@@ -49,14 +42,25 @@ export default function HoldingsProjects() {
           >
             You don't hold tokens for any Juicebox project.
           </div>
-        )
-      ) : (
+        ) : (
+          <div
+            style={{
+              textAlign: 'center',
+              color: colors.text.disabled,
+              padding: 20,
+            }}
+          >
+            {projects.length} {projects.length === 1 ? t`project` : t`projects`}{' '}
+          </div>
+        ))}
+
+      {isLoading && (
         <div style={{ marginTop: 40 }}>
           <Loading />
         </div>
       )}
 
-      <p style={{ marginBottom: 40, marginTop: 20, maxWidth: 800 }}>
+      <p style={{ marginBottom: 40, marginTop: 40, maxWidth: 800 }}>
         <InfoCircleOutlined /> Projects that you hold tokens for.
       </p>
     </React.Fragment>

a5f698f4d58b269b745ebcb3e32d8c3fbac8c51e
aeolian <aeolus.dev@protonmail.com>
2022-02-12 14:33:49 +0930

    V2 project creation MVP
    Successfully create a minimal
    v2 project on rinkeby

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,15 @@
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import { Redirect, useParams } from 'react-router'
+import { Suspense, lazy } from 'react'
 import V1Dashboard from 'components/v1/V1Dashboard'
 import Landing from 'components/Landing'
 import V1Create from 'components/v1/V1Create'
-import V2Create from 'components/v2/V2Create'
 import Projects from 'components/Projects'
 import V2UserProvider from 'providers/v2/UserProvider'
+import Loading from 'components/shared/Loading'
+
+const V2Create = lazy(() => import('components/v2/V2Create'))
+const V2Dashboard = lazy(() => import('components/v2/V2Dashboard'))
 
 function CatchallRedirect() {
   const route = useParams<{ route: string }>()['route']
@@ -22,11 +26,7 @@ export default function Router() {
         <Route path="/create">
           <V1Create />
         </Route>
-        <Route path="/create-v2">
-          <V2UserProvider>
-            <V2Create />
-          </V2UserProvider>
-        </Route>
+
         <Route path="/projects/:owner">
           <Projects />
         </Route>
@@ -35,6 +35,18 @@ export default function Router() {
         </Route>
         <Route path="/p/:handle">
           <V1Dashboard />
+        </Route>
+        <Route path="/v2/create">
+          <Suspense fallback={<Loading />}>
+            <V2Create />
+          </Suspense>
+        </Route>
+        <Route path="/v2/p/:projectId">
+          <Suspense fallback={<Loading />}>
+            <V2UserProvider>
+              <V2Dashboard />
+            </V2UserProvider>
+          </Suspense>
         </Route>
         <Route path="/:route">
           <CatchallRedirect />
--- src/components/v2/V2Project/index.tsx
+++ src/components/v2/V2Project/index.tsx
@@ -1,3 +1,14 @@
+import ProjectHeader from 'components/shared/ProjectHeader'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import { useContext } from 'react'
+
 export default function V2Project() {
-  return <div>V2Project</div>
+  const { projectId, metadata } = useContext(V2ProjectContext)
+  if (!projectId) return null
+
+  return (
+    <div>
+      <ProjectHeader projectId={projectId} metadata={metadata} />
+    </div>
+  )
 }

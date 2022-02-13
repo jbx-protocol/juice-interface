89d4dc76c4725e8eeb009233041a1b46892e70f8
aeolian <aeolus.dev@protonmail.com>
2022-02-13 12:23:55 +0930

    Abstract out v2 project metadata hooks

--- src/components/v2/V2Dashboard/index.tsx
+++ src/components/v2/V2Dashboard/index.tsx
@@ -1,39 +1,31 @@
-/* eslint-disable */
-import { Trans } from '@lingui/macro'
-
 import { V2ProjectContext } from 'contexts/v2/projectContext'
 import useV2ProjectMetadata from 'hooks/v2/ProjectMetadata'
-import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import Loading from 'components/shared/Loading'
 
 import { BigNumber } from '@ethersproject/bignumber'
 
+import useProjectMetadataContent from 'hooks/v2/contractReader/ProjectMetadataContent'
+
 import { layouts } from 'constants/styles/layouts'
 
 import V2Project from '../V2Project'
 import Dashboard404 from './Dashboard404'
-import useV2ContractReader from 'hooks/v2/contractReader/V2ContractReader'
-import { PEEL_METADATA_DOMAIN } from 'constants/v2/metadataDomain'
-import { V2ContractName } from 'models/v2/contracts'
+import ScrollToTopButton from '../../shared/ScrollToTopButton'
 
 export default function V2Dashboard() {
   const { projectId: projectIdParameter }: { projectId?: string } = useParams()
   const projectId = BigNumber.from(projectIdParameter)
 
-  const { data: metadataURI, loading: metadataURILoading } =
-    useV2ContractReader<string>({
-      contract: V2ContractName.JBProjects,
-      functionName: 'metadataContentOf',
-      args: projectId ? [projectId.toHexString(), PEEL_METADATA_DOMAIN] : null,
-    })
+  const { data: metadataCID, loading: metadataURILoading } =
+    useProjectMetadataContent(projectId)
 
   const {
     data: metadata,
     error: metadataError,
     isLoading: metadataLoading,
-  } = useV2ProjectMetadata(metadataURI)
+  } = useV2ProjectMetadata(metadataCID)
 
   if (metadataLoading || metadataURILoading) return <Loading />
 
@@ -50,11 +42,9 @@ export default function V2Dashboard() {
     <V2ProjectContext.Provider value={project}>
       <div style={layouts.maxWidth}>
         <V2Project />
-        <div
-          style={{ textAlign: 'center', cursor: 'pointer', padding: 20 }}
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-        >
-          <Trans>Back to top</Trans>
+
+        <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+          <ScrollToTopButton />
         </div>
       </div>
     </V2ProjectContext.Provider>
--- src/hooks/v2/ProjectMetadata.ts
+++ src/hooks/v2/ProjectMetadata.ts
@@ -1,25 +1,24 @@
-/* eslint-disable*/
 import axios from 'axios'
 
 import { consolidateMetadata } from 'models/project-metadata'
 import { useQuery } from 'react-query'
 import { ipfsCidUrl } from 'utils/ipfs'
 
-export default function useV2ProjectMetadata(metadataUri?: string) {
+export default function useV2ProjectMetadata(metadataCID?: string) {
   return useQuery(
-    ['v2-project-metadata', metadataUri],
+    ['v2-project-metadata', metadataCID],
     async () => {
-      if (!metadataUri) {
+      if (!metadataCID) {
         return
       }
 
-      const url = ipfsCidUrl(metadataUri)
+      const url = ipfsCidUrl(metadataCID)
 
       const response = await axios.get(url)
       return consolidateMetadata(response.data)
     },
     {
-      enabled: Boolean(metadataUri),
+      enabled: Boolean(metadataCID),
       retry: false,
       refetchOnWindowFocus: false,
     },

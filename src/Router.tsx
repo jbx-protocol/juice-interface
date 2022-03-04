dacf4a1659e417bbdb3b2ee7d8496097d23d309b
peripheralist <peripheralist@gmail.com>
2022-03-04 14:25:19 -0500

    POC implementation for querying experimental new subgraph and displaying v2 projects
    adds subgraph entities

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,7 @@ import V1Create from 'components/v1/V1Create'
 import Projects from 'components/Projects'
 import V2UserProvider from 'providers/v2/UserProvider'
 import Loading from 'components/shared/Loading'
+import V2Projects from 'components/v2/V2Projects'
 
 const V2Create = lazy(() => import('components/v2/V2Create'))
 const V2Dashboard = lazy(() => import('components/v2/V2Dashboard'))
@@ -35,6 +36,11 @@ export default function Router() {
         </Route>
         <Route path="/p/:handle">
           <V1Dashboard />
+        </Route>
+        <Route path="/v2/projects">
+          <Suspense fallback={<Loading />}>
+            <V2Projects />
+          </Suspense>
         </Route>
         <Route path="/v2/create">
           <Suspense fallback={<Loading />}>

be4c1e641940f8540e3f420547f9c80d192ca6bd
aeolian <aeolus.dev@protonmail.com>
2022-03-30 17:27:31 +1030

    WIP add Distribute Payouts button

---
+++ src/components/v2/V2Project/V2FundingCycleSection/PayoutSplitsCard.tsx
@@ -0,0 +1,74 @@
+import { Trans } from '@lingui/macro'
+import { Button, Space } from 'antd'
+import { CardSection } from 'components/shared/CardSection'
+import TooltipLabel from 'components/shared/TooltipLabel'
+import SpendingStats from 'components/v1/FundingCycle/Spending/SpendingStats'
+import SplitList from 'components/v2/shared/SplitList'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import { V2CurrencyOption } from 'models/v2/currencyOption'
+import { useContext, useState } from 'react'
+
+import { V2CurrencyName } from 'utils/v2/currency'
+
+export default function PayoutSplitsCard() {
+  const {
+    payoutSplits,
+    distributionLimitCurrency,
+    distributionLimit,
+    usedDistributionLimit,
+    projectOwnerAddress,
+    balanceInDistributionLimitCurrency,
+  } = useContext(V2ProjectContext)
+  const [, setWithdrawModalVisible] = useState<boolean>()
+
+  return (
+    <CardSection>
+      <Space direction="vertical" size="large" style={{ width: '100%' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <SpendingStats
+            hasFundingTarget={distributionLimit?.gt(0)}
+            currency={V2CurrencyName(
+              distributionLimitCurrency?.toNumber() as V2CurrencyOption,
+            )}
+            projectBalanceInCurrency={balanceInDistributionLimitCurrency}
+            targetAmount={distributionLimit}
+            withdrawnAmount={usedDistributionLimit}
+            feePercentage={'2.5'}
+            ownerAddress={projectOwnerAddress}
+          />
+          <Button
+            type="ghost"
+            size="small"
+            onClick={() => setWithdrawModalVisible(true)}
+          >
+            <Trans>Distribute funds</Trans>
+          </Button>
+        </div>
+
+        <div>
+          <TooltipLabel
+            label={
+              <h4 style={{ display: 'inline-block' }}>
+                <Trans>Funding Distribution</Trans>
+              </h4>
+            }
+            tip={
+              <Trans>
+                Available funds are distributed according to the payouts below.
+              </Trans>
+            }
+          />
+          {payoutSplits ? (
+            <SplitList
+              splits={payoutSplits}
+              distributionLimitCurrency={distributionLimitCurrency}
+              distributionLimit={distributionLimit}
+              projectOwnerAddress={projectOwnerAddress}
+              showSplitValues
+            />
+          ) : null}
+        </div>
+      </Space>
+    </CardSection>
+  )
+}

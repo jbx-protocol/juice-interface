86e02fe73faeafe45118950b768f398560757402
aeolian <94939382+aeolianeth@users.noreply.github.com>
2022-04-04 10:23:44 +0930

    Add v2 progress bar (#762)
    * Add V2 progress bar
    
    * Group project context code nicer

--- src/components/v2/V2Project/TreasuryStats/index.tsx
+++ src/components/v2/V2Project/TreasuryStats/index.tsx
@@ -1,14 +1,47 @@
-import { Space } from 'antd'
+import { Skeleton, Space } from 'antd'
+import FundingProgressBar from 'components/shared/Project/FundingProgressBar'
+import { useContext } from 'react'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import useProjectTerminalOverflow from 'hooks/v2/contractReader/ProjectTerminalOverflow'
 
 import ProjectBalance from './ProjectBalance'
 import DistributedRatio from './DistributedRatio'
 import OwnerBalance from './OwnerBalance'
 
 export default function TreasuryStats() {
+  const {
+    balanceInDistributionLimitCurrency,
+    distributionLimit,
+    terminals,
+    projectId,
+  } = useContext(V2ProjectContext)
+
+  const { data: overflow, loading: overflowLoading } =
+    useProjectTerminalOverflow({
+      terminal: terminals?.[0],
+      projectId,
+    })
+
+  const fundingProgressBarLoading = overflowLoading || !distributionLimit
+
   return (
     <Space direction="vertical" style={{ display: 'flex' }}>
       <ProjectBalance />
       <DistributedRatio />
+      <Skeleton
+        loading={fundingProgressBarLoading}
+        title={false}
+        paragraph={{ rows: 1, width: ['100%'] }}
+        active
+      >
+        {!fundingProgressBarLoading ? (
+          <FundingProgressBar
+            targetAmount={distributionLimit}
+            balanceInTargetCurrency={balanceInDistributionLimitCurrency}
+            overflowAmountInTargetCurrency={overflow}
+          />
+        ) : null}
+      </Skeleton>
       <OwnerBalance />
     </Space>
   )

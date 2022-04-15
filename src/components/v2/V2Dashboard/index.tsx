e40c245e7214381fee02474da90c716028fc25e4
Johnny D <johnnyd.eth@protonmail.com>
2022-04-15 23:13:26 +0100

    remove v2 mainnet warnings

--- src/components/v2/V2Dashboard/index.tsx
+++ src/components/v2/V2Dashboard/index.tsx
@@ -1,20 +1,5 @@
-import { NetworkName } from 'models/network-name'
-
-import { readNetwork } from 'constants/networks'
-import V2MainnetWarning from '../shared/V2MainnetWarning'
 import V2Dashboard from './Dashboard'
 
 export default function V2DashboardContainer() {
-  const isRinkeby = readNetwork.name === NetworkName.rinkeby
-
-  return (
-    <>
-      {!isRinkeby && (
-        <div style={{ padding: '1rem', textAlign: 'center' }}>
-          <V2MainnetWarning />
-        </div>
-      )}
-      {isRinkeby && <V2Dashboard />}
-    </>
-  )
+  return <V2Dashboard />
 }

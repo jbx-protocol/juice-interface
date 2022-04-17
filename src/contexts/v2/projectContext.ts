fba5c3f407f45a821c1da07f5dcaf64bf2b39b3e
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-04-17 22:44:13 +0930

    Clean up v2 project context (#818)
    * Clean up v2 project context
    
    - move queued data to where it's needed only
    - start adding some loading states
    
    * Queued FC loading
    
    * Update ballot states for v2
    
    * Add disitrbutionLimit loading state

--- src/contexts/v2/projectContext.ts
+++ src/contexts/v2/projectContext.ts
@@ -1,9 +1,15 @@
 import { BigNumber } from '@ethersproject/bignumber'
-import { BallotState } from 'models/ballot'
+import { V2BallotState } from 'models/ballot'
 import { ProjectMetadataV4 } from 'models/project-metadata'
 import { V2FundingCycle, V2FundingCycleMetadata } from 'models/v2/fundingCycle'
 import { Split } from 'models/v2/splits'
 import { createContext } from 'react'
+
+type V2ProjectLoadingStates = {
+  ETHBalanceLoading: boolean
+  balanceInDistributionLimitCurrencyLoading: boolean
+  distributionLimitLoading: boolean
+}
 
 export type V2ProjectContextType = {
   isPreviewMode?: boolean
@@ -13,30 +19,26 @@ export type V2ProjectContextType = {
   tokenAddress: string | undefined
   tokenSymbol: string | undefined
   terminals: string[] | undefined // array of terminal addresses, 0xABC...
+  primaryTerminal: string | undefined
   ETHBalance: BigNumber | undefined
   projectOwnerAddress: string | undefined
   balanceInDistributionLimitCurrency: BigNumber | undefined
   usedDistributionLimit: BigNumber | undefined // how much has been distributed
 
   fundingCycleMetadata: V2FundingCycleMetadata | undefined
-  queuedFundingCycleMetadata: V2FundingCycleMetadata | undefined
   fundingCycle: V2FundingCycle | undefined
-  queuedFundingCycle: V2FundingCycle | undefined
-  ballotState: BallotState | undefined
+  ballotState: V2BallotState | undefined
 
-  distributionLimit: BigNumber | undefined // previously funding target
+  distributionLimit: BigNumber | undefined
   distributionLimitCurrency: BigNumber | undefined
-  queuedDistributionLimit: BigNumber | undefined
-  queuedDistributionLimitCurrency: BigNumber | undefined
 
   payoutSplits: Split[] | undefined
-  queuedPayoutSplits: Split[] | undefined
-
   reservedTokensSplits: Split[] | undefined
-  queuedReservedTokensSplits: Split[] | undefined
 
   primaryTerminalCurrentOverflow: BigNumber | undefined
   totalTokenSupply: BigNumber | undefined
+
+  loading: V2ProjectLoadingStates
 }
 
 export const V2ProjectContext = createContext<V2ProjectContextType>({
@@ -47,28 +49,28 @@ export const V2ProjectContext = createContext<V2ProjectContextType>({
   tokenAddress: undefined,
   tokenSymbol: undefined,
   terminals: undefined,
+  primaryTerminal: undefined,
   ETHBalance: undefined,
   projectOwnerAddress: undefined,
   balanceInDistributionLimitCurrency: undefined,
   usedDistributionLimit: undefined,
 
   fundingCycleMetadata: undefined,
-  queuedFundingCycleMetadata: undefined,
   fundingCycle: undefined,
-  queuedFundingCycle: undefined,
   ballotState: undefined,
 
   distributionLimit: undefined,
   distributionLimitCurrency: undefined,
-  queuedDistributionLimit: undefined,
-  queuedDistributionLimitCurrency: undefined,
 
   payoutSplits: undefined,
-  queuedPayoutSplits: undefined,
-
   reservedTokensSplits: undefined,
-  queuedReservedTokensSplits: undefined,
 
   primaryTerminalCurrentOverflow: undefined,
   totalTokenSupply: undefined,
+
+  loading: {
+    ETHBalanceLoading: false,
+    balanceInDistributionLimitCurrencyLoading: false,
+    distributionLimitLoading: false,
+  },
 })

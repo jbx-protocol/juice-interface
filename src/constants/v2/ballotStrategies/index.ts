7bf787474a17e4d08fdcf4d8dd53132efc2415e4
aeolian <aeolus.dev@protonmail.com>
2022-04-21 20:02:10 +0930

    Chain specific ballots for V2

--- src/constants/v2/ballotStrategies/index.ts
+++ src/constants/v2/ballotStrategies/index.ts
@@ -1,6 +1,19 @@
 import * as constants from '@ethersproject/constants'
 import { t } from '@lingui/macro'
 
+import { readNetwork } from 'constants/networks'
+
+const BALLOT_ADDRESSES: { [k: string]: { [j: string]: string } } = {
+  THREE_DAY: {
+    rinkeby: '',
+    mainnet: '',
+  },
+  SEVEN_DAY: {
+    rinkeby: '',
+    mainnet: '',
+  },
+}
+
 export function ballotStrategies() {
   return [
     {
@@ -11,12 +24,12 @@ export function ballotStrategies() {
     {
       name: t`7-day delay`,
       description: t`A reconfiguration to an upcoming funding cycle must be submitted at least 7 days before it starts.`,
-      address: '0x7dFB626755dCb7CBddef0307383888331dd63cab',
+      address: BALLOT_ADDRESSES.SEVEN_DAY[readNetwork.name as string],
     },
     {
       name: t`3-day delay`,
       description: t`A reconfiguration to an upcoming funding cycle must be submitted at least 3 days before it starts.`,
-      address: '0x7Db107F1d92CcC9A76299F0BF87Ba506Dbfd0b6c',
+      address: BALLOT_ADDRESSES.THREE_DAY[readNetwork.name as string],
     },
   ]
 }

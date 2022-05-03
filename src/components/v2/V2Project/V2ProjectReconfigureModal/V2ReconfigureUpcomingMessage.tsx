609bd4a4fa0d4d4edbecfa29806bfb67608ee30c
Johnny D <johnnyD.eth@protonmail.com>
2022-05-03 13:57:36 +0100

    Specify which FC# reconfig will take effect (#892)
    * make algo to calculate when reconfiguration will take effect (incomplete)
    
    * alter reconfigure upcoming fc message
    
    * _
    
    * fix non-rendering v2currencyamounts if currency is 0
    
    * remove unnecessary additions
    
    * minor wording changes to upcoming FC message
    
    * ballotStrat.length->durationSeconds (var name change)
    
    Co-authored-by: aeolian <94939382+aeolianeth@users.noreply.github.com>

---
+++ src/components/v2/V2Project/V2ProjectReconfigureModal/V2ReconfigureUpcomingMessage.tsx
@@ -0,0 +1,64 @@
+import { Trans } from '@lingui/macro'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import { useContext } from 'react'
+import { BigNumber } from '@ethersproject/bignumber'
+
+import { detailedTimeString, secondsUntil } from 'utils/formatTime'
+
+import { getBallotStrategyByAddress } from 'constants/v2/ballotStrategies/getBallotStrategiesByAddress'
+
+export default function V2ReconfigureUpcomingMessage() {
+  const { fundingCycle } = useContext(V2ProjectContext)
+  if (!fundingCycle) return null
+
+  const currentFCNumber = fundingCycle.number.toNumber()
+
+  const ballotStrategy = getBallotStrategyByAddress(fundingCycle.ballot)
+  const ballotStrategyLength = ballotStrategy.durationSeconds
+
+  const duration = fundingCycle.duration.toNumber()
+
+  const secondsUntilNextFC = secondsUntil(fundingCycle.start.add(duration))
+
+  // Separating the full message out like this for translation purposes
+  // (they need full sentences, can't chop and change)
+  if (!duration || duration === 0) {
+    // If duration is unset/0, changes take effect immediately to current FC
+    return (
+      <Trans>
+        You don't have a funding cycle duration. Changes you make will take
+        effect immediately.
+      </Trans>
+    )
+  } else if (ballotStrategyLength === undefined) {
+    return (
+      <Trans>
+        Changes will take effect according to the project's custom ballot
+        contract.
+      </Trans>
+    )
+  } else if (ballotStrategyLength > secondsUntilNextFC) {
+    return (
+      <Trans>
+        Changes you make will take effect according to your{' '}
+        <strong>{ballotStrategy.name}</strong> reconfiguration rule: i.e. the
+        first funding cycle following
+        <strong>
+          {detailedTimeString({
+            timeSeconds: BigNumber.from(ballotStrategyLength),
+            fullWords: true,
+          })}
+        </strong>{' '}
+        from now.
+      </Trans>
+    )
+  } else {
+    return (
+      <Trans>
+        Any changes you make will take effect in{' '}
+        <strong>funding cycle #{currentFCNumber + 1}</strong>. The current
+        funding cycle (#{currentFCNumber}) won't be altered.
+      </Trans>
+    )
+  }
+}

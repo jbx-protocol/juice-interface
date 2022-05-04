459984ff47d8ba2f6fdcf90a5636ae243cd40445
Johnny D <johnnyD.eth@protonmail.com>
2022-05-04 13:29:29 +0100

    After deploy modal with Twitter share and project payer deploy (#900)
    * add ability to deploy project payer contract through tools modal
    
    * newDeployModal, project payer tx, add it to tools and newDeployModal
    
    * create project payer deploy tx and add it to v2 tools modal
    
    * minor wording changes to deploy project payer modal
    
    * display error notif when launch project payer fails
    
    * Change project payer help text
    
    * add onCancelled on deployProjectPayer tx
    
    * _
    
    * make step buttons RichButtons
    
    * wording adjustment on newDeploy modal
    
    Co-authored-by: aeolian <aeolus.dev@protonmail.com>

---
+++ src/components/shared/IssueTokenButton.tsx
@@ -0,0 +1,88 @@
+import { Button, Space, Tooltip } from 'antd'
+import { Trans } from '@lingui/macro'
+import { useState } from 'react'
+import { TransactorInstance } from 'hooks/Transactor'
+import { useHistory } from 'react-router-dom'
+import { SettingOutlined } from '@ant-design/icons'
+import { ButtonType } from 'antd/lib/button'
+import { SizeType } from 'antd/lib/config-provider/SizeContext'
+
+import IssueTokenModal from './modals/IssueTokenModal'
+
+export type IssueTokenTxArgs = {
+  name: string
+  symbol: string
+}
+
+export default function IssueTokenButton({
+  useIssueTokensTx,
+  type,
+  text,
+  size,
+  hideIcon,
+  onCompleted,
+  disabled,
+  tooltipText,
+}: {
+  useIssueTokensTx: () => TransactorInstance<IssueTokenTxArgs>
+  type?: ButtonType
+  text?: JSX.Element
+  size?: SizeType
+  hideIcon?: boolean
+  onCompleted?: VoidFunction
+  disabled?: boolean
+  tooltipText?: JSX.Element
+}) {
+  const [modalVisible, setModalVisible] = useState<boolean>(false)
+
+  const history = useHistory()
+
+  function IssueTokensButton() {
+    return (
+      <Tooltip
+        title={
+          !disabled &&
+          (tooltipText ?? (
+            <Trans>
+              Issue an ERC-20 to be used as this project's token. Once issued,
+              anyone can claim their existing token balance in the new token.
+            </Trans>
+          ))
+        }
+      >
+        <Button
+          size={size ?? 'small'}
+          icon={!hideIcon && <SettingOutlined />}
+          onClick={() => setModalVisible(true)}
+          type={type ?? 'default'}
+          disabled={disabled}
+        >
+          <span>{text ?? <Trans>Issue ERC-20</Trans>}</span>
+        </Button>
+      </Tooltip>
+    )
+  }
+
+  const onClose = () => {
+    setModalVisible(false)
+
+    // remove newDeploy=true query parameter
+    history.replace({
+      search: '',
+    })
+  }
+
+  return (
+    <div>
+      <Space>
+        <IssueTokensButton />
+      </Space>
+      <IssueTokenModal
+        visible={modalVisible}
+        useIssueTokensTx={useIssueTokensTx}
+        onClose={onClose}
+        onConfirmed={onCompleted}
+      />
+    </div>
+  )
+}

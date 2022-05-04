86787cbafff4986b196aa1acef1e176a6bf723b5
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-05-05 09:05:23 +0930

    Lint translation files for git merge conflict txt (#926)

--- scripts/lint-translations.sh
+++ scripts/lint-translations.sh
@@ -11,6 +11,15 @@ if ! [ -z "$LOCALE_CHANGED_FILES" ]; then
   echo "🍎 Translation source files are out of date. Run 'yarn i18n:extract' locally and commit changes to the following files:\n$LOCALE_CHANGED_FILES."
   exit 1
 else
+  # bail if .po files contain git merge conflict diff artefact (like <<<<< HEAD)
+  for File in $(find src/locales -maxdepth 2 -regex '.*.po')
+  do
+    if grep -q '<<<' "$File"; then
+      echo "🍎 Translation source files contain artefacts from git merge conflict."
+      exit 1
+    fi
+  done
+
   echo "🍏 Translation source files are up to date."
   exit 0
 fi

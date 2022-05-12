b26b8c389c103c5c9ef45604c79a6d0111623d1c
aeolian <aeolus.dev@protonmail.com>
2022-05-12 09:59:48 +0930

    Add link to v2 project in activity feed

---
+++ src/components/v2/shared/V2ProjectHandle.tsx
@@ -0,0 +1,28 @@
+import { BigNumberish } from '@ethersproject/bignumber'
+import { Trans } from '@lingui/macro'
+import ProjectVersionBadge from 'components/shared/ProjectVersionBadge'
+import { CSSProperties } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function V2ProjectHandle({
+  projectId,
+  style,
+}: {
+  projectId: BigNumberish
+  style?: CSSProperties
+}) {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Link
+        to={`/v2/p/${projectId}`}
+        style={{ fontWeight: 500, ...style }}
+        className="text-primary hover-text-action-primary hover-text-decoration-underline"
+      >
+        <span style={{ marginRight: '0.5rem' }}>
+          <Trans>Project {projectId}</Trans>
+        </span>
+      </Link>
+      <ProjectVersionBadge versionText="V2" size="small" />
+    </div>
+  )
+}

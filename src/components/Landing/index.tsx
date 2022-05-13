9fd7a458c4d404612cb7d7796f12601c791934ac
aeolian <aeolus.dev@protonmail.com>
2022-05-13 21:46:27 +0930

    Translations

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -410,7 +410,7 @@ export default function Landing() {
 
             <Col xs={24} md={10}>
               <img
-                style={{ maxWidth: 420 }}
+                style={{ maxWidth: '100%' }}
                 src="/assets/blueberry-ol.png"
                 alt="Sexy blueberry with bright pink lipstick spraying a can of spraypaint"
                 loading="lazy"

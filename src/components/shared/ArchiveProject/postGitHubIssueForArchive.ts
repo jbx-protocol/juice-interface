cc8321deb80e8b5efe65485d51f306265390cd76
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-05-15 19:07:25 +1000

    V2 archive project (#976)
    * Add support for v2 project archive
    
    * i18n
    
    * refactor to share code
    
    * pass isArchived to v2 projectheader
    
    * i18n
    
    * i18n
    
    * smoler numbers
    
    Co-authored-by: Johnny D <johnnyD.eth@protonmail.com>

---
+++ src/components/shared/ArchiveProject/postGitHubIssueForArchive.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+import { ProjectMetadataV4 } from 'models/project-metadata'
+
+import { readNetwork } from 'constants/networks'
+
+export const postGitHubIssueForArchive = ({
+  archived,
+  projectId,
+  metadata,
+  handle,
+}: {
+  archived: boolean
+  projectId: number | undefined
+  metadata: ProjectMetadataV4 | undefined
+  handle: string | undefined
+}) =>
+  axios.post(
+    'https://api.github.com/repos/jbx-protocol/juice-interface/issues',
+    {
+      title: `[${archived ? 'ARCHIVE' : 'UNARCHIVE'}] Project: "${
+        metadata?.name
+      }"`,
+      body: `<b>Chain:</b> ${
+        readNetwork.name
+      } \n <b>Project ID:</b> ${projectId}${
+        handle ? `\n <b>Handle:</b> ${handle}` : ''
+      }`,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+      },
+    },
+  )

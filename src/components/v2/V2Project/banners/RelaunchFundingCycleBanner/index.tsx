ffdd1e30f546ec79c837c345f73cbba7ab585fe5
aeolian <aeolian.eth@gmail.com>
2022-05-27 15:11:29 +0930

    Add LaunchFundingCycles tx

--- src/components/v2/V2Project/banners/RelaunchFundingCycleBanner/index.tsx
+++ src/components/v2/V2Project/banners/RelaunchFundingCycleBanner/index.tsx
@@ -1,27 +1,70 @@
 import { Trans } from '@lingui/macro'
-import { Button, Modal } from 'antd'
+import { Button } from 'antd'
 import { useContext, useState } from 'react'
 
 import Banner from 'components/shared/Banner'
 import { V2ProjectContext } from 'contexts/v2/projectContext'
 import useDeprecatedProjectCurrentFundingCycle from 'hooks/v2/contractReader/DeprecatedProjectCurrentFundingCycle'
+import { useLaunchFundingCyclesTx } from 'hooks/v2/transactor/LaunchFundingCyclesTx'
+import TransactionModal from 'components/shared/TransactionModal'
+import {
+  V2FundingCycleData,
+  V2FundingCycleMetadata,
+} from 'models/v2/fundingCycle'
 
 import ReconfigurePreview from '../../V2ProjectReconfigureModal/ReconfigurePreview'
 
 export default function RelaunchFundingCycleBanner() {
   const [modalOpen, setModalOpen] = useState<boolean>(false)
+  const [transactionPending, setTransactionPending] = useState<boolean>(false)
   const { projectId } = useContext(V2ProjectContext)
   const { data, loading: deprecatedFundingCycleLoading } =
     useDeprecatedProjectCurrentFundingCycle({
       projectId,
     })
 
-  const onLaunchModalOk = () => {}
+  const launchFundingCycleTx = useLaunchFundingCyclesTx()
 
   const [deprecatedFundingCycle, deprecatedFundingCycleMetadata] = data ?? []
 
-  if (!deprecatedFundingCycle || !deprecatedFundingCycleMetadata) {
+  if (
+    !projectId ||
+    !deprecatedFundingCycle ||
+    !deprecatedFundingCycleMetadata
+  ) {
     return null
+  }
+
+  const onLaunchModalOk = async () => {
+    const fundingCycleData: V2FundingCycleData = {
+      duration: deprecatedFundingCycle.duration,
+      ballot: deprecatedFundingCycle.ballot,
+      weight: deprecatedFundingCycle.weight,
+      discountRate: deprecatedFundingCycle.discountRate,
+    }
+
+    const fundingCycleMetadata: V2FundingCycleMetadata =
+      deprecatedFundingCycleMetadata
+
+    await launchFundingCycleTx(
+      {
+        projectId,
+        fundingCycleData,
+        fundingCycleMetadata,
+        fundAccessConstraints: [],
+        groupedSplits: [],
+      },
+      {
+        onDone() {
+          console.info('Transaction executed. Awaiting confirmation...')
+          setTransactionPending(true)
+        },
+        onConfirmed() {
+          setTransactionPending(false)
+          window.location.reload()
+        },
+      },
+    )
   }
 
   return (
@@ -42,13 +85,18 @@ export default function RelaunchFundingCycleBanner() {
           </>
         }
       />
-      <Modal
+      <TransactionModal
         visible={modalOpen}
         title={<Trans>Launch funding cycle</Trans>}
-        okText={<Trans>Launch funding cycle</Trans>}
+        okText={
+          <span>
+            <Trans>Launch funding cycle</Trans>
+          </span>
+        }
         width={700}
         onOk={onLaunchModalOk}
         onCancel={() => setModalOpen(false)}
+        transactionPending={transactionPending}
       >
         {deprecatedFundingCycleLoading ? (
           'loading...'
@@ -61,7 +109,7 @@ export default function RelaunchFundingCycleBanner() {
             fundAccessConstraints={[]}
           />
         )}
-      </Modal>
+      </TransactionModal>
     </>
   )
 }

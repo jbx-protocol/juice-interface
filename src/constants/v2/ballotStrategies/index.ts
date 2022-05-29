a5b33289feffe0f9637826703fbb57b73fd79c5c
peripheralist <79433522+peripheralist@users.noreply.github.com>
2022-05-29 15:43:40 -0400

    track deprecated ballot addresses and map to new ones in V2 reconfig tx (#1101)

--- src/constants/v2/ballotStrategies/index.ts
+++ src/constants/v2/ballotStrategies/index.ts
@@ -1,10 +1,29 @@
 import * as constants from '@ethersproject/constants'
 import { t } from '@lingui/macro'
 
+import { NetworkName } from 'models/network-name'
+
 import { readNetwork } from 'constants/networks'
 import { SECONDS_IN_DAY } from 'constants/numbers'
 
-const BALLOT_ADDRESSES: { [k: string]: { [j: string]: string } } = {
+type BallotOption = Record<
+  'THREE_DAY' | 'SEVEN_DAY',
+  Partial<Record<NetworkName, string>>
+>
+
+export const DEPRECATED_BALLOT_ADDRESSES: BallotOption = {
+  THREE_DAY: {
+    rinkeby: '0xf91150aa07a1AC707148420713cefd299b8D094A',
+    mainnet: '0x9733F02d3A1068A11B07516fa2f3C3BaEf90e7eF',
+  },
+  SEVEN_DAY: {
+    // No 7 day delay contract deployed with original V2
+    rinkeby: constants.AddressZero,
+    mainnet: constants.AddressZero,
+  },
+}
+
+export const BALLOT_ADDRESSES: BallotOption = {
   THREE_DAY: {
     rinkeby: '0xbb5fe3f1c422a13700d8F990a17813e554c5b380',
     mainnet: '0x138D6d59afC6DbBD1DC0bDdF05Ae27f645Eb4305',
@@ -26,13 +45,13 @@ export function ballotStrategies() {
     {
       name: t`3-day delay`,
       description: t`A reconfiguration to an upcoming funding cycle must be submitted at least 3 days before it starts.`,
-      address: BALLOT_ADDRESSES.THREE_DAY[readNetwork.name as string],
+      address: BALLOT_ADDRESSES.THREE_DAY[readNetwork.name]!,
       durationSeconds: SECONDS_IN_DAY * 3,
     },
     {
       name: t`7-day delay`,
       description: t`A reconfiguration to an upcoming funding cycle must be submitted at least 7 days before it starts.`,
-      address: BALLOT_ADDRESSES.SEVEN_DAY[readNetwork.name as string],
+      address: BALLOT_ADDRESSES.SEVEN_DAY[readNetwork.name]!,
       durationSeconds: SECONDS_IN_DAY * 7,
     },
   ]

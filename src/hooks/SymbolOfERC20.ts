d018eccd44dc5f3f3a8306853ac55f9c46d36256
wraeth-eth <wraeth.dao@gmail.com>
2022-06-19 12:28:16 +1000

    move symbolOfERC20 to global hooks

--- src/hooks/SymbolOfERC20.ts
+++ src/hooks/SymbolOfERC20.ts
@@ -1,6 +1,6 @@
 import { useErc20Contract } from 'hooks/Erc20Contract'
 
-import useContractReader from './ContractReader'
+import useContractReader from './v1/contractReader/ContractReader'
 
 /** Returns symbol for ERC20 token with `address`. */
 export default function useSymbolOfERC20(address: string | undefined) {

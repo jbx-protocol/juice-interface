b1161640799edb92acdb6322f8e7e9350912dd04
Wraeth <wraeth.dao@gmail.com>
2022-06-25 13:53:02 +1000

    high level erc20 contract

--- src/hooks/SymbolOfERC20.ts
+++ src/hooks/SymbolOfERC20.ts
@@ -1,11 +1,22 @@
 import { useErc20Contract } from 'hooks/Erc20Contract'
-
-import useContractReader from './v1/contractReader/ContractReader'
+import { useEffect, useState } from 'react'
 
 /** Returns symbol for ERC20 token with `address`. */
 export default function useSymbolOfERC20(address: string | undefined) {
-  return useContractReader<string>({
-    contract: useErc20Contract(address),
-    functionName: 'symbol',
-  })
+  const [data, setData] = useState<string>()
+
+  const contract = useErc20Contract(address)
+
+  useEffect(() => {
+    async function fetchSymbol() {
+      if (!contract || !address) return
+
+      const symbol = await contract.symbol()
+      setData(symbol)
+    }
+
+    fetchSymbol()
+  }, [address, contract])
+
+  return data
 }

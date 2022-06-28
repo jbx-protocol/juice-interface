f1f2c3cf0be8bce5358cf5a4e0f32818cdd0f423
aeolian <aeolian.eth@gmail.com>
2022-06-28 19:23:03 +0930

    Set tx metadata args to constant 0 value

--- src/hooks/v2/transactor/AddToBalanceTx.ts
+++ src/hooks/v2/transactor/AddToBalanceTx.ts
@@ -2,10 +2,11 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { useContext } from 'react'
 import { V2UserContext } from 'contexts/v2/userContext'
 import { V2ProjectContext } from 'contexts/v2/projectContext'
-import { randomBytes } from '@ethersproject/random'
 
 import { TransactorInstance } from '../../Transactor'
 import { ETH_TOKEN_ADDRESS } from 'constants/v2/juiceboxTokens'
+
+const DEFAULT_METADATA = 0
 
 export function useAddToBalanceTx(): TransactorInstance<{
   value: BigNumber
@@ -24,7 +25,7 @@ export function useAddToBalanceTx(): TransactorInstance<{
     return transactor(
       contracts?.JBETHPaymentTerminal,
       'addToBalanceOf',
-      [projectId, value, ETH_TOKEN_ADDRESS, DEFAULT_MEMO, randomBytes(1)],
+      [projectId, value, ETH_TOKEN_ADDRESS, DEFAULT_MEMO, DEFAULT_METADATA],
       {
         ...txOpts,
         value,

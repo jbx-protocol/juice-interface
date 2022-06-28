2797ea932cf0c776228e6d4de14ceedf0eb9b5e5
aeolian <94939382+aeolianeth@users.noreply.github.com>
2022-06-28 12:56:01 +0930

    IPFS API integration (#1291)
    * Refactor ipfs to use internal server
    
    * Updates

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,4 +1,8 @@
-import pinataClient, { PinataMetadata, PinataPinResponse } from '@pinata/sdk'
+import {
+  PinataMetadata,
+  PinataPinListResponse,
+  PinataPinResponse,
+} from '@pinata/sdk'
 import axios from 'axios'
 
 import { IpfsCacheJsonData } from 'models/ipfs-cache/cache-data'
@@ -8,16 +12,10 @@ import { consolidateMetadata, ProjectMetadataV4 } from 'models/project-metadata'
 import { readNetwork } from 'constants/networks'
 import { IPFS_GATEWAY_HOSTNAME } from 'constants/ipfs'
 
-const pinata_api_key = process.env.REACT_APP_PINATA_PINNER_KEY
-const pinata_secret_api_key = process.env.REACT_APP_PINATA_PINNER_SECRET
-
-if (!pinata_api_key || !pinata_secret_api_key) {
-  throw new Error(
-    'Missing .env vars REACT_APP_PINATA_PINNER_KEY or REACT_APP_PINATA_PINNER_SECRET',
-  )
-}
-
-const pinata = pinataClient(pinata_api_key, pinata_secret_api_key)
+const JUICEBOX_API_HOSTNAME = 'api.juicebox.money'
+const axiosInstance = axios.create({
+  baseURL: `https://${JUICEBOX_API_HOSTNAME}/api`,
+})
 
 export const IPFS_TAGS = {
   [IpfsCacheName.trending]:
@@ -39,10 +37,16 @@ export const IPFS_TAGS = {
 }
 
 // keyvalues will be upserted to existing metadata. A null value will remove an existing keyvalue
-export const editMetadataForCid = (
+export const editMetadataForCid = async (
   cid: string | undefined,
   options?: PinataMetadata,
-) => (cid ? pinata.hashMetadata(cid, { ...options }) : undefined)
+) => {
+  if (!cid) return undefined
+
+  const pinRes = await axiosInstance.put(`/ipfs/pin/${cid}`, { ...options })
+
+  return pinRes.data
+}
 
 export const logoNameForHandle = (handle: string) => `juicebox-@${handle}-logo`
 
@@ -54,7 +58,7 @@ export const ipfsCidUrl = (hash: string) =>
 
 export const cidFromUrl = (url: string | undefined) => url?.split('/').pop()
 
-export const pinFileToIpfs = (
+export const pinFileToIpfs = async (
   file: File | Blob | string,
   metadata?: PinataMetadata,
 ) => {
@@ -71,64 +75,51 @@ export const pinFileToIpfs = (
     )
   }
 
-  // We use axios here because using `pinata.pinFileToIPFS()` leads to this issue: https://github.com/PinataCloud/Pinata-SDK/issues/84
-  return axios
-    .post('https://api.pinata.cloud/pinning/pinFileToIPFS', data, {
-      maxContentLength: Infinity, //this is needed to prevent axios from erroring out with large files
-      headers: {
-        'Content-Type': `multipart/form-data;`,
-        pinata_api_key,
-        pinata_secret_api_key,
-      },
-    })
-    .then(res => res.data as PinataPinResponse)
+  const res = await axiosInstance.post('/ipfs/logo', data)
+
+  return res.data as PinataPinResponse
 }
 
-export const unpinIpfsFileByCid = (cid: string | undefined) =>
-  cid
-    ? pinata.unpin(cid).catch(err => {
-        console.error('Failed to unpin file ', cid, err)
-      })
-    : Promise.reject()
-
-export const uploadProjectMetadata = (
+export const uploadProjectMetadata = async (
   metadata: Omit<ProjectMetadataV4, 'version'>,
   handle?: string,
-) =>
-  pinata.pinJSONToIPFS(consolidateMetadata(metadata), {
-    pinataMetadata: {
-      keyvalues: {
-        tag: IPFS_TAGS.METADATA,
-      } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-      name: handle
-        ? metadataNameForHandle(handle)
-        : 'juicebox-project-metadata.json',
-    },
-  })
-
-export const uploadIpfsJsonCache = <T extends IpfsCacheName>(
-  tag: T,
-  data: IpfsCacheJsonData[T],
-) =>
-  pinata.pinJSONToIPFS(data, {
-    pinataMetadata: {
-      keyvalues: {
-        tag: IPFS_TAGS[tag],
-      } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-      name: IPFS_TAGS[tag] + '.json',
-    },
-  })
-
-export const getPinnedListByTag = (tag: keyof typeof IPFS_TAGS) =>
-  pinata.pinList({
-    pageLimit: 1000,
-    status: 'pinned',
-    metadata: {
-      keyvalues: {
-        tag: {
-          value: IPFS_TAGS[tag],
-          op: 'eq',
-        },
+) => {
+  const res = await axiosInstance.post('/ipfs/pin', {
+    data: consolidateMetadata(metadata),
+    options: {
+      pinataMetadata: {
+        keyvalues: {
+          tag: IPFS_TAGS.METADATA,
+        } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+        name: handle
+          ? metadataNameForHandle(handle)
+          : 'juicebox-project-metadata.json',
       },
     },
   })
+
+  return res.data as PinataPinResponse
+}
+
+export const uploadIpfsJsonCache = async <T extends IpfsCacheName>(
+  tag: T,
+  data: IpfsCacheJsonData[T],
+) => {
+  return await axiosInstance.post('/ipfs/pin', {
+    data,
+    options: {
+      pinataMetadata: {
+        keyvalues: {
+          tag: IPFS_TAGS[tag],
+        } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+        name: IPFS_TAGS[tag] + '.json',
+      },
+    },
+  })
+}
+
+export const getPinnedListByTag = async (tag: keyof typeof IPFS_TAGS) => {
+  const data = await axiosInstance.get(`/ipfs/pin?tag=${IPFS_TAGS[tag]}`)
+
+  return data.data as PinataPinListResponse
+}

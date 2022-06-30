ff5b04a1b2bf39c8763610c570c1381969db0344
0xbakedpotato <107800399+0xbakedpotato@users.noreply.github.com>
2022-06-29 22:17:19 -0400

    add owner to project header (#1278)
    * add owner to project header
    
    * add owner to project header
    
    * change By to by
    
    * review changes
    
    * add space in owned by
    
    * remove import
    
    * add display inline back to paragraph
    
    * Owned by color, trans
    
    Co-authored-by: aeolian <aeolian.eth@gmail.com>

---
+++ src/hooks/v1/contractReader/ProjectOwner.ts
@@ -0,0 +1,11 @@
+import { V1ProjectContext } from 'contexts/v1/projectContext'
+import { useContext } from 'react'
+
+import useOwnerOfProject from './OwnerOfProject'
+
+export function useProjectOwner() {
+  const { projectId } = useContext(V1ProjectContext)
+  const owner = useOwnerOfProject(projectId)
+
+  return { owner }
+}

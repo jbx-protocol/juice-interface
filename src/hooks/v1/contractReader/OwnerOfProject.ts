1c68cc96caeefe906a0fc10f6178cdc86c6fca3f
aeolian <aeolian.eth@gmail.com>
2022-07-03 11:58:38 +0930

    Fix project owner context in V1

--- src/hooks/v1/contractReader/OwnerOfProject.ts
+++ src/hooks/v1/contractReader/OwnerOfProject.ts
@@ -1,4 +1,4 @@
-import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
+import { BigNumberish } from '@ethersproject/bignumber'
 import { V1ContractName } from 'models/v1/contracts'
 
 import useContractReader from './ContractReader'
@@ -8,6 +8,6 @@ export default function useOwnerOfProject(projectId: BigNumberish | undefined) {
   return useContractReader<string>({
     contract: V1ContractName.Projects,
     functionName: 'ownerOf',
-    args: projectId ? [BigNumber.from(projectId).toHexString()] : null,
+    args: projectId ? [projectId] : null,
   })
 }

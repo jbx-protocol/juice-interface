cf8293d1a66f09c6890e99074aa464d48216ffa2
peripheralist <peripheralist@gmail.com>
2022-07-05 13:11:25 -0400

    show 'fee from' on v2 project fee payments
    v2ProjectHandle changes

--- src/models/subgraph-entities/vX/pay-event.ts
+++ src/models/subgraph-entities/vX/pay-event.ts
@@ -16,6 +16,7 @@ export interface PayEvent extends BaseProjectEntity, BaseEventEntity {
   beneficiary: string
   amount: BigNumber
   note: string
+  feeFromV2Project?: number
 }
 
 export type PayEventJson = Partial<
@@ -31,4 +32,7 @@ export const parsePayEventJson = (j: PayEventJson): Partial<PayEvent> => ({
   beneficiary: j.beneficiary,
   amount: j.amount ? BigNumber.from(j.amount) : undefined,
   note: j.note,
+  feeFromV2Project: j.feeFromV2Project
+    ? parseInt(j.feeFromV2Project)
+    : undefined,
 })

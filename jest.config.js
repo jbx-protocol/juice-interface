3804bb6bbf07db67b66ffc0530a948dfd30b4f94
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-07-18 13:33:47 +0930

    V2 project handle link (#1425)
    * Fix V2ProjectHandle link
    
    * Add jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rootDir: './src/',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules', 'src'],
+  verbose: true,
+  setupFiles: ['../jest.setup.ts'],
+}

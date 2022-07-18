b531b271df05f6dc740a62102e33693c3702c0f8
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-07-18 13:33:47 +0930

    V2 project handle link (#1425)
    * Fix V2ProjectHandle link
    
    * Add jest

--- src/components/v2/shared/V2ProjectHandle.tsx
+++ src/components/v2/shared/V2ProjectHandle.tsx
@@ -15,14 +15,12 @@ export default function V2ProjectHandle({
 
   return (
     <Link href={v2ProjectRoute({ projectId, handle })}>
-      <div
-        style={{ fontWeight: 500, ...style }}
+      <a
+        style={{ fontWeight: 500, marginRight: '0.5rem', ...style }}
         className="text-primary hover-text-action-primary hover-text-decoration-underline"
       >
-        <span style={{ marginRight: '0.5rem' }}>
-          {handle ? `@${handle}` : <Trans>Project {projectId}</Trans>}
-        </span>
-      </div>
+        {handle ? `@${handle}` : <Trans>Project {projectId}</Trans>}
+      </a>
     </Link>
   )
 }

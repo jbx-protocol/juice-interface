7af8d6b267ad0d73d4cf45f7133d830b033a5561
Jonathan Miller <jon@jmill.dev>
2022-07-22 08:41:52 -0400

    Add veNft header and name of ERC20 hook

--- src/components/veNft/VeNftContent.tsx
+++ src/components/veNft/VeNftContent.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import VeNftHeader from 'components/veNft/VeNftHeader'
+
+import { tokenSymbolText } from 'utils/tokenSymbolText'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import { t } from '@lingui/macro'
 
 const VeNftContent = () => {
-  return <div>VeNftContent</div>
+  const { tokenSymbol, tokenName, projectMetadata } =
+    useContext(V2ProjectContext)
+
+  const tokenSymbolDisplayText = tokenSymbolText({ tokenSymbol })
+  const projectName = projectMetadata?.name ?? t`Unknown Project`
+
+  return (
+    <>
+      <VeNftHeader
+        tokenName={tokenName}
+        tokenSymbolDisplayText={tokenSymbolDisplayText}
+        projectName={projectName}
+      />
+    </>
+  )
 }
 
 export default VeNftContent

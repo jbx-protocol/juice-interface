25b75d0184324caee8c5aa76a794662ba53caecd
Wraeth <wraeth.dao@gmail.com>
2022-07-26 22:27:52 +1000

    Update middleware to redirect route

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -1,10 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { paginateDepleteProjectsQueryCall } from 'utils/apollo'
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // If request is for a handle id, add the search param with `isHandle`.
   if (request.nextUrl.pathname.startsWith('/@')) {
+    const handle = request.nextUrl.pathname.slice(2) // slice off "/@"
+    const projects = await paginateDepleteProjectsQueryCall({
+      variables: {
+        where: { cv: '2', handle },
+      },
+    })
     const url = request.nextUrl
-    url.searchParams.set('isHandle', 'true')
+    if (!projects.length) {
+      url.pathname = '/404'
+      return NextResponse.rewrite(url)
+    }
+
+    const projectId = projects[0].projectId
+    url.pathname = `/v2/p/${projectId}`
     return NextResponse.rewrite(url)
   }
 }

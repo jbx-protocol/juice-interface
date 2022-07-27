a8a2def767b7d1deb93fbe99fbba55b87b2bd704
Wraeth <wraeth.dao@gmail.com>
2022-07-27 19:01:34 +0200

    quick check on ogurl

--- src/components/common/SEO/SEO.tsx
+++ src/components/common/SEO/SEO.tsx
@@ -26,6 +26,7 @@ export const SEO: FC<Props> = ({
   robots,
   children,
 }) => {
+  console.info(url)
   const formatTwitterHandle = (handle: string | undefined) =>
     handle ? (handle.startsWith('@') ? handle : '@' + handle) : undefined
   const formattedTitle = title
@@ -66,7 +67,7 @@ export const SEO: FC<Props> = ({
 
       <OpenGraphMetaTags
         type="website"
-        url={url ?? process.env.NEXT_PUBLIC_BASE_URL} // default to base url
+        // url={url ?? process.env.NEXT_PUBLIC_BASE_URL} // default to base url
         title={formattedTitle}
         description={description ?? config.description}
         siteName={config.title}

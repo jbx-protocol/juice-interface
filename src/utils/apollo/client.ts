8515b33629d4fc90052344461f6098746c720712
Wraeth <wraeth.dao@gmail.com>
2022-07-27 18:34:09 +0200

    strip off graphql in url

--- src/utils/apollo/client.ts
+++ src/utils/apollo/client.ts
@@ -1,7 +1,19 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+const subgraphUri = () => {
+  const _subgraphUrl = process.env.NEXT_PUBLIC_SUBGRAPH_URL
+  if (!_subgraphUrl) {
+    throw new Error('NEXT_PUBLIC_SUBGRAPH_URL not defined')
+  }
+  const url = new URL(_subgraphUrl)
+  if (url.pathname.match(/graphql$/g)) {
+    return url.href.slice(0, url.href.lastIndexOf('/'))
+  }
+  return url.href
+}
+
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL,
+  uri: subgraphUri(),
   cache: new InMemoryCache(),
 })
 

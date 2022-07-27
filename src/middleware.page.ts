8b2548ec409a8c98209ade55e807804acf5b1ff1
Wraeth <wraeth.dao@gmail.com>
2022-07-27 13:06:27 +0200

    info log

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -5,6 +5,10 @@ export async function middleware(request: NextRequest) {
   // If request is for a handle id, add the search param with `isHandle`.
   if (request.nextUrl.pathname.startsWith('/@')) {
     const handle = request.nextUrl.pathname.slice(2) // slice off "/@"
+    console.info('Project middleware request', {
+      pathname: request.nextUrl.pathname,
+      handle,
+    })
     const projects = await paginateDepleteProjectsQueryCall({
       variables: {
         where: { cv: '2', handle },
@@ -12,12 +16,22 @@ export async function middleware(request: NextRequest) {
     })
     const url = request.nextUrl
     if (!projects.length) {
+      console.info('Page not found', {
+        originalPathname: request.nextUrl.pathname,
+        newPathname: '/404',
+        handle,
+      })
       url.pathname = '/404'
       return NextResponse.rewrite(url)
     }
 
     const projectId = projects[0].projectId
     url.pathname = `/v2/p/${projectId}`
+    console.info('Rewriting to project route', {
+      originalPathname: request.nextUrl.pathname,
+      newPathname: url.pathname,
+      handle,
+    })
     return NextResponse.rewrite(url)
   }
 }

b4f4de5571c241eb7dac11f21424cf893ea85c4c
Jonathan Miller <jon@jmill.dev>
2022-07-29 12:06:54 -0400

    Add comment for permission

--- src/hooks/veNft/transactor/VeNftUnclaimedTokenPermissions.ts
+++ src/hooks/veNft/transactor/VeNftUnclaimedTokenPermissions.ts
@@ -11,7 +11,7 @@ export function useUnclaimedTokensPermissionTx(): TransactorInstance {
   const { transactor, contracts } = useContext(V2UserContext)
   const { projectId } = useContext(V2ProjectContext)
   const contractAddress = VENFT_CONTRACT_ADDRESS
-  const permissionIndexes = [12]
+  const permissionIndexes = [12] // TRANSFER permission, https://github.com/jbx-protocol/juice-contracts-v2/blob/main/contracts/libraries/JBOperations.sol
 
   return (_, txOpts) => {
     if (!transactor || !contracts || !projectId || !contractAddress) {

789f00a57416e0b8cb3e0c34161e58e4b544e627
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-01 15:30:09 +0930

    Revert "Better apollo client config error (#1624)"
    This reverts commit 151dc2ab09ae6fe87612ffdcd0a9eba2505552a9.

--- src/utils/apollo/client.ts
+++ src/utils/apollo/client.ts
@@ -2,20 +2,19 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { isBrowser } from 'utils/isBrowser'
 
 const subgraphUri = () => {
-  const urlKey = isBrowser()
-    ? 'NEXT_PUBLIC_SUBGRAPH_URL'
-    : 'GRAPHQL_SCHEMA_SUBGRAPH_URL'
-
-  const uri = process.env[urlKey]
-  if (!uri) {
-    throw new Error(`${urlKey} environment variable not defined`)
+  let uri: string | undefined
+  if (isBrowser()) {
+    uri = process.env.NEXT_PUBLIC_SUBGRAPH_URL
+  } else {
+    uri = process.env.GRAPHQL_SCHEMA_SUBGRAPH_URL
   }
-
+  if (!uri) {
+    throw new Error('NEXT_PUBLIC_SUBGRAPH_URL not defined')
+  }
   const url = new URL(uri)
   if (url.pathname.match(/graphql$/g)) {
     return url.href.slice(0, url.href.lastIndexOf('/'))
   }
-
   return url.href
 }
 

11e42444d9b81b9a4e3ad43024c3c08a165bc13f
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-02 13:15:59 +0930

    Better apollo env var error (#1632)

--- src/utils/apollo/client.ts
+++ src/utils/apollo/client.ts
@@ -5,12 +5,20 @@ const subgraphUri = () => {
   let uri: string | undefined
   if (isBrowser()) {
     uri = process.env.NEXT_PUBLIC_SUBGRAPH_URL
+    if (!uri) {
+      throw new Error(
+        'NEXT_PUBLIC_SUBGRAPH_URL environment variable not defined',
+      )
+    }
   } else {
     uri = process.env.GRAPHQL_SCHEMA_SUBGRAPH_URL
+    if (!uri) {
+      throw new Error(
+        'GRAPHQL_SCHEMA_SUBGRAPH_URL environment variable not defined',
+      )
+    }
   }
-  if (!uri) {
-    throw new Error('NEXT_PUBLIC_SUBGRAPH_URL not defined')
-  }
+
   const url = new URL(uri)
   if (url.pathname.match(/graphql$/g)) {
     return url.href.slice(0, url.href.lastIndexOf('/'))

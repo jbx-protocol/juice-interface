f0e296df3f100bcb83f54b3ebdf2c7d146ead524
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-03 13:36:35 +1000

    Add messages.pot

---
+++ scripts/extract-translations-template.sh
@@ -0,0 +1,14 @@
+# https://github.com/lingui/js-lingui/issues/952#issuecomment-765449330
+export NODE_ENV=development
+
+lingui extract-template
+
+
+# Append newline to every file,
+# to match the behavior of the Crowdin GitHub
+# action script.
+LOCALE_CHANGED_FILES=$(git diff --name-only src/locales)
+for FILE in $LOCALE_CHANGED_FILES
+do
+	echo "" >> $FILE
+done
--- scripts/lint-translations.sh
+++ scripts/lint-translations.sh
@@ -3,23 +3,23 @@ yarn i18n:extract
 
 # get all translation files that were updated
 # from `yarn i18n:extract`
-LOCALE_CHANGED_FILES=$(git diff --name-only src/locales)
+FILE_DIFF=$(git diff --name-only src/locales/messages.pot)
 
-# Bail if there were changes to translation files
+# Bail if there were changes to the template file.
 # that weren't included in the commit.
-if ! [ -z "$LOCALE_CHANGED_FILES" ]; then
-  echo "🍎 Translation source files are out of date. Run 'yarn i18n:extract' locally and commit changes to the following files:\n$LOCALE_CHANGED_FILES."
+if ! [ -z "$FILE_DIFF" ]; then
+  echo "🍎 messages.pot is out of date. Run 'yarn i18n:extract' locally and commit the changes."
   exit 1
 else
   # bail if .po files contain git merge conflict diff artefact (like <<<<< HEAD)
-  for File in $(find src/locales -maxdepth 2 -regex '.*.po')
+  for File in $(find src/locales -maxdepth 2 -regex 'messages.pot')
   do
     if grep -e '<<<' -e '>>>' -e '====' "$File"; then
-      echo "🍎 Translation source files contain artefacts from git merge conflict."
+      echo "🍎 messages.pot contains artifacts from git merge conflict."
       exit 1
     fi
   done
 
-  echo "🍏 Translation source files are up to date."
+  echo "🍏 messages.pot is up to date."
   exit 0
 fi

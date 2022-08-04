337fa34e2e130ad367314d8acf772c427961cd1c
Wraeth <wraeth.dao@gmail.com>
2022-08-04 12:57:16 +0200

    apply fix for network provider websocket error

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,13 +4,16 @@ import { Head } from 'components/common'
 
 import '../styles/antd.css'
 import '../styles/index.scss'
+import { NetworkProvider } from 'providers/NetworkProvider'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* Default HEAD - overwritten by specific page SEO */}
       <Head />
-      <Component {...pageProps} />
+      <NetworkProvider>
+        <Component {...pageProps} />
+      </NetworkProvider>
     </>
   )
 }

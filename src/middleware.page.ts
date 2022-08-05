1cac8576691819dc5dd69bfd6bfc1e1ff7a425cb
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-08-05 15:43:36 +0200

    Revert nextjs static page revert and apply fix (#1673)
    * Revert "Revert some of NextJS static path/props (#1640)"
    
    This reverts commit eb879f070a1733af48885394847d588a1d18e528.
    
    * apply fix for network provider websocket error

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -1,10 +1,43 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { paginateDepleteProjectsQueryCall } from 'utils/apollo'
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // If request is for a handle id, add the search param with `isHandle`.
   if (request.nextUrl.pathname.startsWith('/@')) {
+    const handle = request.nextUrl.pathname.slice(2) // slice off "/@"
+    console.info('Project middleware request', {
+      pathname: request.nextUrl.pathname,
+      handle,
+    })
+    let projects
+    try {
+      projects = await paginateDepleteProjectsQueryCall({
+        variables: {
+          where: { cv: '2', handle },
+        },
+      })
+    } catch (e) {
+      console.error('Failed to query projects', e)
+      throw e
+    }
     const url = request.nextUrl
-    url.searchParams.set('isHandle', 'true')
+    if (!projects.length) {
+      console.info('Page not found', {
+        originalPathname: request.nextUrl.pathname,
+        newPathname: '/404',
+        handle,
+      })
+      url.pathname = '/404'
+      return NextResponse.rewrite(url)
+    }
+
+    const projectId = projects[0].projectId
+    url.pathname = `/v2/p/${projectId}`
+    console.info('Rewriting to project route', {
+      originalPathname: request.nextUrl.pathname,
+      newPathname: url.pathname,
+      handle,
+    })
     return NextResponse.rewrite(url)
   }
 }
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import { t, Trans } from '@lingui/macro'
 import { Button, Col, Row, Space } from 'antd'
 import FeedbackFormButton from 'components/FeedbackFormButton'
+import { AppWrapper } from 'components/common'
 
 import { ThemeContext } from 'contexts/themeContext'
 import { CSSProperties, useContext } from 'react'
@@ -16,6 +17,14 @@ import { TopProjectsSection } from './home/TopProjectsSection'
 import { StatsSection } from './home/StatsSection'
 import { HowItWorksSection } from './home/HowItWorksSection'
 import { HeroHeading, HeroSubheading } from './home/strings'
+
+export default function LandingPage() {
+  return (
+    <AppWrapper>
+      <Landing />
+    </AppWrapper>
+  )
+}
 
 const BigHeader = ({
   text,
@@ -39,7 +48,7 @@ const BigHeader = ({
   )
 }
 
-export default function Landing() {
+function Landing() {
   const { theme, forThemeOption } = useContext(ThemeContext)
   const colors = theme.colors
   const isMobile = useMobile()

20ed4d40400123dcf5c348e5bb0550f937e348a0
Jonathan Miller <jon@jmill.dev>
2022-08-10 08:25:05 +0100

    [3/x] Add test page

--- src/components/v2/V2Project/V2ProjectSettingsPage/V2ProjectSettings.tsx
+++ src/components/v2/V2Project/V2ProjectSettingsPage/V2ProjectSettings.tsx
@@ -1,4 +1,17 @@
 import { Layout, Menu, MenuProps } from 'antd'
+import { useForm } from 'antd/lib/form/Form'
+import ProjectDetailsForm, {
+  ProjectDetailsFormFields,
+} from 'components/forms/ProjectDetailsForm'
+
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import { useEditV2ProjectDetailsTx } from 'hooks/v2/transactor/EditV2ProjectDetailsTx'
+import Link from 'next/link'
+import { useState, useContext, useCallback, useEffect } from 'react'
+import { uploadProjectMetadata } from 'utils/ipfs'
+import { revalidateProject } from 'utils/revalidateProject'
+
+import { PROJECT_PAY_CHARACTER_LIMIT } from 'constants/numbers'
 const { Content, Sider } = Layout
 
 type MenuItem = Required<MenuProps>['items'][number]
@@ -35,19 +48,99 @@ const items: MenuItem[] = [
 ]
 
 const V2ProjectSettings = () => {
+  const [projectForm] = useForm<ProjectDetailsFormFields>()
+
+  const [loadingSaveChanges, setLoadingSaveChanges] = useState<boolean>()
+  const { projectMetadata, projectId } = useContext(V2ProjectContext)
+
+  const EditV2ProjectDetailsTx = useEditV2ProjectDetailsTx()
+
+  async function onProjectFormSaved() {
+    setLoadingSaveChanges(true)
+
+    const fields = projectForm.getFieldsValue(true)
+
+    const uploadedMetadata = await uploadProjectMetadata({
+      name: fields.name,
+      description: fields.description,
+      logoUri: fields.logoUri,
+      infoUri: fields.infoUri,
+      twitter: fields.twitter,
+      discord: fields.discord,
+      payButton: fields.payButton.substring(0, PROJECT_PAY_CHARACTER_LIMIT), // Enforce limit
+      payDisclosure: fields.payDisclosure,
+    })
+
+    if (!uploadedMetadata.IpfsHash) {
+      setLoadingSaveChanges(false)
+      return
+    }
+
+    EditV2ProjectDetailsTx(
+      { cid: uploadedMetadata.IpfsHash },
+      {
+        onDone: () => setLoadingSaveChanges(false),
+        onConfirmed: async () => {
+          if (projectId) {
+            await revalidateProject({
+              cv: '2',
+              projectId: String(projectId),
+            })
+          }
+          projectForm.resetFields()
+        },
+      },
+    )
+  }
+
+  const resetProjectForm = useCallback(() => {
+    projectForm.setFieldsValue({
+      name: projectMetadata?.name ?? '',
+      infoUri: projectMetadata?.infoUri ?? '',
+      logoUri: projectMetadata?.logoUri ?? '',
+      description: projectMetadata?.description ?? '',
+      twitter: projectMetadata?.twitter ?? '',
+      discord: projectMetadata?.discord ?? '',
+      payButton: projectMetadata?.payButton ?? '',
+      payDisclosure: projectMetadata?.payDisclosure ?? '',
+    })
+  }, [
+    projectMetadata?.name,
+    projectMetadata?.infoUri,
+    projectMetadata?.logoUri,
+    projectMetadata?.description,
+    projectMetadata?.twitter,
+    projectMetadata?.discord,
+    projectMetadata?.payDisclosure,
+    projectMetadata?.payButton,
+    projectForm,
+  ])
+
+  // initially fill form with any existing redux state
+  useEffect(() => {
+    resetProjectForm()
+  }, [resetProjectForm])
+
   return (
     <Layout style={{ minHeight: '100vh', background: 'transparent' }}>
       <Sider style={{ background: 'transparent' }}>
-        <div className="logo" />
+        <Link href={`project`}>{`< Back to Project`}</Link>
         <Menu
           defaultOpenKeys={['project', 'funding', 'manage']}
+          defaultSelectedKeys={['project1']}
           mode="inline"
+          theme="dark"
           items={items}
         />
       </Sider>
       <Layout style={{ background: 'transparent' }}>
         <Content style={{ margin: '0 16px' }}>
-          <div style={{ padding: 24, minHeight: 360 }}>Content</div>
+          <ProjectDetailsForm
+            form={projectForm}
+            onFinish={onProjectFormSaved}
+            hideProjectHandle
+            loading={loadingSaveChanges}
+          />
         </Content>
       </Layout>
     </Layout>

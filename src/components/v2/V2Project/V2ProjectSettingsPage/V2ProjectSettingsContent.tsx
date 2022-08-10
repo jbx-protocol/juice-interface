a28dd687ca735cb9ade64496d7c78893a87f445b
Jonathan Miller <jon@jmill.dev>
2022-08-10 10:11:35 +0100

    [5/x] add page switcher, second page, and fallback page

--- src/components/v2/V2Project/V2ProjectSettingsPage/V2ProjectSettingsContent.tsx
+++ src/components/v2/V2Project/V2ProjectSettingsPage/V2ProjectSettingsContent.tsx
@@ -2,6 +2,7 @@ import { Trans } from '@lingui/macro'
 import { Layout, Divider } from 'antd'
 
 import V2ProjectSettingsGeneralTab from './V2ProjectSettingsGeneralTab'
+import V2ProjectSettingsProjectHandleTab from './V2ProjectSettingsProjectHandleTab'
 
 interface V2ProjectSettingsContentProps {
   activeKey: string | undefined
@@ -10,6 +11,17 @@ interface V2ProjectSettingsContentProps {
 const V2ProjectSettingsContent = ({
   activeKey,
 }: V2ProjectSettingsContentProps) => {
+  const getActiveTab = (activeKey: string | undefined) => {
+    switch (activeKey) {
+      case 'project1':
+        return <V2ProjectSettingsGeneralTab />
+      case 'project2':
+        return <V2ProjectSettingsProjectHandleTab />
+      default:
+        return <p>{activeKey}</p>
+    }
+  }
+
   return (
     <Layout style={{ background: 'transparent' }}>
       <Layout.Content style={{ margin: '0 16px' }}>
@@ -17,7 +29,7 @@ const V2ProjectSettingsContent = ({
           <Trans>{activeKey}</Trans>
         </h2>
         <Divider />
-        <V2ProjectSettingsGeneralTab />
+        {getActiveTab(activeKey)}
       </Layout.Content>
     </Layout>
   )

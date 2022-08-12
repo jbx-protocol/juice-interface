5d82a0952dac952fe4a484ac2e8a0086363906fd
Benjamin Schachter <benschac@gmail.com>
2022-08-12 17:00:54 -0400

    replace util with invariant

--- src/utils/isDefined.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isDefined(condition: any): asserts condition {
-  if (condition === undefined || condition === null || condition === false) {
-    throw new Error(
-      `Expected 'condition' to be defined, but received ${condition}`,
-    )
-  }
-}

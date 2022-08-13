78bdc845db60763b5fcb3c9e914308d3989bda76
NakedFool <medenicablazo@gmail.com>
2022-08-13 12:51:45 +0200

    font size adjusted

--- src/components/inputs/Pay/PayInputSubText.tsx
+++ src/components/inputs/Pay/PayInputSubText.tsx
@@ -125,7 +125,7 @@ export default function PayInputSubText({
   return (
     <div
       style={{
-        fontSize: '.65rem',
+        fontSize: '.7rem',
         width: '100%',
         display: 'flex',
         justifyContent: 'space-between',

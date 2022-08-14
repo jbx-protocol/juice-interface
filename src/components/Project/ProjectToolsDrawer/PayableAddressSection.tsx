865e625e7b5d080d68b482162359ca8f3d5f58ad
Johnny D <johnnyD.eth@protonmail.com>
2022-08-15 06:41:28 +1000

    Move deployed project payers to public section of tools drawer (#1731)
    * remove owner % reserved rate when reserved rate is 0
    
    * reserved rate check added for v2 project
    
    * update and move project payers section to public tab of tools
    
    Co-authored-by: NakedFool <medenicablazo@gmail.com>
    Co-authored-by: peripheralist <79433522+peripheralist@users.noreply.github.com>

--- src/components/Project/ProjectToolsDrawer/PayableAddressSection.tsx
+++ src/components/Project/ProjectToolsDrawer/PayableAddressSection.tsx
@@ -1,11 +1,12 @@
-import { Trans } from '@lingui/macro'
-import { JBDiscordLink } from 'pages/home/QAs'
+import { plural, Trans } from '@lingui/macro'
 import LaunchProjectPayerButton from 'components/v2/V2Project/LaunchProjectPayer/LaunchProjectPayerButton'
-import { ThemeContext } from 'contexts/themeContext'
-import { useContext } from 'react'
-import { ExclamationCircleOutlined } from '@ant-design/icons'
+import { useContext, useState } from 'react'
 import { DeployProjectPayerTxArgs } from 'hooks/v2/transactor/DeployProjectPayerTx'
 import { TransactorInstance } from 'hooks/Transactor'
+import { useProjectPayers } from 'hooks/v2/ProjectPayers'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import { Button } from 'antd'
+import ProjectPayersModal from 'components/v2/V2Project/ProjectPayersModal'
 
 export function PayableAddressSection({
   useDeployProjectPayerTx,
@@ -14,32 +15,54 @@ export function PayableAddressSection({
     | TransactorInstance<DeployProjectPayerTxArgs>
     | undefined
 }) {
-  const {
-    theme: { colors },
-  } = useContext(ThemeContext)
+  const [projectPayersModalIsVisible, setProjectPayersModalIsVisible] =
+    useState<boolean>()
+
+  const { projectId } = useContext(V2ProjectContext)
+
+  const { data: projectPayers } = useProjectPayers(projectId)
 
   return (
-    <section>
-      <h3>
-        <Trans>Create payable address</Trans>
-      </h3>
-      <p>
-        <Trans>
-          Create an Ethereum address that can be used to pay your project
-          directly.
-        </Trans>
-      </p>
-      <LaunchProjectPayerButton
-        useDeployProjectPayerTx={useDeployProjectPayerTx}
-      />
-      <p style={{ marginTop: 10, color: colors.text.secondary }}>
-        <ExclamationCircleOutlined />{' '}
-        <Trans>
-          If you have already deployed a payable address and have lost it,
-          please contact the Juicebox team through{' '}
-          <JBDiscordLink>Discord.</JBDiscordLink>
-        </Trans>
-      </p>
-    </section>
+    <>
+      <section>
+        <h3>
+          <Trans>Create payable address</Trans>
+        </h3>
+        <p>
+          <Trans>
+            Create an Ethereum address that can be used to pay your project
+            directly.
+          </Trans>
+        </p>
+        <div>
+          <LaunchProjectPayerButton
+            useDeployProjectPayerTx={useDeployProjectPayerTx}
+          />
+        </div>
+        {projectPayers && (
+          <>
+            <Button
+              onClick={() => setProjectPayersModalIsVisible(true)}
+              block
+              size="small"
+              style={{
+                marginTop: '15px',
+                width: '55%',
+              }}
+            >
+              {plural(projectPayers.length, {
+                one: 'View # deployed payment address',
+                other: 'View # deployed payment addresses',
+              })}
+            </Button>
+            <ProjectPayersModal
+              visible={projectPayersModalIsVisible}
+              onCancel={() => setProjectPayersModalIsVisible(false)}
+              projectPayers={projectPayers}
+            />
+          </>
+        )}
+      </section>
+    </>
   )
 }

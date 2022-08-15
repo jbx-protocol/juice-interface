321991bcf6ebb85b4eb3f4ff0b8e3681f390d505
Wraeth <wraeth.dao@gmail.com>
2022-08-15 18:48:29 +1000

    Web3-Onboard - Changed useErc20Contract to use useLoadContractFromAddress as shared code [4/n]

--- src/hooks/Erc20Contract.ts
+++ src/hooks/Erc20Contract.ts
@@ -1,34 +1,7 @@
-import { isAddress } from '@ethersproject/address'
-import { Contract } from '@ethersproject/contracts'
-
-import { NetworkContext } from 'contexts/networkContext'
 import erc20Abi from 'erc-20-abi'
-import * as constants from '@ethersproject/constants'
-import { useContext, useEffect, useState } from 'react'
 
-import { readProvider } from 'constants/readProvider'
+import { useLoadContractFromAddress } from './LoadContractFromAddress'
 
 export function useErc20Contract(address: string | undefined) {
-  const [contract, setContract] = useState<Contract>()
-  const { signingProvider } = useContext(NetworkContext)
-
-  useEffect(() => {
-    const provider = signingProvider ?? readProvider
-
-    provider.listAccounts().then(accounts => {
-      if (
-        !address ||
-        !isAddress(address) ||
-        address === constants.AddressZero
-      ) {
-        setContract(undefined)
-      } else if (!accounts.length) {
-        setContract(new Contract(address, erc20Abi, readProvider))
-      } else {
-        setContract(new Contract(address, erc20Abi, provider.getSigner()))
-      }
-    })
-  }, [address, signingProvider])
-
-  return contract
+  return useLoadContractFromAddress({ address, abi: erc20Abi })
 }

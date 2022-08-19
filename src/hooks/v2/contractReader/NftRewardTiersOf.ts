d0e2a8f55b98d05caec5e3334c35db4b18cbeb34
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-19 12:39:16 +0930

    Fix a few runtime exceptions (#1768)
    * Fix RelaunchFCBanner FUOC and useEffect error
    
    * Feature flag veNFT subgraph queries
    
    * Feature flag NFT rewards
    
    * fix

--- src/hooks/v2/contractReader/NftRewardTiersOf.ts
+++ src/hooks/v2/contractReader/NftRewardTiersOf.ts
@@ -2,10 +2,12 @@ import { Contract } from '@ethersproject/contracts'
 import * as constants from '@ethersproject/constants'
 import { useMemo } from 'react'
 import { ContractNftRewardTier } from 'models/v2/nftRewardTier'
+import { featureFlagEnabled } from 'utils/featureFlags'
 
-import useV2ContractReader from './V2ContractReader'
 import { TEMPORARY_NFT_DATASOURCE_ABI } from 'constants/contracts/rinkeby/TEMPORARY_NFT_DATASOUCE_ABI'
 import { readProvider } from 'constants/readProvider'
+import { FEATURE_FLAGS } from 'constants/featureFlags'
+import useV2ContractReader from './V2ContractReader'
 
 export function useNftRewardTiersOf(dataSourceAddress: string | undefined) {
   const dataSourceContract = useMemo(
@@ -21,5 +23,7 @@ export function useNftRewardTiersOf(dataSourceAddress: string | undefined) {
   return useV2ContractReader<ContractNftRewardTier[]>({
     contract: dataSourceContract,
     functionName: 'allTiers',
+    // send null when FF disabled, so the fetch doesn't execute.
+    args: featureFlagEnabled(FEATURE_FLAGS.NFT_REWARDS) ? undefined : null,
   })
 }

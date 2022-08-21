8a778f6c07530557134412afb3883eb5721fe0d3
Wraeth <wraeth.dao@gmail.com>
2022-08-21 11:26:43 +1000

    Activity Share Button - Refactor Activity Event Elements - [1/n]

---
+++ src/components/activityEventElems/ActivityElement/activityElementEvent.ts
@@ -0,0 +1,6 @@
+import { PayEvent } from 'models/subgraph-entities/vX/pay-event'
+
+export type ActivityElementEvent = Pick<PayEvent, 'timestamp' | 'txHash'> & {
+  beneficiary?: string
+  terminal?: string
+}
--- src/components/activityEventElems/DeployedERC20EventElem.tsx
+++ src/components/activityEventElems/DeployedERC20EventElem.tsx
@@ -1,7 +1,6 @@
-import { Trans } from '@lingui/macro'
-import EtherscanLink from 'components/EtherscanLink'
 import { DeployedERC20Event } from 'models/subgraph-entities/vX/deployed-erc20-event'
-import { formatHistoricalDate } from 'utils/format/formatDate'
+import { ActivityEvent } from './ActivityElement/ActivityElement'
+import { primaryContentFontSize } from './styles'
 
 export default function DeployedERC20EventElem({
   event,
@@ -11,24 +10,13 @@ export default function DeployedERC20EventElem({
     | undefined
 }) {
   if (!event) return null
-
   return (
-    <div className="flex content-between justify-between">
-      <div>
-        <div className="text-xs text-grey-400 dark:text-slate-200">
-          <Trans>Deployed ERC20 token</Trans>
-        </div>
-        <div className="text-base">{event.symbol}</div>
-      </div>
-
-      <div className="text-right">
-        {event.timestamp && (
-          <div className="text-xs text-grey-400 dark:text-slate-200">
-            {formatHistoricalDate(event.timestamp * 1000)}{' '}
-            <EtherscanLink value={event.txHash} type="tx" />
-          </div>
-        )}
-      </div>
-    </div>
+    <ActivityEvent
+      header="Deployed ERC20 token"
+      subject={
+        <div style={{ fontSize: primaryContentFontSize }}>{event.symbol}</div>
+      }
+      event={{ ...event, beneficiary: undefined }}
+    />
   )
 }

a9ee800403caf4a95bf64cfc9b94120313866377
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-24 15:20:01 +0930

    Remove reconfigre duplicated code

--- src/components/v2/V2Project/V2ProjectReconfigureModal/V2ReconfigureModalTrigger.tsx
+++ src/components/v2/V2Project/V2ProjectReconfigureModal/V2ReconfigureModalTrigger.tsx
@@ -1,23 +1,14 @@
 import { Button, Tooltip } from 'antd'
-import { useContext, useEffect, useRef, useState } from 'react'
-import { Provider, useDispatch } from 'react-redux'
+import { useRef, useState } from 'react'
+import { Provider } from 'react-redux'
 import store, { createStore } from 'redux/store'
 
 import { SettingOutlined } from '@ant-design/icons'
-
-import { V2ProjectContext } from 'contexts/v2/projectContext'
-import useProjectDistributionLimit from 'hooks/v2/contractReader/ProjectDistributionLimit'
-import useProjectQueuedFundingCycle from 'hooks/v2/contractReader/ProjectQueuedFundingCycle'
-import useProjectSplits from 'hooks/v2/contractReader/ProjectSplits'
-import { V2FundingCycle } from 'models/v2/fundingCycle'
-import { editingV2ProjectActions } from 'redux/slices/editingV2Project'
-import { fromWad } from 'utils/formatNumber'
 
 import { t } from '@lingui/macro'
 
 import { reloadWindow } from 'utils/windowUtils'
 
-import { ETH_PAYOUT_SPLIT_GROUP } from 'constants/v2/splits'
 import V2ProjectReconfigureModal from './index'
 
 // This component uses a local version of the entire Redux store
@@ -32,10 +23,6 @@ export default function V2ReconfigureFundingModalTrigger({
   hideProjectDetails?: boolean
   triggerButton?: (onClick: VoidFunction) => JSX.Element
 }) {
-  const dispatch = useDispatch()
-  const { projectId, fundingCycle, primaryTerminal } =
-    useContext(V2ProjectContext)
-
   const [reconfigureModalVisible, setReconfigureModalVisible] =
     useState<boolean>(false)
 
@@ -50,68 +37,6 @@ export default function V2ReconfigureFundingModalTrigger({
     setReconfigureModalVisible(false)
     reloadWindow()
   }
-
-  // Load queued FC data of project
-  const { data: queuedFundingCycleResponse } = useProjectQueuedFundingCycle({
-    projectId,
-  })
-
-  const [queuedFundingCycle] = queuedFundingCycleResponse ?? []
-
-  let effectiveFundingCycle: V2FundingCycle | undefined
-  // If duration is 0, use current FC (not queued)
-  if (!fundingCycle?.duration || fundingCycle?.duration.eq(0)) {
-    effectiveFundingCycle = fundingCycle
-  } else {
-    effectiveFundingCycle = queuedFundingCycle
-  }
-
-  const { data: queuedDistributionLimitData } = useProjectDistributionLimit({
-    projectId,
-    configuration: effectiveFundingCycle?.configuration.toString(),
-    terminal: primaryTerminal,
-  })
-  const [queuedDistributionLimit, queuedDistributionLimitCurrency] =
-    queuedDistributionLimitData ?? []
-
-  const { data: queuedPayoutSplits } = useProjectSplits({
-    projectId,
-    splitGroup: ETH_PAYOUT_SPLIT_GROUP,
-    domain: effectiveFundingCycle?.configuration?.toString(),
-  })
-
-  // Load queued FC data (if it exists) into redux
-  useEffect(() => {
-    if (queuedDistributionLimitData) {
-      dispatch(
-        editingV2ProjectActions.setDistributionLimit(
-          fromWad(queuedDistributionLimit),
-        ),
-      )
-      dispatch(
-        editingV2ProjectActions.setDistributionLimitCurrency(
-          queuedDistributionLimitCurrency.toNumber().toString(),
-        ),
-      )
-    }
-    if (effectiveFundingCycle?.duration) {
-      dispatch(
-        editingV2ProjectActions.setDuration(
-          effectiveFundingCycle?.duration.toNumber().toString(),
-        ),
-      )
-    }
-    if (queuedPayoutSplits) {
-      dispatch(editingV2ProjectActions.setPayoutSplits(queuedPayoutSplits))
-    }
-  }, [
-    queuedDistributionLimitData,
-    dispatch,
-    effectiveFundingCycle,
-    queuedDistributionLimit,
-    queuedDistributionLimitCurrency,
-    queuedPayoutSplits,
-  ])
 
   return (
     <div style={{ textAlign: 'right' }}>

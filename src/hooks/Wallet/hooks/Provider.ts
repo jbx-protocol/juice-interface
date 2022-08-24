056622070232947ebd20795f67c62f38c2ab64b0
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-08-24 12:07:02 +1000

    Make read only provider more stable (#1802)

--- src/hooks/Wallet/hooks/Provider.ts
+++ src/hooks/Wallet/hooks/Provider.ts
@@ -1,13 +1,19 @@
-import { Web3Provider } from '@ethersproject/providers'
+import { JsonRpcBatchProvider, Web3Provider } from '@ethersproject/providers'
 import { useConnectWallet } from '@web3-onboard/react'
+import { readNetwork } from 'constants/networks'
 import { useMemo } from 'react'
 
 export function useProvider() {
-  const [{ wallet }] = useConnectWallet()
-  const provider = useMemo(
-    () =>
-      wallet?.provider ? new Web3Provider(wallet.provider, 'any') : undefined,
-    [wallet],
+  const readProvider = useMemo(
+    () => new JsonRpcBatchProvider(readNetwork.rpcUrl),
+    [],
   )
+  const [{ wallet }] = useConnectWallet()
+  const provider = useMemo(() => {
+    if (wallet) {
+      return new Web3Provider(wallet.provider, 'any')
+    }
+    return readProvider
+  }, [readProvider, wallet])
   return provider
 }

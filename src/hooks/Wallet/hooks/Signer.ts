3bd45d7a251b318b393da07cd1379e10adf536be
Wraeth <wraeth.dao@gmail.com>
2022-08-26 08:43:01 +1000

    Use readProvider for all provider calls

--- src/hooks/Wallet/hooks/Signer.ts
+++ src/hooks/Wallet/hooks/Signer.ts
@@ -1,20 +1,18 @@
-import { JsonRpcBatchProvider, Web3Provider } from '@ethersproject/providers'
+import { Web3Provider } from '@ethersproject/providers'
+import { useConnectWallet } from '@web3-onboard/react'
 import { useMemo } from 'react'
 
-import { useProvider } from './Provider'
-
 export function useSigner() {
-  const provider = useProvider()
-  const signer = useMemo(() => {
-    if (provider instanceof JsonRpcBatchProvider) {
-      return undefined
-    }
-    if (provider instanceof Web3Provider) {
-      return provider?.getSigner()
-    }
+  const [{ wallet }] = useConnectWallet()
+  const signerProvider = useMemo(() => {
+    if (!wallet) return undefined
+    return new Web3Provider(wallet.provider, 'any')
+  }, [wallet])
 
-    console.error('FATAL: unexpected lack of provider found')
-    throw new Error('FATAL: unexpected lack of provider found')
-  }, [provider])
+  const signer = useMemo(() => {
+    if (!signerProvider) return undefined
+    return signerProvider.getSigner()
+  }, [signerProvider])
+
   return signer
 }

2aab550a899a59171c3b22a800d01bf722c44499
Wraeth <wraeth.dao@gmail.com>
2022-08-26 09:35:04 +1000

    Disable signer if chain is unsupported

--- src/hooks/Wallet/hooks/Signer.ts
+++ src/hooks/Wallet/hooks/Signer.ts
@@ -1,18 +1,22 @@
 import { Web3Provider } from '@ethersproject/providers'
 import { useConnectWallet } from '@web3-onboard/react'
 import { useMemo } from 'react'
+import { useChainUnsupported } from './ChainUnsupported'
 
 export function useSigner() {
   const [{ wallet }] = useConnectWallet()
+  const chainUnsupported = useChainUnsupported()
   const signerProvider = useMemo(() => {
     if (!wallet) return undefined
     return new Web3Provider(wallet.provider, 'any')
   }, [wallet])
 
   const signer = useMemo(() => {
-    if (!signerProvider) return undefined
+    // If the provider is not available or the chain is unsupported, we
+    // shouldn't attempt to do anything
+    if (!signerProvider || chainUnsupported) return undefined
     return signerProvider.getSigner()
-  }, [signerProvider])
+  }, [chainUnsupported, signerProvider])
 
   return signer
 }

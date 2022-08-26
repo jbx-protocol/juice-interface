f16a191a0c55e357189a5b2d588a1fbc77089c2d
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-26 11:13:58 +0930

    [4/4] Reserved token CSV export for v1 (#1823)
    * Add export reserve token allocation to v1
    
    * [5/4] Include owner split in V2 split export (#1831)
    
    * Include owner split in V2 split export
    
    * Add owner remainder mod to v1 csv export (#1832)

--- src/components/v1/V1Project/V1ProjectToolsDrawer/ExportTicketModsButton.tsx
+++ src/components/v1/V1Project/V1ProjectToolsDrawer/ExportTicketModsButton.tsx
@@ -2,58 +2,54 @@ import { DownloadOutlined } from '@ant-design/icons'
 import { t, Trans } from '@lingui/macro'
 import { Button } from 'antd'
 import { V1ProjectContext } from 'contexts/v1/projectContext'
-import { PayoutMod } from 'models/mods'
+import { TicketMod } from 'models/mods'
 import { useContext, useState } from 'react'
 import { downloadCsvFile } from 'utils/csv'
 import { permyriadToPercent } from 'utils/formatNumber'
 import { emitErrorNotification } from 'utils/notifications'
-import { getProjectOwnerRemainderPayoutMod } from 'utils/v1/mods'
+import { getProjectOwnerRemainderTicketMod } from 'utils/v1/mods'
 
-const CSV_HEADER: (keyof PayoutMod)[] = [
+const CSV_HEADER: (keyof TicketMod)[] = [
   'beneficiary',
   'percent',
   'preferUnstaked',
   'lockedUntil',
-  'projectId',
-  'allocator',
 ]
 
-const payoutModToCsvRow = (mod: PayoutMod) => {
+const ticketModToCsvRow = (mod: TicketMod) => {
   return [
     mod.beneficiary,
     `${parseFloat(permyriadToPercent(`${mod.percent}`)) / 100}`,
     `${mod.preferUnstaked}`,
     `${mod.lockedUntil}`,
-    mod.projectId?.toString(),
-    mod.allocator,
   ]
 }
 
-const preparePayoutModsCsv = (
-  mods: PayoutMod[],
+const prepareTicketModsCsv = (
+  mods: TicketMod[],
   projectOwnerAddress: string,
 ): (string | undefined)[][] => {
-  const csvContent = mods.map(payoutModToCsvRow)
+  const csvContent = mods.map(ticketModToCsvRow)
   const rows = [CSV_HEADER, ...csvContent]
 
-  const projectOwnerMod = getProjectOwnerRemainderPayoutMod(
+  const projectOwnerMod = getProjectOwnerRemainderTicketMod(
     projectOwnerAddress,
     mods,
   )
   if (projectOwnerMod.percent > 0) {
-    rows.push(payoutModToCsvRow(projectOwnerMod))
+    rows.push(ticketModToCsvRow(projectOwnerMod))
   }
 
   return rows
 }
 
-export function ExportPayoutModsButton() {
-  const { handle, currentFC, currentPayoutMods, owner } =
+export function ExportTicketModsButton() {
+  const { handle, currentFC, currentTicketMods, owner } =
     useContext(V1ProjectContext)
   const [loading, setLoading] = useState<boolean>(false)
 
   const onExportSplitsButtonClick = () => {
-    if (!currentFC || !currentPayoutMods || !owner) {
+    if (!currentFC || !currentTicketMods || !owner) {
       emitErrorNotification(
         t`CSV data wasn't ready for export. Wait a few seconds and try again.`,
       )
@@ -63,8 +59,8 @@ export function ExportPayoutModsButton() {
     setLoading(true)
 
     try {
-      const csvContent = preparePayoutModsCsv(currentPayoutMods, owner)
-      const filename = `@${handle}_payouts_fc-${currentFC.number}`
+      const csvContent = prepareTicketModsCsv(currentTicketMods, owner)
+      const filename = `@${handle}_reserved-tokens_fc-${currentFC.number}`
 
       downloadCsvFile(filename, csvContent)
     } catch (e) {
@@ -83,7 +79,7 @@ export function ExportPayoutModsButton() {
       loading={loading}
     >
       <span>
-        <Trans>Export payouts CSV</Trans>
+        <Trans>Export token allocation CSV</Trans>
       </span>
     </Button>
   )

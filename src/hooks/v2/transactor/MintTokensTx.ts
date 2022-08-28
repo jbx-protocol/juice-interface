fe46f35e52bf421132786a5896f74c0a09ecc3e8
NakedFool <medenicablazo@gmail.com>
2022-08-28 16:01:09 +0200

    small mistake fixed

--- src/hooks/v2/transactor/MintTokensTx.ts
+++ src/hooks/v2/transactor/MintTokensTx.ts
@@ -24,7 +24,7 @@ export function useMintTokensTx(): TransactorInstance<{
         ? 'transactor'
         : !contracts
         ? 'contracts'
-        : projectId
+        : !projectId
         ? 'projectId'
         : null
 

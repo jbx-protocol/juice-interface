c92293c3d2ff0a243720fc1e5279d77331c4289c
Wraeth <wraeth.dao@gmail.com>
2022-08-30 13:37:37 +1000

    work to converge contract readers

---
+++ src/hooks/ContractReader/ContractReader.ts
@@ -0,0 +1,68 @@
+import { Contract } from '@ethersproject/contracts'
+import { useCallback } from 'react'
+import { useDeepCompareEffectNoCheck } from 'use-deep-compare-effect'
+import { useContractReadValue } from './ContractReadValue'
+import { ContractConfig, ContractUpdateOn } from './types'
+import { convertUpdateOnArrayToSubscriptions } from './util'
+
+export interface ContractReaderProps<ContractName extends string, V> {
+  contract: ContractConfig<ContractName> | undefined
+  contracts: Record<ContractName, Contract> | undefined
+  functionName: string | undefined
+  args: unknown[] | null | undefined
+  updateOn?: ContractUpdateOn<ContractName>[]
+  formatter?: (val?: any) => V | undefined // eslint-disable-line @typescript-eslint/no-explicit-any
+  callback?: (val?: V) => void
+  valueDidChange?: (oldVal?: V, newVal?: V) => boolean
+}
+
+export function useContractReader<ContractName extends string, V>({
+  contract,
+  contracts,
+  functionName,
+  args,
+  updateOn,
+  formatter,
+  callback,
+  valueDidChange,
+}: ContractReaderProps<ContractName, V>) {
+  const _callback = useCallback(
+    (val: any) => (callback ? callback(val) : val), // eslint-disable-line @typescript-eslint/no-explicit-any
+    [callback],
+  )
+
+  const { value, loading, refetchValue } = useContractReadValue({
+    contract,
+    contracts,
+    functionName,
+    args,
+    formatter,
+    valueDidChange,
+  })
+
+  // Call the callback on contract read value changed
+  useDeepCompareEffectNoCheck(() => {
+    _callback(value)
+  }, [value])
+
+  useDeepCompareEffectNoCheck(() => {
+    const listener = () => refetchValue()
+
+    try {
+      const subscriptions = convertUpdateOnArrayToSubscriptions({
+        updateOn,
+        contracts,
+        listener,
+      })
+      return () =>
+        subscriptions.forEach(s => s.contract.off(s.filter, listener))
+    } catch (error) {
+      console.error('Read contract >', {
+        functionName,
+        error,
+      })
+    }
+  }, [contract, contracts, functionName, updateOn, args, _callback])
+
+  return { data: value, loading }
+}
--- src/hooks/v1/contractReader/ContractReader.ts
+++ src/hooks/v1/contractReader/ContractReader.ts
@@ -1,90 +1,15 @@
-import { Contract, EventFilter } from '@ethersproject/contracts'
 import { V1UserContext } from 'contexts/v1/userContext'
-import { useContractReadValue } from 'hooks/ContractReadValue'
+import {
+  ContractReaderProps,
+  useContractReader as useNewContractReader,
+} from 'hooks/ContractReader'
 import { V1ContractName } from 'models/v1/contracts'
-import { useCallback, useContext } from 'react'
-import { useDeepCompareEffectNoCheck } from 'use-deep-compare-effect'
-import { getContract } from 'utils/getContract'
+import { useContext } from 'react'
 
-type ContractUpdateOn = {
-  contract?: ContractConfig
-  eventName?: string
-  topics?: EventFilter['topics']
-}[]
-
-type ContractConfig = V1ContractName | Contract | undefined
-
-export default function useContractReader<V>({
-  contract,
-  functionName,
-  args,
-  updateOn,
-  formatter,
-  callback,
-  valueDidChange,
-}: {
-  contract?: ContractConfig
-  functionName?: string
-  args?: unknown[] | null
-  updateOn?: ContractUpdateOn
-  formatter?: (val?: any) => V | undefined // eslint-disable-line @typescript-eslint/no-explicit-any
-  callback?: (val?: V) => void
-  valueDidChange?: (oldVal?: V, newVal?: V) => boolean
-}): V | undefined {
+export default function useContractReader<V>(
+  props: Omit<ContractReaderProps<V1ContractName, V>, 'contracts'>,
+): V | undefined {
   const { contracts } = useContext(V1UserContext)
-
-  const _callback = useCallback(
-    (val: any) => (callback ? callback(val) : val), // eslint-disable-line @typescript-eslint/no-explicit-any
-    [callback],
-  )
-
-  const { value, refetchValue } = useContractReadValue({
-    contract,
-    contracts,
-    functionName,
-    args,
-    formatter,
-    valueDidChange,
-  })
-
-  // Call the callback on contract read value changed
-  useDeepCompareEffectNoCheck(() => {
-    _callback(value)
-  }, [value])
-
-  useDeepCompareEffectNoCheck(() => {
-    const listener = () => refetchValue()
-
-    const subscriptions: {
-      contract: Contract
-      filter: EventFilter
-    }[] = []
-
-    if (updateOn?.length) {
-      try {
-        // Subscribe listener to updateOn events
-        updateOn.forEach(u => {
-          const _contract = getContract(u.contract, contracts)
-
-          if (!u.eventName || !_contract) return
-
-          const filter = _contract.filters[u.eventName](...(u.topics ?? []))
-          _contract?.on(filter, listener)
-          subscriptions.push({
-            contract: _contract,
-            filter,
-          })
-        })
-      } catch (error) {
-        console.info('[V1] Read contract >', {
-          functionName,
-          error,
-        })
-      }
-    }
-
-    return () => subscriptions.forEach(s => s.contract.off(s.filter, listener))
-  }, [contract, contracts, functionName, updateOn, args, _callback])
-
+  const { data: value } = useNewContractReader({ ...props, contracts })
   return value
 }

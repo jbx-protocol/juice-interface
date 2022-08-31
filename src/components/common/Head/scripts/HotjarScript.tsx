ced066aff34b720c64a12365be5f04f32a6341ad
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-08-31 14:18:16 +0930

    Break scripts up into own components

---
+++ src/components/common/Head/scripts/HotjarScript.tsx
@@ -0,0 +1,19 @@
+export function HotjarScript() {
+  return (
+    <script
+      defer
+      dangerouslySetInnerHTML={{
+        __html: `
+(function(h,o,t,j,a,r){
+h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+h._hjSettings={hjid:3077427,hjsv:6};
+a=o.getElementsByTagName('head')[0];
+r=o.createElement('script');r.async=1;
+r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+`,
+      }}
+    ></script>
+  )
+}

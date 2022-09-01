e94d5f3ae67dc7e1e7aa353ce33d7bf89dfd9fa2
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-01 10:50:15 +0930

    Add reserve tokens

--- src/components/v2/V2Project/V2FundingCycleSection/modals/EditTokenAllocationModal.tsx
+++ src/components/v2/V2Project/V2FundingCycleSection/modals/EditTokenAllocationModal.tsx
@@ -1,16 +1,14 @@
-import { t, Trans } from '@lingui/macro'
-import { Form, Modal } from 'antd'
-import Callout from 'components/Callout'
-import { FormItems } from 'components/formItems'
+import { t } from '@lingui/macro'
+import { Modal } from 'antd'
 import { V2ProjectContext } from 'contexts/v2/projectContext'
 import { useSetProjectSplits } from 'hooks/v2/transactor/SetProjectSplits'
 import { Split } from 'models/v2/splits'
 import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { emitErrorNotification } from 'utils/notifications'
-import { formatReservedRate, preciseFormatSplitPercent } from 'utils/v2/math'
-import { toMod, toSplit } from 'utils/v2/splits'
+import { preciseFormatSplitPercent } from 'utils/v2/math'
 
 import { RESERVED_TOKEN_SPLIT_GROUP } from 'constants/v2/splits'
+import { V2EditReservedTokens } from '../../V2EditReservedTokens'
 
 export const EditTokenAllocationModal = ({
   visible,
@@ -21,9 +19,7 @@ export const EditTokenAllocationModal = ({
   onOk: VoidFunction
   onCancel: VoidFunction
 }) => {
-  const { reservedTokensSplits, fundingCycleMetadata, fundingCycle } =
-    useContext(V2ProjectContext)
-  const reservedRate = fundingCycleMetadata?.reservedRate
+  const { reservedTokensSplits, fundingCycle } = useContext(V2ProjectContext)
 
   const setProjectSplits = useSetProjectSplits({
     domain: fundingCycle?.configuration?.toString(),
@@ -45,6 +41,7 @@ export const EditTokenAllocationModal = ({
         .reduce((a, b) => a + b, 0) ?? 0,
     [editingReservedTokensSplits],
   )
+
   const totalPercentagesInvalid = totalPercentage > 100
 
   const onSaveTokenAllocation = useCallback(async () => {
@@ -88,30 +85,10 @@ export const EditTokenAllocationModal = ({
       onCancel={onCancel}
       okButtonProps={{ disabled: totalPercentagesInvalid }}
     >
-      <Callout style={{ marginBottom: '1rem' }}>
-        <Trans>
-          Changes to your reserved token allocation will take effect
-          immediately.
-        </Trans>
-      </Callout>
-      <Form layout="vertical">
-        <FormItems.ProjectTicketMods
-          mods={editingReservedTokensSplits.map(toMod)}
-          onModsChanged={mods =>
-            setEditingReservedTokensSplits(mods.map(toSplit))
-          }
-          formItemProps={{
-            label: <Trans>Reserved token allocation</Trans>,
-            extra: (
-              <Trans>
-                Allocate a portion of your project's reserved tokens to other
-                Ethereum wallets or Juicebox projects.
-              </Trans>
-            ),
-          }}
-          reservedRate={parseInt(formatReservedRate(reservedRate))}
-        />
-      </Form>
+      <V2EditReservedTokens
+        editingReservedTokensSplits={editingReservedTokensSplits}
+        setEditingReservedTokensSplits={setEditingReservedTokensSplits}
+      />
     </Modal>
   )
 }

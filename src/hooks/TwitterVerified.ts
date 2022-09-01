7b87a97ebdf53cb9d861bcb384b88e24dfb93f5d
Jonathan Miller <jon@jmill.dev>
2022-09-01 06:56:10 -0400

    Fetch verification info from s3 file

--- src/hooks/TwitterVerified.ts
+++ src/hooks/TwitterVerified.ts
@@ -1,5 +1,23 @@
-const useTwitterVerified = () => {
-  return true
+import axios from 'axios'
+import { useQuery } from 'react-query'
+
+export interface TwitterVerificationInfo {
+  verified: boolean
+  handle: string
+  verifiedAt: number
+}
+
+const useTwitterVerified = (projectId: string | undefined) => {
+  const verificationFileUri = process.env
+    .NEXT_PUBLIC_TWITTER_VERIFICATION_FILE_URI as string
+  return useQuery('twitter-verified', async () => {
+    if (!projectId) {
+      return
+    }
+    const { data } = await axios.get(verificationFileUri)
+    const verificationInfo = data[projectId]?.twitter as TwitterVerificationInfo
+    return verificationInfo
+  })
 }
 
 export default useTwitterVerified

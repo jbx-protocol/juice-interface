8d3b718c619a8d57be94e6774578f2b53800f806
Jonathan Miller <jon@jmill.dev>
2022-09-02 23:54:18 -0400

    Add verification link

--- src/components/Project/ProjectHeader/TwitterVerified.tsx
+++ src/components/Project/ProjectHeader/TwitterVerified.tsx
@@ -4,25 +4,53 @@ import {
 } from '@ant-design/icons'
 import { t, Trans } from '@lingui/macro'
 import { Tooltip } from 'antd'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
 
 import useTwitterVerified, {
   TwitterVerificationInfo,
 } from 'hooks/TwitterVerified'
+import { useWallet } from 'hooks/Wallet'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { settingsPagePath } from 'utils/routes'
 
 const TwitterVerified = () => {
-  const [value] = useTwitterVerified('2-4356')
+  const { projectId, handle, projectOwnerAddress } =
+    useContext(V2ProjectContext)
+  const { userAddress } = useWallet()
+  const [value] = useTwitterVerified('2-4653')
   const info = value as TwitterVerificationInfo
-  if (!info) {
-    return null
+  const canVerify =
+    projectOwnerAddress?.toLowerCase() === userAddress?.toLowerCase()
+
+  const renderUnverifiedMessage = () => {
+    return (
+      <>
+        <Trans>Twitter handle unverified.</Trans>
+        {canVerify && (
+          <>
+            {' '}
+            <Trans>
+              <Link
+                href={settingsPagePath('verifytwitter', { projectId, handle })}
+              >
+                Verify here
+              </Link>
+              .
+            </Trans>
+          </>
+        )}
+      </>
+    )
   }
   return (
     <div>
-      {info.verified ? (
+      {info && info.verified ? (
         <Tooltip title={t`Twitter Verified`} placement="right">
           <CheckCircleOutlined />
         </Tooltip>
       ) : (
-        <Tooltip title={<Trans>Twitter unverified.</Trans>} placement="right">
+        <Tooltip title={renderUnverifiedMessage} placement="right">
           <ExclamationCircleOutlined />
         </Tooltip>
       )}

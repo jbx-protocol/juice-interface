c7a817e4d203ee4315161e267f59637fba690fb1
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-09-05 10:35:01 +1000

    The start of create (#1907)

---
+++ src/pages/experimental/create/index.page.tsx
@@ -0,0 +1,10 @@
+import { AppWrapper } from 'components/common'
+import { ThisIsCreate } from 'components/Create'
+
+export default function CreatePage() {
+  return (
+    <AppWrapper>
+      <ThisIsCreate />
+    </AppWrapper>
+  )
+}

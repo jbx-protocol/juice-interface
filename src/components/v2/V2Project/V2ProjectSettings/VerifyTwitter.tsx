e42d37b9164d11023cf81d7e1acaf519e16da212
Jonathan Miller <jon@jmill.dev>
2022-09-11 10:46:33 -0400

    Add authentication initiated request route

--- src/components/v2/V2Project/V2ProjectSettings/VerifyTwitter.tsx
+++ src/components/v2/V2Project/V2ProjectSettings/VerifyTwitter.tsx
@@ -1,15 +1,35 @@
 import { Trans } from '@lingui/macro'
 import { Button } from 'antd'
+import axios from 'axios'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
 import { useTwitterAuth } from 'hooks/socialAuth'
+import { useContext, useState } from 'react'
 
 const VerifyTwitter = () => {
+  const { projectId, projectMetadata } = useContext(V2ProjectContext)
   const { onOpen } = useTwitterAuth()
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const initiateAuthRequest = async () => {
+    if (!projectId || !projectMetadata?.twitter) return
+    setIsAuthenticating(true)
+    await axios.post(`/api/auth/twitter`, {
+      projectId,
+      twitterUsername: projectMetadata.twitter,
+    })
+    onOpen()
+  }
+
   return (
     <div>
       <p>
         <Trans>Verify your project owns the linked Twitter account.</Trans>
       </p>
-      <Button type="primary" onClick={onOpen}>
+      <Button
+        type="primary"
+        onClick={initiateAuthRequest}
+        loading={isAuthenticating}
+      >
         <Trans>Verify Twitter</Trans>
       </Button>
     </div>

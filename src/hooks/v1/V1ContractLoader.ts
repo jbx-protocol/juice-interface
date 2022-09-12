e939235f9ecb08632b972f7a44db7d7925a8f138
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-12 17:01:48 +0930

    Only load Prices contract in useEtherPrice

--- src/hooks/v1/V1ContractLoader.ts
+++ src/hooks/v1/V1ContractLoader.ts
@@ -1,13 +1,22 @@
 import { Contract } from '@ethersproject/contracts'
+import { readNetwork } from 'constants/networks'
+import { readProvider } from 'constants/readProvider'
 import { useWallet } from 'hooks/Wallet'
-import { SignerOrProvider } from 'utils/types'
-
 import { NetworkName } from 'models/network-name'
 import { V1ContractName, V1Contracts } from 'models/v1/contracts'
 import { useEffect, useState } from 'react'
+import { SignerOrProvider } from 'utils/types'
 
-import { readNetwork } from 'constants/networks'
-import { readProvider } from 'constants/readProvider'
+export const loadV1Contract = async (
+  contractName: string,
+  network: NetworkName,
+  signerOrProvider: SignerOrProvider,
+): Promise<Contract> => {
+  const contract = await import(
+    `@jbx-protocol/contracts-v1/deployments/${network}/${contractName}.json`
+  )
+  return new Contract(contract.address, contract.abi, signerOrProvider)
+}
 
 export function useV1ContractLoader() {
   const [contracts, setContracts] = useState<V1Contracts>()
@@ -23,7 +32,7 @@ export function useV1ContractLoader() {
 
         const loadContractKeyPair = async (contractName: V1ContractName) => ({
           key: contractName,
-          val: await loadContract(contractName, network, signerOrProvider),
+          val: await loadV1Contract(contractName, network, signerOrProvider),
         })
 
         const newContracts = (
@@ -48,15 +57,4 @@ export function useV1ContractLoader() {
   }, [setContracts, signer])
 
   return contracts
-}
-
-const loadContract = async (
-  contractName: keyof typeof V1ContractName,
-  network: NetworkName,
-  signerOrProvider: SignerOrProvider,
-): Promise<Contract> => {
-  const contract = await import(
-    `@jbx-protocol/contracts-v1/deployments/${network}/${contractName}.json`
-  )
-  return new Contract(contract.address, contract.abi, signerOrProvider)
 }

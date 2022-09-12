f53d93610eb151412201ef530dbe738b5260f1c1
Benjamin Schachter <benschac@gmail.com>
2022-09-12 18:06:45 -0400

    configure typescript next image

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -2,4 +2,7 @@ declare module '*.png'
 declare module '*.svg'
 declare module '*.jpg'
 declare module '*.webp'
+<<<<<<< HEAD
 declare module '*.gif'
+=======
+>>>>>>> 804d25190 (configure typescript next image)

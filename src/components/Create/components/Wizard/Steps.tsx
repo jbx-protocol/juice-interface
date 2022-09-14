0446159d394d97bc2830f0eb207a68e2bd5ad3f7
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-09-14 16:43:11 +1000

    Add code for a wizard step flow 1/c (#1970)
    ## What does this PR do and why?
    
    This PR contains the work needed to stand up the wizard
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] I have tested this PR in dark mode and light mode (if applicable).

---
+++ src/components/Create/components/Wizard/Steps.tsx
@@ -0,0 +1,3 @@
+export const Steps = () => {
+  return <h3>Placeholder for Steps</h3>
+}
---
+++ src/components/Create/components/Wizard/hooks/Wizard.ts
@@ -0,0 +1,49 @@
+import { ReactNode, useCallback, useEffect, useMemo, useState } from 'react'
+import { PageProps } from '../Page'
+
+const isPage = (element: ReactNode): element is PageProps => {
+  return (element as PageProps)?.name !== undefined
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const useWizard = ({ children }: { children?: any[] }) => {
+  const [currentPage, setCurrentPage] = useState<string>('')
+
+  const pages: PageProps[] = useMemo(() => {
+    if (!children) return []
+    return children
+      .map(child => {
+        if (!child.props || (child.props && !isPage(child.props))) {
+          console.warn('Invalid child in Wizard', { child })
+        }
+        return {
+          name: child.props.name,
+          title: child.props.title,
+          description: child.props.description,
+        }
+      })
+      .filter(p => !!p)
+  }, [children])
+
+  const goToPage = useCallback(
+    (page: string) => {
+      if (pages.find(p => p.name === page)) {
+        setCurrentPage(page)
+        return
+      }
+      console.error('Invalid page called to goToPage', {
+        page,
+        pages: pages.map(p => p.name),
+      })
+    },
+    [pages],
+  )
+
+  useEffect(() => {
+    setCurrentPage(pages[0]?.name ?? '')
+    // We only want to run useEffect once on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return { currentPage, setCurrentPage, pages, goToPage }
+}

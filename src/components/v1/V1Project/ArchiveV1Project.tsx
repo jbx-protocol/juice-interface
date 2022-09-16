45a0ab096f9347b70c3698e954b2e64ee6649b9b
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-16 12:23:51 +0930

    Revert a thing

--- src/components/v1/V1Project/ArchiveV1Project.tsx
+++ src/components/v1/V1Project/ArchiveV1Project.tsx
@@ -3,7 +3,7 @@ import { TransactorInstance } from 'hooks/Transactor'
 import { useContext } from 'react'
 
 import ArchiveProject from 'components/ArchiveProject'
-import { CV_V1, CV_V1_1 } from 'constants/cv'
+import { CV_V1 } from 'constants/cv'
 
 /**
  * V1 Wrapper around `ArchiveProject`.
@@ -18,7 +18,7 @@ export default function ArchiveV1Project({
   const { metadata, projectId, handle, terminal, owner } =
     useContext(V1ProjectContext)
 
-  const canTakePaymentsWhenArchived = !(terminal?.version === CV_V1_1)
+  const canTakePaymentsWhenArchived = !(terminal?.version === '1.1')
 
   return (
     <ArchiveProject

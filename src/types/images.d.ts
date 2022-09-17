35008de9d4d4d78e2e65e9fc911a138e5f27d680
Benjamin Schachter <benschac@gmail.com>
2022-09-17 11:26:34 -0400

    add gif

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -3,6 +3,10 @@ declare module '*.svg'
 declare module '*.jpg'
 declare module '*.webp'
 <<<<<<< HEAD
+<<<<<<< HEAD
 declare module '*.gif'
 =======
 >>>>>>> 804d25190 (configure typescript next image)
+=======
+declare module '*.gif'
+>>>>>>> d39a40f24 (add gif)

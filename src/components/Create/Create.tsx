edc1dda13683c2f9e3ad33ec10797cca8cd327f3
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-09-19 19:29:54 +1000

    Update provider wrappers for new create flow (#2012)

--- src/components/Create/Create.tsx
+++ src/components/Create/Create.tsx
@@ -1,6 +1,9 @@
 import { t, Trans } from '@lingui/macro'
 import ExternalLink from 'components/ExternalLink'
 import { useWallet } from 'hooks/Wallet'
+import { TransactionProvider } from 'providers/TransactionProvider'
+import { V2ContractsProvider } from 'providers/v2/V2ContractsProvider'
+import { V2CurrencyProvider } from 'providers/v2/V2CurrencyProvider'
 import { helpPagePath } from 'utils/routes'
 import { FundingCyclesPage, ProjectDetailsPage } from './components'
 import { Wizard } from './components/Wizard'
@@ -8,40 +11,48 @@ import { Wizard } from './components/Wizard'
 export function Create() {
   const { chain } = useWallet()
   return (
-    <Wizard
-      doneText={
-        // TODO: Handle wallet connect event and text changes
-        chain?.name ? t`Deploy project to ${chain?.name}` : t`Deploy project`
-      }
-    >
-      <Wizard.Page
-        name="projectDetails"
-        title={t`Project Details`}
-        description={t`Enter your project’s details. You can edit your project's details at any time after you deploy project your project, but the transaction will cost gas.`}
-      >
-        <ProjectDetailsPage />
-      </Wizard.Page>
-      <Wizard.Page
-        name="fundingCycles"
-        title={t`Funding Cycles`}
-        description={
-          <Trans>
-            Juicebox projects are funded in cycles. A Funding Cycle is a set
-            period of time in which your project settings are locked.{' '}
-            <ExternalLink
-              href={helpPagePath('/dev/learn/glossary/funding-cycle')}
+    <V2ContractsProvider>
+      <TransactionProvider>
+        <V2CurrencyProvider>
+          <Wizard
+            doneText={
+              // TODO: Handle wallet connect event and text changes
+              chain?.name
+                ? t`Deploy project to ${chain?.name}`
+                : t`Deploy project`
+            }
+          >
+            <Wizard.Page
+              name="projectDetails"
+              title={t`Project Details`}
+              description={t`Enter your project’s details. You can edit your project's details at any time after you deploy project your project, but the transaction will cost gas.`}
             >
-              Learn more.
-            </ExternalLink>
-          </Trans>
-        }
-      >
-        <FundingCyclesPage />
-      </Wizard.Page>
-      <Wizard.Page name="nextStep" title={t`Empty Step`}>
-        <div>TODO</div>
-        <Wizard.Page.ButtonControl />
-      </Wizard.Page>
-    </Wizard>
+              <ProjectDetailsPage />
+            </Wizard.Page>
+            <Wizard.Page
+              name="fundingCycles"
+              title={t`Funding Cycles`}
+              description={
+                <Trans>
+                  Juicebox projects are funded in cycles. A Funding Cycle is a
+                  set period of time in which your project settings are locked.{' '}
+                  <ExternalLink
+                    href={helpPagePath('/dev/learn/glossary/funding-cycle')}
+                  >
+                    Learn more.
+                  </ExternalLink>
+                </Trans>
+              }
+            >
+              <FundingCyclesPage />
+            </Wizard.Page>
+            <Wizard.Page name="nextStep" title={t`Empty Step`}>
+              <div>TODO</div>
+              <Wizard.Page.ButtonControl />
+            </Wizard.Page>
+          </Wizard>
+        </V2CurrencyProvider>
+      </TransactionProvider>
+    </V2ContractsProvider>
   )
 }

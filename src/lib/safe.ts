763ee6f92f7f36ae8ad9781fc98788a3df325722
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-20 14:43:45 +0930

    Safe part 1 (#1965)
    * Fetch queued tx
    
    * Pot
    
    * yeah
    
    * style project safe page
    
    * style safe tabs
    
    * update safe page providers
    
    * rm package-lock
    
    * Styling, components, and more
    
    Co-authored-by: Johnny D <johnnyd.eth@protonmail.com>

---
+++ src/lib/safe.ts
@@ -0,0 +1,9 @@
+import { SafeTransactionType } from 'components/v2/V2Project/V2ProjectSafeDashboard/V2ProjectSafeDashboard'
+
+// Links to a Gnosis safe multisig
+export const generateSafeUrl = (address: string) =>
+  `https://gnosis-safe.io/app/eth:${address}/transactions/queue`
+
+// Links to specific transaction on Gnosis Safe app
+export const generateSafeTxUrl = (transaction: SafeTransactionType) =>
+  `https://gnosis-safe.io/app/eth:${transaction.safe}/transactions/multisig_${transaction.safe}_${transaction.safeTxHash}`

488e65e7cd58c841ab45fe787ab3f32140e1d808
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-20 14:43:45 +0930

    Safe part 1 (#1965)
    * Fetch queued tx
    
    * Pot
    
    * yeah
    
    * style project safe page
    
    * style safe tabs
    
    * update safe page providers
    
    * rm package-lock
    
    * Styling, components, and more
    
    Co-authored-by: Johnny D <johnnyd.eth@protonmail.com>

--- src/pages/projects/ProjectsTabs.tsx
+++ src/pages/projects/ProjectsTabs.tsx
@@ -1,9 +1,8 @@
 import { t } from '@lingui/macro'
 import { Space } from 'antd'
-import { ThemeContext } from 'contexts/themeContext'
+import { Tab } from 'components/Tab'
 import { ProjectCategory } from 'models/project-visibility'
 import Link from 'next/link'
-import { useContext } from 'react'
 
 const TAB_TYPE_NAMES: { [k in ProjectCategory]: string } = {
   all: t`All`,
@@ -12,50 +11,38 @@ const TAB_TYPE_NAMES: { [k in ProjectCategory]: string } = {
   trending: t`Trending`,
 }
 
+const TABS: ProjectCategory[] = ['all', 'holdings', 'myprojects', 'trending']
+
+const ProjectTab = ({
+  type,
+  isSelected,
+}: {
+  type: ProjectCategory
+  isSelected: boolean
+}) => {
+  return (
+    <Link href={`/projects?tab=${type}`}>
+      <a>
+        <Tab name={TAB_TYPE_NAMES[type]} isSelected={isSelected} />
+      </a>
+    </Link>
+  )
+}
+
 export default function ProjectsTabs({
   selectedTab,
 }: {
   selectedTab: ProjectCategory
 }) {
-  const {
-    theme: { colors },
-  } = useContext(ThemeContext)
-
-  const Tab = ({ type }: { type: ProjectCategory }) => {
-    return (
-      <Link href={`/projects?tab=${type}`}>
-        <a
-          style={{
-            textTransform: 'uppercase',
-            cursor: 'pointer',
-            borderBottom: '2px solid transparent',
-            paddingBottom: 6,
-            fontWeight: 500,
-            ...(type === selectedTab
-              ? {
-                  color: colors.text.primary,
-                  borderColor: colors.text.primary,
-                }
-              : {
-                  color: colors.text.secondary,
-                  borderColor: 'transparent',
-                }),
-          }}
-        >
-          {TAB_TYPE_NAMES[type]}
-        </a>
-      </Link>
-    )
-  }
-
   return (
-    <div style={{ height: 40, marginTop: 15 }}>
-      <Space direction="horizontal" size="large">
-        <Tab type="trending" />
-        <Tab type="holdings" />
-        <Tab type="myprojects" />
-        <Tab type="all" />
-      </Space>
-    </div>
+    <Space
+      direction="horizontal"
+      size="large"
+      style={{ height: 40, marginTop: 15 }}
+    >
+      {TABS.map(type => (
+        <ProjectTab type={type} key={type} isSelected={type === selectedTab} />
+      ))}
+    </Space>
   )
 }

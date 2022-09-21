259c4aa769b328f989917ddb9dba36f40c63f96e
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-21 11:58:22 +0930

    Move project ID, better archived badge (#2030)

---
+++ src/components/Project/ProjectHeader/EditProjectHandleButton.tsx
@@ -0,0 +1,39 @@
+import { EditOutlined } from '@ant-design/icons'
+import { t, Trans } from '@lingui/macro'
+import { Button, Tooltip } from 'antd'
+import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
+import { V2ProjectContext } from 'contexts/v2/projectContext'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { settingsPagePath } from 'utils/routes'
+
+export function EditProjectHandleButton() {
+  const { projectId } = useContext(ProjectMetadataContext)
+  const { handle } = useContext(V2ProjectContext)
+
+  return (
+    <Tooltip
+      placement="bottom"
+      title={t`A project's handle is used in its URL, and allows it to be included in search results on the projects page.`}
+    >
+      <div>
+        <Link
+          href={settingsPagePath('projecthandle', {
+            projectId,
+            handle,
+          })}
+        >
+          <Button
+            type="link"
+            icon={<EditOutlined />}
+            style={{ paddingLeft: 0 }}
+          >
+            <span>
+              <Trans>Add handle</Trans>
+            </span>
+          </Button>
+        </Link>
+      </div>
+    </Tooltip>
+  )
+}

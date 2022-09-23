2955f6147e94a92ff8d4d166e2efd02fd7e60186
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-23 13:07:26 +0930

    Determine if project is v3

--- src/pages/v2/p/[projectId]/utils/props.ts
+++ src/pages/v2/p/[projectId]/utils/props.ts
@@ -1,4 +1,4 @@
-import { CV_V3 } from 'constants/cv'
+import { CV_V2, CV_V3 } from 'constants/cv'
 import { JUICEBOX_MONEY_PROJECT_METADATA_DOMAIN } from 'constants/metadataDomain'
 import { readNetwork } from 'constants/networks'
 import { readProvider } from 'constants/readProvider'
@@ -9,27 +9,72 @@ import { GetServerSidePropsResult } from 'next'
 import { findProjectMetadata } from 'utils/server'
 import { loadV2V3Contract } from 'utils/v2v3/loadV2V3Contract'
 
-const DEFAULT_CV: V3CVType | V2CVType = CV_V3
-
 export interface ProjectPageProps {
   metadata: ProjectMetadataV5
   projectId: number
   cv: V3CVType | V2CVType
 }
 
-async function getMetadataCidFromContract(projectId: number) {
+async function loadJBProjects() {
   const network = readNetwork.name
   const contract = await loadV2V3Contract(
     V2V3ContractName.JBProjects,
     network,
     readProvider,
-    DEFAULT_CV, // TODO check this
+    CV_V3, // Note: v2 and v3 use the same JBProjects, so the CV doesn't matter.
   )
-  if (!contract) {
+
+  return contract
+}
+
+async function loadV3JBDirectory() {
+  const network = readNetwork.name
+  const contract = await loadV2V3Contract(
+    V2V3ContractName.JBDirectory,
+    network,
+    readProvider,
+    CV_V3,
+  )
+
+  return contract
+}
+
+async function loadV3JBController() {
+  const network = readNetwork.name
+  const contract = await loadV2V3Contract(
+    V2V3ContractName.JBController,
+    network,
+    readProvider,
+    CV_V3,
+  )
+
+  return contract
+}
+
+async function isV3Project(projectId: number) {
+  const [JBDirectory, JBController] = await Promise.all([
+    loadV3JBDirectory(),
+    loadV3JBController(),
+  ])
+  if (!JBDirectory) {
+    throw new Error(`contract not found ${V2V3ContractName.JBDirectory}`)
+  }
+  if (!JBController) {
+    throw new Error(`contract not found ${V2V3ContractName.JBController}`)
+  }
+
+  const projectControllerAddress = await JBDirectory.controllerOf(projectId)
+
+  return projectControllerAddress === JBController.address
+}
+
+async function getMetadataCidFromContract(projectId: number) {
+  const JBProjects = await loadJBProjects()
+  if (!JBProjects) {
     throw new Error(`contract not found ${V2V3ContractName.JBProjects}`)
   }
 
-  const metadataCid = (await contract.metadataContentOf(
+  const metadataCid = (await JBProjects.metadataContentOf(
     projectId,
     JUICEBOX_MONEY_PROJECT_METADATA_DOMAIN,
   )) as string
@@ -46,8 +91,15 @@ export async function getProjectProps(
 
   try {
     const metadataCid = await getMetadataCidFromContract(projectId)
-    const metadata = await findProjectMetadata({ metadataCid })
-    return { props: { metadata, projectId, cv: DEFAULT_CV } }
+
+    const [metadata, isV3ProjectResult] = await Promise.all([
+      findProjectMetadata({ metadataCid }),
+      isV3Project(projectId),
+    ])
+
+    const cv = isV3ProjectResult ? CV_V3 : CV_V2
+
+    return { props: { metadata, projectId, cv } }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {

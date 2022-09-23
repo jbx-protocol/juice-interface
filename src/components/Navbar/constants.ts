f07dcaf2540d2f79335c64608830c16f7358c8e4
Benjamin Schachter <benschac@gmail.com>
2022-09-23 15:24:55 -0400

    update consts for menu item to use new antd api for menu items

---
+++ src/components/Navbar/constants.ts
@@ -0,0 +1,55 @@
+import { t } from '@lingui/macro'
+import { MenuProps } from 'antd'
+
+export const resourcesMenuItems = (): MenuProps['items'] => {
+  return [
+    {
+      key: 'governance',
+      label: t`Governance`,
+      onClick: () => {
+        window?.open('https://vote.juicebox.money/#/jbdao.eth', '_blank')
+      },
+      className: 'nav-dropdown-item',
+      style: {
+        color: 'var(--text-primary)',
+        fontWeight: 500,
+      },
+    },
+    {
+      key: 'newsletter',
+      label: t`Newsletter`,
+      onClick: () => {
+        window?.open('https://newsletter.juicebox.money', '_blank')
+      },
+      className: 'nav-dropdown-item',
+      style: {
+        color: 'var(--text-primary)',
+        fontWeight: 500,
+      },
+    },
+    {
+      key: 'podcast',
+      label: t`Podcast`,
+      onClick: () => {
+        window?.open('https://podcast.juicebox.money', '_blank')
+      },
+      className: 'nav-dropdown-item',
+      style: {
+        color: 'var(--text-primary)',
+        fontWeight: 500,
+      },
+    },
+    {
+      key: 'peel',
+      label: t`PeelDAO`,
+      onClick: () => {
+        window?.open('https://discord.gg/XvmfY4Hkcz', '_blank')
+      },
+      className: 'nav-dropdown-item',
+      style: {
+        color: 'var(--text-primary)',
+        fontWeight: 500,
+      },
+    },
+  ]
+}

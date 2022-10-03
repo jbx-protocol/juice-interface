4311f936383af5fe2a440c8e3dd1a488b5751960
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-04 07:27:09 +1030

    Add V2V3ProjectPageProvider

--- src/pages/v2/p/[projectId]/settings/index.page.tsx
+++ src/pages/v2/p/[projectId]/settings/index.page.tsx
@@ -4,9 +4,7 @@ import { V2CVType, V3CVType } from 'models/cv'
 import { ProjectMetadataV5 } from 'models/project-metadata'
 import { GetServerSideProps } from 'next'
 import { TransactionProvider } from 'providers/TransactionProvider'
-import { V2V3ContractsProvider } from 'providers/v2v3/V2V3ContractsProvider'
-import V2V3ProjectMetadataProvider from 'providers/v2v3/V2V3ProjectMetadataProvider'
-import V2V3ProjectProvider from 'providers/v2v3/V2V3ProjectProvider'
+import { V2V3ProjectPageProvider } from 'providers/v2v3/V2V3ProjectPageProvider'
 import { getProjectProps, ProjectPageProps } from '../utils/props'
 
 export const getServerSideProps: GetServerSideProps<
@@ -29,18 +27,15 @@ export default function V2V3ProjectSettingsPage({
 }) {
   return (
     <AppWrapper>
-      <V2V3ContractsProvider initialCv={cv}>
+      <V2V3ProjectPageProvider
+        projectId={projectId}
+        metadata={metadata}
+        cv={cv}
+      >
         <TransactionProvider>
-          <V2V3ProjectMetadataProvider
-            projectId={projectId}
-            metadata={metadata}
-          >
-            <V2V3ProjectProvider projectId={projectId}>
-              <V2V3ProjectSettings />
-            </V2V3ProjectProvider>
-          </V2V3ProjectMetadataProvider>
+          <V2V3ProjectSettings />
         </TransactionProvider>
-      </V2V3ContractsProvider>
+      </V2V3ProjectPageProvider>
     </AppWrapper>
   )
 }

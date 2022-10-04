bf3d97ed2e818b5357bd3a7e0141b29ac23595c3
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-04 19:14:51 +1030

    Source JBETHPAymentTerminal from chain

--- src/hooks/v2v3/transactor/RedeemTokensTx.ts
+++ src/hooks/v2v3/transactor/RedeemTokensTx.ts
@@ -1,18 +1,16 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import * as constants from '@ethersproject/constants'
-import { useWallet } from 'hooks/Wallet'
-import { useContext } from 'react'
-
-import { V2V3ContractsContext } from 'contexts/v2v3/V2V3ContractsContext'
-import { V2V3ProjectContext } from 'contexts/v2v3/V2V3ProjectContext'
-
 import { t } from '@lingui/macro'
 import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
 import { TransactionContext } from 'contexts/transactionContext'
+import { V2V3ProjectContext } from 'contexts/v2v3/V2V3ProjectContext'
+import { V2V3ProjectContractsContext } from 'contexts/v2v3/V2V3ProjectContractsContext'
 import {
   handleTransactionException,
   TransactorInstance,
 } from 'hooks/Transactor'
+import { useWallet } from 'hooks/Wallet'
+import { useContext } from 'react'
 import invariant from 'tiny-invariant'
 import { tokenSymbolText } from 'utils/tokenSymbolText'
 
@@ -24,7 +22,7 @@ export function useRedeemTokensTx(): TransactorInstance<{
   memo: string
 }> {
   const { transactor } = useContext(TransactionContext)
-  const { contracts } = useContext(V2V3ContractsContext)
+  const { contracts } = useContext(V2V3ProjectContractsContext)
   const { tokenSymbol } = useContext(V2V3ProjectContext)
   const { projectId, cv } = useContext(ProjectMetadataContext)
 

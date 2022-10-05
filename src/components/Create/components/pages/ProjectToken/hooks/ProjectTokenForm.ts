b916fa8660b012e0ab9a2055d252e42e3fd8ef8b
Wraeth <wraeth.dao@gmail.com>
2022-10-05 13:33:16 +1100

    Add support for custom settings on Project Token Page

--- src/components/Create/components/pages/ProjectToken/hooks/ProjectTokenForm.ts
+++ src/components/Create/components/pages/ProjectToken/hooks/ProjectTokenForm.ts
@@ -1,25 +1,148 @@
 import { Form } from 'antd'
-import { useAppDispatch } from 'hooks/AppDispatch'
+import { useWatch } from 'antd/lib/form/Form'
+import { AllocationSplit } from 'components/Create/components/Allocation'
+import { ONE_MILLION } from 'constants/numbers'
+import { useAppSelector } from 'hooks/AppSelector'
 import { useDebugValue, useEffect, useMemo } from 'react'
+import { editingV2ProjectActions } from 'redux/slices/editingV2Project'
+import { formatIssuanceRate, issuanceRateFrom } from 'utils/v2v3/math'
+import { useFormDispatchWatch } from '../../hooks'
 
 export type ProjectTokensFormProps = Partial<{
   selection: 'default' | 'custom'
+  initialMintRate: string | undefined
+  reservedTokensPercentage: number | undefined
+  reservedTokenAllocation: AllocationSplit[] | undefined
+  discountRate: number | undefined
+  redemptionRate: number | undefined
+  tokenMinting: boolean | undefined
 }>
+
+const DefaultSettings: Required<Omit<ProjectTokensFormProps, 'selection'>> = {
+  initialMintRate: ONE_MILLION.toString(),
+  reservedTokensPercentage: 0,
+  reservedTokenAllocation: [],
+  discountRate: 0,
+  redemptionRate: 100,
+  tokenMinting: false,
+}
 
 export const useProjectTokensForm = () => {
   const [form] = Form.useForm<ProjectTokensFormProps>()
-
+  const {
+    fundingCycleMetadata,
+    fundingCycleData,
+    reservedTokensGroupedSplits,
+  } = useAppSelector(state => state.editingV2Project)
   useDebugValue(form.getFieldsValue())
 
   const initialValues: ProjectTokensFormProps | undefined = useMemo(() => {
-    return undefined
-  }, [])
+    const initialMintRate = fundingCycleData?.weight
+      ? formatIssuanceRate(fundingCycleData.weight)
+      : DefaultSettings.initialMintRate
+    const reservedTokensPercentage = fundingCycleMetadata.reservedRate
+      ? parseFloat(fundingCycleMetadata.reservedRate)
+      : DefaultSettings.reservedTokensPercentage
+    // TODO
+    const reservedTokenAllocation: AllocationSplit[] = []
+    // TODO: we should probably block this if no duration set
+    const discountRate = fundingCycleData.discountRate
+      ? parseFloat(fundingCycleData.discountRate)
+      : DefaultSettings.discountRate
+    const redemptionRate = fundingCycleMetadata.redemptionRate
+      ? parseFloat(fundingCycleMetadata.redemptionRate)
+      : DefaultSettings.redemptionRate
+    const tokenMinting =
+      fundingCycleMetadata.allowMinting !== undefined
+        ? fundingCycleMetadata.allowMinting
+        : DefaultSettings.tokenMinting
 
-  const dispatch = useAppDispatch()
+    return {
+      initialMintRate,
+      reservedTokensPercentage,
+      reservedTokenAllocation,
+      discountRate,
+      redemptionRate,
+      tokenMinting,
+    }
+  }, [
+    fundingCycleData.discountRate,
+    fundingCycleData.weight,
+    fundingCycleMetadata.allowMinting,
+    fundingCycleMetadata.redemptionRate,
+    fundingCycleMetadata.reservedRate,
+  ])
+
+  const selection = useWatch('selection', form)
 
   useEffect(() => {
-    dispatch
-  }, [dispatch])
+    if (selection === 'default') {
+      form.setFieldsValue({ ...DefaultSettings })
+      return
+    }
+  }, [form, selection])
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'initialMintRate',
+    dispatchFunction: editingV2ProjectActions.setWeight,
+    formatter: v => {
+      if (!v || typeof v !== 'string') return ''
+      return issuanceRateFrom(v)
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'reservedTokensPercentage',
+    dispatchFunction: editingV2ProjectActions.setReservedRate,
+    formatter: v => {
+      if (!v || typeof v !== 'number') return ''
+      return v.toString()
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'reservedTokenAllocation',
+    ignoreUndefined: true, // Needed to stop an infinite loop
+    currentValue: reservedTokensGroupedSplits.splits, // Needed to stop an infinite loop
+    dispatchFunction: editingV2ProjectActions.setReservedTokensSplits,
+    formatter: v => {
+      if (!v || typeof v !== 'object') return []
+      return v
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'discountRate',
+    dispatchFunction: editingV2ProjectActions.setDiscountRate,
+    formatter: v => {
+      if (!v || typeof v !== 'number') return ''
+      return v.toString()
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'redemptionRate',
+    dispatchFunction: editingV2ProjectActions.setRedemptionRate,
+    formatter: v => {
+      if (!v || typeof v !== 'number') return ''
+      return v.toString()
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'tokenMinting',
+    dispatchFunction: editingV2ProjectActions.setAllowMinting,
+    formatter: v => {
+      if (typeof v !== 'boolean') return false
+      return v
+    },
+  })
 
   return { form, initialValues }
 }

9847df31fe6bc97a747c794577e2719ef379df16
Johnny D <johnnyd.dev@gmail.com>
2022-10-05 23:00:56 +1100

    Revert "Tomquirk/safe fix (#2129)"
    This reverts commit a667864494bafb77c071ddb059d3291f04d2a452.

--- src/components/v2v3/V2V3Project/ProjectSafeDashboard/SafeTransaction.tsx
+++ src/components/v2v3/V2V3Project/ProjectSafeDashboard/SafeTransaction.tsx
@@ -1,9 +1,8 @@
-import { DownOutlined, UpOutlined } from '@ant-design/icons'
 import { ThemeContext } from 'contexts/themeContext'
 import { SafeTransactionType } from 'models/safe'
-import { CSSProperties, useContext, useMemo, useState } from 'react'
+import { CSSProperties, useContext, useMemo } from 'react'
 import { ReconfigureFundingCyclesOfTransaction } from './juiceboxTransactions/reconfigureFundingCyclesOf'
-import { LinkToSafeButton } from './LinkToSafeButton'
+
 import { TransactionHeader } from './TransactionHeader'
 
 export type SafeTransactionComponentProps = {
@@ -13,15 +12,13 @@ export type SafeTransactionComponentProps = {
 }
 
 export const safeTransactionRowStyle: CSSProperties = {
+  display: 'flex',
   justifyContent: 'space-between',
   fontWeight: 400,
   width: '100%',
   padding: '0.5rem 1rem',
   marginBottom: '1rem',
   transition: 'background-color 100ms linear',
-  display: 'flex',
-  flexDirection: 'column',
-  cursor: 'pointer',
 }
 
 const GenericSafeTransaction = ({
@@ -32,46 +29,22 @@ const GenericSafeTransaction = ({
   const {
     theme: { colors },
   } = useContext(ThemeContext)
-
-  const [expanded, setExpanded] = useState<boolean>(selected)
-
-  const rowStyle: CSSProperties = {
-    ...safeTransactionRowStyle,
-    color: colors.text.primary,
-    paddingRight: '1rem',
-  }
-
-  if (selected) {
-    rowStyle.border = `1px solid ${colors.stroke.action.primary}`
-  }
-
   return (
     <div
-      className="clickable-border"
-      style={rowStyle}
-      onClick={() => {
-        setExpanded(!expanded)
+      style={{
+        ...safeTransactionRowStyle,
+        color: colors.text.primary,
+        border: `1px solid ${
+          selected ? colors.stroke.action.primary : colors.stroke.tertiary
+        }`,
+        paddingRight: '40px',
       }}
       id={`${transaction.safeTxHash}`}
     >
-      <div style={{ display: 'flex', width: '100%' }}>
-        <TransactionHeader
-          transaction={transaction}
-          isPastTransaction={isPastTransaction}
-        />
-        <div style={{ marginLeft: 10 }}>
-          {expanded ? <UpOutlined /> : <DownOutlined />}
-        </div>
-      </div>
-
-      {expanded ? (
-        <LinkToSafeButton
-          transaction={transaction}
-          style={{
-            marginTop: '1rem',
-          }}
-        />
-      ) : null}
+      <TransactionHeader
+        transaction={transaction}
+        isPastTransaction={isPastTransaction}
+      />
     </div>
   )
 }
--- src/components/v2v3/V2V3Project/ProjectSafeDashboard/TransactionHeader.tsx
+++ src/components/v2v3/V2V3Project/ProjectSafeDashboard/TransactionHeader.tsx
@@ -1,8 +1,12 @@
+import { t } from '@lingui/macro'
+import { Tooltip } from 'antd'
+import ExternalLink from 'components/ExternalLink'
 import { ThemeContext } from 'contexts/themeContext'
+import { generateSafeTxUrl } from 'lib/safe'
 import { SafeTransactionType } from 'models/safe'
-import Link from 'next/link'
 import { CSSProperties, useContext } from 'react'
 import { formatHistoricalDate } from 'utils/format/formatDate'
+import { ShareTxButton } from './ShareTxButton'
 import { TransactionSigStatus } from './TransactionSigStatus'
 
 const nonceStyle: CSSProperties = {
@@ -24,7 +28,18 @@ export function TransactionHeader({
   const {
     theme: { colors },
   } = useContext(ThemeContext)
-  const transactionTitle = title ?? transaction?.dataDecoded?.method
+  const _method = title ?? transaction?.dataDecoded?.method
+  const transactionTitle = (
+    <Tooltip title={t`Go to Safe`}>
+      <ExternalLink
+        href={generateSafeTxUrl(transaction)}
+        className="hover-text-action-primary hover-text-decoration-underline color-unset"
+        onClick={e => e.stopPropagation()}
+      >
+        {_method}
+      </ExternalLink>
+    </Tooltip>
+  )
 
   return (
     <div
@@ -34,16 +49,18 @@ export function TransactionHeader({
         justifyContent: 'space-between',
         width: '100%',
       }}
+      id={`${transaction.safeTxHash}`}
     >
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div style={{ ...nonceStyle, color: colors.text.secondary }}>
           {transaction.nonce}
         </div>
-        <Link href={`#${transaction.safeTxHash}`}>
-          <a className="text-primary hover-text-decoration-underline">
-            {transactionTitle}
-          </a>
-        </Link>
+        {transactionTitle}
+        <ShareTxButton
+          transaction={transaction}
+          style={{ cursor: 'pointer' }}
+          isPastTransaction
+        />
       </div>
       <div
         style={{

370562c6c214e5b41355c89f3e97073fe60af672
Wraeth <wraeth.dao@gmail.com>
2022-10-10 11:09:29 +1100

    Break out array logic into separate reusable hook

--- src/components/Create/components/Allocation/hooks/Allocation.ts
+++ src/components/Create/components/Allocation/hooks/Allocation.ts
@@ -1,45 +1,17 @@
+import { useArray } from 'hooks/Array'
 import { FormItemInput } from 'models/formItemInput'
-import { useCallback, useState } from 'react'
 import { AllocationSplit } from '../Allocation'
 
 export const useAllocation = ({
   value,
   onChange,
 }: FormItemInput<AllocationSplit[]>) => {
-  const [_allocations, _setAllocations] = useState<AllocationSplit[]>([])
-  const allocations = value ?? _allocations
-  const setAllocations = onChange ?? _setAllocations
-
-  const addAllocation = useCallback(
-    (allocation: AllocationSplit) => {
-      setAllocations([...allocations, allocation])
-    },
-    [allocations, setAllocations],
-  )
-
-  const removeAllocation = useCallback(
-    (id: string) => {
-      setAllocations(allocations.filter(a => a.id !== id))
-    },
-    [allocations, setAllocations],
-  )
-
-  const upsertAllocation = useCallback(
-    (allocation: AllocationSplit) => {
-      const index = allocations.findIndex(a => a.id === allocation.id)
-      if (index < 0) {
-        addAllocation(allocation)
-        return
-      }
-
-      setAllocations([
-        ...allocations.slice(0, index),
-        allocation,
-        ...allocations.slice(index + 1),
-      ])
-    },
-    [addAllocation, allocations, setAllocations],
-  )
+  const {
+    values: allocations,
+    add: addAllocation,
+    remove: removeAllocation,
+    upsert: upsertAllocation,
+  } = useArray([value, onChange])
 
   return { allocations, addAllocation, removeAllocation, upsertAllocation }
 }

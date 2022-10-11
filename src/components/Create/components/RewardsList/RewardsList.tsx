1a9f4c17baa790c039be024210f5c5cb31949019
Wraeth <wraeth.dao@gmail.com>
2022-10-11 11:04:00 +1100

    Rest of work related to NFT Rewards

--- src/components/Create/components/RewardsList/RewardsList.tsx
+++ src/components/Create/components/RewardsList/RewardsList.tsx
@@ -1,36 +1,108 @@
 import { PlusCircleOutlined } from '@ant-design/icons'
 import { Button, Divider } from 'antd'
+import { useModal } from 'hooks/Modal'
 import { FormItemInput } from 'models/formItemInput'
+import { createContext, useCallback, useContext, useState } from 'react'
+import { AddEditRewardModal } from './AddEditRewardModal'
 import { useRewards } from './hooks'
 import { RewardItem } from './RewardItem'
 import { Reward } from './types'
 
-export const RewardsList = ({ value, onChange }: FormItemInput<Reward[]>) => {
-  const { rewards } = useRewards({ value, onChange })
+const RewardsListContext = createContext<ReturnType<typeof useRewards>>({
+  rewards: [],
+  addReward: () => {
+    console.error('RewardsListContext.addReward called but no provider set')
+  },
+  removeReward: () => {
+    console.error('RewardsListContext.removeReward called but no provider set')
+  },
+  upsertReward: () => {
+    console.error('RewardsListContext.upsertReward called but no provider set')
+  },
+})
+
+const useRewardsInstance = () => {
+  return useContext(RewardsListContext)
+}
+
+export type RewardsListProps = FormItemInput<Reward[]>
+
+export interface RewardsListChildrenExports {
+  Item: typeof RewardItem
+  useRewardsInstance: typeof useRewardsInstance
+}
+
+export const RewardsList: React.FC<RewardsListProps> &
+  RewardsListChildrenExports = ({ value, onChange }: RewardsListProps) => {
+  const rewardsHook = useRewards({ value, onChange })
+  const [selectedReward, setSelectedReward] = useState<Reward>()
+  const modal = useModal()
+  const { rewards, upsertReward, removeReward } = rewardsHook
+
+  const onModalOk = useCallback(
+    (reward: Reward) => {
+      upsertReward(reward)
+      modal.close()
+      setSelectedReward(undefined)
+    },
+    [modal, upsertReward],
+  )
+
+  const onModalCancel = useCallback(() => {
+    modal.close()
+    setSelectedReward(undefined)
+  }, [modal])
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '4rem' }}>
-      {!!rewards.length && (
-        <>
-          {rewards.map(reward => (
-            <>
-              <RewardItem key={reward.id} {...reward} />
-              <Divider style={{ margin: 0 }} />
-            </>
-          ))}
-        </>
-      )}
-      <Button
-        type="dashed"
-        icon={<PlusCircleOutlined />}
-        style={{
-          height: '6rem',
-          // TODO: Use proper imported colors
-          backgroundColor: 'magenta',
-          // backgroundColor: '#E8F5F7', // Light Theme/Primary/Blue Hint
-        }}
-      >
-        Add NFT reward tier
-      </Button>
-    </div>
+    <RewardsListContext.Provider value={rewardsHook}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '4rem' }}>
+        {!!rewards.length && (
+          <>
+            {rewards
+              .sort((a, b) => a.minimumContribution - b.minimumContribution)
+              .map((reward, i) => (
+                <>
+                  <RewardItem
+                    key={reward.id}
+                    tier={i + 1}
+                    reward={reward}
+                    onEditClicked={() => {
+                      setSelectedReward(reward)
+                      modal.open()
+                    }}
+                    onDeleteClicked={() => {
+                      removeReward(reward.id)
+                    }}
+                  />
+                  <Divider style={{ margin: 0 }} />
+                </>
+              ))}
+          </>
+        )}
+        <Button
+          type="dashed"
+          icon={<PlusCircleOutlined />}
+          style={{
+            height: '6rem',
+            // TODO: Use proper imported colors
+            backgroundColor: 'magenta',
+            // backgroundColor: '#E8F5F7', // Light Theme/Primary/Blue Hint
+          }}
+          disabled={rewards.length >= 3}
+          onClick={modal.open}
+        >
+          Add NFT reward tier
+        </Button>
+      </div>
+      <AddEditRewardModal
+        visible={modal.visible}
+        editingData={selectedReward}
+        onOk={onModalOk}
+        onCancel={onModalCancel}
+      />
+    </RewardsListContext.Provider>
   )
 }
+
+RewardsList.Item = RewardItem
+RewardsList.useRewardsInstance = useRewardsInstance
--- src/components/Create/components/pages/NftRewards/hooks/NftRewardsForm.ts
+++ src/components/Create/components/pages/NftRewards/hooks/NftRewardsForm.ts
@@ -1,7 +1,82 @@
 import { Form } from 'antd'
+import { Reward } from 'components/Create/components/RewardsList'
+import { useAppSelector } from 'hooks/AppSelector'
+import { useMemo } from 'react'
+import { editingV2ProjectActions } from 'redux/slices/editingV2Project'
+import { v4 } from 'uuid'
+import { useFormDispatchWatch } from '../../hooks'
 
+type NftRewardsFormProps = Partial<{
+  rewards: Reward[]
+  collectionName?: string
+  collectionSymbol?: string
+}>
+
+// TODO: Upload is not called here, but we need to remember to call it in the final deploy - uploadNftRewardsToIPFS
 export const useNftRewardsForm = () => {
-  const [form] = Form.useForm()
-  const initialValues = undefined
+  const [form] = Form.useForm<NftRewardsFormProps>()
+  const { collectionMetadata, rewardTiers } = useAppSelector(
+    state => state.editingV2Project.nftRewards,
+  )
+  const initialValues: NftRewardsFormProps = useMemo(() => {
+    const collectionName = collectionMetadata.name
+    const collectionSymbol = collectionMetadata.symbol
+
+    const rewards: Reward[] = rewardTiers?.map(t => ({
+      id: v4(),
+      title: t.name,
+      minimumContribution: t.contributionFloor,
+      description: t.description,
+      maximumSupply: t.maxSupply,
+      url: t.externalLink,
+      imgUrl: t.imageUrl,
+    }))
+
+    return { rewards, collectionName, collectionSymbol }
+  }, [collectionMetadata.name, collectionMetadata.symbol, rewardTiers])
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'rewards',
+    ignoreUndefined: true, // Needed to stop an infinite loop
+    currentValue: rewardTiers,
+    dispatchFunction: editingV2ProjectActions.setNftRewardTiers,
+    formatter: v => {
+      if (!v) return []
+      if (typeof v !== 'object') {
+        console.error('Invalid type passed to setNftRewardTiers dispatch', v)
+        throw new Error('Invalid type passed to setNftRewardTiers dispatch')
+      }
+      return v.map(reward => ({
+        contributionFloor: reward.minimumContribution,
+        maxSupply: reward.maximumSupply,
+        remainingSupply: reward.maximumSupply,
+        imageUrl: reward.imgUrl,
+        name: reward.title,
+        externalLink: reward.url,
+        description: reward.description,
+      }))
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'collectionName',
+    dispatchFunction: editingV2ProjectActions.setNftRewardsName,
+    formatter: v => {
+      if (!v || typeof v !== 'string') return ''
+      return v
+    },
+  })
+
+  useFormDispatchWatch({
+    form,
+    fieldName: 'collectionSymbol',
+    dispatchFunction: editingV2ProjectActions.setNftRewardsSymbol,
+    formatter: v => {
+      if (!v || typeof v !== 'string') return ''
+      return v
+    },
+  })
   return { form, initialValues }
 }

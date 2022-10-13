303d725b7e6da9ea66430c0e79d43ecd887a5dca
Johnny D <johnnyd.eth@protonmail.com>
2022-10-13 22:33:52 +1000

    Safe notice on project page when connected wallet is on multisig and has unsigned transactions

--- src/components/Project/ProjectHeader/GnosisSafeBadge.tsx
+++ src/components/Project/ProjectHeader/GnosisSafeBadge.tsx
@@ -1,11 +1,22 @@
 import Icon from '@ant-design/icons'
-import { Trans } from '@lingui/macro'
+import { plural, Trans } from '@lingui/macro'
 import { Tooltip } from 'antd'
 import { ThemeContext } from 'contexts/themeContext'
+import { useQueuedSafeTransactions } from 'hooks/safe/QueuedSafeTransactions'
+import { useWallet } from 'hooks/Wallet'
+import { GnosisSafe } from 'models/safe'
 import Link from 'next/link'
 import { useContext } from 'react'
+import { getUniqueNonces, getUnsignedTxsOfUser, isSafeSigner } from 'utils/safe'
 
-export function GnosisSafeBadge({ href }: { href: string }) {
+export function GnosisSafeBadge({
+  safe,
+  href,
+}: {
+  safe: GnosisSafe
+  href: string
+}) {
+  const { userAddress } = useWallet()
   function SafeIcon() {
     const { isDarkMode } = useContext(ThemeContext)
     const src = isDarkMode
@@ -21,17 +32,90 @@ export function GnosisSafeBadge({ href }: { href: string }) {
     )
   }
 
-  return (
-    <Tooltip
-      placement="bottom"
-      title={
-        <Trans>
-          This project is owned by a Safe.{' '}
-          <Link href={href}>See transactions</Link>.
-        </Trans>
-      }
-    >
-      <Icon component={SafeIcon} />
-    </Tooltip>
-  )
+  function DefaultBadge() {
+    return (
+      <Tooltip
+        placement="bottom"
+        title={
+          <Trans>
+            This project is owned by a Safe.{' '}
+            <Link href={href}>View transactions</Link>.
+          </Trans>
+        }
+      >
+        <Icon component={SafeIcon} />
+      </Tooltip>
+    )
+  }
+
+  // Red dot that appears conditionally next to Safe badge when connected user is on multisig and has unsiged tranctions
+  function Notice() {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'red',
+          borderRadius: '100%',
+          width: '6px',
+          height: '6px',
+          top: '0px',
+          left: '13px',
+        }}
+      />
+    )
+  }
+
+  // This shows a notice of queued transaction count to users with connected wallets on the multisig
+  // Using this component so `useQueuedSafeTransactions` is only called when the connected wallet is on the multisig
+  function BadgeMightHaveNotice() {
+    const { data: queuedSafeTransactions, isLoading } =
+      useQueuedSafeTransactions({
+        safeAddress: safe.address,
+      })
+    if (isLoading || !userAddress) return <DefaultBadge />
+
+    const unsignedNoncesOfUser = getUniqueNonces(
+      getUnsignedTxsOfUser({
+        address: userAddress,
+        transactions: queuedSafeTransactions,
+      }),
+    )
+    if (queuedSafeTransactions && !unsignedNoncesOfUser.length) {
+      return <DefaultBadge />
+    }
+    return (
+      <Tooltip
+        placement="bottom"
+        overlayStyle={{ width: 700 }}
+        title={
+          <>
+            <div>
+              <Trans>
+                {plural(unsignedNoncesOfUser.length, {
+                  one: '# transaction needs',
+                  other: '# transactions need',
+                })}{' '}
+                your signature.
+              </Trans>
+            </div>
+            <div>
+              <Trans>
+                <Link href={href}>View transactions.</Link>
+              </Trans>
+            </div>
+          </>
+        }
+      >
+        <div style={{ display: 'flex', position: 'relative' }}>
+          <Icon component={SafeIcon} />
+          <Notice />
+        </div>
+      </Tooltip>
+    )
+  }
+
+  const isMultisigMember = isSafeSigner({ safe, address: userAddress })
+  if (isMultisigMember) return <BadgeMightHaveNotice />
+
+  return <DefaultBadge />
 }
--- src/utils/safe.ts
+++ src/utils/safe.ts
@@ -1,4 +1,4 @@
-import { SafeTransactionType } from 'models/safe'
+import { GnosisSafe, GnosisSignature, SafeTransactionType } from 'models/safe'
 
 // e.g. [ {nonce: 69}, {nonce: 45}, {nonce: 69}] returns [69, 45]
 export function getUniqueNonces(transactions: SafeTransactionType[]) {
@@ -7,4 +7,39 @@ export function getUniqueNonces(transactions: SafeTransactionType[]) {
       transactions?.map((tx: SafeTransactionType) => tx.nonce),
     ),
   ]
+}
+
+// returns subset of given transactions for which a given address has not signed
+export function getUnsignedTxsOfUser({
+  address,
+  transactions,
+}: {
+  address: string
+  transactions: SafeTransactionType[]
+}) {
+  const unsignedTxs: SafeTransactionType[] = []
+  transactions.forEach((transaction: SafeTransactionType) => {
+    let hasSigned = false
+    transaction.confirmations?.forEach((confirmation: GnosisSignature) => {
+      if (confirmation.owner.toLowerCase() === address) {
+        hasSigned = true
+      }
+    })
+    if (!hasSigned) unsignedTxs.push(transaction)
+  })
+  return unsignedTxs
+}
+
+// returns whether given wallet is member of a given Safe multisig
+export function isSafeSigner({
+  address,
+  safe,
+}: {
+  address: string | undefined
+  safe: GnosisSafe
+}) {
+  if (!address) return false
+  return safe.owners.some(
+    owner => owner.toLowerCase() === address.toLowerCase(),
+  )
 }

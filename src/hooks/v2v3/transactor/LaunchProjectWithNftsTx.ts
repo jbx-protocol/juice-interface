672224a96b873a5f46142ac752f8fd279d1028d9
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-15 16:03:17 +1030

    Start updating launch nft

--- src/hooks/v2v3/transactor/LaunchProjectWithNftsTx.ts
+++ src/hooks/v2v3/transactor/LaunchProjectWithNftsTx.ts
@@ -1,29 +1,32 @@
 import { getAddress } from '@ethersproject/address'
+import { BigNumber } from '@ethersproject/bignumber'
 import * as constants from '@ethersproject/constants'
+import { parseEther } from '@ethersproject/units'
+import { t } from '@lingui/macro'
+import { JUICEBOX_MONEY_PROJECT_METADATA_DOMAIN } from 'constants/metadataDomain'
+import { MaxUint48 } from 'constants/numbers'
+import { TransactionContext } from 'contexts/transactionContext'
 import { V2V3ContractsContext } from 'contexts/v2v3/V2V3ContractsContext'
+import { TransactorInstance } from 'hooks/Transactor'
 import { useWallet } from 'hooks/Wallet'
+import { NftRewardTier } from 'models/nftRewardTier'
+import { GroupedSplits, SplitGroup } from 'models/splits'
 import {
   V2V3FundAccessConstraint,
   V2V3FundingCycleData,
   V2V3FundingCycleMetadata,
 } from 'models/v2v3/fundingCycle'
 import { useContext } from 'react'
-
-import { NftRewardTier } from 'models/nftRewardTier'
-import { GroupedSplits, SplitGroup } from 'models/splits'
-
-import { BigNumber } from '@ethersproject/bignumber'
-import { parseEther } from '@ethersproject/units'
 import { encodeIPFSUri, ipfsCidUrl } from 'utils/ipfs'
 import { getLatestNftDelegateStoreContractAddress } from 'utils/nftRewards'
 import { isValidMustStartAtOrAfter } from 'utils/v2v3/fundingCycle'
-
-import { t } from '@lingui/macro'
-import { JUICEBOX_MONEY_PROJECT_METADATA_DOMAIN } from 'constants/metadataDomain'
-import { MaxUint48 } from 'constants/numbers'
-import { TransactionContext } from 'contexts/transactionContext'
-import { TransactorInstance } from 'hooks/Transactor'
 import { useV2ProjectTitle } from '../ProjectTitle'
+
+enum JB721GovernanceType {
+  NONE = 'NONE',
+  TIERED = 'TIERED',
+  GLOBAL = 'GLOBAL',
+}
 
 const DEFAULT_MUST_START_AT_OR_AFTER = '1' // start immediately
 const DEFAULT_MEMO = ''
@@ -38,6 +41,7 @@ async function getJBDeployTiered721DelegateData({
   nftRewards,
   ownerAddress,
   directory,
+  JBFundingCycleStoreAddress,
 }: {
   collectionCID: string
   collectionName: string
@@ -45,6 +49,7 @@ async function getJBDeployTiered721DelegateData({
   nftRewards: TxNftArg
   ownerAddress: string
   directory: string
+  JBFundingCycleStoreAddress: string
 }) {
   const JBTiered721DelegateStoreAddress =
     await getLatestNftDelegateStoreContractAddress()
@@ -73,15 +78,21 @@ async function getJBDeployTiered721DelegateData({
     directory,
     name: collectionName,
     symbol: collectionSymbol,
-    tokenUriResolver: constants.AddressZero,
+    fundingCycleStore: JBFundingCycleStoreAddress,
     baseUri: ipfsCidUrl(''),
+    tokenUriResolver: constants.AddressZero,
     contractUri: ipfsCidUrl(collectionCID),
     owner: ownerAddress,
-    tiers: tiersArg,
+    pricing: tiersArg,
     reservedTokenBeneficiary: constants.AddressZero,
     store: JBTiered721DelegateStoreAddress,
-    lockReservedTokenChanges: false,
-    lockVotingUnitChanges: false,
+    flags: {
+      lockReservedTokenChanges: false,
+      lockVotingUnitChanges: false,
+      lockManualMintingChanges: false,
+      pausable: false,
+    },
+    governanceType: JB721GovernanceType.TIERED,
   }
 }
 
@@ -99,6 +110,7 @@ export function useLaunchProjectWithNftsTx(): TransactorInstance<{
 }> {
   const { transactor } = useContext(TransactionContext)
   const { contracts } = useContext(V2V3ContractsContext)
+
   const { userAddress } = useWallet()
   const projectTitle = useV2ProjectTitle()
 
@@ -120,8 +132,7 @@ export function useLaunchProjectWithNftsTx(): TransactorInstance<{
     if (
       !transactor ||
       !userAddress ||
-      !contracts?.JBController ||
-      !contracts.JBETHPaymentTerminal ||
+      !contracts ||
       !isValidMustStartAtOrAfter(mustStartAtOrAfter, fundingCycleData.duration)
     ) {
       txOpts?.onDone?.()
@@ -135,6 +146,9 @@ export function useLaunchProjectWithNftsTx(): TransactorInstance<{
       nftRewards,
       ownerAddress: userAddress,
       directory: getAddress(contracts.JBDirectory.address),
+      JBFundingCycleStoreAddress: getAddress(
+        contracts.JBFundingCycleStore.address,
+      ),
     })
 
     const args = [

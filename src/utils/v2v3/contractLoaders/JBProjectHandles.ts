604d53fac78d96fee73350a50cd7620272be02bc
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-16 15:44:48 +1030

    Clean up project handles UI (#2249)
    * Clean up project handles UI
    
    * Pot

--- src/utils/v2v3/contractLoaders/JBProjectHandles.ts
+++ src/utils/v2v3/contractLoaders/JBProjectHandles.ts
@@ -2,9 +2,6 @@ import { NETWORKS_BY_NAME } from 'constants/networks'
 import { NetworkName } from 'models/network-name'
 import { ForgeDeploy } from '../loadV2V3Contract'
 
-// TODO ask contract crew to add this to their package.
-const GOERLI_ADDRESS = '0x3ff1f0583a41ce8b9463f74a1227c75fc13f7c27'
-
 export const loadJBProjectHandlesContract = async (network: NetworkName) => {
   const contractJson = {
     abi: (
@@ -12,14 +9,11 @@ export const loadJBProjectHandlesContract = async (network: NetworkName) => {
         `@jbx-protocol/project-handles/out/JBProjectHandles.sol/JBProjectHandles.json`
       )
     ).abi,
-    address:
-      network === NetworkName.goerli
-        ? GOERLI_ADDRESS
-        : (
-            (await import(
-              `@jbx-protocol/project-handles/broadcast/Deploy.sol/${NETWORKS_BY_NAME[network].chainId}/run-latest.json`
-            )) as ForgeDeploy
-          ).receipts[0].contractAddress, // contractAddress is prefixed `0x0x` in error, trim first `0x`
+    address: (
+      (await import(
+        `@jbx-protocol/project-handles/broadcast/Deploy.sol/${NETWORKS_BY_NAME[network].chainId}/run-latest.json`
+      )) as ForgeDeploy
+    ).receipts[0].contractAddress, // contractAddress is prefixed `0x0x` in error, trim first `0x`
   }
 
   return contractJson

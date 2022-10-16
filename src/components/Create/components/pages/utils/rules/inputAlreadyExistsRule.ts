706ebeb408222cb1da0f26ca63f5beda04db32e8
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-10-17 10:36:21 +1100

    Add rules for nft rewards (#2186)
    ## What does this PR do and why?
    
    _Provide a description of what this PR does. Link to any relevant GitHub issues, Notion tasks or Discord discussions._
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] I have tested this PR in dark mode and light mode (if applicable).

--- src/components/Create/components/pages/utils/rules/inputAlreadyExistsRule.ts
+++ src/components/Create/components/pages/utils/rules/inputAlreadyExistsRule.ts
@@ -4,13 +4,16 @@ import { RuleObject } from 'antd/lib/form'
 /**
  * Rule to determine if the input already matches other `inputs`.
  */
-export const inputAlreadyExistsRule = (props: { inputs: string[] }) => ({
+export const inputAlreadyExistsRule = (props: {
+  inputs: string[]
+  msg?: string
+}) => ({
   validator: (rule: RuleObject, value: unknown) => {
     if (value === undefined) return Promise.resolve()
     if (typeof value !== 'string')
       return Promise.reject(t`Invalid type - contact Juicebox Support`)
     if (props.inputs.includes(value)) {
-      return Promise.reject(t`Value has already been submitted`)
+      return Promise.reject(props.msg ?? t`Value has already been submitted`)
     }
 
     return Promise.resolve()

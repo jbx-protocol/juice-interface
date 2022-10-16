9ddaf28e71b46e03a81ffa74c1338c466fdcdff7
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-17 10:22:38 +1030

    Safe icon fix

--- src/components/Project/ProjectHeader/GnosisSafeBadge.tsx
+++ src/components/Project/ProjectHeader/GnosisSafeBadge.tsx
@@ -1,26 +1,25 @@
-import Icon from '@ant-design/icons'
 import { Trans } from '@lingui/macro'
 import { Tooltip } from 'antd'
 import { ThemeContext } from 'contexts/themeContext'
 import Link from 'next/link'
 import { useContext } from 'react'
 
+function SafeIcon({ href }: { href: string }) {
+  const { isDarkMode } = useContext(ThemeContext)
+  const src = isDarkMode
+    ? '/assets/icons/gnosis_od.svg'
+    : '/assets/icons/gnosis_ol.svg'
+
+  return (
+    <Link href={href}>
+      <a>
+        <img src={src} alt="Safe" width={15} height={15} />
+      </a>
+    </Link>
+  )
+}
+
 export function GnosisSafeBadge({ href }: { href: string }) {
-  function SafeIcon() {
-    const { isDarkMode } = useContext(ThemeContext)
-    const src = isDarkMode
-      ? '/assets/icons/gnosis_od.svg'
-      : '/assets/icons/gnosis_ol.svg'
-
-    return (
-      <Link href={href}>
-        <a>
-          <img src={src} alt="Safe" width={15} height={15} />
-        </a>
-      </Link>
-    )
-  }
-
   return (
     <Tooltip
       placement="bottom"
@@ -31,7 +30,9 @@ export function GnosisSafeBadge({ href }: { href: string }) {
         </Trans>
       }
     >
-      <Icon component={SafeIcon} />
+      <span>
+        <SafeIcon href={href} />
+      </span>
     </Tooltip>
   )
 }

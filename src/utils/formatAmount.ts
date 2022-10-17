5ca86720f1045374054d3cc4f049638844cc347d
Wraeth <wraeth.dao@gmail.com>
2022-10-18 09:03:06 +1100

    comment

--- src/utils/formatAmount.ts
+++ src/utils/formatAmount.ts
@@ -1,3 +1,10 @@
+/*
+ * Formats a number (or string) to a string with a fixed number of decimals of 2.
+ *
+ * If no decimals are supplied, the number is rounded to the nearest integer.
+ *
+ * @param amount - The number to format.
+ */
 export const formatAmount = (amount: number | string) => {
   let a = amount
   if (typeof amount === 'string') {

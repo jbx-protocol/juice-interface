51d001a4b30e1534214b336ab0bebe9c101e9ead
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-10-18 17:05:11 +1100

    Add bottom matter to review page (#2225)
    * Add bottom matter to review page
    
    * copy
    
    * cookie -> data

--- src/components/Create/components/pages/ReviewDeploy/ReviewDeployPage.tsx
+++ src/components/Create/components/pages/ReviewDeploy/ReviewDeployPage.tsx
@@ -1,8 +1,14 @@
 import { CheckCircleFilled } from '@ant-design/icons'
-import { Trans } from '@lingui/macro'
+import { t, Trans } from '@lingui/macro'
+import { Checkbox, Form, Modal } from 'antd'
+import Callout from 'components/Callout'
+import ExternalLink from 'components/ExternalLink'
 import { ThemeContext } from 'contexts/themeContext'
-import { useContext } from 'react'
+import { useModal } from 'hooks/Modal'
+import { useCallback, useContext } from 'react'
+import { useDispatch } from 'react-redux'
 import { useSetCreateFurthestPageReached } from 'redux/hooks/EditingCreateFurthestPageReached'
+import { editingV2ProjectActions } from 'redux/slices/editingV2Project'
 import { CreateCollapse } from '../../CreateCollapse'
 import { Wizard } from '../../Wizard'
 import {
@@ -26,7 +32,22 @@ const Header: React.FC = ({ children }) => {
 }
 
 export const ReviewDeployPage = () => {
+  const {
+    theme: { colors },
+  } = useContext(ThemeContext)
   useSetCreateFurthestPageReached('reviewDeploy')
+  const [form] = Form.useForm<{ termsAccepted: boolean }>()
+  const termsAccepted = Form.useWatch('termsAccepted', form)
+  const modal = useModal()
+
+  const dispatch = useDispatch()
+
+  const handleStartOverClicked = useCallback(() => {
+    dispatch(editingV2ProjectActions.resetState())
+    window.location.reload()
+  }, [dispatch])
+
+  const isNextEnabled = termsAccepted
   return (
     <>
       <CreateCollapse>
@@ -81,7 +102,64 @@ export const ReviewDeployPage = () => {
           <RulesReview />
         </CreateCollapse.Panel>
       </CreateCollapse>
-      <Wizard.Page.ButtonControl />
+      <Form form={form} initialValues={{ termsAccepted: false }}>
+        <Callout iconComponent={null}>
+          <div style={{ display: 'flex', gap: '1rem' }}>
+            <Form.Item noStyle name="termsAccepted" valuePropName="checked">
+              <Checkbox />
+            </Form.Item>
+            <div>
+              <Trans>
+                I have read and accept the{' '}
+                <ExternalLink href="https://info.juicebox.money/tos/">
+                  Terms of Service
+                </ExternalLink>
+                , and understand that I CANNOT make changes to my project's
+                funding configuration until AFTER Funding Cycle #1 has finished.
+              </Trans>
+            </div>
+          </div>
+        </Callout>
+        <Wizard.Page.ButtonControl isNextEnabled={isNextEnabled} />
+      </Form>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          paddingTop: '4.5rem',
+          color: colors.text.tertiary,
+        }}
+      >
+        <div>
+          <Trans>
+            Not quite ready to launch? Your project details will be saved as a
+            'draft' as long as you don't clear your browser's data.
+          </Trans>
+        </div>
+        <span>
+          <Trans>Made a mistake?</Trans>{' '}
+          <a onClick={modal.open}>
+            <Trans>Start over</Trans>
+          </a>
+          .
+        </span>
+      </div>
+      <Modal
+        title={
+          <h2>
+            <Trans>Are you sure?</Trans>
+          </h2>
+        }
+        okText={t`Yes, start over`}
+        cancelText={t`No, keep editing`}
+        open={modal.visible}
+        onOk={handleStartOverClicked}
+        onCancel={modal.close}
+      >
+        <Trans>Starting over will erase all currently saved progress.</Trans>
+      </Modal>
     </>
   )
 }

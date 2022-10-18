7adc532f9105539d62aa0e7b88a64a67ea754f35
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-10-18 16:44:32 +1100

    Break out funding cycle formatting utils to be reused (#2208)
    * Make review page 800px
    
    * Add Project Details Revuiew Page
    
    * feedback
    
    * Add property isEditable to AllocationList
    
    * Add support of this to payout lists and reserved tokens. side fix for reserved tokens as well
    
    * Break out editing split redux into hooks that can be reused
    
    * Break out funding cycle formatting utils to be reused
    
    * formatfundingcycleduration

--- src/components/Create/components/RecallCard/hooks/FundingCycleRecallValue.ts
+++ src/components/Create/components/RecallCard/hooks/FundingCycleRecallValue.ts
@@ -1,35 +1,5 @@
+import { formatFundingCycleDuration } from 'components/Create/utils/formatFundingCycleDuration'
 import { useAppSelector } from 'hooks/AppSelector'
-import { deriveDurationUnit, secondsToOtherUnit } from 'utils/format/formatTime'
-
-const formatUnit = ({ unit, plural }: { unit: string; plural: boolean }) => {
-  let formatted = unit.charAt(0).toUpperCase() + unit.slice(1)
-  if (formatted.endsWith('s')) {
-    if (!plural) {
-      formatted = formatted.slice(0, -1)
-    }
-  }
-  return formatted
-}
-
-const formatFundingCycle = (duration: string) => {
-  const durationAsNumber = parseInt(duration)
-  if (isNaN(durationAsNumber)) return 'No duration'
-
-  const derivedUnit = deriveDurationUnit(durationAsNumber)
-  const formattedDuration = secondsToOtherUnit({
-    duration: durationAsNumber,
-    unit: derivedUnit,
-  })
-
-  const formattedUnit = formatUnit({
-    unit: derivedUnit,
-    plural: formattedDuration > 1,
-  })
-  return `${secondsToOtherUnit({
-    duration: durationAsNumber,
-    unit: derivedUnit,
-  })} ${formattedUnit}`
-}
 
 export const useFundingCycleRecallValue = () => {
   const {
@@ -40,5 +10,5 @@ export const useFundingCycleRecallValue = () => {
 
   if (duration === '0') return 'No duration'
 
-  return formatFundingCycle(duration)
+  return formatFundingCycleDuration(duration)
 }

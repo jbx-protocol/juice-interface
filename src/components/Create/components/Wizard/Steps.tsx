cc1dd98d0f53071188ae25bfa0d85216afde7863
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2022-10-18 16:52:41 +1100

    Add foundation for Steps - adds the core components to render (#2220)
    * Breakout splitToAllocation for reuse util
    
    * fix
    
    * Add Project Token Review
    
    * add nft reward review collapsible
    
    * Add rules review
    
    * Add foundation for Steps - adds the core components to render
    
    * steps feedback

--- src/components/Create/components/Wizard/Steps.tsx
+++ src/components/Create/components/Wizard/Steps.tsx
@@ -1,3 +1,21 @@
+import { Steps as AntSteps } from 'antd'
+import { useSteps } from './hooks/Steps'
+
 export const Steps = () => {
-  return <h3>Placeholder for Steps</h3>
+  const { steps, current, onStepClicked } = useSteps()
+
+  return (
+    <div style={{ padding: '2.5rem 5rem' }}>
+      <AntSteps
+        current={current.index}
+        progressDot
+        size="small"
+        onChange={onStepClicked}
+      >
+        {steps?.map(({ id, title }) => (
+          <AntSteps.Step key={id} title={title} />
+        )) ?? null}
+      </AntSteps>
+    </div>
+  )
 }

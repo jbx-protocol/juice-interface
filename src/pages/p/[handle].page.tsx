e6520102c454888f8f07f90a70cbdac3ccbb7f86
Jonathan Miller <33093632+jmilldotdev@users.noreply.github.com>
2022-10-18 16:19:24 -0400

    Revert "Revalidate project page every 10s (#2268)" (#2279)
    This reverts commit aee14f39535f681983d0f64b8b23e02dbd4fe5ba.

--- src/pages/p/[handle].page.tsx
+++ src/pages/p/[handle].page.tsx
@@ -2,6 +2,7 @@ import { AppWrapper, SEO } from 'components/common'
 import { DesmosScript } from 'components/common/Head/scripts/DesmosScript'
 import { FeedbackFormButton } from 'components/FeedbackFormButton'
 import Loading from 'components/Loading'
+import NewDeployNotAvailable from 'components/NewDeployNotAvailable'
 import Project404 from 'components/Project404'
 import ScrollToTopButton from 'components/ScrollToTopButton'
 import { V1Project } from 'components/v1/V1Project'
@@ -64,10 +65,7 @@ export const getStaticProps: GetStaticProps<{
     const metadata = await findProjectMetadata({
       metadataCid: projects[0].metadataUri,
     })
-    return {
-      props: { metadata, handle },
-      revalidate: 10, // 10 seconds https://nextjs.org/docs/api-reference/data-fetching/get-static-props#revalidate
-    }
+    return { props: { metadata, handle } }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
@@ -124,11 +122,17 @@ function V1Dashboard() {
   const { projectId } = useContext(ProjectMetadataContext)
   const router = useRouter()
 
+  // Checks URL to see if user was just directed from project deploy
   const handle = router.query.handle as string
+  const isNewDeploy = Boolean(router.query.newDeploy)
 
   if (!handle) return <Project404 projectId={handle} />
   if (!projectId) return <Loading />
   if (projectId === 0) {
+    if (isNewDeploy) {
+      return <NewDeployNotAvailable handleOrId={handle} />
+    }
+
     return <Project404 projectId={handle} />
   }
 

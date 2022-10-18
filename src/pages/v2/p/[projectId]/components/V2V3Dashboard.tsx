aee14f39535f681983d0f64b8b23e02dbd4fe5ba
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-18 18:59:49 +1030

    Revalidate project page every 10s (#2268)
    * Random props
    
    * Revalidate project page every 10s

--- src/pages/v2/p/[projectId]/components/V2V3Dashboard.tsx
+++ src/pages/v2/p/[projectId]/components/V2V3Dashboard.tsx
@@ -1,11 +1,15 @@
 import ScrollToTopButton from 'components/ScrollToTopButton'
 import { V2V3Project } from 'components/v2v3/V2V3Project'
 import { layouts } from 'constants/styles/layouts'
+import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
 import { TransactionProvider } from 'providers/TransactionProvider'
 import { NftRewardsProvider } from 'providers/v2v3/NftRewardsProvider'
 import { VeNftProvider } from 'providers/v2v3/VeNftProvider'
+import { useContext } from 'react'
 
-export function V2V3Dashboard({ projectId }: { projectId: number }) {
+export function V2V3Dashboard() {
+  const { projectId } = useContext(ProjectMetadataContext)
+
   return (
     <div style={layouts.maxWidth}>
       <TransactionProvider>

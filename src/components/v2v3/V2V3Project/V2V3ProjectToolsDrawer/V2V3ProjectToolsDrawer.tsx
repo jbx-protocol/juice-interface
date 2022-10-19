726730aa72ff3ccdd602eaf1af7569cbaacc759b
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-19 16:10:10 +1030

    Fix Antd Tab deprecation warnings (#2298)
    * Fix Antd Tab deprecation warnings
    
    * Make settings sidebar thinner

--- src/components/v2v3/V2V3Project/V2V3ProjectToolsDrawer/V2V3ProjectToolsDrawer.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectToolsDrawer/V2V3ProjectToolsDrawer.tsx
@@ -1,25 +1,19 @@
 import { Trans } from '@lingui/macro'
-import { Divider, Drawer, Space, Tabs } from 'antd'
+import { Divider, Drawer, Space } from 'antd'
 import { AddToProjectBalanceForm } from 'components/Project/ProjectToolsDrawer/AddToProjectBalanceForm'
 import { ExportSection } from 'components/Project/ProjectToolsDrawer/ExportSection'
 import {
   ETH_PAYOUT_SPLIT_GROUP,
   RESERVED_TOKEN_SPLIT_GROUP,
 } from 'constants/splits'
-import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
 import { V2V3ProjectContext } from 'contexts/v2v3/V2V3ProjectContext'
-import { useIsUserAddress } from 'hooks/IsUserAddress'
 import useMobile from 'hooks/Mobile'
 import { useAddToBalanceTx } from 'hooks/v2v3/transactor/AddToBalanceTx'
 import { useDeployProjectPayerTx } from 'hooks/v2v3/transactor/DeployProjectPayerTx'
 import { ETHPayoutSplitGroup, ReservedTokensSplitGroup } from 'models/splits'
-import Link from 'next/link'
 import { useContext } from 'react'
-import { settingsPagePath } from 'utils/routes'
 import { ExportSplitsButton } from './ExportSplitsButton'
 import { PaymentAddressSection } from './PaymentAddressSection'
-
-const { TabPane } = Tabs
 
 export function V2V3ProjectToolsDrawer({
   open,
@@ -28,12 +22,9 @@ export function V2V3ProjectToolsDrawer({
   open?: boolean
   onClose?: VoidFunction
 }) {
-  const { projectOwnerAddress, payoutSplits, reservedTokensSplits, handle } =
-    useContext(V2V3ProjectContext)
-  const { projectId } = useContext(ProjectMetadataContext)
+  const { payoutSplits, reservedTokensSplits } = useContext(V2V3ProjectContext)
 
   const isMobile = useMobile()
-  const isOwnerWallet = useIsUserAddress(projectOwnerAddress)
 
   return (
     <Drawer
@@ -46,64 +37,52 @@ export function V2V3ProjectToolsDrawer({
         <Trans>Tools</Trans>
       </h1>
 
-      <Tabs>
-        <TabPane tab={<Trans>General</Trans>} key="1">
-          <Space direction="vertical" size="middle" style={{ width: '100%' }}>
-            <section>
-              <h3>
-                <Trans>Create Payment Address</Trans>
-              </h3>
+      <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+        <section>
+          <h3>
+            <Trans>Create Payment Address</Trans>
+          </h3>
 
-              <PaymentAddressSection
-                useDeployProjectPayerTx={useDeployProjectPayerTx}
-              />
-            </section>
+          <PaymentAddressSection
+            useDeployProjectPayerTx={useDeployProjectPayerTx}
+          />
+        </section>
 
-            <Divider />
+        <Divider />
 
-            <section>
-              <AddToProjectBalanceForm useAddToBalanceTx={useAddToBalanceTx} />
-            </section>
+        <section>
+          <AddToProjectBalanceForm useAddToBalanceTx={useAddToBalanceTx} />
+        </section>
 
-            <Divider />
+        <Divider />
 
-            <ExportSection
-              exportPayoutsButton={
-                payoutSplits ? (
-                  <ExportSplitsButton<ETHPayoutSplitGroup>
-                    groupedSplits={{
-                      splits: payoutSplits,
-                      group: ETH_PAYOUT_SPLIT_GROUP,
-                    }}
-                  >
-                    <Trans>Export payouts CSV</Trans>
-                  </ExportSplitsButton>
-                ) : undefined
-              }
-              exportReservedTokensButton={
-                reservedTokensSplits ? (
-                  <ExportSplitsButton<ReservedTokensSplitGroup>
-                    groupedSplits={{
-                      splits: reservedTokensSplits,
-                      group: RESERVED_TOKEN_SPLIT_GROUP,
-                    }}
-                  >
-                    <Trans>Export token allocation CSV</Trans>
-                  </ExportSplitsButton>
-                ) : undefined
-              }
-            />
-          </Space>
-        </TabPane>
-        {isOwnerWallet && (
-          <TabPane tab={<Trans>Owner tools</Trans>} key="2">
-            Owner tools have moved.{' '}
-            <Link href={settingsPagePath('general', { projectId, handle })}>
-              <a onClick={() => onClose?.()}>Go to project settings.</a>
-            </Link>
-          </TabPane>
-        )}
-      </Tabs>
+        <ExportSection
+          exportPayoutsButton={
+            payoutSplits ? (
+              <ExportSplitsButton<ETHPayoutSplitGroup>
+                groupedSplits={{
+                  splits: payoutSplits,
+                  group: ETH_PAYOUT_SPLIT_GROUP,
+                }}
+              >
+                <Trans>Export payouts CSV</Trans>
+              </ExportSplitsButton>
+            ) : undefined
+          }
+          exportReservedTokensButton={
+            reservedTokensSplits ? (
+              <ExportSplitsButton<ReservedTokensSplitGroup>
+                groupedSplits={{
+                  splits: reservedTokensSplits,
+                  group: RESERVED_TOKEN_SPLIT_GROUP,
+                }}
+              >
+                <Trans>Export token allocation CSV</Trans>
+              </ExportSplitsButton>
+            ) : undefined
+          }
+        />
+      </Space>
     </Drawer>
   )
 }

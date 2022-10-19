538a2bb7e3064be1e2cbed0518d99f8b8ab41ee0
Wraeth <wraeth.dao@gmail.com>
2022-10-19 12:27:39 +1100

    add deploy button state with connect and change network

--- src/components/Create/components/Wizard/PageButtonControl/components/DoneButton.tsx
+++ src/components/Create/components/Wizard/PageButtonControl/components/DoneButton.tsx
@@ -1,7 +1,8 @@
 import { t } from '@lingui/macro'
 import { Button, ButtonProps } from 'antd'
+import { ReactNode } from 'react'
 
-export const DoneButton = (props: ButtonProps & { text?: string }) => {
+export const DoneButton = (props: ButtonProps & { text?: ReactNode }) => {
   return (
     <Button htmlType="submit" type="primary" size="large" {...props}>
       {props.text ?? t`Done`}

67d4f89c2f6bc03e884c39094163a206064309a8
benjamin <benschac@gmail.com>
2022-10-24 11:51:21 -0400

    remove mobile check for transaction list style + move mobile into desktop component (#2239)
    ## What does this PR do and why?
    
    Move mobile collapse into desktop navigation. Desktopnavigation will eventually turn into `Sitenavigation` to encapsulate nav logic. The goal here was to simplify the `NavBar component`, only use `useMobile` hook in one place, and remove additional desktop/mobile logic in the `MobileCollapse` component.
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] I have tested this PR in dark mode and light mode (if applicable).

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,5 @@
-import useMobile from 'hooks/Mobile'
 import DesktopNavigation from './DeskTopNavigation'
-import MobileCollapse from './Mobile/MobileCollapse'
 
 export default function Navbar() {
-  const isMobile = useMobile()
-  const desktop = !isMobile
-
-  if (isMobile) return <MobileCollapse />
-
-  return <DesktopNavigation desktop={desktop} />
+  return <DesktopNavigation />
 }

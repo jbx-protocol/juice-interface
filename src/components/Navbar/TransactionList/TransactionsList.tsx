c7842f49e64f61b0642447a6054a246497cefa0d
Jonathan Miller <jon@jmill.dev>
2022-10-24 22:50:18 -0400

    transactionslist height fix

--- src/components/Navbar/TransactionList/TransactionsList.tsx
+++ src/components/Navbar/TransactionList/TransactionsList.tsx
@@ -43,6 +43,7 @@ export function TransactionsList({
           paddingLeft: 10,
           paddingRight: 5,
           borderRadius: 15,
+          height: 30,
           userSelect: 'none',
         }}
         onClick={() => setIsExpanded(!isExpanded)}

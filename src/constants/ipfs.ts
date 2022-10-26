8b6a4d69d80e98c08c480290b14a4b9209886fc7
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-26 12:25:01 +1030

    Try open gateway (#2372)
    * Try open gateway
    
    * ya ya ding dong

--- src/constants/ipfs.ts
+++ src/constants/ipfs.ts
@@ -1,4 +1,13 @@
-export const DEFAULT_PINATA_GATEWAY = 'gateway.pinata.cloud'
+import { NetworkName } from 'models/network-name'
+import { readNetwork } from './networks'
 
+const IPFS_OPEN_GATEWAY_HOSTNAME = 'jbx-open.mypinata.cloud' // restricted to juicebox.money domains.
+const PINATA_PUBLIC_GATEWAY_HOSTNAME = 'gateway.pinata.cloud' // heavily rate-limited, shouldn't be used.
+
+export const DEFAULT_PINATA_GATEWAY =
+  process.env.NODE_ENV === 'development' ||
+  readNetwork.name === NetworkName.mainnet // TODO(aeolian) remove this line after testing on goerli.
+    ? PINATA_PUBLIC_GATEWAY_HOSTNAME
+    : IPFS_OPEN_GATEWAY_HOSTNAME
 export const IPFS_GATEWAY_HOSTNAME =
   process.env.NEXT_PUBLIC_PINATA_GATEWAY_HOSTNAME || DEFAULT_PINATA_GATEWAY

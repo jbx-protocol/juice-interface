09d12a11b6ce8833549ecb5741486691a0fe939c
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-26 16:08:44 +1030

    Fix ipfsToHttps (#2375)

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -4,6 +4,11 @@ import {
 } from 'constants/ipfs'
 import { base58 } from 'ethers/lib/utils'
 
+const IPFS_URL_REGEX = /ipfs:\/\/(.+)/
+
+/**
+ * Return a HTTP URL to the IPFS gateway at the given [hostname] for the given [cid].
+ */
 const ipfsGatewayUrl = (cid: string | undefined = '', hostname: string) => {
   return `https://${hostname}/ipfs/${cid}`
 }
@@ -30,26 +35,40 @@ export const restrictedIpfsUrl = (cid: string | undefined): string => {
 /**
  * Return an IPFS URI using the IPFS URI scheme.
  */
-export function ipfsUrl(cid: string) {
-  return `ipfs://${cid}`
+export function ipfsUrl(cid: string, path?: string) {
+  return `ipfs://${cid}${path ?? ''}`
 }
 
+/**
+ * Return the IPFS CID from a given [url].
+ *
+ * Assumes that the last path segment is the CID.
+ * @todo this isn't a great assumption. We should make this more robust, perhaps using a regex.
+ */
 export const cidFromUrl = (url: string) => url.split('/').pop()
 
 /**
  * Returns a native IPFS link (`ipfs://`) as a https link.
  */
 export function ipfsToHttps(ipfsUri: string): string {
-  const cid = cidFromUrl(ipfsUri)
-
-  return publicIpfsUrl(cid)
+  const suffix = ipfsUri.match(IPFS_URL_REGEX)?.[1]
+  return publicIpfsUrl(suffix)
 }
 
-// How IPFS URI's are stored in the contracts to save storage (/gas)
+/**
+ * Return a hex-encoded CID to store on-chain.
+ *
+ * Hex-encoded CIDs are used to store some CIDs on-chain because they are more gas-efficient.
+ */
 export function encodeIPFSUri(cid: string) {
   return '0x' + Buffer.from(base58.decode(cid).slice(2)).toString('hex')
 }
 
+/**
+ * Return the IPFS CID from a given hex-endoded string.
+ *
+ * Hex-encoded CIDs are used to store some CIDs on-chain because they are more gas-efficient.
+ */
 export function decodeEncodedIPFSUri(hex: string) {
   // Add default ipfs values for first 2 bytes:
   // - function:0x12=sha2, size:0x20=256 bits

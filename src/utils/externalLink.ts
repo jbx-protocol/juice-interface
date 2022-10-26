9609e0232ed4afcf6cf27033ef51e2d78f26a4e3
Wraeth <wraeth.dao@gmail.com>
2022-10-26 11:37:35 +1100

    Fix bug with collapsed form values

--- src/utils/externalLink.ts
+++ src/utils/externalLink.ts
@@ -6,3 +6,14 @@ export function withHttps(link: string | undefined) {
   }
   return link
 }
+
+export function withoutHttp(link: string | undefined) {
+  if (!link) return
+  if (link?.slice(0, 8) === 'https://') {
+    return link.slice(8)
+  }
+  if (link?.slice(0, 7) === 'http://') {
+    return link.slice(7)
+  }
+  return link
+}

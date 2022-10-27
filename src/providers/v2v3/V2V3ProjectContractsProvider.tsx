dd2be72dee24c232fb674b3e2a6adaa87bafdb24
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-27 14:46:53 +1030

    Client side cv fetch

--- src/providers/v2v3/V2V3ProjectContractsProvider.tsx
+++ src/providers/v2v3/V2V3ProjectContractsProvider.tsx
@@ -1,10 +1,13 @@
 import { V2V3ProjectContractsContext } from 'contexts/v2v3/V2V3ProjectContractsContext'
 import { useV2V3ProjectContracts } from 'hooks/v2v3/V2V3ProjectContracts'
+import { useSetCv } from './V2V3ContractsProvider'
 
 export const V2V3ProjectContractsProvider: React.FC<{
   projectId: number
 }> = ({ children, projectId }) => {
   const contracts = useV2V3ProjectContracts({ projectId })
+
+  useSetCv(projectId)
 
   return (
     <V2V3ProjectContractsContext.Provider
--- src/providers/v2v3/V2V3ProjectPageProvider.tsx
+++ src/providers/v2v3/V2V3ProjectPageProvider.tsx
@@ -1,5 +1,5 @@
 import { ProjectPageProps } from 'utils/server/pages/props'
-import { useSetCv, V2V3ContractsProvider } from './V2V3ContractsProvider'
+import { V2V3ContractsProvider } from './V2V3ContractsProvider'
 import { V2V3ProjectContractsProvider } from './V2V3ProjectContractsProvider'
 import V2V3ProjectMetadataProvider from './V2V3ProjectMetadataProvider'
 import V2V3ProjectProvider from './V2V3ProjectProvider'
@@ -11,8 +11,6 @@ export const V2V3ProjectPageProvider: React.FC<ProjectPageProps> = ({
   projectId,
   children,
 }) => {
-  useSetCv(projectId)
-
   return (
     <V2V3ContractsProvider>
       <V2V3ProjectContractsProvider projectId={projectId}>

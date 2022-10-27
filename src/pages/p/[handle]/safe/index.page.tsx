25473700a9f5d2d562b277610f7fb86b24081da3
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-10-27 10:30:55 +1030

    Remove serverside data fetching from v1 safe page

--- src/pages/p/[handle]/safe/index.page.tsx
+++ src/pages/p/[handle]/safe/index.page.tsx
@@ -1,25 +1,11 @@
 import { AppWrapper } from 'components/common'
-import Loading from 'components/Loading'
 import { ProjectSafeDashboard } from 'components/ProjectSafeDashboard'
 import { V1ProjectContext } from 'contexts/v1/projectContext'
-import { ProjectMetadataV5 } from 'models/project-metadata'
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
 import { V1UserProvider } from 'providers/v1/UserProvider'
 import { V1ProjectMetadataProvider } from 'providers/v1/V1ProjectMetadataProvider'
 import { V1ProjectProvider } from 'providers/v1/V1ProjectProvider'
 import { useContext } from 'react'
-import { getV1StaticPaths, getV1StaticProps } from '../pageLoaders'
-
-export const getStaticPaths: GetStaticPaths = async context => {
-  return getV1StaticPaths(context)
-}
-
-export const getStaticProps: GetStaticProps<{
-  metadata: ProjectMetadataV5
-  handle: string
-}> = async context => {
-  return getV1StaticProps(context)
-}
 
 function V1ProjectSafeDashboard({ handle }: { handle: string }) {
   const { owner } = useContext(V1ProjectContext)
@@ -32,23 +18,21 @@ function V1ProjectSafeDashboard({ handle }: { handle: string }) {
   )
 }
 
-export default function V1ProjectSafeDashboardPage({
-  handle,
-  metadata,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function V1ProjectSafeDashboardPage() {
+  const router = useRouter()
+
+  const { handle } = router.query as { handle: string }
+  if (!handle) return null
+
   return (
     <AppWrapper>
-      {metadata ? (
-        <V1UserProvider>
-          <V1ProjectMetadataProvider handle={handle} metadata={metadata}>
-            <V1ProjectProvider handle={handle}>
-              <V1ProjectSafeDashboard handle={handle} />
-            </V1ProjectProvider>
-          </V1ProjectMetadataProvider>
-        </V1UserProvider>
-      ) : (
-        <Loading />
-      )}
+      <V1UserProvider>
+        <V1ProjectMetadataProvider handle={handle} metadata={undefined}>
+          <V1ProjectProvider handle={handle}>
+            <V1ProjectSafeDashboard handle={handle} />
+          </V1ProjectProvider>
+        </V1ProjectMetadataProvider>
+      </V1UserProvider>
     </AppWrapper>
   )
 }

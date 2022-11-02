f6616aafcb82ffbc760540a577e78038e26bab79
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-02 16:43:49 +1030

    Redo FC launch banner (#2431)
    * Add project contracts loading states
    
    * Add deprecatedJBDirectory to project controller hook
    
    * Re-implement ProjectBanners

--- src/hooks/v2v3/V2V3ProjectContracts.ts
+++ src/hooks/v2v3/V2V3ProjectContracts.ts
@@ -9,19 +9,24 @@ import { useLoadV2V3Contract } from './LoadV2V3Contract'
 /**
  * Load project-specific JB contracts.
  */
-export function useV2V3ProjectContracts({
-  projectId,
-}: {
-  projectId: number
-}): V2V3ProjectContracts {
+export function useV2V3ProjectContracts({ projectId }: { projectId: number }): {
+  data: V2V3ProjectContracts
+  loading: {
+    JBControllerLoading: boolean
+    JBETHPaymentTerminalLoading: boolean
+  }
+} {
   const { cv } = useContext(V2V3ContractsContext)
 
-  const { data: controllerAddress } = useProjectController({
-    projectId,
-  })
-  const { data: primaryETHTerminal } = useProjectPrimaryEthTerminal({
-    projectId,
-  })
+  const { data: controllerAddress, loading: JBControllerLoading } =
+    useProjectController({
+      projectId,
+    })
+
+  const { data: primaryETHTerminal, loading: JBETHPaymentTerminalLoading } =
+    useProjectPrimaryEthTerminal({
+      projectId,
+    })
 
   const JBController = useLoadV2V3Contract({
     cv,
@@ -36,7 +41,13 @@ export function useV2V3ProjectContracts({
   })
 
   return {
-    JBController,
-    JBETHPaymentTerminal,
+    data: {
+      JBController,
+      JBETHPaymentTerminal,
+    },
+    loading: {
+      JBControllerLoading,
+      JBETHPaymentTerminalLoading,
+    },
   }
 }

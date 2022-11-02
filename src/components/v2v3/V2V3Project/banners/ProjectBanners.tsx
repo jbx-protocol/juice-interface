5c20585a760a5a392f9ed91de9605fbdc23a92e9
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-02 16:43:49 +1030

    Redo FC launch banner (#2431)
    * Add project contracts loading states
    
    * Add deprecatedJBDirectory to project controller hook
    
    * Re-implement ProjectBanners

--- src/components/v2v3/V2V3Project/banners/ProjectBanners.tsx
+++ src/components/v2v3/V2V3Project/banners/ProjectBanners.tsx
@@ -1,8 +1,10 @@
+import { AddressZero } from '@ethersproject/constants'
 import { Space } from 'antd'
 import { CV_V2 } from 'constants/cv'
 import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
 import { V2V3ProjectContext } from 'contexts/v2v3/V2V3ProjectContext'
-import useProjectQueuedFundingCycle from 'hooks/v2v3/contractReader/ProjectQueuedFundingCycle'
+import { V2V3ProjectContractsContext } from 'contexts/v2v3/V2V3ProjectContractsContext'
+import useProjectController from 'hooks/v2v3/contractReader/ProjectController'
 import { useV2ConnectedWalletHasPermission } from 'hooks/v2v3/contractReader/V2ConnectedWalletHasPermission'
 import { V2OperatorPermission } from 'models/v2v3/permissions'
 import { useContext } from 'react'
@@ -11,28 +13,37 @@ import { V2BugNoticeBanner } from './V2BugNoticeBanner'
 
 export function ProjectBanners() {
   const { projectId, cv } = useContext(ProjectMetadataContext)
-  const { isPreviewMode, fundingCycle } = useContext(V2V3ProjectContext)
-
-  const { data: queuedFundingCycleResponse } = useProjectQueuedFundingCycle({
-    projectId,
-  })
-  const [queuedFundingCycle] = queuedFundingCycleResponse || []
+  const { isPreviewMode } = useContext(V2V3ProjectContext)
+  const {
+    contracts,
+    loading: { projectContractsLoading },
+  } = useContext(V2V3ProjectContractsContext)
 
   const canReconfigureFundingCycles = useV2ConnectedWalletHasPermission(
     V2OperatorPermission.RECONFIGURE,
   )
 
-  const hasQueuedFundingCycle = queuedFundingCycle?.number.gt(0)
-  const hasCurrentFundingCycle = fundingCycle?.number.gt(0)
+  // get the projects controller address on the deprecated JBDirectory (the bugged version)
+  const { data: controllerAddress } = useProjectController({
+    projectId,
+    useDeprecatedContract: true,
+  })
 
-  // If a V2 project (_not_ a V3 project) has no current or queued FC, we assume that
-  // it's because it's using the old bugged contracts.
-  // TODO probably should check the contract address instead.
+  const isV2Project = !isPreviewMode && cv === CV_V2
+
+  // if the project was created on the bugged version of the JBDirectory, it will have a non-zero controller address.
+  const hasProjectOnDeprecatedContracts =
+    controllerAddress && controllerAddress !== AddressZero
+
+  // if the project has upgraded to the new contracts, have a defined JBController.
+  const hasUpgradedJBController = contracts?.JBController !== undefined
+
+  // show banner if a V2 project was launched on the bugged contracts and hasn't upgraded.
   const showV2BugNoticeBanner =
-    !isPreviewMode &&
-    cv === CV_V2 &&
-    hasCurrentFundingCycle === false &&
-    hasQueuedFundingCycle === false
+    isV2Project &&
+    !projectContractsLoading?.JBControllerLoading &&
+    hasProjectOnDeprecatedContracts &&
+    !hasUpgradedJBController
 
   const showRelaunchFundingCycleBanner =
     showV2BugNoticeBanner && canReconfigureFundingCycles

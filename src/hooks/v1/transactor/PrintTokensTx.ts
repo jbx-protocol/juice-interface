cc02c6fd22d0cd2aaf4bb96b7a54f9b0bd69ec26
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-03 16:23:49 +1030

    Revert "Revert "Transactor arcx fix (#2442)" (#2444)"
    This reverts commit 38562cb3320b1d2c3b1b244aa923896cb71ee285.

--- src/hooks/v1/transactor/PrintTokensTx.ts
+++ src/hooks/v1/transactor/PrintTokensTx.ts
@@ -29,7 +29,7 @@ export function usePrintTokensTx(): TransactorInstance<{
 
     let terminalContract: Contract
     let functionName: string
-    let args: any[] // eslint-disable-line @typescript-eslint/no-explicit-any
+    let args: unknown[]
 
     switch (terminal.version) {
       case '1':
--- src/providers/v1/UserProvider.tsx
+++ src/providers/v1/UserProvider.tsx
@@ -1,17 +1,10 @@
-import { BigNumber } from '@ethersproject/bignumber'
 import { V1UserContext } from 'contexts/v1/userContext'
-import { useGasPriceQuery } from 'hooks/GasPrice'
 import { useTransactor } from 'hooks/Transactor'
 import { useV1ContractLoader } from 'hooks/v1/V1ContractLoader'
 
 export const V1UserProvider: React.FC = ({ children }) => {
   const contracts = useV1ContractLoader()
-
-  const { data: gasPrice } = useGasPriceQuery('average')
-
-  const transactor = useTransactor({
-    gasPrice: gasPrice ? BigNumber.from(gasPrice) : undefined,
-  })
+  const transactor = useTransactor()
 
   return (
     <V1UserContext.Provider

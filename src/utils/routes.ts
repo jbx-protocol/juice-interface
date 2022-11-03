1ecb27d656526c28b4f98dd8ed725c042ad90944
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-03 15:22:18 +1030

    Fix next link prefetching

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,6 +2,8 @@ import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
 import { MenuKey, V2V3SettingsPageKey, V2VeNftPageKey } from 'models/menu-keys'
 import { NextRouter } from 'next/router'
 
+const HELP_PAGE_HOSTNAME = 'https://info.juicebox.money'
+
 export const v2v3ProjectRoute = ({
   projectId,
   handle,
@@ -12,8 +14,6 @@ export const v2v3ProjectRoute = ({
   if (handle) return `/@${handle}`
   return `/v2/p/${BigNumber.from(projectId).toNumber()}`
 }
-
-const HELP_PAGE_HOSTNAME = 'https://info.juicebox.money'
 
 export function helpPagePath(path: string): string {
   return new URL(path, HELP_PAGE_HOSTNAME).toString()

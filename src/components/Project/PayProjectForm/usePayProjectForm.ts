d643d06cc2f8d5693994055b1291b790b623a10b
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-05 00:57:17 +1030

    Pay metadata for nft rewards (#2450)
    * Init set pay metadata
    
    * Pay metadata
    
    * Fix
    
    * Rename

--- src/components/Project/PayProjectForm/usePayProjectForm.ts
+++ src/components/Project/PayProjectForm/usePayProjectForm.ts
@@ -2,11 +2,25 @@ import { CurrencyContext } from 'contexts/currencyContext'
 import { CurrencyOption } from 'models/currencyOption'
 import { Dispatch, SetStateAction, useContext, useState } from 'react'
 
+export interface JB721DelegatePayMetadata {
+  tierIdsToMint: number[]
+  dontMint?: boolean
+  expectMintFromExtraFunds?: boolean
+  dontOverspend?: boolean
+}
+
+export type PayMetadata = JB721DelegatePayMetadata // in future, maybe more
+
 export interface PayProjectForm {
   payAmount: string
   setPayAmount: Dispatch<SetStateAction<string>>
+
+  payMetadata: PayMetadata | undefined
+  setPayMetadata: Dispatch<SetStateAction<PayMetadata | undefined>>
+
   payInCurrency: CurrencyOption
   setPayInCurrency: Dispatch<SetStateAction<CurrencyOption>>
+
   errorMessage: string
   setErrorMessage: Dispatch<SetStateAction<string>>
 }
@@ -18,11 +32,15 @@ export function usePayProjectForm(): PayProjectForm {
 
   const [payAmount, setPayAmount] = useState<string>('0')
   const [payInCurrency, setPayInCurrency] = useState<CurrencyOption>(ETH)
+  const [payMetadata, setPayMetadata] = useState<PayMetadata | undefined>()
   const [errorMessage, setErrorMessage] = useState<string>('')
 
   return {
     payAmount,
     setPayAmount,
+
+    payMetadata,
+    setPayMetadata,
 
     payInCurrency,
     setPayInCurrency,

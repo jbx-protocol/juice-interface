9303ba06267e313691d15341a82328788def8bb8
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-07 19:00:37 +1030

    Use the delegate store from chain (#2475)

--- src/utils/v2v3/loadV2V3Contract.ts
+++ src/utils/v2v3/loadV2V3Contract.ts
@@ -5,7 +5,6 @@ import { SignerOrProvider } from 'models/signerOrProvider'
 import { V2V3ContractName } from 'models/v2v3/contracts'
 import { loadJBProjectHandlesContract } from './contractLoaders/JBProjectHandles'
 import { loadJBTiered721DelegateProjectDeployerContract } from './contractLoaders/JBTiered721DelegateProjectDeployer'
-import { loadJBTiered721DelegateStoreContract } from './contractLoaders/JBTiered721DelegateStore'
 import { loadJuiceboxV2OrV3Contract } from './contractLoaders/JuiceboxV2OrV3'
 import { loadPublicResolverContract } from './contractLoaders/PublicResolver'
 import { loadJBV1TokenPaymentTerminalContract } from './contractLoaders/V1TokenPaymentTerminal'
@@ -40,8 +39,6 @@ export const loadV2V3Contract = async (
     contractName === V2V3ContractName.JBTiered721DelegateProjectDeployer
   ) {
     contractJson = await loadJBTiered721DelegateProjectDeployerContract()
-  } else if (contractName === V2V3ContractName.JBTiered721DelegateStore) {
-    contractJson = await loadJBTiered721DelegateStoreContract()
   } else if (contractName === V2V3ContractName.JBVeNftDeployer) {
     contractJson = await loadVeNftDeployer()
   } else if (contractName === V2V3ContractName.JBVeTokenUriResolver) {

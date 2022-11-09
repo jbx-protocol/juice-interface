22f5f738619ce3063b8a7e8bfe9fd1b81ac8d334
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-09 21:53:28 +1030

    Pin IPFS images to Infura too

--- src/lib/infura/ipfs.ts
+++ src/lib/infura/ipfs.ts
@@ -1,20 +1,31 @@
 import axios from 'axios'
-// import { OPEN_IPFS_GATEWAY_HOSTNAME } from 'constants/ipfs'
+import { INFURA_IPFS_API_BASE_URL } from 'constants/ipfs'
 
 const INFURA_IPFS_PROJECT_ID = process.env.NEXT_PUBLIC_INFURA_IPFS_PROJECT_ID
 const INFURA_IPFS_API_SECRET = process.env.NEXT_PUBLIC_INFURA_IPFS_API_SECRET
 
-const axiosInstance = axios.create({
-  // baseURL: OPEN_IPFS_GATEWAY_HOSTNAME,
+const AUTH_HEADER = `Basic ${Buffer.from(
+  `${INFURA_IPFS_PROJECT_ID}:${INFURA_IPFS_API_SECRET}`,
+).toString('base64')}`
+
+const infuraApi = axios.create({
+  baseURL: INFURA_IPFS_API_BASE_URL,
   headers: {
-    Authorization:
-      'Basic ' +
-      Buffer.from(
-        `${INFURA_IPFS_PROJECT_ID}:${INFURA_IPFS_API_SECRET}`,
-      ).toString('base64'),
+    Authorization: AUTH_HEADER,
   },
 })
 
+/**
+ * Execute a GET request to a given [url], with the Infura Authorization header set.
+ */
 export function ipfsGet(url: string) {
-  return axiosInstance.get(url)
+  return axios.get(url, {
+    headers: {
+      Authorization: AUTH_HEADER,
+    },
+  })
+}
+
+export function pin(hash: string) {
+  return infuraApi.post(`/api/v0/pin/add?arg=${hash}`)
 }

1f0d5f9758ccafe78b0241f7f4d5af8366da2448
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-09 21:26:41 +1030

    Revert "Pin to infura in the background (#2491)"
    This reverts commit 7454b5e2147443ef6bd09d0469759b2330d77c7e.

--- src/lib/infura/ipfs.ts
+++ src/lib/infura/ipfs.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { OPEN_IPFS_GATEWAY_HOSTNAME } from 'constants/ipfs'
+// import { OPEN_IPFS_GATEWAY_HOSTNAME } from 'constants/ipfs'
 
 const INFURA_IPFS_PROJECT_ID = process.env.NEXT_PUBLIC_INFURA_IPFS_PROJECT_ID
 const INFURA_IPFS_API_SECRET = process.env.NEXT_PUBLIC_INFURA_IPFS_API_SECRET
@@ -17,10 +17,4 @@ const axiosInstance = axios.create({
 
 export function ipfsGet(url: string) {
   return axiosInstance.get(url)
-}
-
-export function pin(hash: string) {
-  return axiosInstance.post(
-    `https://${OPEN_IPFS_GATEWAY_HOSTNAME}/api/v0/pin/add?arg==${hash}`,
-  )
 }

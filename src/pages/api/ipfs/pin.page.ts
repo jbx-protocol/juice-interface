c105561a7afc1113ceeb3992705b948032975a40
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-09 19:02:59 +1030

    Pin to infura in the background (#2491)

--- src/pages/api/ipfs/pin.page.ts
+++ src/pages/api/ipfs/pin.page.ts
@@ -1,4 +1,5 @@
 import { PinataMetadata } from '@pinata/sdk'
+import { pin } from 'lib/infura/ipfs'
 import { getPinata, getPinnedListByTag } from 'lib/pinata'
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -32,7 +33,12 @@ const handler = async (req: ApiRequest, res: NextApiResponse) => {
       const { data, options } = req.body
 
       const pinata = getPinata()
+      // pin on Pinata
       const pinData = await pinata.pinJSONToIPFS(data, options)
+
+      // pin on Infura too.
+      // TODO eventually we should only pin on Infura.
+      pin(pinData.IpfsHash)
 
       return res.status(200).json({
         ...pinData,

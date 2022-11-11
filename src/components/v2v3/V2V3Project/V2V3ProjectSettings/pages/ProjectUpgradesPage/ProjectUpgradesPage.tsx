e8c895ef506fb6cd4b26d0f24230ea231fcb9468
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-12 10:08:05 +1030

    More

--- src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectUpgradesPage/ProjectUpgradesPage.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectUpgradesPage/ProjectUpgradesPage.tsx
@@ -1,15 +1,13 @@
 import { Trans } from '@lingui/macro'
-import { Button, Space } from 'antd'
-import { useState } from 'react'
+import { Button, Space, Statistic } from 'antd'
+import { V2V3ContractsContext } from 'contexts/v2v3/V2V3ContractsContext'
+import { useContext, useState } from 'react'
 
 import { useIsUpgradeAvailable } from './hooks/useIsUpgradeAvailable'
 import { UpgradeWizard } from './UpgradeWizard/UpgradeWizard'
 
-// Uses of `cv` have been commented out: `cv` has changed and is no longer an indicator of a project's contracts version. We could use `project.pv` instead, but `pv` only relates to the JBProjects contract version and only goes up to "2". To get the version number we need, it may need to be a function of both `pv` AND wether or not the project has launched v3 funding cycles. Leaving commented sections here for now as a reference to the page's intial design.
-// - peri
-
 export function ProjectUpgradesPage() {
-  // const { cv } = useContext(ProjectMetadataContext)
+  const { cv } = useContext(V2V3ContractsContext)
 
   const [upgradeWizardOpen, setUpgradeWizardOpen] = useState(false)
 
@@ -20,7 +18,7 @@ export function ProjectUpgradesPage() {
   return (
     <div>
       <Space direction="vertical" size="large">
-        {/* <Statistic title="Current version" value={`V${cv}`} /> */}
+        <Statistic title="Current version" value={`V${cv}`} />
         {!isUpgradeAvailable && (
           <p>
             <Trans>Your project is up to date!</Trans>
--- src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectUpgradesPage/hooks/useIsUpgradeAvailable.ts
+++ src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectUpgradesPage/hooks/useIsUpgradeAvailable.ts
@@ -1,3 +1,9 @@
+import { CV_V3 } from 'constants/cv'
+import { V2V3ContractsContext } from 'contexts/v2v3/V2V3ContractsContext'
+import { useContext } from 'react'
+
 export function useIsUpgradeAvailable() {
-  return true // TODO(@aeolian): implement
+  const { cvs } = useContext(V2V3ContractsContext)
+
+  return !cvs?.find(cv => cv === CV_V3)
 }

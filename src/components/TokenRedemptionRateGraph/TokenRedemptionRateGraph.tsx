1068b5a3ef6c944ecfe620041a918be82a1fb4a2
Wraeth <wraeth.dao@gmail.com>
2022-11-11 16:59:06 +1100

    remove desmos in favor of recharts

--- src/components/TokenRedemptionRateGraph/TokenRedemptionRateGraph.tsx
+++ src/components/TokenRedemptionRateGraph/TokenRedemptionRateGraph.tsx
@@ -1,19 +1,13 @@
-import { Trans } from '@lingui/macro'
+import { t } from '@lingui/macro'
 import { DEFAULT_BONDING_CURVE_RATE_PERCENTAGE } from 'components/formItems/ProjectRedemptionRate'
 import { ThemeContext } from 'contexts/themeContext'
-import {
-  CSSProperties,
-  useCallback,
-  useContext,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react'
+import { CSSProperties, useCallback, useContext, useMemo } from 'react'
+import { CartesianGrid, Label, Line, LineChart, XAxis, YAxis } from 'recharts'
+
+const NUM_POINTS = 10
 
 export const TokenRedemptionRateGraph = ({
   graphSize,
-  graphPad,
   value,
 }: {
   value?: number | undefined
@@ -24,157 +18,84 @@ export const TokenRedemptionRateGraph = ({
     theme: { colors },
   } = useContext(ThemeContext)
 
-  const graphRef = useRef<HTMLDivElement | null>(null)
-
-  const [calculator, setCalculator] = useState<any>() // eslint-disable-line @typescript-eslint/no-explicit-any
-
-  const bondingCurveId = 'bonding-curve'
-  const baseCurveId = 'base-curve'
-
   const labelStyle: CSSProperties = {
     fontSize: '.9rem',
     fontWeight: 500,
   }
 
-  useLayoutEffect(() => {
-    try {
-      // https://www.desmos.com/api/v1.6/docs/index.html
-      setCalculator(
-        Desmos.GraphingCalculator(graphRef.current, {
-          keypad: false,
-          expressions: false,
-          settingsMenu: false,
-          zoomButtons: false,
-          expressionsTopbar: false,
-          pointsOfInterest: false,
-          trace: false,
-          border: false,
-          lockViewport: true,
-          images: false,
-          folders: false,
-          notes: false,
-          sliders: false,
-          links: false,
-          distributions: false,
-          pasteTableData: false,
-          showGrid: false,
-          showXAxis: false,
-          showYAxis: false,
-          xAxisNumbers: false,
-          yAxisNumbers: false,
-          polarNumbers: false,
-        }),
-      )
-    } catch (e) {
-      console.error('Error setting calculator', e)
+  const axisProps = {
+    domain: [0, 10],
+    stroke: colors.stroke.secondary,
+    tick: false,
+  }
+
+  // Calculate the redemption rate using the below formula:
+  // Formula: https://www.desmos.com/calculator/sp9ru6zbpk
+  const calculateRedemptionDataPoints = useCallback((_value?: number) => {
+    if (_value === undefined) return
+    const overflow = NUM_POINTS
+    const supply = NUM_POINTS
+    const normValue = _value / 100
+
+    const dataPoints = []
+    for (let i = 0; i <= supply; i++) {
+      dataPoints.push({
+        x: i,
+        y: overflow * (i / supply) * (normValue + (i - i * normValue) / supply),
+      })
     }
+    return dataPoints
   }, [])
 
-  const graphCurve = useCallback(
-    (_value?: number) => {
-      if (_value === undefined || !calculator) return
-
-      const overflow = 10
-      const supply = 10
-
-      calculator.setMathBounds({
-        left: 0,
-        bottom: 0,
-        right: 10,
-        top: 10,
-      })
-      calculator.removeExpressions([
-        { id: bondingCurveId },
-        { id: baseCurveId },
-      ])
-      calculator.setExpression({
-        id: baseCurveId,
-        latex: `y=x`,
-        color: colors.stroke.secondary,
-      })
-      if (_value === 0) {
-        calculator.setExpression({
-          id: bondingCurveId,
-          latex: `y=0`,
-          color: colors.text.brand.primary,
-        })
-        return
-      }
-      calculator.setExpression({
-        id: bondingCurveId,
-        latex: `y=${overflow} * (x/${supply}) * (${_value / 100} + (x - x${
-          _value / 100
-        })/${supply})`,
-        color: colors.text.brand.primary,
-      })
-    },
-    [calculator, colors.stroke.secondary, colors.text.brand.primary],
+  const initialDataPoints = useMemo(
+    () => calculateRedemptionDataPoints(DEFAULT_BONDING_CURVE_RATE_PERCENTAGE),
+    [calculateRedemptionDataPoints],
   )
 
-  useEffect(
-    () => graphCurve(value ?? DEFAULT_BONDING_CURVE_RATE_PERCENTAGE),
-    [calculator, graphCurve, value],
-  )
+  const dataPoints = calculateRedemptionDataPoints(value)
 
   return (
-    <table
-      style={{ tableLayout: 'fixed', width: graphSize, height: graphSize }}
-    >
-      <tbody>
-        <tr>
-          <td style={{ ...labelStyle }}>
-            <div style={{ display: 'table' }}>
-              <div style={{ padding: '50% 0.5rem', height: 0 }}>
-                <div
-                  style={{
-                    display: 'block',
-                    transformOrigin: 'top left',
-                    transform: 'rotate(-90deg) translate(-100%)',
-                    marginTop: '-50%',
-                    whiteSpace: 'nowrap',
-                  }}
-                >
-                  <Trans>Token redeem value</Trans>
-                </div>
-              </div>
-            </div>
-          </td>
-          <td
-            style={{
-              width: graphSize - graphPad,
-              borderLeft: '1px solid',
-              borderBottom: '1px solid',
-              borderColor: colors.stroke.secondary,
-            }}
-          >
-            <div
-              ref={graphRef}
-              style={{
-                width: graphSize - graphPad,
-                height: graphSize - graphPad,
-              }}
+    <>
+      <LineChart width={graphSize} height={graphSize} data={dataPoints}>
+        <CartesianGrid stroke="1" />
+        <XAxis
+          {...axisProps}
+          dataKey="x"
+          label={
+            <Label
+              value={t`% tokens redeemed`}
+              style={labelStyle}
+              fill={colors.text.primary}
             />
-          </td>
-        </tr>
-        <tr
-          style={{
-            height: graphPad,
-            width: '100%',
-            textAlign: 'center',
-            ...labelStyle,
-          }}
-        >
-          <td style={{ width: graphPad }} />
-          <td
-            style={{
-              whiteSpace: 'nowrap',
-              textAlign: 'center',
-            }}
-          >
-            <Trans>% tokens redeemed</Trans>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          }
+        />
+        <YAxis
+          {...axisProps}
+          dataKey="y"
+          label={
+            <Label
+              transform="rotate(-90)"
+              value={t`Token redeem value`}
+              angle={-90}
+              style={labelStyle}
+              fill={colors.text.primary}
+            />
+          }
+        />
+        <Line
+          type="monotone"
+          data={initialDataPoints}
+          dataKey="y"
+          stroke={colors.stroke.secondary}
+          strokeWidth={2}
+        />
+        <Line
+          type="monotone"
+          dataKey="y"
+          stroke={colors.text.brand.primary}
+          strokeWidth={3}
+        />
+      </LineChart>
+    </>
   )
 }
--- src/components/common/Head/scripts/DesmosScript.tsx
+++
@@ -1,8 +0,0 @@
-export function DesmosScript() {
-  return (
-    <script
-      async
-      src="/vendor/desmos/calculator.min.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"
-    ></script>
-  )
-}

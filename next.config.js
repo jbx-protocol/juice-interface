20c6c37e8fc5c1c7eeffd285e96597211600b767
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-12 12:44:19 +1030

    Fix walletconnect csp once and for all

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 const webpack = require('webpack')
 
+const WALLET_CONNECT_URLS = [
+  'https://*.walletconnect.com',
+  'https://*.walletconnect.org',
+  'wss://*.walletconnect.org',
+].join(' ')
+
 const ContentSecurityPolicy = `
   default-src 'none';
   script-src 'self' https://static.hotjar.com https://script.hotjar.com https://*.juicebox.money 'unsafe-inline' 'unsafe-eval';
   style-src 'self' https://fonts.googleapis.com 'unsafe-inline';
   font-src 'self' https://fonts.gstatic.com data:;
   img-src 'self' https://*.juicebox.money https://juicebox.money https://*.infura-ipfs.io https://jbx.mypinata.cloud data:;
-  connect-src 'self' https://*.juicebox.money https://juicebox.money https://*.infura.io https://*.infura-ipfs.io https://jbx.mypinata.cloud https://api.studio.thegraph.com https://gateway.thegraph.com https://api.arcx.money https://api.tenderly.co https://*.hotjar.com https://*.hotjar.io wss://*.hotjar.com https://*.gnosis.io https://*.safe.global https://*.snapshot.org https://*.wallet.coinbase.com https://*.walletconnect.com https://*.walletconnect.org;
+  connect-src 'self' https://*.juicebox.money https://juicebox.money https://*.infura.io https://*.infura-ipfs.io https://jbx.mypinata.cloud https://api.studio.thegraph.com https://gateway.thegraph.com https://api.arcx.money https://api.tenderly.co https://*.hotjar.com https://*.hotjar.io wss://*.hotjar.com https://*.gnosis.io https://*.safe.global https://*.snapshot.org https://*.wallet.coinbase.com ${WALLET_CONNECT_URLS};
   manifest-src 'self';
   prefetch-src 'self';
   frame-src 'self' https://vars.hotjar.com/ https://gnosis-safe.io https://app.safe.global;

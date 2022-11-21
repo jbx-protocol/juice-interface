8b83ee014edac8b492f6d09526b9ffc3dd584378
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-22 08:24:58 +1030

    Set origin better

--- src/lib/infura/ipfs.ts
+++ src/lib/infura/ipfs.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { INFURA_IPFS_API_BASE_URL } from 'constants/ipfs'
-import { getBaseUrl } from 'utils/baseUrl'
+import { readNetwork } from 'constants/networks'
+import { NetworkName } from 'models/network-name'
 
 const INFURA_IPFS_PROJECT_ID = process.env.INFURA_IPFS_PROJECT_ID
 const INFURA_IPFS_API_SECRET = process.env.INFURA_IPFS_API_SECRET
@@ -9,11 +10,22 @@ const AUTH_HEADER = `Basic ${Buffer.from(
   `${INFURA_IPFS_PROJECT_ID}:${INFURA_IPFS_API_SECRET}`,
 ).toString('base64')}`
 
+const DEV_ORIGIN = 'http://localhost:3000'
+const GOERLI_ORIGIN = 'http://goerli.juicebox.money'
+const MAINNET_ORIGIN = 'https://juicebox.money'
+
+const origin =
+  process.env.NODE_ENV === 'development'
+    ? DEV_ORIGIN
+    : readNetwork.name === NetworkName.goerli
+    ? GOERLI_ORIGIN
+    : MAINNET_ORIGIN
+
 const infuraApi = axios.create({
   baseURL: INFURA_IPFS_API_BASE_URL,
   headers: {
     Authorization: AUTH_HEADER,
-    origin: getBaseUrl()?.origin ?? 'https://juicebox.money',
+    origin,
   },
 })
 

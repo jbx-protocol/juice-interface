8514d4953edefc38eb7fd52bbb2aff0edccf6697
Wraeth <wraeth.dao@gmail.com>
2022-11-25 14:13:04 +1100

    Add class to body

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,14 +1,25 @@
 import { ThemeContext } from 'contexts/themeContext'
 import { useJuiceTheme } from 'hooks/JuiceTheme'
+import { useLayoutEffect } from 'react'
 
 export const ThemeProvider: React.FC = ({ children }) => {
   const juiceTheme = useJuiceTheme()
   const isDarkMode = juiceTheme.isDarkMode
 
+  // Set the theme on the body element
+  // This is needed for tailwind css dark theme classes to work
+  useLayoutEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  }, [isDarkMode])
+
   return (
     <ThemeContext.Provider value={juiceTheme}>
       {/* Set the dark mode flag if dark mode is enabled */}
-      <div className={isDarkMode ? 'dark' : undefined}>{children}</div>
+      <div>{children}</div>
     </ThemeContext.Provider>
   )
 }

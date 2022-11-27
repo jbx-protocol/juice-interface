d06cd6cbe08a224df0a79f825abb93aa73de3a89
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-27 22:57:17 +1030

    Utilize useEnsName hook in FormattedAddress

--- src/components/FormattedAddress.tsx
+++ src/components/FormattedAddress.tsx
@@ -1,85 +1,9 @@
-import { isAddress } from '@ethersproject/address'
 import { Tooltip } from 'antd'
-import { CSSProperties, MouseEventHandler, useEffect, useState } from 'react'
+import { CSSProperties, MouseEventHandler } from 'react'
 import CopyTextButton from 'components/CopyTextButton'
 import EtherscanLink from 'components/EtherscanLink'
 import { truncateEthAddress } from 'utils/format/formatAddress'
-import { SECONDS_IN_DAY } from 'constants/numbers'
-import { readProvider } from 'constants/readProvider'
-
-type EnsRecord = {
-  name: string | null
-  expires: number
-}
-
-const ENS_LOCALSTORAGE_KEY = `jb_ensDict_${readProvider.network.chainId}`
-
-const getEnsDict = () => {
-  if (typeof window === 'undefined') return {}
-
-  try {
-    return JSON.parse(
-      window.localStorage.getItem(ENS_LOCALSTORAGE_KEY) ?? '{}',
-    ) as Record<string, EnsRecord>
-  } catch (e) {
-    console.warn('ENS storage not found', e)
-    return {}
-  }
-}
-
-const cacheEnsRecord = (address: string, record: EnsRecord) => {
-  window.localStorage?.setItem(
-    ENS_LOCALSTORAGE_KEY,
-    JSON.stringify({
-      ...getEnsDict(),
-      [address]: record,
-    }),
-  )
-}
-
-const resolveAddress = async (address: string) => {
-  const name = await readProvider.lookupAddress(address)
-
-  // Reverse lookup to check validity
-  if (
-    name &&
-    (await readProvider.resolveName(name))?.toLowerCase() ===
-      address.toLowerCase()
-  ) {
-    return name
-  }
-}
-
-const getEnsRecord = async (
-  address: string,
-): Promise<EnsRecord | undefined> => {
-  const now = new Date().valueOf()
-
-  // Try getting from cache first
-  const cache = getEnsDict()
-  const record = cache[address]
-  if (record?.expires > now) {
-    return record
-  }
-
-  // If no cache hit, resolve address
-  try {
-    const name = await resolveAddress(address)
-    if (!name) return
-
-    const newRecord = {
-      name,
-      expires: now + SECONDS_IN_DAY * 1000, // Expires in one day
-    }
-
-    // cache the newly resolved address
-    cacheEnsRecord(address, newRecord)
-
-    return newRecord
-  } catch (e) {
-    return
-  }
-}
+import { useEnsName } from 'hooks/ensName'
 
 export default function FormattedAddress({
   address,
@@ -96,20 +20,7 @@ export default function FormattedAddress({
   style?: CSSProperties
   onClick?: MouseEventHandler
 }) {
-  const [ensName, setEnsName] = useState<string | null>()
-
-  useEffect(() => {
-    async function loadEnsName() {
-      if (!address || !isAddress(address)) return
-
-      const ensRecord = await getEnsRecord(address.toLowerCase())
-      if (!ensRecord) return
-
-      setEnsName(ensRecord.name)
-    }
-
-    loadEnsName()
-  }, [address])
+  const ensName = useEnsName(address)
 
   if (!address) return null
 

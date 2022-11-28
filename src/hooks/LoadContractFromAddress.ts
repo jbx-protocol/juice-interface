0aa3904c55bcdf932947c7cbf08f7d920d067321
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-11-28 11:13:46 +1030

    Use single source of truch read provider (#2581)

--- src/hooks/LoadContractFromAddress.ts
+++ src/hooks/LoadContractFromAddress.ts
@@ -1,5 +1,6 @@
 import * as constants from '@ethersproject/constants'
 import { Contract, ContractInterface } from '@ethersproject/contracts'
+import { readProvider } from 'constants/readProvider'
 import { isAddress } from 'ethers/lib/utils'
 import { useEffect, useState } from 'react'
 import { useWallet } from './Wallet'
@@ -13,7 +14,7 @@ export const useLoadContractFromAddress = <ABI extends ContractInterface>({
 }) => {
   const [contract, setContract] = useState<Contract>()
 
-  const { signer, provider } = useWallet()
+  const { signer } = useWallet()
 
   const isInputAddressValid = (
     address: string | undefined,
@@ -29,8 +30,8 @@ export const useLoadContractFromAddress = <ABI extends ContractInterface>({
       return setContract(undefined)
     }
 
-    setContract(new Contract(address, abi, signer ?? provider))
-  }, [address, abi, signer, provider])
+    setContract(new Contract(address, abi, signer ?? readProvider))
+  }, [address, abi, signer])
 
   return contract
 }

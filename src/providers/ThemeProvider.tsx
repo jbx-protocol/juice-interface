5bfb292e59828786f593509c1278f9294b64822b
Wraeth <wraeth.dao@gmail.com>
2022-11-29 16:38:16 +1100

    move to juiceTheme hook

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,20 +1,8 @@
 import { ThemeContext } from 'contexts/themeContext'
 import { useJuiceTheme } from 'hooks/JuiceTheme'
-import { useLayoutEffect } from 'react'
 
 export const ThemeProvider: React.FC = ({ children }) => {
   const juiceTheme = useJuiceTheme()
-  const isDarkMode = juiceTheme.isDarkMode
-
-  // Set the theme on the body element
-  // This is needed for tailwind css dark theme classes to work
-  useLayoutEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-    }
-  }, [isDarkMode])
 
   return (
     <ThemeContext.Provider value={juiceTheme}>

8ab49eb129d4698ec4071548a297eabb26c2e5c2
peripheralist <79433522+peripheralist@users.noreply.github.com>
2022-12-10 21:47:21 -0500

    Cleanup quick project search (#2653)
    * cleanup quick project search. debounce search when typing. clear timers. better cmd + k behavior
    
    * -
    
    * -

--- src/components/QuickProjectSearch.tsx
+++ src/components/QuickProjectSearch.tsx
@@ -1,5 +1,5 @@
 import { ArrowRightOutlined, CloseCircleOutlined } from '@ant-design/icons'
-import { t } from '@lingui/macro'
+import { t, Trans } from '@lingui/macro'
 import Input from 'antd/lib/input/Input'
 import Modal from 'antd/lib/modal/Modal'
 import { PV_V2 } from 'constants/pv'
@@ -16,13 +16,26 @@ import V2V3ProjectHandleLink from './v2v3/shared/V2V3ProjectHandleLink'
 const HOT_KEY = 'k'
 const INPUT_ID = 'quickProjectSearch'
 const MAX_RESULTS = 8
+const DEBOUNCE_MILLIS = 250
 
 export default function QuickProjectSearch() {
+  const [inputText, setInputText] = useState<string>()
   const [searchText, setSearchText] = useState<string>()
   const [highlightIndex, setHighlightIndex] = useState<number>()
-  const [spotlightActive, setSpotlightActive] = useState<boolean>()
+  const [modalVisible, setModalVisible] = useState<boolean>()
 
   const router = useRouter()
+
+  // Debounce typing. Only set new `searchText` if `inputText` doesn't change for `DEBOUNCE_MILLIS`
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchText(inputText)
+    }, DEBOUNCE_MILLIS)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [inputText])
 
   const { data: searchPages, isLoading: isLoadingSearch } =
     useProjectsSearch(searchText)
@@ -40,17 +53,17 @@ export default function QuickProjectSearch() {
   }, [router, searchPages, highlightIndex])
 
   const reset = useCallback(() => {
-    setSpotlightActive(false)
+    setModalVisible(false)
     setHighlightIndex(undefined)
     setSearchText(undefined)
   }, [])
 
-  // Hot key listener
+  // Hot key listener to open modal
   useEffect(() => {
     const listener = (e: KeyboardEvent) => {
       if (e.isComposing) return // i think right idk
 
-      if (spotlightActive) {
+      if (modalVisible) {
         switch (e.key) {
           case 'escape':
             reset()
@@ -62,24 +75,32 @@ export default function QuickProjectSearch() {
             break
         }
       } else if (e.key === HOT_KEY && (e.metaKey || e.ctrlKey)) {
-        setSpotlightActive(true)
-        setTimeout(
-          () =>
-            // delay to wait for render when opening modal
-            (
-              document.getElementById(INPUT_ID) as HTMLInputElement | null
-            )?.focus(),
-          100,
-        )
+        setModalVisible(true)
       }
     }
 
     window.addEventListener('keydown', listener)
 
-    return () => window.removeEventListener('keydown', listener)
-  }, [spotlightActive, searchText, router, goToProject, reset])
+    return () => {
+      window.removeEventListener('keydown', listener)
+    }
+  }, [modalVisible, searchText, router, goToProject, reset])
 
-  // Arrow key up/down / tab listener
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // timeout to wait for dom render after opening modal
+
+      if (modalVisible) {
+        ;(document.getElementById(INPUT_ID) as HTMLInputElement | null)?.focus()
+      }
+    }, 0)
+
+    return () => {
+      if (timer) clearTimeout(timer)
+    }
+  }, [modalVisible])
+
+  // Arrow key up/down & tab listener
   useEffect(() => {
     const listener = (e: KeyboardEvent) => {
       if (!searchPages || isLoadingSearch) return
@@ -95,15 +116,15 @@ export default function QuickProjectSearch() {
           )
           break
       }
-
-      e.stopPropagation()
     }
 
     window.addEventListener('keydown', listener)
 
-    return () => window.removeEventListener('keydown', listener)
+    return () => {
+      window.removeEventListener('keydown', listener)
+    }
   }, [
-    spotlightActive,
+    modalVisible,
     searchText,
     router,
     goToProject,
@@ -114,7 +135,7 @@ export default function QuickProjectSearch() {
   return (
     <Modal
       centered
-      open={spotlightActive}
+      open={modalVisible}
       onCancel={reset}
       okButtonProps={{ hidden: true }}
       cancelButtonProps={{ hidden: true }}
@@ -124,48 +145,58 @@ export default function QuickProjectSearch() {
       bodyStyle={{ padding: 0 }}
     >
       <div className="flex items-center gap-5 p-5">
-        <CloseCircleOutlined onClick={() => setSpotlightActive(false)} />
+        <CloseCircleOutlined onClick={() => setModalVisible(false)} />
         <Input
           id={INPUT_ID}
           placeholder={t`Find a project`}
-          onChange={e => setSearchText(e.target.value)}
+          onChange={e => setInputText(e.target.value)}
         />
       </div>
-      {isLoadingSearch && <Loading />}
-      {!!searchPages?.length && (
-        <div className="flex flex-col pb-5">
-          {searchPages.slice(0, MAX_RESULTS).map((p, i) => (
-            <div
-              key={p.id}
-              className={twMerge(
-                'flex cursor-pointer items-baseline gap-2 py-2 px-5',
-                highlightIndex === i ? 'bg-smoke-75 dark:bg-slate-600' : '',
-              )}
-              onClick={goToProject}
-              onMouseEnter={() => setHighlightIndex(i)}
-            >
-              {p.pv === PV_V2 ? (
-                <V2V3ProjectHandleLink
-                  projectId={p.projectId}
-                  handle={p.handle}
-                />
-              ) : (
-                <V1ProjectHandle projectId={p.projectId} handle={p.handle} />
-              )}
 
-              <div style={{ flex: 1 }}>
-                <ProjectVersionBadge
-                  transparent
-                  size="small"
-                  versionText={`V${p.pv}`}
-                />
+      <div hidden={!searchText} className="pb-5">
+        {isLoadingSearch && <Loading />}
+
+        {!!searchPages?.length && (
+          <div className="flex flex-col">
+            {searchPages.slice(0, MAX_RESULTS).map((p, i) => (
+              <div
+                key={p.id}
+                className={twMerge(
+                  'flex cursor-pointer items-baseline gap-2 py-2 px-5',
+                  highlightIndex === i ? 'bg-smoke-75 dark:bg-slate-600' : '',
+                )}
+                onClick={goToProject}
+                onMouseEnter={() => setHighlightIndex(i)}
+              >
+                {p.pv === PV_V2 ? (
+                  <V2V3ProjectHandleLink
+                    projectId={p.projectId}
+                    handle={p.handle}
+                  />
+                ) : (
+                  <V1ProjectHandle projectId={p.projectId} handle={p.handle} />
+                )}
+
+                <div style={{ flex: 1 }}>
+                  <ProjectVersionBadge
+                    transparent
+                    size="small"
+                    versionText={`V${p.pv}`}
+                  />
+                </div>
+
+                {highlightIndex === i && <ArrowRightOutlined />}
               </div>
+            ))}
+          </div>
+        )}
 
-              {highlightIndex === i && <ArrowRightOutlined />}
-            </div>
-          ))}
-        </div>
-      )}
+        {searchText && !isLoadingSearch && searchPages?.length === 0 && (
+          <div className="text-center text-grey-400">
+            <Trans>No results</Trans>
+          </div>
+        )}
+      </div>
     </Modal>
   )
 }

4f9a3f76ce0f123b5e91e9a5d8ab9c93eab336dc
Johnny D <johnnyd.eth@protonmail.com>
2022-12-15 11:52:28 +1000

    add deployed split allocators and enable feature flag

--- src/constants/contracts/mainnet/Allocators.ts
+++ src/constants/contracts/mainnet/Allocators.ts
@@ -1,5 +1,7 @@
 import * as constants from '@ethersproject/constants'
 
-export const V2_V3_ALLOCATOR_ADDRESS = 'TODO_V2_V3_ALLOCATOR_ADDRESS'
-export const V1_V3_ALLOCATOR_ADDRESS = 'TODO_V1_V3_ALLOCATOR_ADDRESS'
+export const V2_V3_ALLOCATOR_ADDRESS =
+  '0x6aaF7afeF64c6852EE507876f9D25F92bd9A1aE7'
+export const V1_V3_ALLOCATOR_ADDRESS =
+  '0x1eb759829b1a3d55193472142f18df3091BcAc4B'
 export const NULL_ALLOCATOR_ADDRESS = constants.AddressZero

ae44e33e789f8ae6a1410481b36aff7ba532df22
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-01-04 12:29:54 +1030

    Add some nft support to redeem activity

--- src/components/activityEventElems/RedeemEventElem.tsx
+++ src/components/activityEventElems/RedeemEventElem.tsx
@@ -1,10 +1,11 @@
-import { t, Trans } from '@lingui/macro'
+import { plural, t, Trans } from '@lingui/macro'
 import ETHAmount from 'components/currency/ETHAmount'
 import { ThemeContext } from 'contexts/themeContext'
 import { V1ProjectContext } from 'contexts/v1/projectContext'
 import { RedeemEvent } from 'models/subgraph-entities/vX/redeem-event'
 import { useContext } from 'react'
 import { formatWad } from 'utils/format/formatNumber'
+import { decodeJB721DelegateRedeemMetadata } from 'utils/nftRewards'
 import { tokenSymbolText } from 'utils/tokenSymbolText'
 
 import { ActivityEvent } from './ActivityElement'
@@ -23,6 +24,7 @@ export default function RedeemEventElem({
         | 'timestamp'
         | 'returnAmount'
         | 'terminal'
+        | 'metadata'
       >
     | undefined
 }) {
@@ -33,18 +35,39 @@ export default function RedeemEventElem({
 
   if (!event) return null
 
+  const [, , redeemedTokenIds] = decodeJB721DelegateRedeemMetadata(
+    event.metadata,
+  )
+
+  const redeemedTokens = `${formatWad(event.amount, {
+    precision: 0,
+  })} ${tokenSymbolText({
+    tokenSymbol: tokenSymbol,
+    capitalize: false,
+    plural: true,
+  })}`
+
   return (
     <ActivityEvent
       event={event}
       header={t`Redeemed`}
       subject={
         <div style={{ fontSize: primaryContentFontSize }}>
-          {formatWad(event.amount, { precision: 0 })}{' '}
-          {tokenSymbolText({
-            tokenSymbol: tokenSymbol,
-            capitalize: false,
-            plural: true,
-          })}
+          {redeemedTokenIds.length > 0 ? (
+            <div>
+              <div>
+                {plural(redeemedTokenIds.length, {
+                  one: '# NFT',
+                  other: '# NFTs',
+                })}
+              </div>
+              <div className="text-xs text-grey-500 dark:text-grey-300">
+                {redeemedTokens}
+              </div>
+            </div>
+          ) : (
+            <span>{redeemedTokens}</span>
+          )}
         </div>
       }
       extra={

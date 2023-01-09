0ba59c4898ccb852176e06c1a2351912d86426f6
Wraeth <wraeth.dao@gmail.com>
2023-01-09 11:28:16 +1100

    wip

--- src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/V1V2TokenMigrationSettingsPage/V1TokenMigrationSetupSection/V1TokenMigrationSetupModal/V1TokenMigrationSetupModal.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/V1V2TokenMigrationSettingsPage/V1TokenMigrationSetupSection/V1TokenMigrationSetupModal/V1TokenMigrationSetupModal.tsx
@@ -1,74 +1,140 @@
 import { CheckCircleFilled } from '@ant-design/icons'
 import { Trans } from '@lingui/macro'
-import { Modal, ModalProps, Space } from 'antd'
+import { Modal, ModalProps } from 'antd'
 import { Callout } from 'components/Callout'
+import Loading from 'components/Loading'
+import { ProjectVersionBadge } from 'components/ProjectVersionBadge'
 import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
-import { useV1ProjectIdOfV2Project } from 'hooks/v2v3/contractReader/V1ProjectIdOfV2Project'
-import { useHasV1TokenPaymentTerminal } from 'hooks/v2v3/hasV1TokenPaymentTerminal'
-import { useContext, useState } from 'react'
-
-import { AddTerminalSection } from './AddTerminalSection'
-import { SetV1ProjectSection } from './SetV1ProjectSection'
+import useProjectToken from 'hooks/v2v3/contractReader/ProjectToken'
+import { V1UserProvider } from 'providers/v1/UserProvider'
+import { useCallback, useContext, useState } from 'react'
+import { AddTerminalSection as DeployMigrationTokenSection } from './DeployMigrationTokenSection'
+import { SetMigratedTokenSection } from './SetMigratedTokenSection'
+import * as constants from '@ethersproject/constants'
+import FormattedAddress from 'components/FormattedAddress'
+import CopyTextButton from 'components/CopyTextButton'
 
 export function V1TokenMigrationSetupModal({ ...props }: ModalProps) {
   const { projectId } = useContext(ProjectMetadataContext)
-  const [
-    migrationTerminalSectionComplete,
-    setMigrationTerminalSectionComplete,
-  ] = useState<boolean>(false)
-  const [v1ProjectSectionComplete, setV1ProjectSectionComplete] =
-    useState<boolean>(false)
+  const [deployedMigrationToken, setDeployedMigrationToken] = useState<string>()
+  const [migrationTokenSet, setMigrationTokenSet] = useState<boolean>(false)
+  useState<boolean>(false)
 
-  const hasMigrationTerminal =
-    useHasV1TokenPaymentTerminal() || migrationTerminalSectionComplete
-  const { data: v1Project } = useV1ProjectIdOfV2Project(projectId)
-  const hasSetV1Project =
-    Boolean(v1Project?.toNumber()) || v1ProjectSectionComplete
+  // Check if token already set
+  const { data: tokenAddress, loading: tokenAddressLoading } = useProjectToken({
+    projectId,
+  })
 
-  const completed = hasMigrationTerminal && hasSetV1Project
+  const onDeployMigrationTokenCompleted = useCallback(
+    (deployedMigrationToken: string) => {
+      setDeployedMigrationToken(deployedMigrationToken)
+    },
+    [],
+  )
+
+  const tokenAlreadySet =
+    !!tokenAddress && tokenAddress !== constants.AddressZero
+
+  const completed = !!deployedMigrationToken && migrationTokenSet
 
   return (
     <Modal
-      title={<Trans>Set up V1 token migration</Trans>}
+      width={600}
+      title={
+        <span className="text-lg text-black dark:text-slate-100">
+          <Trans>Set up token migration</Trans>
+        </span>
+      }
       okButtonProps={{ hidden: true }}
       cancelText={<Trans>Close</Trans>}
       {...props}
     >
-      <p className="mb-8">
-        <Trans>
-          Set up your Juicebox V2 project for migration from your Juicebox V1
-          project.
-        </Trans>
-      </p>
+      {tokenAddressLoading ? (
+        <Loading />
+      ) : tokenAlreadySet ? (
+        <>
+          <Trans>
+            The <ProjectVersionBadge.V3 /> has already been set for this
+            project.
+          </Trans>
+        </>
+      ) : (
+        <>
+          <p className="mb-8">
+            <Trans>
+              Set up the option for user token migration from{' '}
+              <ProjectVersionBadge.V1 /> or <ProjectVersionBadge.V2 /> projects
+              to <ProjectVersionBadge.V3 />.
+            </Trans>
+          </p>
 
-      <Space direction="vertical" size="large">
-        <AddTerminalSection
-          completed={hasMigrationTerminal}
-          onCompleted={() => setMigrationTerminalSectionComplete(true)}
-        />
-        <SetV1ProjectSection
-          completed={hasSetV1Project}
-          disabled={!hasMigrationTerminal}
-          onCompleted={() => setV1ProjectSectionComplete(true)}
-        />
+          <div className="flex flex-col gap-6">
+            {/* Required to read v1 project ids from handles */}
+            <V1UserProvider>
+              <DeployMigrationTokenSection
+                completed={!!deployedMigrationToken}
+                onCompleted={onDeployMigrationTokenCompleted}
+              />
+            </V1UserProvider>
 
-        {completed && (
-          <Callout
-            className="bg-smoke-75 dark:bg-slate-400"
-            iconComponent={<CheckCircleFilled />}
-          >
-            <p>
-              <Trans>You're all set!</Trans> 🎉
-            </p>
-            <p className="m-0">
-              <Trans>
-                V1 token holders can swap their tokens for your V2 tokens on
-                your V2 project's <strong>Tokens</strong> section.
-              </Trans>
-            </p>
-          </Callout>
-        )}
-      </Space>
+            {!!deployedMigrationToken && (
+              <>
+                <Callout.Info collapsible={false}>
+                  <p>
+                    <Trans>
+                      Migration token was successfully deployed to{' '}
+                      <FormattedAddress
+                        truncateTo={16}
+                        tooltipDisabled
+                        address={deployedMigrationToken}
+                      />
+                    </Trans>
+                    <CopyTextButton
+                      className="ml-1"
+                      value={deployedMigrationToken}
+                    />
+                  </p>
+                </Callout.Info>
+                <Callout.Warning collapsible={false}>
+                  <p>
+                    Please make sure you make a copy of this address if you are
+                    not ready to complete step 2.{' '}
+                  </p>
+                  <p>
+                    <strong>
+                      Once you navigate away from this page, this token will be
+                      lost and very hard to recover.
+                    </strong>
+                  </p>
+                </Callout.Warning>
+              </>
+            )}
+
+            <SetMigratedTokenSection
+              deployedMigrationToken={deployedMigrationToken}
+              completed={completed}
+              onCompleted={() => setMigrationTokenSet(true)}
+            />
+
+            {completed && (
+              <Callout
+                className="bg-smoke-75 dark:bg-slate-400"
+                iconComponent={<CheckCircleFilled />}
+              >
+                <p>
+                  <Trans>You're all set!</Trans> 🎉
+                </p>
+                <p className="m-0">
+                  <Trans>
+                    V1 token holders can swap their tokens for your V2 tokens on
+                    your V2 project's <strong>Tokens</strong> section.
+                  </Trans>
+                </p>
+              </Callout>
+            )}
+          </div>
+        </>
+      )}
     </Modal>
   )
 }

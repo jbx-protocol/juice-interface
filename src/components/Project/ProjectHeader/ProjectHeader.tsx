93183e3cf35e779fb7683eafdd9ae5c67aa73319
benjamin <benschac@gmail.com>
2023-01-09 21:35:29 -0500

    Update project header social buttons (#2721)
    Co-authored-by: Tom Quirk <12551741+tomquirk@users.noreply.github.com>

--- src/components/Project/ProjectHeader/ProjectHeader.tsx
+++ src/components/Project/ProjectHeader/ProjectHeader.tsx
@@ -1,15 +1,13 @@
 import { t, Trans } from '@lingui/macro'
-import { Tooltip } from 'antd'
+import { Divider, Space, Tooltip } from 'antd'
 import { Badge } from 'components/Badge'
 import FormattedAddress from 'components/FormattedAddress'
 import Paragraph from 'components/Paragraph'
 import { GnosisSafeBadge } from 'components/Project/ProjectHeader/GnosisSafeBadge'
 import ProjectLogo from 'components/ProjectLogo'
 import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
-import useMobile from 'hooks/Mobile'
 import { useGnosisSafe } from 'hooks/safe/GnosisSafe'
 import { useContext } from 'react'
-import { classNames } from 'utils/classNames'
 import { EditProjectHandleButton } from './EditProjectHandleButton'
 import SocialLinks from './SocialLinks'
 
@@ -30,11 +28,14 @@ export function ProjectHeader({
     ProjectMetadataContext,
   )
 
-  const isMobile = useMobile()
   const { data: gnosisSafe, isLoading: gnosisSafeLoading } =
     useGnosisSafe(projectOwnerAddress)
 
   const projectTitle = projectMetadata?.name || t`Untitled project`
+  const hasSocialLinks =
+    projectMetadata?.discord ||
+    projectMetadata?.twitter ||
+    projectMetadata?.infoUri
 
   return (
     <header className="flex flex-wrap items-start justify-between">
@@ -48,79 +49,81 @@ export function ProjectHeader({
       </div>
 
       <div className="min-w-[70%] flex-1">
-        <div className="flex flex-wrap items-start justify-between">
-          <div
-            className={classNames(
-              'flex items-center',
-              isMobile ? 'max-w-full' : 'max-w-[75%]',
-            )}
-          >
-            <h1
-              className={classNames(
-                'm-0 overflow-hidden text-ellipsis text-4xl',
-                projectMetadata?.name
-                  ? 'text-black dark:text-slate-100'
-                  : 'text-grey-400 dark:text-grey-600',
-              )}
-              title={projectTitle}
-            >
-              {projectTitle}
-            </h1>
-            {isArchived && (
-              <Badge className="ml-4" upperCase variant="warning">
-                <Trans>Archived</Trans>
-              </Badge>
-            )}
-          </div>
+        <Space direction="vertical" className="w-full">
+          <div className="flex flex-wrap items-start justify-between gap-y-2">
+            <div className="max-w-md md:max-w-xl">
+              <div className="flex items-center">
+                <h1
+                  className="mb-2 overflow-hidden text-ellipsis text-4xl text-black dark:text-slate-100"
+                  title={projectTitle}
+                >
+                  {projectTitle}
+                </h1>
+                {isArchived && (
+                  <div>
+                    <Badge className="ml-4" upperCase variant="warning">
+                      <Trans>Archived</Trans>
+                    </Badge>
+                  </div>
+                )}
+              </div>
 
-          {actions ?? null}
-        </div>
+              <div className="flex items-baseline gap-x-5">
+                <span className="font-medium text-grey-600 dark:text-grey-300">
+                  {handle ? (
+                    <Tooltip title={t`Project ID: ${projectId}`}>
+                      <span>@{handle}</span>
+                    </Tooltip>
+                  ) : (
+                    <Trans>Project #{projectId}</Trans>
+                  )}
+                </span>
 
-        <div className="flex flex-wrap items-baseline gap-x-5 pt-2 pb-1 font-medium">
-          <span className="font-medium text-grey-500 dark:text-grey-300">
-            {handle ? (
-              <Tooltip title={t`Project ID: ${projectId}`}>
-                <span>@{handle}</span>
-              </Tooltip>
-            ) : (
-              <Trans>Project #{projectId}</Trans>
-            )}
-          </span>
+                {!handle && canEditProjectHandle && projectId ? (
+                  <EditProjectHandleButton />
+                ) : null}
+              </div>
+            </div>
 
-          {!handle && canEditProjectHandle && projectId ? (
-            <EditProjectHandleButton />
-          ) : null}
-
-          <SocialLinks
-            discord={projectMetadata?.discord}
-            twitter={projectMetadata?.twitter}
-            infoUri={projectMetadata?.infoUri}
-          />
-        </div>
-
-        {projectMetadata?.description && !hideDescription && (
-          <Paragraph
-            className="text-grey-900 dark:text-slate-100"
-            description={projectMetadata.description}
-            characterLimit={250}
-          />
-        )}
-
-        {projectOwnerAddress && (
-          <div className="mt-2 flex items-center">
-            <span className="mr-2">
-              <Trans>
-                Owned by <FormattedAddress address={projectOwnerAddress} />
-              </Trans>
-            </span>
-            {!gnosisSafeLoading && gnosisSafe && (
-              <GnosisSafeBadge
-                safe={gnosisSafe}
-                href={`${window.location.href}/safe`}
+            <div className="flex items-center">
+              <SocialLinks
+                discord={projectMetadata?.discord}
+                twitter={projectMetadata?.twitter}
+                infoUri={projectMetadata?.infoUri}
               />
-            )}
+
+              {hasSocialLinks && actions ? (
+                <Divider type="vertical" className="mx-5 h-9 md:h-8" />
+              ) : null}
+
+              {actions ?? null}
+            </div>
           </div>
-        )}
+
+          {projectMetadata?.description && !hideDescription && (
+            <Paragraph
+              className="text-grey-900 dark:text-slate-100"
+              description={projectMetadata.description}
+              characterLimit={250}
+            />
+          )}
+
+          {projectOwnerAddress && (
+            <div className="flex items-center">
+              <span className="mr-2">
+                <Trans>
+                  Owned by <FormattedAddress address={projectOwnerAddress} />
+                </Trans>
+              </span>
+              {!gnosisSafeLoading && gnosisSafe && (
+                <GnosisSafeBadge
+                  safe={gnosisSafe}
+                  href={`${window.location.href}/safe`}
+                />
+              )}
+            </div>
+          )}
+        </Space>
       </div>
     </header>
   )

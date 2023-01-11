d22c11dae5f017b057e8487acceea1b876f14602
Filip V <f@filip.world>
2023-01-10 23:58:51 -0500

    Upgrade v1/v2v3 project activity CSV exports (#2632)
    * Add additional CSV exports for projects
    
    * Use space and icons in activity modals
    
    * Rename csv download helper file
    
    Co-authored-by: Tom Quirk <12551741+tomquirk@users.noreply.github.com>

--- src/components/v2v3/V2V3Project/modals/V2V3DownloadActivityModal.tsx
+++ src/components/v2v3/V2V3Project/modals/V2V3DownloadActivityModal.tsx
@@ -1,35 +1,107 @@
 import { t, Trans } from '@lingui/macro'
-import { Button, Modal, ModalProps } from 'antd'
-import { useState } from 'react'
-
-import V2V3DownloadPaymentsModal from './V2V3DownloadPaymentsModal'
+import { Button, Modal, ModalProps, Space } from 'antd'
+import { readProvider } from 'constants/readProvider'
+import { useEffect, useState, useContext } from 'react'
+import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
+import InputAccessoryButton from 'components/InputAccessoryButton'
+import FormattedNumberInput from 'components/inputs/FormattedNumberInput'
+import {
+  downloadParticipants,
+  downloadV2V3Payouts,
+  downloadPayments,
+  downloadRedemptions,
+  downloadAdditionsToBalance,
+} from 'utils/csvDownloadHelpers'
+import { DownloadOutlined } from '@ant-design/icons'
 
 export default function V2V3DownloadActivityModal(props: ModalProps) {
-  const [paymentsModalVisible, setPaymentsModalVisible] = useState<boolean>()
+  const [latestBlockNumber, setLatestBlockNumber] = useState<number>()
+  const [blockNumber, setBlockNumber] = useState<number>()
+
+  useEffect(() => {
+    readProvider.getBlockNumber().then(val => {
+      setLatestBlockNumber(val)
+      setBlockNumber(val)
+    })
+  }, [])
+
+  const { projectId, pv } = useContext(ProjectMetadataContext)
 
   return (
     <Modal
       cancelText={t`Close`}
       okButtonProps={{ hidden: true }}
       centered
+      title={<Trans>Download project activity CSV</Trans>}
       {...props}
     >
-      <div>
-        <h4>
-          <Trans>Export project activity</Trans>
-        </h4>
-
-        <div className="mt-5">
-          <Button block onClick={() => setPaymentsModalVisible(true)}>
-            <Trans>Download payments CSV</Trans>
-          </Button>
-        </div>
-      </div>
-
-      <V2V3DownloadPaymentsModal
-        open={paymentsModalVisible}
-        onCancel={() => setPaymentsModalVisible(false)}
+      <label className="mb-1 block">
+        <Trans>Block number</Trans>
+      </label>
+      <FormattedNumberInput
+        value={blockNumber?.toString()}
+        onChange={val => setBlockNumber(val ? parseInt(val) : undefined)}
+        accessory={
+          <InputAccessoryButton
+            content={t`Latest`}
+            onClick={() => setBlockNumber(latestBlockNumber)}
+            disabled={blockNumber === latestBlockNumber}
+          />
+        }
+        className="mb-4"
       />
+
+      <Space direction="vertical" className="w-full">
+        <Button
+          block
+          icon={<DownloadOutlined />}
+          onClick={() => downloadParticipants(blockNumber, projectId, pv)}
+        >
+          <span>
+            <Trans>Participants</Trans>
+          </span>
+        </Button>
+
+        <Button
+          block
+          icon={<DownloadOutlined />}
+          onClick={() => downloadV2V3Payouts(blockNumber, projectId)}
+        >
+          <span>
+            <Trans>Payouts</Trans>
+          </span>
+        </Button>
+
+        <Button
+          block
+          icon={<DownloadOutlined />}
+          onClick={() => downloadPayments(blockNumber, projectId, pv)}
+        >
+          <span>
+            <Trans>Payments</Trans>
+          </span>
+        </Button>
+
+        <Button
+          block
+          icon={<DownloadOutlined />}
+          onClick={() => downloadRedemptions(blockNumber, projectId, pv)}
+        >
+          <span>
+            <Trans>Redemptions</Trans>
+          </span>
+        </Button>
+
+        <Button
+          block
+          icon={<DownloadOutlined />}
+          onClick={() => downloadAdditionsToBalance(blockNumber, projectId, pv)}
+        >
+          <span>
+            <Trans>Additions to balance</Trans>
+          </span>
+        </Button>
+      </Space>
     </Modal>
   )
 }

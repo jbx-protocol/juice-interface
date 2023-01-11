ade2dbaac34e2fa1196d6fd9805af3c4fa42b1a6
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-01-11 18:09:08 +1100

    minor changes for token migration (#2706)
    * Enhance v1 contracts to include loading states
    
    * add version badges
    
    * rename settings pages
    
    * minor changes for token migration

--- src/hooks/v2v3/contractReader/ProjectController.ts
+++ src/hooks/v2v3/contractReader/ProjectController.ts
@@ -1,18 +1,23 @@
+import { ContractConfig } from 'hooks/ContractReader/types'
 import { V2V3ContractName } from 'models/v2v3/contracts'
-
 import useV2ContractReader from './V2ContractReader'
 
 export default function useProjectController({
   projectId,
+  contract,
   useDeprecatedContract,
 }: {
   projectId?: number
+  contract?: ContractConfig<V2V3ContractName> | undefined
   useDeprecatedContract?: boolean
 }) {
-  return useV2ContractReader<string>({
-    contract: useDeprecatedContract
+  if (!contract) {
+    contract = useDeprecatedContract
       ? V2V3ContractName.DeprecatedJBDirectory
-      : V2V3ContractName.JBDirectory,
+      : V2V3ContractName.JBDirectory
+  }
+  return useV2ContractReader<string>({
+    contract,
     functionName: 'controllerOf',
     args: projectId ? [projectId] : null,
   })

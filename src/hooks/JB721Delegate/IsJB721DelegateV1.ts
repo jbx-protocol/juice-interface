be4ffbb2092efa98b660c33bb2067668c9b15eef
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-01-17 13:52:09 +1030

    Fix

--- src/hooks/JB721Delegate/IsJB721DelegateV1.ts
+++ src/hooks/JB721Delegate/IsJB721DelegateV1.ts
@@ -1,5 +1,6 @@
 import { useContractReadValue } from 'hooks/ContractReader'
-import { useJB721TieredDelegate } from 'hooks/contracts/JB721Delegate/useJB721TieredDelegate'
+import { useLoadContractFromAddress } from 'hooks/LoadContractFromAddress'
+import JB721TieredGovernanceJson from '@jbx-protocol/juice-721-delegate/out/JB721TieredGovernance.sol/JB721TieredGovernance.json'
 
 const IJB721TieredDelegate_V1_INTERFACE_ID = '0xf34282c8'
 // const IJB721TieredDelegate_V1_1_INTERFACE_ID = '0x8b1f9b1c'
@@ -9,7 +10,10 @@ export function useIsJB721DelegateV1({
 }: {
   dataSourceAddress: string | undefined
 }): boolean {
-  const contract = useJB721TieredDelegate({ address: dataSourceAddress })
+  const contract = useLoadContractFromAddress({
+    address: dataSourceAddress,
+    abi: JB721TieredGovernanceJson.abi,
+  })
   const { value: isJB721DelegateV1 } = useContractReadValue({
     contract,
     functionName: 'supportsInterface',

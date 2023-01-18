f1739f67b84ca40667481f4db84a71f835853b53
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-01-18 15:26:10 +1030

    Newsletter better mobiel

--- src/pages/home/NewsletterSection.tsx
+++ src/pages/home/NewsletterSection.tsx
@@ -1,10 +1,13 @@
 import { Trans } from '@lingui/macro'
 import { Button, Col, Row } from 'antd'
+import useMobile from 'hooks/Mobile'
 import Image from 'next/image'
 import { SectionHeading } from './SectionHeading'
 import blueberry from '/public/assets/blueberry-ol.png'
 
 export function NewsletterSection() {
+  const isMobile = useMobile()
+
   return (
     <section className="bg-smoke-50 p-8 py-20 px-7 text-black dark:bg-slate-600">
       <div className="my-0 mx-auto max-w-5xl">
@@ -24,19 +27,22 @@ export function NewsletterSection() {
                 href="https://newsletter.juicebox.money"
                 target="_blank"
                 rel="noopener noreferrer"
+                block={isMobile}
               >
                 Subscribe Now
               </Button>
             </div>
           </Col>
 
-          <Col xs={24} md={10}>
-            <Image
-              src={blueberry}
-              alt="Sexy Juicebox blueberry with bright pink lipstick spraying a can of spraypaint"
-              loading="lazy"
-            />
-          </Col>
+          {!isMobile && (
+            <Col xs={24} md={10}>
+              <Image
+                src={blueberry}
+                alt="Sexy Juicebox blueberry with bright pink lipstick spraying a can of spraypaint"
+                loading="lazy"
+              />
+            </Col>
+          )}
         </Row>
       </div>
     </section>

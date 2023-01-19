64df524b52172a0ab3bd245e06b67b4f5f03518a
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-01-19 16:14:11 +1030

    Use tailwind media queries in place of ismobile

--- src/components/Create/components/JuiceImgPreview.tsx
+++ src/components/Create/components/JuiceImgPreview.tsx
@@ -1,10 +1,8 @@
 import { CloseOutlined } from '@ant-design/icons'
 import { Image, ImageProps } from 'antd'
-import useMobile from 'hooks/Mobile'
-import { classNames } from 'utils/classNames'
 
 export const JUICE_IMG_PREVIEW_CONTAINER_CLASS =
-  'fixed top-0 left-0 z-[10000] flex h-full w-full items-center justify-center overflow-auto bg-[rgba(0,0,0,0.8)]'
+  'fixed top-0 left-0 z-[10000] flex h-full w-full items-center justify-center overflow-auto bg-black/0.8'
 
 export function JuiceImgPreview({
   src,
@@ -16,21 +14,17 @@ export function JuiceImgPreview({
   visible: boolean
   onClose: VoidFunction
 }) {
-  const isMobile = useMobile()
   if (!visible) return null
+
   return (
     <div className={JUICE_IMG_PREVIEW_CONTAINER_CLASS} onClick={onClose}>
-      <div className={classNames(!isMobile ? 'w-[600px]' : 'w-[90vw]')}>
+      <div className="md:w-xl w-[90vw]">
         <div className={'mb-4 flex w-full items-center justify-end'}>
           <CloseOutlined className="pl-4 text-slate-100" onClick={onClose} />
         </div>
         <div className="flex justify-center">
           <Image
-            className={classNames(
-              !isMobile
-                ? 'max-h-[60vh] max-w-[600px]'
-                : 'max-h-[50vh] max-w-[90vw]',
-            )}
+            className="max-h-[50vh] max-w-[90vw] md:max-h-[60vh] md:max-w-xl"
             alt={alt}
             src={src}
             onClick={e => e.stopPropagation()}
--- src/components/NftRewards/NftPreview.tsx
+++ src/components/NftRewards/NftPreview.tsx
@@ -1,11 +1,9 @@
 import { CloseOutlined, LinkOutlined } from '@ant-design/icons'
 import { Trans } from '@lingui/macro'
 import ExternalLink from 'components/ExternalLink'
-import useMobile from 'hooks/Mobile'
 import { DEFAULT_NFT_MAX_SUPPLY } from 'hooks/NftRewards'
 import { NftRewardTier } from 'models/nftRewardTier'
 import { useContext } from 'react'
-
 import { ProjectMetadataContext } from 'contexts/projectMetadataContext'
 import { classNames } from 'utils/classNames'
 import { JUICE_IMG_PREVIEW_CONTAINER_CLASS } from 'components/Create/components/JuiceImgPreview'
@@ -23,8 +21,6 @@ export function NftPreview({
 }) {
   const { projectMetadata } = useContext(ProjectMetadataContext)
 
-  const isMobile = useMobile()
-
   if (!open) return null
 
   const hasLimitedSupply = Boolean(
@@ -35,10 +31,7 @@ export function NftPreview({
 
   return (
     <div className={JUICE_IMG_PREVIEW_CONTAINER_CLASS} onClick={onClose}>
-      <div
-        className={classNames(!isMobile ? 'w-[600px]' : 'w-[90vw]')}
-        onClick={e => e.stopPropagation()}
-      >
+      <div className="md:w-xl w-[90vw]" onClick={e => e.stopPropagation()}>
         <div className="mb-4 flex w-full items-center justify-between">
           <h3 className="mb-0 uppercase text-slate-100">
             <Trans>{projectMetadata?.name}</Trans>
@@ -47,18 +40,14 @@ export function NftPreview({
         </div>
         <div className="flex justify-center" onClick={onClose}>
           <img
-            className={classNames(
-              !isMobile
-                ? 'max-h-[60vh] max-w-[600px]'
-                : 'max-h-[50vh] max-w-[90vw]',
-            )}
+            className={'max-h-[50vh] max-w-[90vw] md:max-h-[60vh] md:max-w-xl'}
             alt={rewardTier.name}
             src={imageUrl}
             onClick={e => e.stopPropagation()}
             crossOrigin="anonymous"
           />
         </div>
-        <h3 className='"mb-0 text-slate-100" mt-5'>{rewardTier.name}</h3>
+        <h3 className="mb-0 mt-5 text-slate-100">{rewardTier.name}</h3>
         <p className="mt-2 text-slate-100">{rewardTier.description}</p>
         {hasLimitedSupply || rewardTier.externalLink ? (
           <div className="mt-5 flex text-xs text-slate-100">

785c3d1cca6888ebd24757287be822209c7530fc
Johnny D <johnnyd.eth@protonmail.com>
2023-01-20 10:31:43 +1000

    _

--- src/components/v2v3/shared/DiffedSplits/DiffedSplitFields/DiffedJBProjectBeneficiary.tsx
+++ src/components/v2v3/shared/DiffedSplits/DiffedSplitFields/DiffedJBProjectBeneficiary.tsx
@@ -1,7 +1,7 @@
 import FormattedAddress from 'components/FormattedAddress'
 import { Split } from 'models/splits'
 import { DiffedItem } from '../../DiffedItem'
-import { JBProjectBeneficiaryContainer } from '../../SplitItem/containers/JBProjectBeneficiaryContainer'
+import { JuiceboxProjectBeneficiary } from '../../SplitItem/JuiceboxProjectBeneficiary'
 
 export function DiffedJBProjectBeneficiary({
   split,
@@ -15,29 +15,23 @@ export function DiffedJBProjectBeneficiary({
   const hasDiff = oldSplit && oldSplit.beneficiary !== split.beneficiary
 
   return (
-    <JBProjectBeneficiaryContainer
+    <JuiceboxProjectBeneficiary
       split={split}
       projectOwnerAddress={projectOwnerAddress}
-    >
-      {hasDiff ? (
-        <div className="flex">
-          <DiffedItem
-            value={<FormattedAddress address={oldSplit.beneficiary} />}
-            diffStatus="old"
-          />
-          <DiffedItem
-            value={<FormattedAddress address={split.beneficiary} />}
-            diffStatus="new"
-          />
-        </div>
-      ) : (
-        <div className="ml-2">
-          <FormattedAddress
-            address={split.beneficiary}
-            className={'text-sm font-light text-grey-500 dark:text-grey-300'}
-          />
-        </div>
-      )}
-    </JBProjectBeneficiaryContainer>
+      value={
+        hasDiff ? (
+          <div className="flex">
+            <DiffedItem
+              value={<FormattedAddress address={oldSplit.beneficiary} />}
+              diffStatus="old"
+            />
+            <DiffedItem
+              value={<FormattedAddress address={split.beneficiary} />}
+              diffStatus="new"
+            />
+          </div>
+        ) : undefined
+      }
+    />
   )
 }
--- src/components/v2v3/shared/SplitItem/JuiceboxProjectBeneficiary.tsx
+++ src/components/v2v3/shared/SplitItem/JuiceboxProjectBeneficiary.tsx
@@ -1,25 +1,60 @@
-import FormattedAddress from 'components/FormattedAddress'
+import { Trans } from '@lingui/macro'
+import { Space, Tooltip } from 'antd'
+import TooltipLabel from 'components/TooltipLabel'
+import { NULL_ALLOCATOR_ADDRESS } from 'constants/contracts/mainnet/Allocators'
 import { Split } from 'models/splits'
-import { JBProjectBeneficiaryContainer } from './containers/JBProjectBeneficiaryContainer'
+import { AllocatorBadge } from '../FundingCycleConfigurationDrawers/AllocatorBadge'
+import V2V3ProjectHandleLink from '../V2V3ProjectHandleLink'
+import { CrownFilled } from '@ant-design/icons'
+import FormattedAddress from 'components/FormattedAddress'
 
 export function JuiceboxProjectBeneficiary({
   projectOwnerAddress,
   split,
+  value,
 }: {
   split: Split
   projectOwnerAddress: string | undefined
+  value?: string | JSX.Element
 }) {
+  if (!split.projectId) return null
+
+  const isProjectOwner = projectOwnerAddress === split.beneficiary
+
   return (
-    <JBProjectBeneficiaryContainer
-      split={split}
-      projectOwnerAddress={projectOwnerAddress}
-    >
-      <div className="ml-2">
-        <FormattedAddress
-          address={split.beneficiary}
-          className={'text-sm font-light text-grey-500 dark:text-grey-300'}
-        />
-      </div>
-    </JBProjectBeneficiaryContainer>
+    <div>
+      <Space size="middle">
+        <V2V3ProjectHandleLink projectId={parseInt(split.projectId)} />
+        <AllocatorBadge allocator={split.allocator} />
+      </Space>
+      {split.allocator === NULL_ALLOCATOR_ADDRESS ? (
+        <Space>
+          <div className="ml-2 flex items-center text-sm text-grey-500 dark:text-grey-300">
+            <TooltipLabel
+              label={<Trans>Tokens:</Trans>}
+              tip={
+                <Trans>
+                  This address will receive any tokens minted when the recipient
+                  project gets paid.
+                </Trans>
+              }
+            />
+            {value ?? (
+              <FormattedAddress
+                address={split.beneficiary}
+                className={
+                  'text-sm font-light text-grey-500 dark:text-grey-300'
+                }
+              />
+            )}
+            {isProjectOwner && (
+              <Tooltip title={<Trans>Project owner</Trans>} className="ml-1">
+                <CrownFilled />
+              </Tooltip>
+            )}
+          </div>
+        </Space>
+      ) : null}
+    </div>
   )
 }

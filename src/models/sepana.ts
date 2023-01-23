f9ef7e99a2a51f1e44342cd471ff135ec6556ca2
peripheralist <peripheralist@gmail.com>
2023-01-23 16:49:02 -0500

    -

--- src/models/sepana.ts
+++ src/models/sepana.ts
@@ -3,41 +3,34 @@ import { ProjectMetadataV5 } from 'models/project-metadata'
 
 import { PV } from './pv'
 
-type SepanaProjectMetadataProps = Pick<
-  ProjectMetadataV5,
-  'description' | 'logoUri' | 'name'
-> & {
-  lastUpdated: number
-}
-
-type BaseSepanaProject = {
+type SepanaProject = {
   id: string
   projectId: number
+  createdAt: number
   pv: PV
   handle: string | null | undefined
   metadataUri: string
-  currentBalance: string
-  totalPaid: string
-  createdAt: number
-  trendingScore: string
+  currentBalance: BigNumber
+  trendingScore: BigNumber
+  totalPaid: BigNumber
   deployer: string | null | undefined
   name?: string
   description?: string
   logoUri?: string
   metadataResolved: boolean // Helper method to signify if metadata has been successfully resolved from IPFS
-}
+} & Pick<ProjectMetadataV5, 'description' | 'logoUri' | 'name'> & {
+    lastUpdated: number
+  }
 
 // Project type stored in Sepana db
-export type SepanaProjectJson = BaseSepanaProject & SepanaProjectMetadataProps
-
-export type SepanaProject = Omit<
-  BaseSepanaProject,
+export type SepanaProjectJson = Omit<
+  SepanaProject,
   'currentBalance' | 'trendingScore' | 'totalPaid'
 > & {
-  currentBalance: BigNumber
-  trendingScore: BigNumber
-  totalPaid: BigNumber
-} & SepanaProjectMetadataProps
+  currentBalance: string
+  trendingScore: string
+  totalPaid: string
+}
 
 export type SepanaSearchResponse<T> = {
   hits: {
@@ -55,5 +48,3 @@ export type SepanaSearchResponse<T> = {
     }[]
   }
 }
-
-export type SepanaBigNumber = { type: 'BigNumber'; hex: string }

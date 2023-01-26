434dc08a85ad0278138ae7630511c015f922ff0a
Filip V <f@filip.world>
2023-01-26 17:09:42 -0500

    Update project creation success screen and next steps modal (#2847)
    * Update project creation success screen and next steps modal
    
    * Minor updates
    
    * Vertical buttons, props, formatting
    
    * Clean up styling, imports

--- src/components/Create/components/pages/ReviewDeploy/components/DeploySuccess/DeploySuccess.tsx
+++ src/components/Create/components/pages/ReviewDeploy/components/DeploySuccess/DeploySuccess.tsx
@@ -1,20 +1,17 @@
-import { ShareAltOutlined } from '@ant-design/icons'
+import { TwitterOutlined, ShareAltOutlined } from '@ant-design/icons'
 import { t, Trans } from '@lingui/macro'
-import { Button, Space } from 'antd'
+import { Button } from 'antd'
 import ExternalLink from 'components/ExternalLink'
 import { NEW_DEPLOY_QUERY_PARAM } from 'components/v2v3/V2V3Project/modals/NewDeployModal'
 import { readNetwork } from 'constants/networks'
-import useMobile from 'hooks/Mobile'
 import { useWallet } from 'hooks/Wallet'
 import { NetworkName } from 'models/network-name'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo, useState } from 'react'
-import { classNames } from 'utils/classNames'
 
 export const DeploySuccess = ({ projectId }: { projectId: number }) => {
   console.info('Deploy: SUCCESS', projectId)
-  const isMobile = useMobile()
   const router = useRouter()
   const { chain } = useWallet()
   let deployGreeting = t`Your project has successfully launched!`
@@ -50,32 +47,34 @@ export const DeploySuccess = ({ projectId }: { projectId: number }) => {
   }, [projectId, router])
 
   return (
-    <div
-      className={classNames(
-        'flex flex-col items-center justify-center text-center',
-        isMobile ? 'mt-4' : 'mt-28',
-      )}
-    >
+    <div className="mt-4 flex flex-col items-center justify-center text-center">
       <Image src="/assets/dancing.gif" width={218} height={218} />
       <div className="pt-8 text-4xl font-medium">Congratulations!</div>
       <div className="mt-6 text-xl font-normal">{deployGreeting}</div>
-      <div className="flex gap-2 pt-16">
-        <ExternalLink href={twitterShareUrl}>
-          <Button>
-            {/* Spacing is weird when you use button icon - do this instead */}
-            <Space>
-              <ShareAltOutlined />
-              <Trans> Share project</Trans>
-            </Space>
-          </Button>
-        </ExternalLink>
+      <div className="flex flex-col gap-2 py-12">
         <Button
           type="primary"
           onClick={handleGoToProject}
           loading={gotoProjectClicked}
         >
-          <Trans>Go to project</Trans>
+          <span>
+            <Trans>Go to project</Trans>
+          </span>
         </Button>
+        <ExternalLink href={twitterShareUrl}>
+          <Button icon={<TwitterOutlined />}>
+            <span>
+              <Trans> Share on Twitter</Trans>
+            </span>
+          </Button>
+        </ExternalLink>
+        <ExternalLink href="https://discord.gg/juicebox">
+          <Button icon={<ShareAltOutlined />}>
+            <span>
+              <Trans> Share on Discord</Trans>
+            </span>
+          </Button>
+        </ExternalLink>
       </div>
     </div>
   )

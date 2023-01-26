89ed3e3dcbd4c27bd93147a71617cf1e472cace8
filipvvv <f@filip.world>
2023-01-26 10:18:22 -0500

    Clean up styling, imports

--- src/components/Create/components/pages/ReviewDeploy/components/DeploySuccess/DeploySuccess.tsx
+++ src/components/Create/components/pages/ReviewDeploy/components/DeploySuccess/DeploySuccess.tsx
@@ -4,17 +4,14 @@ import { Button } from 'antd'
 import ExternalLink from 'components/ExternalLink'
 import { NEW_DEPLOY_QUERY_PARAM } from 'components/v2v3/V2V3Project/modals/NewDeployModal'
 import { readNetwork } from 'constants/networks'
-import useMobile from 'hooks/Mobile'
 import { useWallet } from 'hooks/Wallet'
 import { NetworkName } from 'models/network-name'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo, useState } from 'react'
-import { classNames } from 'utils/classNames'
 
 export const DeploySuccess = ({ projectId }: { projectId: number }) => {
   console.info('Deploy: SUCCESS', projectId)
-  const isMobile = useMobile()
   const router = useRouter()
   const { chain } = useWallet()
   let deployGreeting = t`Your project has successfully launched!`
@@ -50,21 +47,15 @@ export const DeploySuccess = ({ projectId }: { projectId: number }) => {
   }, [projectId, router])
 
   return (
-    <div
-      className={classNames(
-        'flex flex-col items-center justify-center text-center',
-        isMobile ? 'mt-4' : 'mt-28',
-      )}
-    >
+    <div className="mt-4 flex flex-col items-center justify-center text-center">
       <Image src="/assets/dancing.gif" width={218} height={218} />
       <div className="pt-8 text-4xl font-medium">Congratulations!</div>
       <div className="mt-6 text-xl font-normal">{deployGreeting}</div>
-      <div className="flex flex-col gap-2 pt-16">
+      <div className="flex flex-col gap-2 py-12">
         <Button
           type="primary"
           onClick={handleGoToProject}
           loading={gotoProjectClicked}
-          className="gap-3"
         >
           <span>
             <Trans>Go to project</Trans>

0a7059dcb505c28587180a8e82b439a73ffdd79d
peripheralist <peripheralist@gmail.com>
2023-01-30 18:20:09 -0500

    -

--- src/utils/format/formatError.ts
+++ src/utils/format/formatError.ts
@@ -2,6 +2,6 @@ export function formatError(error: unknown): string {
   return typeof error === 'string'
     ? error
     : (error as { message: string }).message
-    ? JSON.stringify(error)
+    ? (error as { message: string }).message
     : 'Failed to parse error'
 }

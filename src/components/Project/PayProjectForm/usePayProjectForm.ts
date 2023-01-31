4299462c1cd3cf718393c712e800a3cda54d884d
Johnny D <johnnyD.eth@protonmail.com>
2023-01-31 14:57:55 +1000

    Pay metadata considerations for `DelegateVersion 1.1` (#2912)
    * add delegateVersion to nftRewardsContext
    
    * delegate v1.1 support - payMetadata considerations
    
    * allow upper-case (with underscores) for types ESLINT
    
    * _

--- src/components/Project/PayProjectForm/usePayProjectForm.ts
+++ src/components/Project/PayProjectForm/usePayProjectForm.ts
@@ -7,14 +7,23 @@ import { CurrencyOption } from 'models/currencyOption'
 import { Dispatch, SetStateAction, useContext, useState } from 'react'
 import { parseWad } from 'utils/format/formatNumber'
 
-export interface JB721DelegatePayMetadata {
+export interface JB721DELAGATE_V1_PAY_METADATA {
   tierIdsToMint: number[]
   dontMint?: boolean
   expectMintFromExtraFunds?: boolean
   dontOverspend?: boolean
 }
 
-type PayMetadata = JB721DelegatePayMetadata // in future, maybe more
+export interface JB721DELAGATE_V1_1_PAY_METADATA {
+  tierIdsToMint: number[]
+  allowOverspending?: boolean
+}
+
+type PayMetadata =
+  | JB721DELAGATE_V1_PAY_METADATA
+  | JB721DELAGATE_V1_1_PAY_METADATA // in future, maybe more
+
+export const DEFAULT_ALLOW_OVERSPENDING = true
 
 const DEFAULT_PAY_METADATA: PayMetadata = {
   tierIdsToMint: [],
--- src/constants/delegateVersions.ts
+++ src/constants/delegateVersions.ts
@@ -1,7 +1,4 @@
-import {
-  JB721DelegateVersion_1,
-  JB721DelegateVersion_1_1,
-} from 'models/nftRewardTier'
+import { JB721DELEGATE_V1, JB721DELEGATE_V1_1 } from 'models/nftRewardTier'
 
-export const DV_V1: JB721DelegateVersion_1 = '1'
-export const DV_V1_1: JB721DelegateVersion_1_1 = '1.1'
+export const DV_V1: JB721DELEGATE_V1 = '1'
+export const DV_V1_1: JB721DELEGATE_V1_1 = '1.1'

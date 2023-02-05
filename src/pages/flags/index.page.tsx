74fd3eee6e076158c4a9f8c72c36c90f13ca740f
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-02-06 09:59:17 +1030

    Add nofollow

--- src/pages/flags/index.page.tsx
+++ src/pages/flags/index.page.tsx
@@ -4,6 +4,7 @@ import { FEATURE_FLAGS } from 'constants/featureFlags'
 import { readNetwork } from 'constants/networks'
 import { useCallback, useState } from 'react'
 import { featureFlagEnabled, setFeatureFlag } from 'utils/featureFlags'
+import Head from 'next/head'
 
 export default function FlagsPage() {
   const [, updateState] = useState({})
@@ -17,26 +18,32 @@ export default function FlagsPage() {
   }
 
   return (
-    <div className="container-lg container mx-auto mt-10">
-      <h1 className="flex items-center">
-        Feature flags
-        <Badge variant="info" className="ml-2 capitalize">
-          {readNetwork.name}
-        </Badge>
-      </h1>
+    <>
+      <Head>
+        <meta key="robots" name="robots" content="noindex,follow" />
+        <meta key="googlebot" name="googlebot" content="noindex,follow" />
+      </Head>
+      <div className="container-lg container mx-auto mt-10">
+        <h1 className="flex items-center">
+          Feature flags
+          <Badge variant="info" className="ml-2 capitalize">
+            {readNetwork.name}
+          </Badge>
+        </h1>
 
-      <div className="flex flex-col gap-y-5">
-        {flags.map(key => {
-          return (
-            <JuiceSwitch
-              label={FEATURE_FLAGS[key]}
-              key={key}
-              value={featureFlagEnabled(FEATURE_FLAGS[key])}
-              onChange={v => onSwitchChange(FEATURE_FLAGS[key], v)}
-            />
-          )
-        })}
+        <div className="flex flex-col gap-y-5">
+          {flags.map(key => {
+            return (
+              <JuiceSwitch
+                label={FEATURE_FLAGS[key]}
+                key={key}
+                value={featureFlagEnabled(FEATURE_FLAGS[key])}
+                onChange={v => onSwitchChange(FEATURE_FLAGS[key], v)}
+              />
+            )
+          })}
+        </div>
       </div>
-    </div>
+    </>
   )
 }

7cca083ecb973dbed24bf1dea69ba105dbb8498b
Blazo <medenicablazo@gmail.com>
2023-02-08 23:37:47 +0100

    Add custom error pages ( 404 and 500 ) (#2991)

---
+++ src/pages/404/index.page.tsx
@@ -0,0 +1,39 @@
+import { Trans } from '@lingui/macro'
+import Link from 'antd/lib/typography/Link'
+import LanguageProvider from 'providers/LanguageProvider'
+import { useContext } from 'react'
+import { ThemeContext } from '../../contexts/themeContext'
+import error404 from '/public/assets/error404.png'
+
+const Custom404 = () => {
+  const {
+    theme: { colors },
+  } = useContext(ThemeContext)
+  return (
+    <LanguageProvider>
+      <div className="flex flex-col items-center justify-center p-6 md:p-40">
+        <img
+          src={error404.src}
+          alt="Image"
+          className="mb-6 h-60 w-60 object-center md:h-80 md:w-80"
+        />
+        <Trans>
+          <p className="mb-6 text-sm text-grey-600 md:text-base">
+            This page could not be found.
+          </p>
+        </Trans>
+        <Trans>
+          <Link
+            href="/"
+            style={{ color: colors.text.action.primary }}
+            className="text-sm md:text-base"
+          >
+            Back to home
+          </Link>
+        </Trans>
+      </div>
+    </LanguageProvider>
+  )
+}
+
+export default Custom404

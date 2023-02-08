f9cdfb9dd5937f7e2b32c4ace58bab8bf753f6db
NakedFool <medenicablazo@gmail.com>
2023-02-08 10:32:37 +0100

    add custom error pages

--- src/pages/404/index.page.tsx
+++ src/pages/404/index.page.tsx
@@ -1,7 +1,5 @@
-import { LeftOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
 import Link from 'antd/lib/typography/Link'
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
 import error404 from '/public/assets/error404.png'
 
@@ -10,21 +8,21 @@ const Custom404 = () => {
     theme: { colors },
   } = useContext(ThemeContext)
   return (
-    <div className="flex flex-col items-center justify-center p-20">
-      <img src={error404.src} alt="Image" className="mb-6 h-80 w-80" />
-      <h1 className="mb-6 text-2xl text-base font-medium md:text-2xl">
-        Oops, something went wrong!
-      </h1>
-      <p className="mb-6  text-grey-600">This page could not be found.</p>
-      <Link href="/">
-        <Button
-          type="link"
-          style={{ color: colors.text.action.primary }}
-          size="large"
-          icon={<LeftOutlined />}
-        >
-          Back to home
-        </Button>
+    <div className="flex flex-col items-center justify-center p-6 md:p-40">
+      <img
+        src={error404.src}
+        alt="Image"
+        className="mb-6 h-60 w-60 object-center md:h-80 md:w-80"
+      />
+      <p className="mb-6 text-sm text-grey-600 md:text-base">
+        This page could not be found.
+      </p>
+      <Link
+        href="/"
+        style={{ color: colors.text.action.primary }}
+        className="text-sm md:text-base"
+      >
+        Back to home
       </Link>
     </div>
   )
--- src/pages/500/index.page.tsx
+++ src/pages/500/index.page.tsx
@@ -1,7 +1,5 @@
-import { LeftOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
 import Link from 'antd/lib/typography/Link'
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
 import error500 from '/public/assets/error500.png'
 
@@ -10,23 +8,21 @@ const Custom500 = () => {
     theme: { colors },
   } = useContext(ThemeContext)
   return (
-    <div className="flex flex-col items-center justify-center p-20">
-      <img src={error500.src} alt="Image" className="mb-6 h-80 w-80" />
-      <h1 className="mb-6 text-2xl text-base font-medium md:text-2xl">
-        Oops, something went wrong!
-      </h1>
-      <p className="mb-6  text-grey-600">
-        Something is going on with the servers, please try again later!
+    <div className="flex flex-col items-center justify-center p-6 md:p-40">
+      <img
+        src={error500.src}
+        alt="Image"
+        className="mb-6 h-64 w-64 object-center md:h-96 md:w-96"
+      />
+      <p className="mb-6 text-sm text-grey-600 md:text-base">
+        The servers are having an issue, please try again later!
       </p>
-      <Link href="/">
-        <Button
-          type="link"
-          style={{ color: colors.text.action.primary }}
-          size="large"
-          icon={<LeftOutlined />}
-        >
-          Back to home
-        </Button>
+      <Link
+        href="/"
+        style={{ color: colors.text.action.primary }}
+        className="text-sm md:text-base"
+      >
+        Back to home
       </Link>
     </div>
   )

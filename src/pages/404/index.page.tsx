2b453e5f656931409e61b16c53334fb8e9230ffa
NakedFool <medenicablazo@gmail.com>
2023-02-08 10:39:59 +0100

    add language provider

--- src/pages/404/index.page.tsx
+++ src/pages/404/index.page.tsx
@@ -1,3 +1,5 @@
+import { Trans } from '@lingui/macro'
+import LanguageProvider from 'providers/LanguageProvider'
 import Link from 'antd/lib/typography/Link'
 import { useContext } from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
@@ -8,23 +10,27 @@ const Custom404 = () => {
     theme: { colors },
   } = useContext(ThemeContext)
   return (
-    <div className="flex flex-col items-center justify-center p-6 md:p-40">
-      <img
-        src={error404.src}
-        alt="Image"
-        className="mb-6 h-60 w-60 object-center md:h-80 md:w-80"
-      />
-      <p className="mb-6 text-sm text-grey-600 md:text-base">
-        This page could not be found.
-      </p>
-      <Link
-        href="/"
-        style={{ color: colors.text.action.primary }}
-        className="text-sm md:text-base"
-      >
-        Back to home
-      </Link>
-    </div>
+    <LanguageProvider>
+      <div className="flex flex-col items-center justify-center p-6 md:p-40">
+        <img
+          src={error404.src}
+          alt="Image"
+          className="mb-6 h-60 w-60 object-center md:h-80 md:w-80"
+        />
+        <Trans>
+          <p className="mb-6 text-sm text-grey-600 md:text-base">
+            This page could not be found.
+          </p>
+          <Link
+            href="/"
+            style={{ color: colors.text.action.primary }}
+            className="text-sm md:text-base"
+          >
+            Back to home
+          </Link>
+        </Trans>
+      </div>
+    </LanguageProvider>
   )
 }
 
--- src/pages/500/index.page.tsx
+++ src/pages/500/index.page.tsx
@@ -1,3 +1,5 @@
+import { Trans } from '@lingui/macro'
+import LanguageProvider from 'providers/LanguageProvider'
 import Link from 'antd/lib/typography/Link'
 import { useContext } from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
@@ -8,23 +10,27 @@ const Custom500 = () => {
     theme: { colors },
   } = useContext(ThemeContext)
   return (
-    <div className="flex flex-col items-center justify-center p-6 md:p-40">
-      <img
-        src={error500.src}
-        alt="Image"
-        className="mb-6 h-64 w-64 object-center md:h-96 md:w-96"
-      />
-      <p className="mb-6 text-sm text-grey-600 md:text-base">
-        The servers are having an issue, please try again later!
-      </p>
-      <Link
-        href="/"
-        style={{ color: colors.text.action.primary }}
-        className="text-sm md:text-base"
-      >
-        Back to home
-      </Link>
-    </div>
+    <LanguageProvider>
+      <div className="flex flex-col items-center justify-center p-6 md:p-40">
+        <img
+          src={error500.src}
+          alt="Image"
+          className="mb-6 h-64 w-64 object-center md:h-96 md:w-96"
+        />
+        <Trans>
+          <p className="mb-6 text-sm text-grey-600 md:text-base">
+            The servers are having an issue, please try again later!
+          </p>
+          <Link
+            href="/"
+            style={{ color: colors.text.action.primary }}
+            className="text-sm md:text-base"
+          >
+            Back to home
+          </Link>
+        </Trans>
+      </div>
+    </LanguageProvider>
   )
 }
 

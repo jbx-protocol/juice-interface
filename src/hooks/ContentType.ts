848e5aa33d43e0c965546feb8205f97f05c197e1
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-02-09 14:32:32 +1030

    Upgrade recharts

--- src/hooks/ContentType.ts
+++ src/hooks/ContentType.ts
@@ -3,15 +3,7 @@ import { useQuery } from 'react-query'
 import { loadURLContentType } from 'utils/http/loadURLContentType'
 
 export function useContentType(uri?: string) {
-  return useQuery(
-    ['content-type', uri],
-    async () => {
-      if (!uri) {
-        throw new Error('Project URI not specified.')
-      }
-
-      const response = await loadURLContentType(uri)
-      return response
-    },
-  )
+  return useQuery(['content-type', uri], async () => {
+    return await loadURLContentType(uri)
+  })
 }

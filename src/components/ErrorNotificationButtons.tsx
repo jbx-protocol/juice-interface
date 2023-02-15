1e2fcd70f39b42da674e914d4d34496be50f46de
filipvvv <f@filip.world>
2023-02-15 07:27:54 -0500

    Add language provider to error notification buttons

--- src/components/ErrorNotificationButtons.tsx
+++ src/components/ErrorNotificationButtons.tsx
@@ -2,6 +2,7 @@ import { Button, Space } from 'antd'
 import { helpPagePath } from 'utils/routes'
 import { WarningOutlined } from '@ant-design/icons'
 import { Trans } from '@lingui/macro'
+import LanguageProvider from 'contexts/Language/LanguageProvider'
 
 const resetSite = () => {
   localStorage.clear()
@@ -12,23 +13,25 @@ const resetSite = () => {
 
 export default function ErrorNotificationButtons() {
   return (
-    <Space>
-      <Button
-        type="link"
-        size="small"
-        href={helpPagePath('/user/resources/troubleshoot/')}
-      >
-        <Trans>Troubleshoot</Trans>
-      </Button>
-      <Button
-        icon={<WarningOutlined />}
-        className="bg-error-500 dark:bg-error-300"
-        type="primary"
-        size="small"
-        onClick={resetSite}
-      >
-        <Trans>Reset Website</Trans>
-      </Button>
-    </Space>
+    <LanguageProvider>
+      <Space>
+        <Button
+          type="link"
+          size="small"
+          href={helpPagePath('/user/resources/troubleshoot/')}
+        >
+          <Trans>Troubleshoot</Trans>
+        </Button>
+        <Button
+          icon={<WarningOutlined />}
+          className="bg-error-500 dark:bg-error-300"
+          type="primary"
+          size="small"
+          onClick={resetSite}
+        >
+          <Trans> Reset Website</Trans>
+        </Button>
+      </Space>
+    </LanguageProvider>
   )
 }

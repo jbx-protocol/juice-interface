1d82de02f4448b1da592d4f18e6f1c6fd6c2f5d8
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-02-22 17:28:16 +1030

    bamp

--- src/components/v2v3/shared/FundingCycleConfigurationDrawers/NftDrawer/shared/NftCollectionDetailsFormItems.tsx
+++ src/components/v2v3/shared/FundingCycleConfigurationDrawers/NftDrawer/shared/NftCollectionDetailsFormItems.tsx
@@ -5,7 +5,10 @@ import { Callout } from 'components/Callout'
 import ExternalLink from 'components/ExternalLink'
 import { MinimalCollapse } from 'components/MinimalCollapse'
 import { RadioItem } from 'components/RadioItem'
+import { FEATURE_FLAGS } from 'constants/featureFlags'
 import { JB721GovernanceType } from 'models/nftRewardTier'
+import { useMemo } from 'react'
+import { featureFlagEnabled } from 'utils/featureFlags'
 import { helpPagePath } from 'utils/routes'
 
 export function NftCollectionDetailsFormItems({
@@ -13,6 +16,14 @@ export function NftCollectionDetailsFormItems({
 }: {
   isReconfigure?: boolean
 }) {
+  const showOnChainGoverance = useMemo(() => {
+    // If not reconfiguring, always return true
+    if (!isReconfigure) return true
+    // Only show if feature flag is enabled
+    return featureFlagEnabled(
+      FEATURE_FLAGS.RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE,
+    )
+  }, [isReconfigure])
   return (
     <>
       <Form.Item name="collectionName" label={t`Collection name`} required>
@@ -32,72 +43,74 @@ export function NftCollectionDetailsFormItems({
       </Form.Item>
 
       {/* TODO: Remove this one feature flag is removed */}
-      <MinimalCollapse header={<Trans>On-chain Governance</Trans>}>
-        <div className="flex flex-col gap-6">
-          {isReconfigure && (
-            <Callout.Info
-              className="bg-smoke-100 dark:bg-slate-500"
-              transparent
-            >
-              Once deployed, on-chain governance cannot be changed
-            </Callout.Info>
-          )}
-          <Form.Item name="onChainGovernance">
-            <Radio.Group
-              className="flex flex-col gap-5"
-              disabled={isReconfigure}
-              defaultValue={JB721GovernanceType.NONE}
-            >
-              <RadioItem
-                value={JB721GovernanceType.NONE}
-                title={
-                  <>
-                    <Trans>No on-chain governance</Trans>{' '}
-                    <Badge variant="info" upperCase>
-                      <Trans>Default</Trans>
-                    </Badge>
-                  </>
-                }
-                description={t`Your project's NFTs will not have on-chain governance capabilities.`}
-              />
-              <RadioItem
-                value={JB721GovernanceType.GLOBAL}
-                title={t`Standard on-chain governance`}
-                description={
-                  <Trans>
-                    Track the historical voting weight of each token holder
-                    across all tiers of NFTs.{' '}
-                    <ExternalLink
-                      href={helpPagePath(
-                        '/user/configuration/#on-chain-governance',
-                      )}
-                    >
-                      Learn more.
-                    </ExternalLink>
-                  </Trans>
-                }
-              />
-              <RadioItem
-                value={JB721GovernanceType.TIERED}
-                title={t`Tier-based on-chain governance`}
-                description={
-                  <Trans>
-                    Track the historical voting weight of each token holder
-                    within each tier of NFTs.{' '}
-                    <ExternalLink
-                      href={helpPagePath(
-                        '/user/configuration/#on-chain-governance',
-                      )}
-                    >
-                      Learn more.
-                    </ExternalLink>
-                  </Trans>
-                }
-              />
-            </Radio.Group>
-          </Form.Item>
-        </div>
-      </MinimalCollapse>
+      {showOnChainGoverance && (
+        <MinimalCollapse header={<Trans>On-chain Governance</Trans>}>
+          <div className="flex flex-col gap-6">
+            {isReconfigure && (
+              <Callout.Info
+                className="bg-smoke-100 dark:bg-slate-500"
+                transparent
+              >
+                Once deployed, on-chain governance cannot be changed
+              </Callout.Info>
+            )}
+            <Form.Item name="onChainGovernance">
+              <Radio.Group
+                className="flex flex-col gap-5"
+                disabled={isReconfigure}
+                defaultValue={JB721GovernanceType.NONE}
+              >
+                <RadioItem
+                  value={JB721GovernanceType.NONE}
+                  title={
+                    <>
+                      <Trans>No on-chain governance</Trans>{' '}
+                      <Badge variant="info" upperCase>
+                        <Trans>Default</Trans>
+                      </Badge>
+                    </>
+                  }
+                  description={t`Your project's NFTs will not have on-chain governance capabilities.`}
+                />
+                <RadioItem
+                  value={JB721GovernanceType.GLOBAL}
+                  title={t`Standard on-chain governance`}
+                  description={
+                    <Trans>
+                      Track the historical voting weight of each token holder
+                      across all tiers of NFTs.{' '}
+                      <ExternalLink
+                        href={helpPagePath(
+                          '/user/configuration/#on-chain-governance',
+                        )}
+                      >
+                        Learn more.
+                      </ExternalLink>
+                    </Trans>
+                  }
+                />
+                <RadioItem
+                  value={JB721GovernanceType.TIERED}
+                  title={t`Tier-based on-chain governance`}
+                  description={
+                    <Trans>
+                      Track the historical voting weight of each token holder
+                      within each tier of NFTs.{' '}
+                      <ExternalLink
+                        href={helpPagePath(
+                          '/user/configuration/#on-chain-governance',
+                        )}
+                      >
+                        Learn more.
+                      </ExternalLink>
+                    </Trans>
+                  }
+                />
+              </Radio.Group>
+            </Form.Item>
+          </div>
+        </MinimalCollapse>
+      )}
     </>
   )
 }
--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -5,6 +5,7 @@ type FeatureFlag =
   | 'SIMULATE_TXS'
   | 'SEPANA_SEARCH'
   | 'DELEGATE_V1_1'
+  | 'RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE'
 
 export const FEATURE_FLAGS: { [k in FeatureFlag]: string } = {
   V1_TOKEN_SWAP: 'v1TokenSwap',
@@ -13,4 +14,5 @@ export const FEATURE_FLAGS: { [k in FeatureFlag]: string } = {
   SIMULATE_TXS: 'simulateTxs',
   SEPANA_SEARCH: 'sepanaSearch',
   DELEGATE_V1_1: 'delegate_v1_1',
+  RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE: 'RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE',
 }

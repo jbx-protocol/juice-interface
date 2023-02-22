3154ed871a8c38a49a0164b49a4fe20fb8c2c222
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-02-22 19:33:18 +1030

    Fix mbuild maybe

--- src/pages/p/[handle]/pageLoaders.ts
+++ src/pages/p/[handle]/pageLoaders.ts
@@ -1,8 +1,8 @@
 import { PV_V1, PV_V1_1 } from 'constants/pv'
 import { paginateDepleteProjectsQueryCall } from 'lib/apollo'
-import { ProjectMetadataV6 } from 'models/projectMetadata'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { findProjectMetadata } from 'utils/server'
+import { V1StaticProps } from './index.page'
 
 export const getV1StaticPaths: GetStaticPaths = async () => {
   if (process.env.BUILD_CACHE_V1_PROJECTS === 'true') {
@@ -28,10 +28,9 @@ export const getV1StaticPaths: GetStaticPaths = async () => {
   return { paths: [], fallback: 'blocking' }
 }
 
-export const getV1StaticProps: GetStaticProps<{
-  metadata: ProjectMetadataV6 | undefined
-  handle: string
-}> = async context => {
+export const getV1StaticProps: GetStaticProps<
+  V1StaticProps
+> = async context => {
   if (!context.params) throw new Error('params not supplied')
   const handle = context.params.handle as string
   const projects = await paginateDepleteProjectsQueryCall({

7bae68385991c6e40796c5d9fa38e664ab2c0e78
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-03-01 16:42:18 +1030

    Check for controller 3_0_1 too

--- src/hooks/v2v3/contractReader/ProjectReservedTokens.ts
+++ src/hooks/v2v3/contractReader/ProjectReservedTokens.ts
@@ -2,6 +2,7 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { V2V3ProjectContractsContext } from 'contexts/v2v3/ProjectContracts/V2V3ProjectContractsContext'
 import { useContext, useMemo } from 'react'
 import { useIsJBControllerV3_0 } from '../JBController/IsJBControllerV3_0'
+import { useIsJBControllerV3_0_1 } from '../JBController/IsJBControllerV3_0_1'
 import { useIsJBControllerV3_1 } from '../JBController/IsJBControllerV3_1'
 import useContractReader from './V2ContractReader'
 
@@ -15,6 +16,9 @@ export default function useProjectReservedTokens({
   const { contracts } = useContext(V2V3ProjectContractsContext)
 
   const isJBControllerV3_0 = useIsJBControllerV3_0({
+    controllerAddress: contracts?.JBController?.address,
+  })
+  const isJBControllerV3_0_1 = useIsJBControllerV3_0_1({
     controllerAddress: contracts?.JBController?.address,
   })
   const isJBControllerV3_1 = useIsJBControllerV3_1({
@@ -32,17 +36,18 @@ export default function useProjectReservedTokens({
     return {
       contract: contracts.JBController,
       functionName: 'reservedTokenBalanceOf(uint256)',
-      args: projectId ? [projectId] : null,
+      args: projectId ? [projectId] : null, // JBControllerV3_1 doesn't need reservedRate
     }
   }, [contracts, projectId])
 
   const args = useMemo(() => {
-    if (isJBControllerV3_0) return JBControllerArgsV3_0
+    if (isJBControllerV3_0 || isJBControllerV3_0_1) return JBControllerArgsV3_0
     if (isJBControllerV3_1) return JBControllerArgsV3_1
 
     return { contract: undefined, functionName: undefined, args: undefined }
   }, [
     isJBControllerV3_0,
+    isJBControllerV3_0_1,
     JBControllerArgsV3_0,
     isJBControllerV3_1,
     JBControllerArgsV3_1,

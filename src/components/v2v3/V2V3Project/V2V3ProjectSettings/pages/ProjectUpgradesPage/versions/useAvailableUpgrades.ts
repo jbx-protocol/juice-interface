e2a3c5f58dab527b6df33039ced09e3d051f2301
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-03-01 16:34:21 +1030

    Feature-flag jb 3_1 upgrade

--- src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectUpgradesPage/versions/useAvailableUpgrades.ts
+++ src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectUpgradesPage/versions/useAvailableUpgrades.ts
@@ -1,8 +1,10 @@
 import { CV_V3 } from 'constants/cv'
+import { FEATURE_FLAGS } from 'constants/featureFlags'
 import { V2V3ContractsContext } from 'contexts/v2v3/Contracts/V2V3ContractsContext'
 import { V2V3ProjectContractsContext } from 'contexts/v2v3/ProjectContracts/V2V3ProjectContractsContext'
 import { useContext } from 'react'
 import { isEqualAddress } from 'utils/address'
+import { featureFlagEnabled } from 'utils/featureFlags'
 import { JBUpgrade } from './upgrades'
 
 export function useAvailableUpgrades(): JBUpgrade[] | undefined {
@@ -16,6 +18,7 @@ export function useAvailableUpgrades(): JBUpgrade[] | undefined {
   }
 
   if (
+    featureFlagEnabled(FEATURE_FLAGS.JB_V3_1_UPGRADE) &&
     projectContracts?.JBController &&
     projectContracts?.JBETHPaymentTerminal &&
     contracts?.JBController3_1 &&

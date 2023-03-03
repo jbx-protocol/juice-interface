8db36f6d3095a59023c0ed539e79ef9f61ce38ae
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-03-03 16:04:13 +1030

    ship

--- src/hooks/ContractReader/ContractReader.ts
+++ src/hooks/ContractReader/ContractReader.ts
@@ -8,7 +8,7 @@ export interface ContractReaderProps<ContractName extends string, V> {
   contract: ContractConfig<ContractName> | undefined
   contracts: Record<ContractName, Contract> | undefined
   functionName: string | undefined
-  args?: unknown[] | null
+  args?: unknown[] | null // use null when you don't want the function to run.
   updateOn?: ContractUpdateOn<ContractName>[]
   formatter?: (val?: any) => V | undefined // eslint-disable-line @typescript-eslint/no-explicit-any
   callback?: (val?: V) => void

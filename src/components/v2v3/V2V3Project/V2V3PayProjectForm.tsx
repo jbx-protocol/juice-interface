37c59e5801007f3c95193490f2811948a23a0ba5
Johnny D <johnnyD.eth@protonmail.com>
2023-03-03 14:41:49 +1000

    Cleanup tech debt from new project tabs (#3135)

---
+++ src/components/v2v3/V2V3Project/V2V3PayProjectForm.tsx
@@ -0,0 +1,14 @@
+import { PayProjectForm } from 'components/Project/PayProjectForm'
+import { V2V3ProjectContext } from 'contexts/v2v3/Project/V2V3ProjectContext'
+import { useValidatePrimaryEthTerminal } from 'hooks/v2v3/ValidatePrimaryEthTerminal'
+import { useContext } from 'react'
+
+export function V2V3PayProjectForm() {
+  const { fundingCycle } = useContext(V2V3ProjectContext)
+  const hasCurrentFundingCycle = fundingCycle?.number.gt(0)
+  const isPrimaryETHTerminalValid = useValidatePrimaryEthTerminal()
+
+  const disabled = !hasCurrentFundingCycle || !isPrimaryETHTerminalValid
+
+  return <PayProjectForm disabled={disabled} />
+}

0693a7426296adff7464563a8423e9c6eb0718ce
Wraeth <wraeth.dao@gmail.com>
2023-03-05 08:21:24 +1100

    User authorization to DB via supabase and email confirmation

---
+++ src/pages/api/account/add-email.page.ts
@@ -0,0 +1,28 @@
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const supabase = createServerSupabaseClient({ req, res })
+    const session = await supabase.auth.getSession()
+    if (!session.data.session) {
+      return res.status(401).json({ message: 'Unauthorized.' })
+    }
+    if (req.method !== 'POST') {
+      return res.status(405).json({ message: 'Method not allowed.' })
+    }
+    const { email } = req.body ?? {}
+    if (!email) {
+      return res.status(400).json({ message: 'Invalid request.' })
+    }
+
+    const result = await supabase.auth.updateUser({ email })
+    if (result.error) throw result.error
+    return res.status(200).json('Success')
+  } catch (e) {
+    console.error('Error occurred', e)
+    return res.status(500).json({ message: 'Internal server error occurred.' })
+  }
+}
+
+export default handler

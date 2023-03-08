3d652c35127534052845fd5b9f117a86cd181dc8
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-03-08 14:00:52 +1030

    Address peri review feedback

--- src/hooks/v2v3/transactor/DistributePayouts.ts
+++ src/hooks/v2v3/transactor/DistributePayouts.ts
@@ -12,26 +12,67 @@ import { V2V3ProjectContractsContext } from 'contexts/v2v3/ProjectContracts/V2V3
 import { TransactorInstance } from 'hooks/Transactor'
 import { V2V3CurrencyOption } from 'models/v2v3/currencyOption'
 import { useContext } from 'react'
+import invariant from 'tiny-invariant'
 import { useV2ProjectTitle } from '../ProjectTitle'
-import { ETH_PAYMENT_TERMINAL_V_3_1 } from '../V2V3ProjectContracts/projectContractLoaders/ProjectPrimaryEthTerminal'
+import {
+  JBETHPaymentTerminalVersion,
+  JB_ETH_PAYMENT_TERMINAL_V_3,
+  JB_ETH_PAYMENT_TERMINAL_V_3_1,
+} from '../V2V3ProjectContracts/projectContractLoaders/ProjectPrimaryEthTerminal'
 
 interface DistributePayoutsTxBaseParams {
   amount: BigNumber | undefined
   currency: V2V3CurrencyOption | undefined
 }
 
-type DistributePayoutsTx3Params = DistributePayoutsTxBaseParams & {
+type DISTRIBUTE_PAYOUTS_TX_V3_PARAMS = DistributePayoutsTxBaseParams & {
   memo?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-type DistributePayoutsTx3_1Params = DistributePayoutsTxBaseParams & {
+type DISTRIBUTE_PAYOUTS_TX_V3_1_PARAMS = DistributePayoutsTxBaseParams & {
   metadata?: string // JB terminal 3.1 replaced `memo` with `metadata`
 }
 
-type DistributePayoutsTx = TransactorInstance<
-  DistributePayoutsTx3Params | DistributePayoutsTx3_1Params
->
+type DistributePayoutsTxArgs =
+  | DISTRIBUTE_PAYOUTS_TX_V3_PARAMS
+  | DISTRIBUTE_PAYOUTS_TX_V3_1_PARAMS
+
+type DistributePayoutsTx = TransactorInstance<DistributePayoutsTxArgs>
+
+/**
+ * Factory function for building the arguments for the `distributePayoutsOf` function, depending on the version of the JBETHPaymentTerminal contract.
+ */
+function buildTxArgs({
+  JBETHPaymentTerminalVersion,
+  projectId,
+  args,
+}: {
+  JBETHPaymentTerminalVersion: JBETHPaymentTerminalVersion | undefined
+  projectId: number
+  args: DistributePayoutsTxArgs
+}) {
+  const baseArgs = [
+    projectId,
+    args.amount,
+    args.currency,
+    ETH_TOKEN_ADDRESS, // _token
+    DEFAULT_MIN_RETURNED_TOKENS, // _minReturnedTokens
+  ]
+
+  if (JBETHPaymentTerminalVersion === JB_ETH_PAYMENT_TERMINAL_V_3) {
+    return [
+      ...baseArgs,
+      (args as DISTRIBUTE_PAYOUTS_TX_V3_PARAMS).memo ?? DEFAULT_MEMO, // _memo
+    ]
+  }
+
+  if (JBETHPaymentTerminalVersion === JB_ETH_PAYMENT_TERMINAL_V_3_1) {
+    return [
+      ...baseArgs,
+      (args as DISTRIBUTE_PAYOUTS_TX_V3_1_PARAMS).metadata ?? DEFAULT_METADATA, // _metadata
+    ]
+  }
+}
 
 export function useDistributePayoutsTx(): DistributePayoutsTx {
   const { transactor } = useContext(TransactionContext)
@@ -52,19 +93,18 @@ export function useDistributePayoutsTx(): DistributePayoutsTx {
       return Promise.resolve(false)
     }
 
+    const txArgs = buildTxArgs({
+      JBETHPaymentTerminalVersion: versions.JBETHPaymentTerminal,
+      projectId,
+      args,
+    })
+
+    invariant(txArgs)
+
     return transactor(
       contracts.JBETHPaymentTerminal,
       'distributePayoutsOf',
-      [
-        projectId,
-        args.amount,
-        args.currency,
-        ETH_TOKEN_ADDRESS, // _token
-        DEFAULT_MIN_RETURNED_TOKENS, // _minReturnedTokens
-        versions.JBETHPaymentTerminal === ETH_PAYMENT_TERMINAL_V_3_1
-          ? (args as DistributePayoutsTx3_1Params).metadata ?? DEFAULT_METADATA // _metadata
-          : (args as DistributePayoutsTx3Params).memo ?? DEFAULT_MEMO, // _memo
-      ],
+      txArgs,
       {
         ...txOpts,
         title: t`Send ${projectTitle} payouts`,

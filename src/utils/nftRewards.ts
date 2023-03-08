165dbc56d41ce96b992382cc460ab093722074a0
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-03-08 13:06:51 +1030

    Fix ts

--- src/utils/nftRewards.ts
+++ src/utils/nftRewards.ts
@@ -2,7 +2,6 @@ import { BigNumber } from '@ethersproject/bignumber'
 import * as constants from '@ethersproject/constants'
 import { NftFileType } from 'components/inputs/UploadNoStyle'
 import {
-  DEFAULT_ALLOW_OVERSPENDING,
   JB721DELAGATE_V1_1_PAY_METADATA,
   JB721DELAGATE_V1_PAY_METADATA,
 } from 'components/Project/PayProjectForm/hooks/PayProjectForm'
@@ -10,6 +9,7 @@ import { VIDEO_FILE_TYPES } from 'constants/fileTypes'
 import { juiceboxEmojiImageUri } from 'constants/images'
 import { readNetwork } from 'constants/networks'
 import { WAD_DECIMALS } from 'constants/numbers'
+import { DEFAULT_ALLOW_OVERSPENDING } from 'constants/transactionDefaults'
 import { DEFAULT_NFT_MAX_SUPPLY } from 'contexts/NftRewards/NftRewards'
 import { defaultAbiCoder, parseEther } from 'ethers/lib/utils'
 import { pinJson } from 'lib/api/ipfs'

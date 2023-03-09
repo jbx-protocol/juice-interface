82c78c63ed851ca55e5727aa94ed50d4a60ab45d
peripheralist <peripheralist@gmail.com>
2023-03-08 18:26:25 -0700

    basic working leaderboard

---
+++ src/components/Leaderboard/index.ts
@@ -0,0 +1,1 @@
+export { Leaderboard } from './Leaderboard'
---
+++ src/pages/leaderboard/index.page.tsx
@@ -0,0 +1,10 @@
+import { AppWrapper } from 'components/common'
+import { Leaderboard } from 'components/Leaderboard'
+
+export default function LeaderboardPage() {
+  return (
+    <AppWrapper>
+      <Leaderboard />
+    </AppWrapper>
+  )
+}

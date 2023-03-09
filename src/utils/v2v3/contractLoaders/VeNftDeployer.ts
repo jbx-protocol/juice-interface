5e508e656af198555c639ffb5a25ae81901bbcc3
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-03-09 18:20:55 +1030

    asd

--- src/utils/v2v3/contractLoaders/VeNftDeployer.ts
+++ src/utils/v2v3/contractLoaders/VeNftDeployer.ts
@@ -1,24 +1,7 @@
-import { FEATURE_FLAGS } from 'constants/featureFlags'
-import { VENFT_DEPLOYER_ADDRESS } from 'constants/contracts/goerli/veNftProject'
-import { featureFlagEnabled } from 'utils/featureFlags'
-
+/**
+ *
+ * @deprecated ve nft is going to be removed.
+ */
 export const loadVeNftDeployer = async () => {
-  if (!featureFlagEnabled(FEATURE_FLAGS.VENFT)) return
-
-  const contractJson = {
-    abi: (
-      await import(
-        `@jbx-protocol/ve-nft/out/JBVeNftDeployer.sol/JBVeNftDeployer.json`
-      )
-    ).abi,
-    address: VENFT_DEPLOYER_ADDRESS,
-    // TODO: replace when broadcast is updated
-    // address: (
-    //   (await import(
-    //     `@jbx-protocol/ve-nft/broadcast/deploy.sol/${NETWORKS_BY_NAME[network].chainId}/run-latest.json`
-    //   )) as ForgeDeploy
-    // ).receipts[0].contractAddress,
-  }
-
-  return contractJson
+  return { address: undefined, abi: undefined }
 }

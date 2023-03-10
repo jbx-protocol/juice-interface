d62a258dcd74cba9e04b2dc243814251bb435374
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-03-10 17:35:00 +1100

    [1/n] better contrast on dark mode (#3184)

--- src/components/Create/components/pages/ReviewDeploy/components/RulesReview/RulesReview.tsx
+++ src/components/Create/components/pages/ReviewDeploy/components/RulesReview/RulesReview.tsx
@@ -1,13 +1,9 @@
 import { t } from '@lingui/macro'
-import { Row } from 'antd'
 import FormattedAddress from 'components/FormattedAddress'
-import useMobile from 'hooks/Mobile'
-import { DescriptionCol } from '../DescriptionCol'
+import { ReviewDescription } from '../ReviewDescription'
 import { useRulesReview } from './hooks/RulesReview'
-import { MobileRulesReview } from './MobileRulesReview'
 
 export const RulesReview = () => {
-  const isMobile = useMobile()
   const {
     customAddress,
     pausePayments,
@@ -20,78 +16,52 @@ export const RulesReview = () => {
   } = useRulesReview()
 
   return (
-    <div className="flex flex-col gap-10 pt-5 pb-8">
-      {isMobile ? (
-        <MobileRulesReview />
-      ) : (
-        <>
-          <Row gutter={20}>
-            <DescriptionCol
-              span={5}
-              title={t`Reconfiguration`}
-              desc={
-                <div className="text-base font-medium">
-                  {strategy ? (
-                    strategy
-                  ) : customAddress ? (
-                    <FormattedAddress address={customAddress} />
-                  ) : (
-                    '??'
-                  )}
-                </div>
-              }
-            />
-            <DescriptionCol
-              span={5}
-              title={t`Payments`}
-              desc={
-                <div className="text-base font-medium">{pausePayments}</div>
-              }
-            />
-            <DescriptionCol
-              span={5}
-              title={t`Hold fees`}
-              desc={<div className="text-base font-medium">{holdFees}</div>}
-            />
-          </Row>
-          <Row gutter={20}>
-            <DescriptionCol
-              span={5}
-              title={t`Terminal configuration`}
-              desc={
-                <div className="text-base font-medium">
-                  {terminalConfiguration}
-                </div>
-              }
-            />
-            <DescriptionCol
-              span={5}
-              title={t`Controller configuration`}
-              desc={
-                <div className="text-base font-medium">
-                  {controllerConfiguration}
-                </div>
-              }
-            />
-            <DescriptionCol
-              span={5}
-              title={t`Terminal migration`}
-              desc={
-                <div className="text-base font-medium">{terminalMigration}</div>
-              }
-            />
-            <DescriptionCol
-              span={5}
-              title={t`Controller migration`}
-              desc={
-                <div className="text-base font-medium">
-                  {controllerMigration}
-                </div>
-              }
-            />
-          </Row>
-        </>
-      )}
+    <div className="flex flex-col gap-y-10 pt-5 pb-8 md:grid md:grid-cols-4">
+      <ReviewDescription
+        title={t`Reconfiguration`}
+        desc={
+          <div className="text-base font-medium">
+            {strategy ? (
+              strategy
+            ) : customAddress ? (
+              <FormattedAddress address={customAddress} />
+            ) : (
+              '??'
+            )}
+          </div>
+        }
+      />
+      <ReviewDescription
+        title={t`Payments`}
+        desc={<div className="text-base font-medium">{pausePayments}</div>}
+      />
+      <ReviewDescription
+        className="col-span-2"
+        title={t`Hold fees`}
+        desc={<div className="text-base font-medium">{holdFees}</div>}
+      />
+      <ReviewDescription
+        title={t`Terminal configuration`}
+        desc={
+          <div className="text-base font-medium">{terminalConfiguration}</div>
+        }
+      />
+      <ReviewDescription
+        title={t`Controller configuration`}
+        desc={
+          <div className="text-base font-medium">{controllerConfiguration}</div>
+        }
+      />
+      <ReviewDescription
+        title={t`Terminal migration`}
+        desc={<div className="text-base font-medium">{terminalMigration}</div>}
+      />
+      <ReviewDescription
+        title={t`Controller migration`}
+        desc={
+          <div className="text-base font-medium">{controllerMigration}</div>
+        }
+      />
     </div>
   )
 }

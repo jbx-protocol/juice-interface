7bc994f68c6298632661983dd4cb4dff659acfac
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-03-12 12:43:07 +1100

    twitter validation on front end (#3200)

--- src/components/AccountSettingsDashboard/lib/AccountSettingsSchema.ts
+++ src/components/AccountSettingsDashboard/lib/AccountSettingsSchema.ts
@@ -4,5 +4,12 @@ export const AccountSettingsSchema = Yup.object().shape({
   bio: Yup.string(),
   email: Yup.string().email('Invalid email'),
   website: Yup.string().url('Invalid URL'),
-  twitter: Yup.string(),
+
+  // https://help.twitter.com/en/managing-your-account/twitter-username-rules#:~:text=Your%20username%20cannot%20be%20longer,of%20underscores%2C%20as%20noted%20above.
+  twitter: Yup.string()
+    .matches(/^[a-zA-z0-9_]{1,15}$/, {
+      message: 'Invalid Twitter handle',
+    })
+    .min(4, 'Invalid Twitter handle')
+    .max(15, 'Invalid Twitter handle'),
 })

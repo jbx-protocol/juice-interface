58e44ec14ddbfed822bcfb4cb5361fd1194b29ac
peripheralist <peripheralist@gmail.com>
2023-03-16 21:07:32 -0600

    setFundAccessConstraints event elems show 'unlimited' if distribution limit is max

--- src/components/v2v3/V2V3Project/ProjectActivity/eventElems/SetFundAccessConstraintsEventElem.tsx
+++ src/components/v2v3/V2V3Project/ProjectActivity/eventElems/SetFundAccessConstraintsEventElem.tsx
@@ -4,6 +4,7 @@ import CurrencySymbol from 'components/CurrencySymbol'
 import { SetFundAccessConstraintsEvent } from 'models/subgraph-entities/v2/set-fund-access-constraints-event'
 import { formatWad } from 'utils/format/formatNumber'
 import { V2V3CurrencyName } from 'utils/v2v3/currency'
+import { MAX_DISTRIBUTION_LIMIT } from 'utils/v2v3/math'
 
 export default function SetFundAccessConstraintsEventElem({
   event,
@@ -28,10 +29,16 @@ export default function SetFundAccessConstraintsEventElem({
       header={t`Edited payout`}
       subject={
         <div>
-          <CurrencySymbol
-            currency={V2V3CurrencyName(event.distributionLimitCurrency)}
-          />
-          {formatWad(event.distributionLimit)}
+          {event.distributionLimit.eq(MAX_DISTRIBUTION_LIMIT) ? (
+            t`Unlimited`
+          ) : (
+            <>
+              <CurrencySymbol
+                currency={V2V3CurrencyName(event.distributionLimitCurrency)}
+              />
+              {formatWad(event.distributionLimit)}
+            </>
+          )}
         </div>
       }
     />

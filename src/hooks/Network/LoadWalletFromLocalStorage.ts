0cdb93af8046bf9db7f63e2f782bb39cbc89ffd1
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-03-20 15:47:04 +1100

    fix! metamask popup no longer re-appears when connecting wallet after lock (#3244)

--- src/hooks/Network/LoadWalletFromLocalStorage.ts
+++ src/hooks/Network/LoadWalletFromLocalStorage.ts
@@ -1,10 +1,28 @@
+import { Web3Provider } from '@ethersproject/providers'
 import { useWallet } from 'hooks/Wallet'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 
 export function useLoadWalletFromLocalStorage() {
   const { connect } = useWallet()
 
+  const ethereumWebProvider = useMemo(() => {
+    if (typeof window === 'undefined') return
+    const { ethereum } = window
+    if (typeof ethereum === 'undefined') return
+
+    return new Web3Provider(ethereum)
+  }, [])
+
+  // Returns if a wallet is currently connected to Juicebox.
+  const walletIsAvailable = useCallback(async () => {
+    const accounts = (await ethereumWebProvider?.listAccounts()) ?? []
+    return accounts.length > 0
+  }, [ethereumWebProvider])
+
   const loadWalletFromLocalStorage = useCallback(async () => {
+    // Check to see if a wallet has been connected before, otherwise don't worry about it.
+    if (!(await walletIsAvailable())) return
+
     let previouslyConnectedWallets
     const rawConnectedWallets = localStorage.getItem('connectedWallets')
     if (rawConnectedWallets) {
@@ -14,7 +32,7 @@ export function useLoadWalletFromLocalStorage() {
       const wallet = previouslyConnectedWallets[0]
       await connect({ autoSelect: { label: wallet, disableModals: true } })
     }
-  }, [connect])
+  }, [connect, walletIsAvailable])
 
   return loadWalletFromLocalStorage
 }

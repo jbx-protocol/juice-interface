bcdd4d096faaecef4c526f2bca6e1923995164e8
peripheralist <peripheralist@gmail.com>
2023-03-25 14:11:18 -0600

    project header tags layout

--- src/components/Project/ProjectHeader/ProjectHeader.tsx
+++ src/components/Project/ProjectHeader/ProjectHeader.tsx
@@ -26,7 +26,7 @@ function ProjectSubheading({
   projectOwnerAddress: string | undefined
   canEditProjectHandle?: boolean
 }) {
-  const { projectId, projectMetadata } = useContext(ProjectMetadataContext)
+  const { projectId } = useContext(ProjectMetadataContext)
 
   const { data: gnosisSafe, isLoading: gnosisSafeLoading } =
     useGnosisSafe(projectOwnerAddress)
@@ -80,21 +80,6 @@ function ProjectSubheading({
           </div>
         </>
       )}
-
-      {projectMetadata?.tags?.length ? (
-        <>
-          <Divider
-            type="vertical"
-            className="m-0 h-6 bg-grey-100 dark:bg-grey-900"
-          />
-          <span className="mr-2">
-            <ProjectTagsRow
-              tags={projectMetadata.tags}
-              tagClassName="text-xs"
-            />
-          </span>
-        </>
-      ) : null}
     </div>
   )
 }
@@ -187,6 +172,12 @@ export function ProjectHeader({
               <div className="hidden md:block">
                 <Divider type="vertical" className="h-8" />
               </div>
+            ) : null}
+            {projectMetadata?.tags?.length ? (
+              <ProjectTagsRow
+                tags={projectMetadata.tags}
+                tagClassName="text-xs"
+              />
             ) : null}
             {actions && (
               <div className="flex w-full justify-center">{actions}</div>

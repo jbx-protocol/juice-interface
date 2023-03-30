b435084c861a22e4fa64ac9d131299cd51823246
Wraeth <wraeth.dao@gmail.com>
2023-03-31 09:00:13 +1100

    Add new subscription button to project header

--- src/components/v2v3/V2V3Project/V2V3ProjectHeaderActions/V2V3ProjectHeaderActions.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectHeaderActions/V2V3ProjectHeaderActions.tsx
@@ -1,6 +1,7 @@
 import { SettingOutlined, ToolOutlined } from '@ant-design/icons'
 import { t, Trans } from '@lingui/macro'
 import { Button, Tooltip } from 'antd'
+import { SubscribeButton } from 'components/SubscribeButton'
 import { V2V3ProjectToolsDrawer } from 'components/v2v3/V2V3Project/V2V3ProjectToolsDrawer'
 import { ProjectMetadataContext } from 'contexts/shared/ProjectMetadataContext'
 import { V2V3ProjectContext } from 'contexts/v2v3/Project/V2V3ProjectContext'
@@ -21,11 +22,14 @@ export function V2V3ProjectHeaderActions() {
 
   const { projectId } = useContext(ProjectMetadataContext)
 
+  if (projectId === undefined) return null
+
   return (
     <>
       <div className="flex items-center">
         <div className="flex items-center gap-x-4">
-          <div>
+          <SubscribeButton projectId={projectId} tooltipPlacement={'bottom'} />
+          <>
             <ContractVersionSelect />
             <Tooltip title={t`Project tools`} placement="bottom">
               <Button
@@ -34,7 +38,7 @@ export function V2V3ProjectHeaderActions() {
                 type="text"
               />
             </Tooltip>
-          </div>
+          </>
           {canReconfigure && (
             <div>
               <Link href={settingsPagePath('general', { handle, projectId })}>

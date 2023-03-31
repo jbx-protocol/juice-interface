8570961a80f3e967bb3ae265651a00d998551d45
Wraeth <wraeth.dao@gmail.com>
2023-03-31 12:37:16 +1100

    ensure fk on user_subscriptions

--- supabase/migrations/20230330212553_project_subscription_impl.sql
+++ supabase/migrations/20230330212553_project_subscription_impl.sql
@@ -41,5 +41,7 @@ to public
 using (((auth.jwt() ->> 'sub'::text) = (user_id)::text))
 with check (((auth.jwt() ->> 'sub'::text) = (user_id)::text));
 
+alter table "public"."user_subscriptions" add constraint "user_subscriptions_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) not valid;
 
+alter table "public"."user_subscriptions" validate constraint "user_subscriptions_user_id_fkey";
 

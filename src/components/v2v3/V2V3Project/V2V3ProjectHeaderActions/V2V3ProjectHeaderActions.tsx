3445f60e64462c54b4247bcb299b6cffeaacbfc6
Wraeth <wraeth.dao@gmail.com>
2023-04-03 14:58:28 +1000

    remove fragment

--- src/components/v2v3/V2V3Project/V2V3ProjectHeaderActions/V2V3ProjectHeaderActions.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectHeaderActions/V2V3ProjectHeaderActions.tsx
@@ -1,5 +1,5 @@
 import { SettingOutlined, ToolOutlined } from '@ant-design/icons'
-import { t, Trans } from '@lingui/macro'
+import { Trans, t } from '@lingui/macro'
 import { Button, Tooltip } from 'antd'
 import { SubscribeButton } from 'components/SubscribeButton'
 import { V2V3ProjectToolsDrawer } from 'components/v2v3/V2V3Project/V2V3ProjectToolsDrawer'
@@ -29,16 +29,14 @@ export function V2V3ProjectHeaderActions() {
       <div className="flex items-center">
         <div className="flex items-center gap-x-4">
           <SubscribeButton projectId={projectId} tooltipPlacement={'bottom'} />
-          <>
-            <ContractVersionSelect />
-            <Tooltip title={t`Project tools`} placement="bottom">
-              <Button
-                onClick={() => setToolDrawerVisible(true)}
-                icon={<ToolOutlined />}
-                type="text"
-              />
-            </Tooltip>
-          </>
+          <ContractVersionSelect />
+          <Tooltip title={t`Project tools`} placement="bottom">
+            <Button
+              onClick={() => setToolDrawerVisible(true)}
+              icon={<ToolOutlined />}
+              type="text"
+            />
+          </Tooltip>
           {canReconfigure && (
             <div>
               <Link href={settingsPagePath('general', { handle, projectId })}>

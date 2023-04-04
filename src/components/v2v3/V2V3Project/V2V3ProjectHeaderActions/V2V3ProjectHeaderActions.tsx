2eba13b36a6ae74c57081a264151f7e2a9f444f3
Wraeth <wraeth.dao@gmail.com>
2023-04-04 11:47:11 +1000

    refactor: change button to get updates

--- src/components/v2v3/V2V3Project/V2V3ProjectHeaderActions/V2V3ProjectHeaderActions.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectHeaderActions/V2V3ProjectHeaderActions.tsx
@@ -1,6 +1,6 @@
 import { SettingOutlined, ToolOutlined } from '@ant-design/icons'
 import { Trans, t } from '@lingui/macro'
-import { Button, Tooltip } from 'antd'
+import { Button, Divider, Tooltip } from 'antd'
 import { SubscribeButton } from 'components/SubscribeButton'
 import { V2V3ProjectToolsDrawer } from 'components/v2v3/V2V3Project/V2V3ProjectToolsDrawer'
 import { ProjectMetadataContext } from 'contexts/shared/ProjectMetadataContext'
@@ -30,6 +30,7 @@ export function V2V3ProjectHeaderActions() {
         <div className="flex items-center gap-x-4">
           <SubscribeButton projectId={projectId} tooltipPlacement={'bottom'} />
           <ContractVersionSelect />
+          <Divider className="h-8" type="vertical" />
           <Tooltip title={t`Project tools`} placement="bottom">
             <Button
               onClick={() => setToolDrawerVisible(true)}

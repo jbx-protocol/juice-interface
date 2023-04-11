3126c503b743584b2b1b0235a9d8df3cfae965a8
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-04-11 13:26:51 +0930

    Make eth amount even prettier

--- src/components/currency/ETHAmount.tsx
+++ src/components/currency/ETHAmount.tsx
@@ -1,13 +1,12 @@
-import { Tooltip } from 'antd'
-
 import { BigNumber } from '@ethersproject/bignumber'
+import { Tooltip } from 'antd'
+import { PRECISION_ETH } from 'constants/currency'
 import { betweenZeroAndOne } from 'utils/bigNumbers'
 import { formatWad, fromWad } from 'utils/format/formatNumber'
-
 import CurrencySymbol from '../CurrencySymbol'
-
-import { PRECISION_ETH } from 'constants/currency'
 import ETHToUSD from './ETHToUSD'
+
+const MIN_ETH_PRECISION = 5
 
 /**
  * Render a given amount formatted as ETH. Displays USD amount in a tooltip on hover.
@@ -36,9 +35,10 @@ export default function ETHAmount({
   }
 
   const decimalsCount = fromWad(amount).split('.')[1]?.length ?? 0
+  const isBelowZero = betweenZeroAndOne(amount)
 
-  const precisionAdjusted = betweenZeroAndOne(amount)
-    ? Math.min(decimalsCount, PRECISION_ETH)
+  const precisionAdjusted = isBelowZero
+    ? Math.min(MIN_ETH_PRECISION, Math.max(decimalsCount, PRECISION_ETH))
     : precision
 
   const formattedETHAmount = formatWad(amount, {
@@ -46,11 +46,30 @@ export default function ETHAmount({
     padEnd,
   })
 
+  if (isBelowZero) {
+    return (
+      <Tooltip
+        title={
+          <span>
+            <CurrencySymbol currency="ETH" />
+            {formatWad(amount)}
+          </span>
+        }
+        open={hideTooltip ? !hideTooltip : undefined}
+      >
+        {formattedETHAmount === '0' ? '~' : null}
+        <CurrencySymbol currency="ETH" />
+        {formattedETHAmount}
+      </Tooltip>
+    )
+  }
+
   return (
     <Tooltip
       title={<ETHToUSD ethAmount={amount} />}
       open={hideTooltip ? !hideTooltip : undefined}
     >
+      {formattedETHAmount === '0' ? '~' : null}
       <CurrencySymbol currency="ETH" />
       {formattedETHAmount}
     </Tooltip>
--- src/components/v2v3/shared/V2V3CurrencyAmount.tsx
+++ src/components/v2v3/shared/V2V3CurrencyAmount.tsx
@@ -19,11 +19,11 @@ export default function V2V3CurrencyAmount({
   if (amount === undefined) return null
 
   if (currency?.eq(NO_CURRENCY) || currency?.eq(V2V3_CURRENCY_ETH)) {
-    return <ETHAmount amount={amount} precision={precision} padEnd />
+    return <ETHAmount amount={amount} precision={precision} />
   }
 
   if (currency?.eq(V2V3_CURRENCY_USD)) {
-    return <USDAmount amount={amount} precision={precision} padEnd />
+    return <USDAmount amount={amount} precision={precision} />
   }
 
   return null

42c5a62823724b343fe5a7aa11f9ddb6a466ef2b
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-04-13 13:19:54 +1000

    feat: reskin announcement introduction modals (#3390)
    ## What does this PR do and why?
    
    ## Testing instructions:
    
    New subgraph URL: <INSERT>
    
    ## Subgraph QA checklist
    
    - [x] V1 project 
       - [x] page renders
       - [x] shows volume
       - [x] shows activity feed
       - [x] can filter activity feed
       - [x] can download activity feed CSV 
    - [x] V3 Project on JB 3.0 renders
    - [x] V3 Project on JB 3.1 renders
       - [x] page renders
       - [x] shows volume
       - [x] shows activity feed
       - [x] can filter activity feed
       - [x] can download activity feed CSV 
    - [x] V3 Project **with a handle** renders
    - [x] Homepage 
       - [x] Trending projects renders
       - [x] Homepage activity feed renders

--- src/components/announcements/IntroProjectTagsAnnouncement.tsx
+++ src/components/announcements/IntroProjectTagsAnnouncement.tsx
@@ -1,10 +1,9 @@
 import { Trans, t } from '@lingui/macro'
 import { JuiceModalProps } from 'components/JuiceModal'
 import { ProjectTagsRow } from 'components/ProjectTagsRow'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
-import { Announcement } from './Announcement'
+import { NewFeatureAnnouncement } from './NewFeatureAnnouncement'
 
 export const IntroProjectTagsAnnouncement = (
   props: Pick<JuiceModalProps, 'open' | 'setOpen'>,
@@ -16,32 +15,22 @@ export const IntroProjectTagsAnnouncement = (
   }, [router])
 
   return (
-    <Announcement
+    <NewFeatureAnnouncement
       {...props}
-      title={
-        <span className="font-heading text-2xl font-bold">
-          <span className="text-juice-500">
-            <Trans>NEW</Trans>
-          </span>
-          : <Trans>Project tags</Trans>
-        </span>
-      }
+      title={t`Project tags`}
       position="topRight"
       okText={t`Go to settings`}
       cancelText={t`Got it`}
       onOk={onOk}
     >
-      <ProjectTagsRow />
       <p className="mt-2">
         <Trans>
-          You can now add tags in{' '}
-          <Link href="settings?page=general" as="settings?page=general">
-            settings
-          </Link>{' '}
-          to describe your project, and help supporters find it. Anyone can
-          search for projects by tag on the Explore page.
+          You can now add tags in settings to describe your project, and help
+          supporters find it. Anyone can search for projects by tag on the
+          Explore page.
         </Trans>
       </p>
-    </Announcement>
+      <ProjectTagsRow />
+    </NewFeatureAnnouncement>
   )
 }
---
+++ src/components/announcements/NewFeatureAnnouncement.tsx
@@ -0,0 +1,24 @@
+import { Trans } from '@lingui/macro'
+import { JuiceModalProps } from 'components/JuiceModal'
+import { Announcement } from './Announcement'
+
+export const NewFeatureAnnouncement: React.FC<JuiceModalProps> = props => {
+  return (
+    <Announcement
+      {...props}
+      title={
+        <>
+          <div className="text-secondary mb-4 text-xs uppercase">
+            <Trans>Introducing</Trans>
+          </div>
+          <div className="text-primary font-heading text-2xl">
+            {props.title}
+          </div>
+        </>
+      }
+      position="topRight"
+    >
+      {props.children}
+    </Announcement>
+  )
+}

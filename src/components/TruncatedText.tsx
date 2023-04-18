33956ad03bae0f6113a56c7291a3aa01a8141896
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-04-18 17:20:26 +1000

    feat: add new TruncatedText and add to BuiltBy section (#3374)

---
+++ src/components/TruncatedText.tsx
@@ -0,0 +1,59 @@
+import { Tooltip } from 'antd'
+import { useEffect, useRef, useState } from 'react'
+
+interface TruncatedTextProps {
+  text: string
+  className?: string
+  placement?:
+    | 'top'
+    | 'topRight'
+    | 'right'
+    | 'bottomRight'
+    | 'bottom'
+    | 'bottomLeft'
+    | 'left'
+    | 'topLeft'
+}
+
+export const TruncatedText: React.FC<TruncatedTextProps> = ({
+  text,
+  className,
+  placement,
+}) => {
+  const [show, setShow] = useState(false)
+  const textRef = useRef<HTMLDivElement>(null)
+  const [isTruncated, setIsTruncated] = useState(false)
+
+  const checkTruncated = () => {
+    if (!textRef.current) return
+
+    const { scrollWidth, clientWidth } = textRef.current
+    setIsTruncated(scrollWidth > clientWidth)
+  }
+
+  useEffect(() => {
+    checkTruncated()
+    window.addEventListener('resize', checkTruncated)
+
+    return () => {
+      window.removeEventListener('resize', checkTruncated)
+    }
+  }, [])
+
+  const open = show && isTruncated
+
+  return (
+    <Tooltip open={open} title={text} placement={placement}>
+      <div
+        ref={textRef}
+        className={`truncate ${className}`}
+        aria-label={isTruncated ? text : ''}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+        onClick={() => setShow(!show)}
+      >
+        {text}
+      </div>
+    </Tooltip>
+  )
+}

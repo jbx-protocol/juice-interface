d57b5368a7bd2514c7e63e92996d0722f4a0c94a
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-04-19 14:11:46 +0930

    fix: tick icon, gnosis logo

--- src/components/TickIconListItem.tsx
+++ src/components/TickIconListItem.tsx
@@ -1,10 +1,10 @@
-import { CheckIcon } from '@heroicons/react/24/solid'
+import { CheckIcon } from './icons/Check'
 
 export function TickIconListItem({ text }: { text: string | JSX.Element }) {
   return (
     <li className="flex items-center gap-x-3 py-2 text-lg">
-      <span className="rounded-full bg-bluebs-100 p-1 dark:bg-bluebs-900">
-        <CheckIcon className="h-5 w-5 text-bluebs-500" />
+      <span className="rounded-full bg-bluebs-100 p-2 dark:bg-bluebs-900">
+        <CheckIcon className="h-4 w-4 text-bluebs-500" />
       </span>
       <div>{text}</div>
     </li>

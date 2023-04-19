0bc1ca1e4462f656bd915b9da29d51b8bea499e9
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-04-19 19:29:02 +0930

    feat: stub success stories

--- src/pages/home/NewHomePage/SuccessStoriesSection/SuccessStoriesSection.tsx
+++ src/pages/home/NewHomePage/SuccessStoriesSection/SuccessStoriesSection.tsx
@@ -1,23 +1,46 @@
-import { t, Trans } from '@lingui/macro'
+import { Trans } from '@lingui/macro'
 import { XLButton } from 'components/XLButton'
-import useMobile from 'hooks/Mobile'
+import { PV_V1, PV_V2 } from 'constants/pv'
 import { useProjectsQuery } from 'hooks/Projects'
+import { GraphResult } from 'hooks/SubgraphQuery'
+import { Project } from 'models/subgraph-entities/vX/project'
 import Link from 'next/link'
-import { ProjectCarousel } from '../ProjectCarousel'
 import { SectionContainer } from '../SectionContainer'
 import { SectionHeading } from '../SectionHeading'
+import { SuccessStoriesCard } from './SuccessStoriesCard'
+
+const CASE_STUDY_PROJECTS = [
+  {
+    pv: PV_V1,
+    id: 36, // cdao
+  },
+  {
+    pv: PV_V1,
+    id: 199, // moondao
+  },
+  {
+    pv: PV_V1,
+    id: 7, // sharkdao
+  },
+  {
+    pv: PV_V2,
+    id: 311, // studiodao
+  },
+]
 
 export function SuccessStoriesSection() {
-  const { data: topProjects } = useProjectsQuery({
-    pageSize: 4,
+  const { data } = useProjectsQuery({
+    projectIds: CASE_STUDY_PROJECTS.map(p => p.id),
   })
 
-  const isMobile = useMobile()
+  const topProjects = CASE_STUDY_PROJECTS.map(p =>
+    data?.find(proj => proj.projectId === p.id && proj.pv === p.pv),
+  ).filter(Boolean) as GraphResult<'project', keyof Project>
 
   return (
     <SectionContainer>
       <SectionHeading
-        heading={t`Success stories`}
+        heading={<Trans>Success stories</Trans>}
         subheading={
           <Trans>
             Juicebox gives you the tools to automate web3 fundraising so you can
@@ -25,26 +48,19 @@ export function SuccessStoriesSection() {
           </Trans>
         }
       />
-      <div className="flex justify-center">
-        <ProjectCarousel projects={topProjects} />
+      <div className="flex flex-wrap justify-center gap-8">
+        {topProjects.map(project => (
+          <SuccessStoriesCard key={project.id} project={project} />
+        ))}
       </div>
-      <div className="w-full text-center">
-        <div className="mt-16 flex flex-col flex-wrap justify-center gap-3 md:flex-row">
-          {/* <Link href="/case-studies">
-            <a>
-              <XLButton size="large" block={isMobile}>
-                <Trans>Case studies</Trans>
-              </XLButton>
-            </a>
-          </Link> */}
-          <Link href="/create">
-            <a>
-              <XLButton size="large" type="primary" block={isMobile}>
-                <Trans>Create a project</Trans>
-              </XLButton>
-            </a>
-          </Link>
-        </div>
+      <div className="mt-16 w-full text-center">
+        <Link href="/create">
+          <a>
+            <XLButton size="large" type="primary">
+              <Trans>Create a project</Trans>
+            </XLButton>
+          </a>
+        </Link>
       </div>
     </SectionContainer>
   )
---
+++ src/pages/success-stories/constitutiondao.page.tsx
@@ -0,0 +1,9 @@
+import { AppWrapper } from 'components/common'
+
+export default function ConstitutionDAOPage() {
+  return (
+    <AppWrapper>
+      <h1>Hello</h1>
+    </AppWrapper>
+  )
+}

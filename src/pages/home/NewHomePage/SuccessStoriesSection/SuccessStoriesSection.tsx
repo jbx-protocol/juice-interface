337b7d87d1e01ef680c3eb794b0e29a002ac347b
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-04-19 19:56:10 +0930

    feat: tags on success cards

--- src/pages/home/NewHomePage/SuccessStoriesSection/SuccessStoriesSection.tsx
+++ src/pages/home/NewHomePage/SuccessStoriesSection/SuccessStoriesSection.tsx
@@ -2,7 +2,6 @@ import { Trans } from '@lingui/macro'
 import { XLButton } from 'components/XLButton'
 import { PV_V1, PV_V2 } from 'constants/pv'
 import { useProjectsQuery } from 'hooks/Projects'
-import { GraphResult } from 'hooks/SubgraphQuery'
 import { Project } from 'models/subgraph-entities/vX/project'
 import Link from 'next/link'
 import { SectionContainer } from '../SectionContainer'
@@ -13,18 +12,22 @@ const CASE_STUDY_PROJECTS = [
   {
     pv: PV_V1,
     id: 36, // cdao
+    tags: ['Fundraiser', 'DAO'],
   },
   {
     pv: PV_V1,
     id: 199, // moondao
+    tags: ['Fundraiser', 'DAO'],
   },
   {
     pv: PV_V1,
     id: 7, // sharkdao
+    tags: ['DAO'],
   },
   {
     pv: PV_V2,
     id: 311, // studiodao
+    tags: ['NFT', 'DAO'],
   },
 ]
 
@@ -33,9 +36,13 @@ export function SuccessStoriesSection() {
     projectIds: CASE_STUDY_PROJECTS.map(p => p.id),
   })
 
-  const topProjects = CASE_STUDY_PROJECTS.map(p =>
-    data?.find(proj => proj.projectId === p.id && proj.pv === p.pv),
-  ).filter(Boolean) as GraphResult<'project', keyof Project>
+  const topProjects = CASE_STUDY_PROJECTS.map(p => ({
+    project: data?.find(proj => proj.projectId === p.id && proj.pv === p.pv),
+    tags: p.tags,
+  })) as {
+    project: Project | undefined
+    tags: string[]
+  }[]
 
   return (
     <SectionContainer>
@@ -48,19 +55,36 @@ export function SuccessStoriesSection() {
           </Trans>
         }
       />
-      <div className="flex flex-wrap justify-center gap-8">
-        {topProjects.map(project => (
-          <SuccessStoriesCard key={project.id} project={project} />
-        ))}
+      <div className="flex justify-center">
+        <div className="flex gap-8 overflow-x-auto">
+          {topProjects.map(project =>
+            project.project ? (
+              <SuccessStoriesCard
+                key={project.project.projectId}
+                project={project.project}
+                tags={project.tags}
+              />
+            ) : null,
+          )}
+        </div>
       </div>
-      <div className="mt-16 w-full text-center">
-        <Link href="/create">
-          <a>
-            <XLButton size="large" type="primary">
-              <Trans>Create a project</Trans>
-            </XLButton>
-          </a>
-        </Link>
+      <div className="w-full text-center">
+        <div className="mt-16 flex flex-col flex-wrap justify-center gap-3 md:flex-row">
+          {/* <Link href="/case-studies">
+            <a>
+              <XLButton size="large" block={isMobile}>
+                <Trans>Case studies</Trans>
+              </XLButton>
+            </a>
+          </Link> */}
+          <Link href="/create">
+            <a>
+              <XLButton size="large" type="primary">
+                <Trans>Create a project</Trans>
+              </XLButton>
+            </a>
+          </Link>
+        </div>
       </div>
     </SectionContainer>
   )

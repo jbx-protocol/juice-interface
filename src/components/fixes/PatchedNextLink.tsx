dd2a235784f57c314ece43047e09bd1cba9bdd6a
Wraeth <wraeth.dao@gmail.com>
2023-04-20 17:28:11 +1000

    fix: patch next link errors and pass forward refs

---
+++ src/components/fixes/PatchedNextLink.tsx
@@ -0,0 +1,23 @@
+import Link, { LinkProps } from 'next/link'
+import { ForwardRefRenderFunction, forwardRef } from 'react'
+
+type PatchedNextLinkProps = LinkProps & {
+  children: React.ReactNode
+  className?: string
+}
+
+// https://headlessui.com/react/menu#integrating-with-next-js
+const PatchedNextLink: ForwardRefRenderFunction<
+  HTMLAnchorElement,
+  PatchedNextLinkProps
+> = (props, ref) => {
+  const { href, children, className, ...rest } = props
+  return (
+    <Link href={href}>
+      <a className={className} ref={ref} {...rest}>
+        {children}
+      </a>
+    </Link>
+  )
+}
+export default forwardRef(PatchedNextLink)

4a84e255dec0f7914d40974b1fc470f357386585
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-04-21 11:31:04 +0930

    feat: add rotate effect to why jb cards

--- src/pages/home/NewHomePage/WhyJuiceboxSection/WhyJuiceboxCard.tsx
+++ src/pages/home/NewHomePage/WhyJuiceboxSection/WhyJuiceboxCard.tsx
@@ -1,3 +1,5 @@
+import useMobile from 'hooks/Mobile'
+import { useRef } from 'react'
 import { twJoin } from 'tailwind-merge'
 
 export function WhyJuiceboxCard({
@@ -13,12 +15,36 @@ export function WhyJuiceboxCard({
   heading: string | JSX.Element
   content: string | JSX.Element
 }) {
+  const cardRef = useRef<HTMLDivElement>(null)
+  const isMobile = useMobile()
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!cardRef.current) return
+
+    const rect = cardRef.current.getBoundingClientRect()
+    const xAxis = (rect.width / 2 - (e.clientX - rect.left)) / 25
+    const yAxis = (rect.height / 2 - (e.clientY - rect.top)) / 25
+    cardRef.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+  }
+
+  const handleMouseLeave = () => {
+    if (!cardRef.current) return
+    cardRef.current.style.transform = 'rotateX(0) rotateY(0)'
+  }
+
   return (
     <div
+      ref={cardRef}
+      onMouseMove={!isMobile ? handleMouseMove : undefined}
+      onMouseLeave={!isMobile ? handleMouseLeave : undefined}
       className={twJoin(
-        'max-w-xs flex-shrink-0 rounded-lg px-5 py-8 text-center',
+        'max-w-xs flex-shrink-0 rounded-lg px-5 py-8 text-center shadow-sm transition-transform',
         className,
       )}
+      style={{
+        transformStyle: 'preserve-3d',
+        transformOrigin: 'center',
+      }}
     >
       <div className="flex w-full justify-center">
         <div

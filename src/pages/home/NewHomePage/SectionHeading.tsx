af479b7ad101370d794d7cc0942bc490f75d33fc
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-04-21 16:03:44 +0930

    feat: just more homepage polish

--- src/pages/home/NewHomePage/SectionHeading.tsx
+++ src/pages/home/NewHomePage/SectionHeading.tsx
@@ -6,7 +6,7 @@ export const SectionHeading: React.FC<{
   subheading?: string | JSX.Element
 }> = ({ className, heading, subheading }) => {
   return (
-    <div className="mx-auto mb-8 max-w-3xl">
+    <div className="mx-auto mb-12 max-w-3xl">
       <h2
         className={twMerge(
           'text-primary mb-6 text-center text-3xl leading-tight md:text-5xl',

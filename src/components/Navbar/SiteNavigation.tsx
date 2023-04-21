0ce7954ded0c67389d1e882e28ffdcb71180ab6f
Wraeth <wraeth.dao@gmail.com>
2023-04-21 14:32:15 +1000

    feat: mobile nav button change

--- src/components/Navbar/SiteNavigation.tsx
+++ src/components/Navbar/SiteNavigation.tsx
@@ -6,7 +6,7 @@ import PatchedNextLink from 'components/fixes/PatchedNextLink'
 import useMobile from 'hooks/Mobile'
 import { useWallet } from 'hooks/Wallet'
 import { DropdownMenu } from './components/DropdownMenu'
-import { HamburgerMenuButton } from './components/HamburgerMenuButton'
+import { MobileMenuButton } from './components/HamburgerMenuButton'
 import { LogoHomeButton } from './components/LogoHomeButton'
 import NavLanguageSelector from './components/NavLanguageSelector'
 import ThemePicker from './components/ThemePicker'
@@ -22,57 +22,64 @@ export default function SiteNavigation() {
         className="bg-l0 p-4 md:flex md:items-center md:gap-10 md:px-0 md:py-6"
         as="nav"
       >
-        <div className="flex items-center justify-between md:inline-flex">
-          <LogoHomeButton />
-          <HamburgerMenuButton className="md:hidden" />
-        </div>
-        <Menu.Items
-          static={!isMobile}
-          className="stroke-tertiary flex flex-col gap-5 border-b pt-8 pb-4 md:inline-flex md:w-full md:min-w-0 md:flex-row md:items-center md:justify-between md:border-b-0 md:p-0"
-        >
-          {/* Main site links */}
-          <div className="flex flex-col gap-4 px-6 md:min-w-0 md:flex-shrink md:flex-row md:gap-8 md:px-0">
-            {isMobile && (
-              <Menu.Item>
-                <PatchedNextLink className="text-primary font-medium" href="/">
-                  <Trans>Home</Trans>
-                </PatchedNextLink>
-              </Menu.Item>
-            )}
-            <Menu.Item>
-              <PatchedNextLink
-                className="text-primary font-medium md:text-sm"
-                href="/projects"
-              >
-                <Trans>Explore</Trans>
-              </PatchedNextLink>
-            </Menu.Item>
-            <ResourcesMenu />
-            <Menu.Item>
-              <PatchedNextLink
-                className="text-primary font-medium md:min-w-0 md:max-w-xs md:truncate md:text-sm"
-                href="/create"
-              >
-                <Trans>Create a project</Trans>
-              </PatchedNextLink>
-            </Menu.Item>
-          </div>
-
-          <div className="flex flex-col gap-4 md:flex-row md:items-center md:gap-6">
-            <div className="flex flex-col gap-4 px-6 md:flex-row md:items-center md:gap-6 md:px-0">
-              <NavLanguageSelector className="md:order-2" />
-              <ThemePicker className="md:order-3" />
-              <QuickProjectSearch className="md:order-1" />
+        {({ open }) => (
+          <>
+            <div className="flex items-center justify-between md:inline-flex">
+              <LogoHomeButton />
+              <MobileMenuButton className="md:hidden" open={open} />
             </div>
+            <Menu.Items
+              static={!isMobile}
+              className="stroke-tertiary flex flex-col gap-5 border-b pt-8 pb-4 md:inline-flex md:w-full md:min-w-0 md:flex-row md:items-center md:justify-between md:border-b-0 md:p-0"
+            >
+              {/* Main site links */}
+              <div className="flex flex-col gap-4 px-6 md:min-w-0 md:flex-shrink md:flex-row md:gap-8 md:px-0">
+                {isMobile && (
+                  <Menu.Item>
+                    <PatchedNextLink
+                      className="text-primary font-medium"
+                      href="/"
+                    >
+                      <Trans>Home</Trans>
+                    </PatchedNextLink>
+                  </Menu.Item>
+                )}
+                <Menu.Item>
+                  <PatchedNextLink
+                    className="text-primary font-medium md:text-sm"
+                    href="/projects"
+                  >
+                    <Trans>Explore</Trans>
+                  </PatchedNextLink>
+                </Menu.Item>
+                <ResourcesMenu />
+                <Menu.Item>
+                  <PatchedNextLink
+                    className="text-primary font-medium md:min-w-0 md:max-w-xs md:truncate md:text-sm"
+                    href="/create"
+                  >
+                    <Trans>Create a project</Trans>
+                  </PatchedNextLink>
+                </Menu.Item>
+              </div>
 
-            <WalletButton />
-            {isConnected && (
-              <Button className="md:hidden" onClick={disconnect} block>
-                <Trans>Disconnect</Trans>
-              </Button>
-            )}
-          </div>
-        </Menu.Items>
+              <div className="flex flex-col gap-4 md:flex-row md:items-center md:gap-6">
+                <div className="flex flex-col gap-4 px-6 md:flex-row md:items-center md:gap-6 md:px-0">
+                  <NavLanguageSelector className="md:order-2" />
+                  <ThemePicker className="md:order-3" />
+                  <QuickProjectSearch className="md:order-1" />
+                </div>
+
+                <WalletButton />
+                {isConnected && (
+                  <Button className="md:hidden" onClick={disconnect} block>
+                    <Trans>Disconnect</Trans>
+                  </Button>
+                )}
+              </div>
+            </Menu.Items>
+          </>
+        )}
       </Menu>
     </div>
   )

116d2d2edf9e55a5b392ed97acc611e29b613411
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-04-22 10:05:22 +1000

    feat: add mobile open transition (#3459)

--- src/components/Navbar/SiteNavigation.tsx
+++ src/components/Navbar/SiteNavigation.tsx
@@ -1,10 +1,11 @@
-import { Menu } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react'
 import { Trans, t } from '@lingui/macro'
 import { Button } from 'antd'
 import QuickProjectSearch from 'components/QuickProjectSearch'
 import PatchedNextLink from 'components/fixes/PatchedNextLink'
 import useMobile from 'hooks/Mobile'
 import { useWallet } from 'hooks/Wallet'
+import { Fragment } from 'react'
 import { DropdownMenu } from './components/DropdownMenu'
 import { MobileMenuButton } from './components/HamburgerMenuButton'
 import { LogoHomeButton } from './components/LogoHomeButton'
@@ -28,56 +29,68 @@ export default function SiteNavigation() {
               <LogoHomeButton />
               <MobileMenuButton className="md:hidden" open={open} />
             </div>
-            <Menu.Items
-              static={!isMobile}
-              className="stroke-tertiary flex flex-col gap-5 border-b pt-8 pb-4 md:inline-flex md:w-full md:min-w-0 md:flex-row md:items-center md:justify-between md:border-b-0 md:p-0"
+
+            <Transition
+              show={open || !isMobile}
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-150"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
             >
-              {/* Main site links */}
-              <div className="flex flex-col gap-4 px-6 md:min-w-0 md:flex-shrink md:flex-row md:gap-8 md:px-0">
-                {isMobile && (
+              <Menu.Items
+                static={!isMobile}
+                className="stroke-tertiary flex flex-col gap-5 border-b pt-8 pb-4 md:inline-flex md:w-full md:min-w-0 md:flex-row md:items-center md:justify-between md:border-b-0 md:p-0"
+              >
+                {/* Main site links */}
+                <div className="flex flex-col gap-4 px-6 md:min-w-0 md:flex-shrink md:flex-row md:gap-8 md:px-0">
+                  {isMobile && (
+                    <Menu.Item>
+                      <PatchedNextLink
+                        className="text-primary font-medium"
+                        href="/"
+                      >
+                        <Trans>Home</Trans>
+                      </PatchedNextLink>
+                    </Menu.Item>
+                  )}
                   <Menu.Item>
                     <PatchedNextLink
-                      className="text-primary font-medium"
-                      href="/"
+                      className="text-primary font-medium md:text-sm"
+                      href="/projects"
                     >
-                      <Trans>Home</Trans>
+                      <Trans>Explore</Trans>
                     </PatchedNextLink>
                   </Menu.Item>
-                )}
-                <Menu.Item>
-                  <PatchedNextLink
-                    className="text-primary font-medium md:text-sm"
-                    href="/projects"
-                  >
-                    <Trans>Explore</Trans>
-                  </PatchedNextLink>
-                </Menu.Item>
-                <ResourcesMenu />
-                <Menu.Item>
-                  <PatchedNextLink
-                    className="text-primary font-medium md:min-w-0 md:max-w-xs md:truncate md:text-sm"
-                    href="/create"
-                  >
-                    <Trans>Create a project</Trans>
-                  </PatchedNextLink>
-                </Menu.Item>
-              </div>
-
-              <div className="flex flex-col gap-4 md:flex-row md:items-center md:gap-6">
-                <div className="flex flex-col gap-4 px-6 md:flex-row md:items-center md:gap-6 md:px-0">
-                  <NavLanguageSelector className="md:order-2" />
-                  <ThemePicker className="md:order-3" />
-                  <QuickProjectSearch className="md:order-1" />
+                  <ResourcesMenu />
+                  <Menu.Item>
+                    <PatchedNextLink
+                      className="text-primary font-medium md:min-w-0 md:max-w-xs md:truncate md:text-sm"
+                      href="/create"
+                    >
+                      <Trans>Create a project</Trans>
+                    </PatchedNextLink>
+                  </Menu.Item>
                 </div>
 
-                <WalletButton />
-                {isConnected && (
-                  <Button className="md:hidden" onClick={disconnect} block>
-                    <Trans>Disconnect</Trans>
-                  </Button>
-                )}
-              </div>
-            </Menu.Items>
+                <div className="flex flex-col gap-4 md:flex-row md:items-center md:gap-6">
+                  <div className="flex flex-col gap-4 px-6 md:flex-row md:items-center md:gap-6 md:px-0">
+                    <NavLanguageSelector className="md:order-2" />
+                    <ThemePicker className="md:order-3" />
+                    <QuickProjectSearch className="md:order-1" />
+                  </div>
+
+                  <WalletButton />
+                  {isConnected && (
+                    <Button className="md:hidden" onClick={disconnect} block>
+                      <Trans>Disconnect</Trans>
+                    </Button>
+                  )}
+                </div>
+              </Menu.Items>
+            </Transition>
           </>
         )}
       </Menu>

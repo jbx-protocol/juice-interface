b65eb709457e0adb9b3d90801f2f73bd20efbc2c
peripheralist <peripheralist@gmail.com>
2023-04-24 12:04:21 -0600

    -

--- src/components/activityEventElems/ActivityElement/activityElementEvent.ts
+++ src/components/activityEventElems/ActivityElement/activityElementEvent.ts
@@ -1,11 +1,7 @@
 export interface ActivityElementEvent {
   timestamp: number
   txHash: string
-<<<<<<< HEAD
   from: string
-=======
-  from?: string // TODO should always be defined
->>>>>>> 71c3d8837 (wip)
   beneficiary?: string
   terminal?: string
 }

25a08c5b65e94c93789625a47fca89d592e2d54c
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-04-24 16:50:28 +1000

    feat: add explore categories section (#3483)

---
+++ src/pages/home/NewHomePage/JuicyPicksSection/components/ExploreCategories.tsx
@@ -0,0 +1,52 @@
+import { Trans } from '@lingui/macro'
+import Loading from 'components/Loading'
+import template from 'lodash/template'
+import { HomepageCard } from '../../HomepageCard'
+import { ProjectCarousel } from '../../ProjectCarousel'
+import { SectionHeading } from '../../SectionHeading'
+import { useExploreCategories } from '../hooks/useExploreCategories'
+
+export const ExploreCategories = () => {
+  const { tags, isLoading, error } = useExploreCategories()
+  return (
+    <>
+      <SectionHeading
+        heading={<Trans>Explore categories</Trans>}
+        subheading={
+          <Trans>
+            Whether it's a fundraiser or NFT project, we've got you covered.
+          </Trans>
+        }
+      />
+
+      {isLoading ? (
+        <Loading size="large" />
+      ) : error ? (
+        <div className="text-error">{error}</div>
+      ) : (
+        <ProjectCarousel
+          items={tags.map(tag => (
+            <HomepageCard
+              key={tag}
+              title={tag.charAt(0).toUpperCase() + tag.slice(1)}
+              img={
+                <img
+                  className="h-full w-full object-cover object-center"
+                  src={CategoryImagePath({ tag })}
+                  alt={tag}
+                />
+              }
+              href={CategoryLink({ tag })}
+            />
+          ))}
+        />
+      )}
+    </>
+  )
+}
+
+const CategoryImagePath = template(
+  '/assets/images/home/categories/<%= tag %>.jpg',
+)
+
+const CategoryLink = template('/projects?tab=all&search=&tags=<%= tag %>')

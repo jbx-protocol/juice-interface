f7167ca5d8fa80d02bab54b5e3e2554493154fba
Johnny D <johnnyD.eth@protonmail.com>
2023-04-27 14:33:59 +1000

    Fix activity page styles (#3505)

--- src/pages/activity/index.page.tsx
+++ src/pages/activity/index.page.tsx
@@ -5,13 +5,13 @@ import Payments from 'pages/home/OldHomePage/Payments'
 export default function ActivityPage() {
   return (
     <AppWrapper>
-      <div className="mx-auto max-w-xl px-4">
-        <h1 className="m-0 text-3xl font-normal text-black dark:text-slate-100">
+      <div className="mx-auto max-w-xl px-4 pt-10 pb-20">
+        <h1 className="m-0 mb-4 font-display text-3xl text-black dark:text-slate-100">
           <Trans>Activity</Trans>
         </h1>
-        <p>
+        <p className="mb-10 text-grey-600 dark:text-slate-100">
           <Trans>
-            View all of the latest project activity made on Juicebox.
+            View all of the latest project activity and payments on Juicebox.
           </Trans>
         </p>
 

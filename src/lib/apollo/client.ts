f4b0d3e015cb3e27900f457d7f7a450fffe3de5b
peripheralist <peripheralist@gmail.com>
2023-05-01 10:44:23 -0600

    wip parse graphql

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -1,5 +1,15 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { BigNumber } from '@ethersproject/bignumber'
+import { isBigNumberish } from '@ethersproject/bignumber/lib/bignumber'
+import { FunctionsMap, withScalars } from 'apollo-link-scalars'
+import { IntrospectionQuery, buildClientSchema } from 'graphql'
 import { isBrowser } from 'utils/isBrowser'
+import introspectionResult from '../../../graphql.schema.json'
 
 const subgraphUri = () => {
   let uri: string | undefined
@@ -26,9 +36,31 @@ const subgraphUri = () => {
   return url.href
 }
 
+const typesMap: FunctionsMap = {
+  BigInt: {
+    serialize: (parsed: unknown): string | null => {
+      return BigNumber.isBigNumber(parsed) ? parsed.toHexString() : null
+    },
+    parseValue: (raw: unknown): BigNumber | null => {
+      if (raw === undefined || raw === null) return null
+
+      if (isBigNumberish(raw)) return BigNumber.from(raw)
+
+      throw new Error('Parse BigInt: Invalid BigNumber')
+    },
+  },
+}
+
+const schema = buildClientSchema(
+  introspectionResult as unknown as IntrospectionQuery,
+)
+
+const scalarsLink = withScalars({ schema, typesMap })
+const httpLink = new HttpLink({ uri: subgraphUri() })
+
 const client = new ApolloClient({
-  uri: subgraphUri(),
   cache: new InMemoryCache(),
+  link: ApolloLink.from([scalarsLink, httpLink]),
 })
 
 export default client

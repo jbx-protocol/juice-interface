c56340eae5351f4cb73381615a4cf260eec5bf96
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-02 15:49:33 +0930

    fix: account page load time (#3530)

--- src/lib/api/ens.ts
+++ src/lib/api/ens.ts
@@ -7,18 +7,18 @@ import { NetworkName } from 'models/networkName'
  * Try to resolve an ENS name or address.
  *
  * If mainnet, first tries ENSIdeas API. Then, falls back to our API.
- * @param address - ens name or address
+ * @param addressOrEnsName - ens name or address
  * @returns
  */
-export function resolveAddress(address: string) {
+export function resolveAddress(addressOrEnsName: string) {
   try {
     if (readNetwork.name !== NetworkName.mainnet)
       throw new Error('Not mainnet, skipping to resolver fallback.')
-    return resolveAddressEnsIdeas(address)
+    return resolveAddressEnsIdeas(addressOrEnsName)
   } catch (e) {
     return axios
       .get<{ name: string; address: string }>(
-        `${process.env.NEXT_PUBLIC_BASE_URL}api/ens/resolve/${address}`,
+        `${process.env.NEXT_PUBLIC_BASE_URL}api/ens/resolve/${addressOrEnsName}`,
       )
       .then(data => data.data)
   }
--- src/pages/account/[addressOrEnsName]/index.page.tsx
+++ src/pages/account/[addressOrEnsName]/index.page.tsx
@@ -1,85 +1,59 @@
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import axios from 'axios'
 import { AccountDashboard } from 'components/AccountDashboard'
+import Loading from 'components/Loading'
 import { AppWrapper, SEO } from 'components/common'
 import { resolveAddress } from 'lib/api/ens'
 import { Profile } from 'models/database'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { Database } from 'types/database.types'
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
 import { truncateEthAddress } from 'utils/format/formatAddress'
 
-interface AccountPageProps {
-  address: string
-  ensName: string | null
-  profile: Profile | null
+function useEnsNamePair(addressOrEnsName: string | undefined) {
+  return useQuery(['ensNamePair', addressOrEnsName], async () => {
+    if (!addressOrEnsName) return
+
+    const data = await resolveAddress(addressOrEnsName)
+    return data
+  })
 }
 
-export const getServerSideProps: GetServerSideProps<
-  AccountPageProps
-> = async context => {
-  context.res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59',
-  )
+function useAccount({ address }: { address: string | undefined }) {
+  return useQuery(['account', address], async () => {
+    if (!address) return
 
-  if (!context.params?.addressOrEnsName) {
-    return {
-      notFound: true,
-    }
-  }
-
-  const addressOrEnsName = context.params.addressOrEnsName as string
-
-  const pair = await resolveAddress(addressOrEnsName).catch(() => undefined)
-  if (!pair) {
-    return {
-      notFound: true,
-    }
-  }
-
-  const { address, name: ensName } = pair
-  if (!address) {
-    return {
-      notFound: true,
-    }
-  }
-
-  const supabase = createServerSupabaseClient<Database>(context)
-
-  let profile = null
-  const profileResult = await supabase
-    .from('user_profiles')
-    .select('*')
-    .eq('wallet', address.toLowerCase())
-    .maybeSingle()
-  if (profileResult.error) {
-    console.error(
-      'Error occurred while retrieving user profile',
-      address.toLowerCase(),
+    const profile = await axios.get<{ profile: Profile | null }>(
+      `/api/account/${address}`,
     )
-  }
-
-  if (profileResult.data) {
-    profile = profileResult.data
-  }
-
-  return {
-    props: {
-      address,
-      ensName,
-      profile,
-    },
-  }
+    return profile.data.profile
+  })
 }
 
-export default function AccountPage({
-  address,
-  ensName,
-  profile,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+function _AccountPage({ addressOrEnsName }: { addressOrEnsName: string }) {
+  const { data: ensNamePair, isLoading: ensLoading } =
+    useEnsNamePair(addressOrEnsName)
+  const { name: ensName, address } = ensNamePair ?? {}
+
+  const { data: profile, isLoading: accountLoading } = useAccount({ address })
+
+  if (ensLoading || accountLoading) return <Loading />
+  if (!address) return null
+
   return (
-    <AppWrapper>
+    <>
       <SEO title={ensName ?? truncateEthAddress({ address })} />
       <AccountDashboard address={address} ensName={ensName} profile={profile} />
+    </>
+  )
+}
+
+export default function AccountPage() {
+  const router = useRouter()
+  const { addressOrEnsName } = router.query as { addressOrEnsName: string }
+  if (!addressOrEnsName) return null
+
+  return (
+    <AppWrapper>
+      <_AccountPage addressOrEnsName={addressOrEnsName as string} />
     </AppWrapper>
   )
 }

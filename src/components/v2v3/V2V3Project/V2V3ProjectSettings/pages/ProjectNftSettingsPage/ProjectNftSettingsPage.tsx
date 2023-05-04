dde1b712df49c17ef88b9fa4125e5e93564dcb28
Filip <f@filip.world>
2023-05-04 10:54:52 -0400

    Move svg preview into its own component

--- src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectNftSettingsPage/ProjectNftSettingsPage.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/ProjectNftSettingsPage/ProjectNftSettingsPage.tsx
@@ -5,12 +5,12 @@ import { JuiceInput } from 'components/inputs/JuiceTextInput'
 import { ProjectMetadataContext } from 'contexts/shared/ProjectMetadataContext'
 import { V2V3ProjectContext } from 'contexts/v2v3/Project/V2V3ProjectContext'
 import { useSetThemeTx } from 'hooks/v2v3/transactor/SetThemeTx'
-import { CSSProperties, useContext, useState } from 'react'
-import styles from './Capsules.module.css'
+import { useContext, useState } from 'react'
 import { Rule } from 'antd/lib/form'
 import { CustomResolverForm } from './CustomResolverForm'
 import { helpPagePath } from 'utils/routes'
 import ExternalLink from 'components/ExternalLink'
+import { ProjectNftPreview } from './ProjectNftPreview'
 
 export function ProjectNftSettingsPage() {
   const { handle } = useContext(V2V3ProjectContext)
@@ -62,14 +62,6 @@ export function ProjectNftSettingsPage() {
     { required: true, message: t`Each field is required.` },
   ]
 
-  const textStyle: CSSProperties = {
-    fill: '#' + previewTextColor,
-    whiteSpace: 'pre',
-    fontSize: '16px',
-    fontWeight: 500,
-    fontFamily: '"Capsules-500", monospace',
-  }
-
   return (
     <>
       <h3>
@@ -84,126 +76,13 @@ export function ProjectNftSettingsPage() {
       <h5>
         <Trans>Preview</Trans>
       </h5>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="289"
-        height="150"
-        viewBox="0 0 289 150"
-        className={styles as unknown as string}
-      >
-        <g clipPath="url(#clip0)">
-          <path d="M289 0H0V150H289V0Z" fill="url(#paint0)" />
-          <rect width="289" height="22" fill={'#' + previewTextColor} />
-          <g id="head" filter="url(#filter2)">
-            <text
-              x="16"
-              y="16"
-              style={{ ...textStyle, fill: '#' + previewBgColor }}
-            >
-              {handle ? '@' + handle : 'Project ' + projectId}
-            </text>
-            <text
-              x="259.25"
-              y="16"
-              style={{ ...textStyle, fill: '#' + previewBgColor }}
-            >
-              
-            </text>
-          </g>
-          <g filter="url(#filter1)">
-            <text x="0" y="48" style={textStyle}>
-              {'  '}cʏcʟᴇ 10{'           '}7 ᴅᴀʏs
-            </text>
-            <text x="0" y="64" style={textStyle}></text>
-            <text x="0" y="80" style={textStyle}>
-              {'  '}ʙᴀʟᴀɴcᴇ{'               '}Ξ850
-            </text>
-            <text x="0" y="96" style={textStyle}>
-              {'  '}ᴘᴀʏouᴛs{'            '}$150000
-            </text>
-            <text x="0" y="112" style={textStyle}>
-              {'  '}ᴛoᴋᴇɴ suᴘᴘʟʏ{'    '}1234567890
-            </text>
-            <text x="0" y="128" style={textStyle}>
-              {'  '}ᴘʀoᴊᴇcᴛ owɴᴇʀ{'  '}0x0000…0000
-            </text>
-          </g>
-        </g>
-        <defs>
-          <filter
-            id="filter1"
-            x="-3.36"
-            y="26.04"
-            width="298"
-            height="150"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feMorphology
-              operator="dilate"
-              radius="0.1"
-              in="SourceAlpha"
-              result="thicken"
-            />
-            <feGaussianBlur in="thicken" stdDeviation="0.5" result="blurred" />
-            <feFlood floodColor={'#' + previewTextColor} result="glowColor" />
-            <feComposite
-              in="glowColor"
-              in2="blurred"
-              operator="in"
-              result="softGlow_colored"
-            />
-            <feMerge>
-              <feMergeNode in="softGlow_colored" />
-              <feMergeNode in="SourceGraphic" />
-            </feMerge>
-          </filter>
-          <filter
-            id="filter2"
-            x="0"
-            y="0"
-            width="298"
-            height="150"
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feMorphology
-              operator="dilate"
-              radius="0.05"
-              in="SourceAlpha"
-              result="thicken"
-            />
-            <feGaussianBlur in="thicken" stdDeviation="0.25" result="blurred" />
-            <feFlood floodColor={'#' + previewBgColor} result="glowColor" />
-            <feComposite
-              in="glowColor"
-              in2="blurred"
-              operator="in"
-              result="softGlow_colored"
-            />
-            <feMerge>
-              <feMergeNode in="softGlow_colored" />
-              <feMergeNode in="SourceGraphic" />
-            </feMerge>
-          </filter>
-          <linearGradient
-            id="paint0"
-            x1="0"
-            y1="202"
-            x2="289"
-            y2="202"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor={'#' + previewAltBgColor} />
-            <stop offset="0.119792" stopColor={'#' + previewBgColor} />
-            <stop offset="0.848958" stopColor={'#' + previewBgColor} />
-            <stop offset="1" stopColor={'#' + previewAltBgColor} />
-          </linearGradient>
-          <clipPath id="clip0">
-            <rect width="289" height="150" />
-          </clipPath>
-        </defs>
-      </svg>
+      <ProjectNftPreview
+        previewTextColor={previewTextColor}
+        previewBgColor={previewBgColor}
+        previewAltBgColor={previewAltBgColor}
+        handle={handle}
+        projectId={projectId}
+      />
 
       <Form
         form={themeForm}
@@ -270,6 +149,7 @@ export function ProjectNftSettingsPage() {
           interface.
         </Trans>
       </p>
+
       <CustomResolverForm />
     </>
   )

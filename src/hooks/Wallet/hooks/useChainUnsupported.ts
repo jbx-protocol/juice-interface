566ed42e0170d209a7f397de34c42c75274c0bc6
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-16 17:00:06 +0930

    feat: family wallet (#3545)
    Co-authored-by: Wraeth <wraeth.dao@gmail.com>

--- src/hooks/Wallet/hooks/useChainUnsupported.ts
+++ src/hooks/Wallet/hooks/useChainUnsupported.ts
@@ -1,11 +1,11 @@
-import { useSetChain } from '@web3-onboard/react'
+import { readNetwork } from 'constants/networks'
 import { BigNumber } from 'ethers'
 import { useMemo } from 'react'
-
-import { readNetwork } from 'constants/networks'
+import { useNetwork } from 'wagmi'
 
 export function useChainUnsupported() {
-  const [{ connectedChain }] = useSetChain()
+  const { chain: connectedChain } = useNetwork()
+
   const chainUnsupported = useMemo(() => {
     if (!connectedChain) return false
     return !BigNumber.from(connectedChain.id).eq(readNetwork.chainId)

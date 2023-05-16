02905e4cef41fafcb9b46abacde5ca20355ea5e0
Johnny D <johnnyD.eth@protonmail.com>
2023-05-16 11:26:55 +1000

    Show amounts in Edit Payouts modal (#3573)

--- src/components/v2v3/shared/Allocation/components/PercentageInput.tsx
+++ src/components/v2v3/shared/Allocation/components/PercentageInput.tsx
@@ -1,5 +1,11 @@
+import CurrencySymbol from 'components/CurrencySymbol'
 import NumberSlider from 'components/inputs/NumberSlider'
-import { useCallback, useState } from 'react'
+import round from 'lodash/round'
+import { useCallback, useMemo, useState } from 'react'
+import { formatWad, stripCommas } from 'utils/format/formatNumber'
+import { V2V3CurrencyName } from 'utils/v2v3/currency'
+import { isInfiniteDistributionLimit } from 'utils/v2v3/fundingCycle'
+import { Allocation } from '../Allocation'
 import { AmountPercentageInput } from '../types'
 
 export const PercentageInput = ({
@@ -15,6 +21,18 @@ export const PercentageInput = ({
     value: '',
     isPercent: true,
   })
+
+  const { totalAllocationAmount, allocationCurrency } =
+    Allocation.useAllocationInstance()
+
+  const hasTotalAllocationAmount = useMemo(
+    () =>
+      totalAllocationAmount &&
+      !totalAllocationAmount.eq(0) &&
+      !isInfiniteDistributionLimit(totalAllocationAmount),
+    [totalAllocationAmount],
+  )
+
   const percentage = value ?? _percentage
   const setPercentage = onChange ?? _setPercentage
 
@@ -26,18 +44,38 @@ export const PercentageInput = ({
     [setPercentage],
   )
 
+  const totalAllocationAmountNum = parseFloat(
+    stripCommas(formatWad(totalAllocationAmount) ?? '0'),
+  )
+  const currencyName = V2V3CurrencyName(allocationCurrency)
+  const roundedAmount = round(
+    (percentage ? parseFloat(percentage.value) / 100 : 0) *
+      totalAllocationAmountNum,
+    currencyName === 'ETH' ? 4 : 2,
+  )
   return (
-    <NumberSlider
-      sliderValue={percentage ? parseFloat(percentage.value) : 0}
-      onChange={percentage =>
-        onAmountInputChange({
-          value: percentage?.toString() ?? '',
-          isPercent: true,
-        })
-      }
-      step={0.01}
-      defaultValue={0}
-      suffix="%"
-    />
+    <div className="flex items-center">
+      <div className="h-9 flex-grow">
+        <NumberSlider
+          sliderValue={percentage ? parseFloat(percentage.value) : 0}
+          onChange={percentage =>
+            onAmountInputChange({
+              value: percentage?.toString() ?? '',
+              isPercent: true,
+            })
+          }
+          step={0.01}
+          defaultValue={0}
+          suffix="%"
+        />
+      </div>
+      {/* Read-only amount if distribution limit is not infinite  */}
+      {hasTotalAllocationAmount ? (
+        <div className="ml-3">
+          <CurrencySymbol currency={currencyName} />
+          {roundedAmount}
+        </div>
+      ) : null}
+    </div>
   )
 }

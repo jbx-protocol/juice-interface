ef53f14805a06c25d91f1afb4fd358d65c6b93f4
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-16 11:42:34 +0930

    fix: bongs

--- src/hooks/v2v3/contractReader/useProjectDistributionLimit.ts
+++ src/hooks/v2v3/contractReader/useProjectDistributionLimit.ts
@@ -2,6 +2,7 @@ import { ETH_TOKEN_ADDRESS } from 'constants/v2v3/juiceboxTokens'
 import { V2V3ProjectContractsContext } from 'contexts/v2v3/ProjectContracts/V2V3ProjectContractsContext'
 import { BigNumber } from 'ethers'
 import { useContext } from 'react'
+import { JB_CONTROLLER_V_3_1 } from '../V2V3ProjectContracts/projectContractLoaders/useProjectController'
 import useV2ContractReader from './useV2ContractReader'
 
 export default function useProjectDistributionLimit({
@@ -15,15 +16,20 @@ export default function useProjectDistributionLimit({
 }) {
   const {
     contracts: { JBController, JBFundAccessConstraintsStore },
+    versions: { JBControllerVersion },
   } = useContext(V2V3ProjectContractsContext)
 
+  // v3_1 introduced JBFundAccessConstraintsStore, which should be used instead of JB controller.
+  const contract =
+    JBControllerVersion === JB_CONTROLLER_V_3_1
+      ? JBFundAccessConstraintsStore
+      : JBController
+
   return useV2ContractReader<BigNumber[]>({
-    // v3_1 introduced JBFundAccessConstraintsStore which should be used instead of JB controller.
-    // If a project doesn't have a JBFundAccessConstraintsStore, use the JBController, then its version is <3_1
-    contract: JBFundAccessConstraintsStore ?? JBController,
+    contract,
     functionName: 'distributionLimitOf',
     args:
-      projectId && configuration && terminal
+      projectId && configuration && terminal && contract
         ? [projectId, configuration, terminal, ETH_TOKEN_ADDRESS]
         : null,
   })

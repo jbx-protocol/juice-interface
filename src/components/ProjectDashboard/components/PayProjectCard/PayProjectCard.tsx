5e5e77baf7fc3dd458dfd0628cc81225124fce0d
Wraeth <wraeth.dao@gmail.com>
2023-05-25 18:44:28 +1000

    current cycle card

--- src/components/ProjectDashboard/components/PayProjectCard/PayProjectCard.tsx
+++ src/components/ProjectDashboard/components/PayProjectCard/PayProjectCard.tsx
@@ -9,13 +9,13 @@ import { PayInput } from './components/PayInput'
 export const PayProjectCard = ({ className }: { className?: string }) => {
   const { tokensPerCurrentPay } = usePayProjectCard()
   return (
-    <DisplayCard className={twMerge('flex flex-col gap-2', className)}>
+    <DisplayCard className={twMerge('flex flex-col gap-2 pr-9', className)}>
       <div className="font-medium">
         <Trans>Pay Project</Trans>
       </div>
       {/* // TODO: Formik */}
       <div className="flex gap-2">
-        <PayInput />
+        <PayInput className="flex-1" />
         <Button className="h-full" type="primary">
           <Trans>Add payment</Trans>
         </Button>

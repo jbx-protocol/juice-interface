f722e084db663284c02b8706ed424e30f88fdbe4
Wraeth <wraeth.dao@gmail.com>
2023-05-25 16:47:54 +1000

    cards

---
+++ src/components/ProjectDashboard/components/CurrentCycleCard/CurrentCycleCard.tsx
@@ -0,0 +1,5 @@
+import { DisplayCard } from '../ui'
+
+export const CurrentCycleCard = ({ className }: { className?: string }) => {
+  return <DisplayCard className={className}>Current Cycle</DisplayCard>
+}
---
+++ src/components/ProjectDashboard/components/NftRewardsCard/NftRewardCard.tsx
@@ -0,0 +1,5 @@
+import { DisplayCard } from '../ui'
+
+export const NftRewardsCard = ({ className }: { className?: string }) => {
+  return <DisplayCard className={className}>NFTs & Rewards</DisplayCard>
+}

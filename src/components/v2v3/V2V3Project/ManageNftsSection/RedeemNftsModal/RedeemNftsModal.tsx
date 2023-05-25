00157625f5071ac4bf6f85c00bf2e4007212d3e0
peripheralist <peripheralist@gmail.com>
2023-05-25 11:06:26 -0600

    -

--- src/components/v2v3/V2V3Project/ManageNftsSection/RedeemNftsModal/RedeemNftsModal.tsx
+++ src/components/v2v3/V2V3Project/ManageNftsSection/RedeemNftsModal/RedeemNftsModal.tsx
@@ -144,11 +144,11 @@ export function RedeemNftsModal({
     }
   }
 
-  const jb721DelegateTokens = data?.jb721DelegateTokens.map(t => ({
+  const nfts = data?.jb721DelegateTokens.map(t => ({
     ...t,
     tokenId: t.tokenId.toHexString(),
   }))
-  const nftBalanceFormatted = jb721DelegateTokens?.length ?? 0
+  const nftBalanceFormatted = nfts?.length ?? 0
   const hasOverflow = primaryTerminalCurrentOverflow?.gt(0)
   const hasRedemptionRate = fundingCycleMetadata.redemptionRate.gt(0)
   const canRedeem = hasOverflow && hasRedemptionRate
@@ -242,7 +242,7 @@ export function RedeemNftsModal({
           <Form form={form} layout="vertical">
             <Form.Item label={t`Select NFTs to redeem`}>
               <Row gutter={[20, 20]}>
-                {jb721DelegateTokens?.map(nft => {
+                {nfts?.map(nft => {
                   const isSelected = tokenIdsToRedeem.includes(nft.tokenId)
                   return (
                     <Col span={8} key={nft.tokenId}>

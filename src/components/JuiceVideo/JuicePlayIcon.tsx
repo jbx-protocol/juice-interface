01ff50bbd48860da550935b35d95ce44bc8a3f69
Johnny D <johnnyD.eth@protonmail.com>
2023-05-25 12:37:55 +1000

    Fix NFT Mp4 UI (#3608)

--- src/components/JuiceVideo/JuicePlayIcon.tsx
+++ src/components/JuiceVideo/JuicePlayIcon.tsx
@@ -3,7 +3,7 @@ import { CaretRightOutlined } from '@ant-design/icons'
 export function JuicePlayIcon() {
   return (
     <div className="h-[30px] w-[30px] rounded-full bg-grey-400/[0.4]">
-      <CaretRightOutlined className="margin-auto ml-[1px] h-[15px] w-full text-2xl text-white" />
+      <CaretRightOutlined className="margin-auto ml-[1px] h-[17px] w-full text-2xl text-white" />
     </div>
   )
 }

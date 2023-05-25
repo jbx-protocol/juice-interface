5f2f1d4fe1f9ac5cc9173b65b37f37b1161f8b43
Wraeth <wraeth.dao@gmail.com>
2023-05-25 18:48:45 +1000

    smol fixes

--- src/components/ProjectDashboard/components/ui/ProjectTab.tsx
+++ src/components/ProjectDashboard/components/ui/ProjectTab.tsx
@@ -2,13 +2,10 @@ import { Tab } from '@headlessui/react'
 import { twMerge } from 'tailwind-merge'
 
 export const ProjectTab = ({ name }: { name: string }) => (
-  <Tab>
+  <Tab className="outline-none">
     {({ selected }) => (
       <div
-        className={twMerge(
-          'px-1 pb-5 outline-none',
-          selected && 'border-b-2 border-black',
-        )}
+        className={twMerge('px-1 pb-5', selected && 'border-b-2 border-black')}
       >
         {name}
       </div>

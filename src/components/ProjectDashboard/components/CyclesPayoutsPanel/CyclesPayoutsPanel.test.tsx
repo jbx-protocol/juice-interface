689d7e5043075ecde9ef3b980d1d98d9289d691b
Wraeth <wraeth.dao@gmail.com>
2023-05-29 16:48:20 +1000

    configuration

--- src/components/ProjectDashboard/components/CyclesPayoutsPanel/CyclesPayoutsPanel.test.tsx
+++ src/components/ProjectDashboard/components/CyclesPayoutsPanel/CyclesPayoutsPanel.test.tsx
@@ -4,6 +4,12 @@
 import { render } from '@testing-library/react'
 import { CyclesPayoutsPanel } from './CyclesPayoutsPanel'
 
+jest.mock('./components/CurrentUpcomingSubPanel', () => ({
+  CurrentUpcomingSubPanel: jest
+    .fn()
+    .mockReturnValue(<div>CurrentUpcomingSubPanel</div>),
+}))
+
 describe('CyclesPayoutsPanel', () => {
   it('renders', () => {
     const { container } = render(<CyclesPayoutsPanel />)
---
+++ src/components/ProjectDashboard/components/CyclesPayoutsPanel/components/ConfigurationDisplayCard.tsx
@@ -0,0 +1,53 @@
+import { Disclosure, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
+import { Trans } from '@lingui/macro'
+import { Fragment } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { useConfigurationDisplayCard } from '../hooks/useConfigurationDisplayCard'
+import { ConfigurationPanel } from './ConfigurationPanel'
+
+export const ConfigurationDisplayCard = ({
+  type,
+}: {
+  type: 'current' | 'upcoming'
+}) => {
+  const { title } = useConfigurationDisplayCard(type)
+
+  return (
+    <Disclosure
+      as="div"
+      className={twMerge('rounded-lg bg-smoke-50 py-5 px-6')}
+    >
+      {({ open }) => (
+        <Disclosure.Button className="w-full outline-none">
+          <div className="flex w-full items-center justify-between text-start">
+            <div className="flex flex-col gap-2 text-sm font-medium text-grey-600">
+              {title}
+              <div className="font-heading text-2xl font-medium">
+                <Trans>Configuration</Trans>
+              </div>
+            </div>
+            <ChevronDownIcon
+              className={twMerge('h-6 w-6', open && 'rotate-180')}
+            />
+          </div>
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition-all ease-in-out duration-300"
+            enterFrom="max-h-0 overflow-hidden opacity-0"
+            enterTo="max-h-[1000px] overflow-hidden opacity-100"
+            leave="transition-all ease-in-out duration-300"
+            leaveFrom="max-h-[1000px] overflow-hidden opacity-100"
+            leaveTo="max-h-0 overflow-hidden opacity-0"
+          >
+            <Disclosure.Panel className="mt-4">
+              <ConfigurationPanel type={type} />
+            </Disclosure.Panel>
+          </Transition>
+        </Disclosure.Button>
+      )}
+    </Disclosure>
+  )
+}

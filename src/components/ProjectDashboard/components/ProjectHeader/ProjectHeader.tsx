e33e4e854600215bd56b8a3db8ba39b3e9091408
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-02 16:51:32 +0930

    feat: new project header

--- src/components/ProjectDashboard/components/ProjectHeader/ProjectHeader.tsx
+++ src/components/ProjectDashboard/components/ProjectHeader/ProjectHeader.tsx
@@ -5,6 +5,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { Trans, t } from '@lingui/macro'
 import { Divider } from 'antd'
+import EthereumAddress from 'components/EthereumAddress'
 import { useProjectHeader } from 'components/ProjectDashboard/hooks'
 import { TRENDING_WINDOW_DAYS } from 'pages/projects/RankingExplanation'
 import { ProjectHeaderLogo } from './components/ProjectHeaderLogo'
@@ -20,6 +21,7 @@ export const ProjectHeader = () => {
     totalVolume,
     last7DaysPercent,
   } = useProjectHeader()
+
   return (
     <div className="relative mt-6 flex w-full flex-col gap-4">
       <ProjectHeaderLogo className="absolute left-0 -top-[146px] border-6 border-white dark:border-slate-900" />
@@ -42,12 +44,17 @@ export const ProjectHeader = () => {
           <div className="text-grey-500 dark:text-slate-200">
             <span>{handle}</span>
             <Divider className="mx-4" type="vertical" />
-            <span>Owned by: {owner}</span>
+            <span>
+              Owned by: <EthereumAddress address={owner} />
+            </span>
           </div>
         </div>
         <div className="flex gap-12">
           <HeaderStat title={t`Payments`} stat={payments} />
-          <HeaderStat title={t`Total volume`} stat={`Ξ${totalVolume}`} />
+          <HeaderStat
+            title={t`Total volume`}
+            stat={<ETHAmount amount={totalVolume} precision={2} />}
+          />
           <HeaderStat
             title={<Trans>Last {TRENDING_WINDOW_DAYS} days</Trans>}
             stat={`${last7DaysPercent}%`}
--- src/components/ProjectDashboard/hooks/useProjectHeader.ts
+++ src/components/ProjectDashboard/hooks/useProjectHeader.ts
@@ -1,12 +1,19 @@
-// TODO: fill out data
+import { V2V3ProjectContext } from 'contexts/v2v3/Project/V2V3ProjectContext'
+import { useContext } from 'react'
+import { useProjectMetadata } from './useProjectMetadata'
+
 export const useProjectHeader = () => {
+  const { projectMetadata } = useProjectMetadata()
+  const { handle, projectOwnerAddress, totalVolume } =
+    useContext(V2V3ProjectContext)
+
   return {
-    title: 'PyroDAO',
-    subtitle: 'A place for pyromaniacs of all kinds',
-    handle: '@pyrodao',
-    owner: 'pyrodao.eth',
-    payments: 227,
-    totalVolume: 612,
-    last7DaysPercent: -71,
+    title: projectMetadata?.name,
+    subtitle: 'A place for pyromaniacs of all kinds', // TODO
+    handle,
+    owner: projectOwnerAddress,
+    payments: 227, // TODO
+    totalVolume,
+    last7DaysPercent: 0, // TODO
   }
 }

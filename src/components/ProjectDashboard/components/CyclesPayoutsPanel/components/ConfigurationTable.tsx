1597ee442ad85da8fb657dd6f8d1490b3150692e
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-06-02 16:06:22 +1000

    minor fixes for configuration table view (#3643)
    ## What does this PR do and why?
    
    _Provide a description of what this PR does. Link to any relevant GitHub issues, Notion tasks or Discord discussions._
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] (if relevant) I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] (if relevant) I have tested this PR in dark mode and light mode (if applicable).

--- src/components/ProjectDashboard/components/CyclesPayoutsPanel/components/ConfigurationTable.tsx
+++ src/components/ProjectDashboard/components/CyclesPayoutsPanel/components/ConfigurationTable.tsx
@@ -37,25 +37,43 @@ export const ConfigurationTable = ({
   )
 
   return (
-    <div>
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th className="text-start text-base font-semibold">{title}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map(row => (
-            <ConfigurationTableRow
-              key={row.id}
-              name={row.name}
-              value={row.value}
-              loading={row.loading}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div>
+        <div className="w-full">
+          <div className="text-start text-base font-semibold">{title}</div>
+          <div>
+            {rows.map(row => (
+              <ConfigurationTableRow
+                key={row.id}
+                name={row.name}
+                value={row.value}
+                loading={row.loading}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* <div>
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="text-start text-base font-semibold">{title}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map(row => (
+              <ConfigurationTableRow
+                key={row.id}
+                name={row.name}
+                value={row.value}
+                loading={row.loading}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div> */}
+    </>
   )
 }
 
@@ -68,16 +86,16 @@ const ConfigurationTableRow = ({
   value: ReactNode
   loading: boolean | undefined
 }) => (
-  <tr className="flex justify-between border-b border-grey-200 py-3">
-    <td className="flex justify-between gap-3">{name}</td>
+  <div className="flex justify-between gap-10 border-b border-grey-200 py-3">
+    <div className="flex justify-between gap-3">{name}</div>
     {!loading ? (
-      <td className="text-end">{value}</td>
+      <div className="truncate text-end">{value}</div>
     ) : (
-      <td className="text-end">
+      <div className="text-end">
         <div className="h-4 w-20 animate-pulse rounded bg-grey-200" />
-      </td>
+      </div>
     )}
-  </tr>
+  </div>
 )
 
 const Diff: React.FC<{ old?: ReactNode; new: ReactNode }> = props => (

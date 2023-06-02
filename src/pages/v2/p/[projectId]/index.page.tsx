84d4888711d28a446373a79baf87bd77ae0031a2
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-02 15:58:32 +0930

    chore: feature flag new project page (#3645)

--- src/pages/v2/p/[projectId]/index.page.tsx
+++ src/pages/v2/p/[projectId]/index.page.tsx
@@ -1,14 +1,20 @@
+import Loading from 'components/Loading'
 import { AppWrapper, SEO } from 'components/common'
+import { FEATURE_FLAGS } from 'constants/featureFlags'
 import { PV_V2 } from 'constants/pv'
 import { V2V3ProjectPageProvider } from 'contexts/v2v3/V2V3ProjectPageProvider'
 import { paginateDepleteProjectsQueryCall } from 'lib/apollo/paginateDepleteProjectsQuery'
+import { ProjectMetadata } from 'models/projectMetadata'
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import { Suspense, lazy } from 'react'
+import { featureFlagEnabled } from 'utils/featureFlags'
 import { cidFromUrl, ipfsPublicGatewayUrl } from 'utils/ipfs'
 import {
   ProjectPageProps,
   getProjectStaticProps,
 } from 'utils/server/pages/props'
 import { V2V3Dashboard } from './components/V2V3Dashboard'
+const ProjectDashboard = lazy(() => import('components/ProjectDashboard'))
 
 export const getStaticPaths: GetStaticPaths = async () => {
   if (process.env.BUILD_CACHE_V2_PROJECTS === 'true') {
@@ -43,30 +49,51 @@ export const getStaticProps: GetStaticProps<
   }
 }
 
+const ProjectPageSEO = ({
+  metadata,
+  projectId,
+}: {
+  metadata?: ProjectMetadata
+  projectId: number
+}) => (
+  <SEO
+    // Set known values, leave others undefined to be overridden
+    title={metadata?.name}
+    url={`${process.env.NEXT_PUBLIC_BASE_URL}v2/p/${projectId}`}
+    description={metadata?.description}
+    twitter={{
+      card: 'summary',
+      creator: metadata?.twitter,
+      handle: metadata?.twitter,
+      // Swap out all gateways with ipfs.io public gateway until we can resolve our meta tag issue.
+      image: metadata?.logoUri
+        ? ipfsPublicGatewayUrl(cidFromUrl(metadata.logoUri))
+        : undefined,
+    }}
+  />
+)
+
 export default function V2ProjectPage({
   metadata,
   projectId,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const newProjectPageEnabled = featureFlagEnabled(
+    FEATURE_FLAGS.NEW_PROJECT_PAGE,
+  )
+
   return (
     <>
-      <SEO
-        // Set known values, leave others undefined to be overridden
-        title={metadata?.name}
-        url={`${process.env.NEXT_PUBLIC_BASE_URL}v2/p/${projectId}`}
-        description={metadata?.description}
-        twitter={{
-          card: 'summary',
-          creator: metadata?.twitter,
-          handle: metadata?.twitter,
-          // Swap out all gateways with ipfs.io public gateway until we can resolve our meta tag issue.
-          image: metadata?.logoUri
-            ? ipfsPublicGatewayUrl(cidFromUrl(metadata.logoUri))
-            : undefined,
-        }}
-      />
+      <ProjectPageSEO metadata={metadata} projectId={projectId} />
+
       <AppWrapper>
         <V2V3ProjectPageProvider projectId={projectId} metadata={metadata}>
-          <V2V3Dashboard />
+          {newProjectPageEnabled ? (
+            <Suspense fallback={<Loading />}>
+              <ProjectDashboard />
+            </Suspense>
+          ) : (
+            <V2V3Dashboard />
+          )}
         </V2V3ProjectPageProvider>
       </AppWrapper>
     </>

8297676117f3ddf8bfca3d8b3827d1eecf4d12eb
Wraeth <wraeth.dao@gmail.com>
2023-06-12 14:16:38 +1000

    transaction state

--- src/components/ProjectDashboard/ProjectDashboard.tsx
+++ src/components/ProjectDashboard/ProjectDashboard.tsx
@@ -1,4 +1,6 @@
+import { AnnouncementLauncher } from 'contexts/Announcements/AnnouncementLauncher'
 import { NftRewardsContext } from 'contexts/NftRewards/NftRewardsContext'
+import { TransactionProvider } from 'contexts/Transaction/TransactionProvider'
 import { useContext } from 'react'
 import { Cart } from './components/Cart'
 import { CoverPhoto } from './components/CoverPhoto'
@@ -15,25 +17,29 @@ export const ProjectDashboard = () => {
     nftRewards: { CIDs },
   } = useContext(NftRewardsContext)
   return (
-    <FundingCycleCountdownProvider>
-      <ProjectCartProvider>
-        {/* // TODO: Remove pb-48, just there for testing */}
-        <div className="flex w-full flex-col items-center pb-48">
-          <CoverPhoto />
-          <div className="flex w-full justify-center px-6">
-            <div className="flex w-full max-w-6xl flex-col">
-              <ProjectHeader />
-              <div className="mt-10 flex w-full gap-6">
-                <PayProjectCard className="flex-1" />
-                {CIDs?.length ? <NftRewardsCard /> : null}
-                <CurrentCycleCard />
+    <TransactionProvider>
+      <AnnouncementLauncher>
+        <FundingCycleCountdownProvider>
+          <ProjectCartProvider>
+            {/* // TODO: Remove pb-48, just there for testing */}
+            <div className="flex w-full flex-col items-center pb-48">
+              <CoverPhoto />
+              <div className="flex w-full justify-center px-6">
+                <div className="flex w-full max-w-6xl flex-col">
+                  <ProjectHeader />
+                  <div className="mt-10 flex w-full gap-6">
+                    <PayProjectCard className="flex-1" />
+                    {CIDs?.length ? <NftRewardsCard /> : null}
+                    <CurrentCycleCard />
+                  </div>
+                  <ProjectTabs className="mt-16" />
+                </div>
               </div>
-              <ProjectTabs className="mt-16" />
             </div>
-          </div>
-        </div>
-        <Cart />
-      </ProjectCartProvider>
-    </FundingCycleCountdownProvider>
+            <Cart />
+          </ProjectCartProvider>
+        </FundingCycleCountdownProvider>
+      </AnnouncementLauncher>
+    </TransactionProvider>
   )
 }

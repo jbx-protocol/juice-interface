9ebfcd7652aea92cd3b32220f6a5e4fc4d385da5
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-14 07:34:18 +0930

    feat: JB721_DELEGATE_V3_3 mainnet (#3698)

--- src/hooks/defaultContracts/useDefaultJB721Delegate.ts
+++ src/hooks/defaultContracts/useDefaultJB721Delegate.ts
@@ -1,12 +1,5 @@
-import {
-  JB721_DELEGATE_V3_2,
-  JB721_DELEGATE_V3_3,
-} from 'constants/delegateVersions'
-import { readNetwork } from 'constants/networks'
-import { NetworkName } from 'models/networkName'
+import { JB721_DELEGATE_V3_3 } from 'constants/delegateVersions'
 import { JB721DelegateVersion } from 'models/nftRewards'
 
 export const DEFAULT_JB_721_DELEGATE_VERSION: JB721DelegateVersion =
-  readNetwork.name === NetworkName.goerli
-    ? JB721_DELEGATE_V3_3
-    : JB721_DELEGATE_V3_2
+  JB721_DELEGATE_V3_3

9a8115e2e54dfb3eaaf42ba9733deac825e77905
Wraeth <wraeth.dao@gmail.com>
2023-06-15 07:45:33 +1000

    feat: success view allow subscribe notifications

--- src/components/SubscribeButton/SubscribeButtonIcon.tsx
+++ src/components/SubscribeButton/SubscribeButtonIcon.tsx
@@ -1,14 +1,21 @@
 import { BellIcon as BellIconOutline } from '@heroicons/react/24/outline'
 import { BellIcon as BellIconSolid } from '@heroicons/react/24/solid'
+import { useMemo } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export const SubscribeButtonIcon = ({
+  className,
   isSubscribed,
 }: {
+  className?: string
   isSubscribed: boolean
 }) => {
-  return isSubscribed ? (
-    <BellIconSolid className="inline h-6 w-6" />
-  ) : (
-    <BellIconOutline className="inline h-6 w-6" />
-  )
+  const Icon = useMemo(() => {
+    if (isSubscribed) {
+      return BellIconSolid
+    }
+    return BellIconOutline
+  }, [isSubscribed])
+
+  return <Icon className={twMerge('inline h-6 w-6', className)} />
 }

3d50b9f225590353bc22e7310ffe2df2599f24cc
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-06-18 08:35:59 +1000

    fix: dark mode nft rewards and image correction (#3747)
    ## What does this PR do and why?
    
    _Provide a description of what this PR does. Link to any relevant GitHub issues, Notion tasks or Discord discussions._
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] (if relevant) I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] (if relevant) I have tested this PR in dark mode and light mode (if applicable).

--- src/components/ProjectDashboard/components/NftRewardsPanel/NftReward/NftDetails.tsx
+++ src/components/ProjectDashboard/components/NftRewardsPanel/NftReward/NftDetails.tsx
@@ -1,7 +1,8 @@
 import { Skeleton } from 'antd'
+import { TruncatedText } from 'components/TruncatedText'
 import ETHAmount from 'components/currency/ETHAmount'
 import { NftRewardTier } from 'models/nftRewards'
-import { classNames } from 'utils/classNames'
+import { twMerge } from 'tailwind-merge'
 import { parseWad } from 'utils/format/formatNumber'
 
 export function NftDetails({
@@ -17,9 +18,9 @@ export function NftDetails({
 }) {
   return (
     <div
-      className={classNames(
-        'flex h-full w-full flex-col justify-between rounded-b-lg bg-white pr-3.5 pl-3 pb-2.5',
-        !loading ? 'pt-3.5' : 'pt-1',
+      className={twMerge(
+        'flex h-full w-full flex-col justify-between rounded-b-lg bg-white p-4 dark:bg-slate-700',
+        !loading ? 'pt-4' : 'pt-1',
       )}
     >
       <Skeleton
@@ -28,19 +29,13 @@ export function NftDetails({
         title={false}
         paragraph={{ rows: 1, width: ['100%'] }}
       >
-        <span
-          className={classNames(
-            'text-base font-medium',
-            'text-ellipsis',
-            'overflow-hidden',
-            'text-black dark:text-slate-100',
-          )}
-        >
-          {rewardTier?.name}
-        </span>
+        <TruncatedText
+          className="text-base font-medium text-black dark:text-slate-50"
+          text={rewardTier?.name ?? ''}
+        />
       </Skeleton>
       {!hideAttributes ? (
-        <div className="mt-2 flex items-center justify-between">
+        <div className="mt-3 flex items-center justify-between">
           {rewardTier?.contributionFloor ? (
             <Skeleton
               className="mt-1"
@@ -49,7 +44,7 @@ export function NftDetails({
               title={false}
               paragraph={{ rows: 1, width: ['50%'] }}
             >
-              <span className="text-lg font-medium text-grey-900 dark:text-slate-50">
+              <span className="text-lg font-medium text-grey-900 dark:text-slate-100">
                 <ETHAmount amount={parseWad(rewardTier.contributionFloor)} />
               </span>
             </Skeleton>
@@ -61,7 +56,7 @@ export function NftDetails({
             title={false}
             paragraph={{ rows: 1, width: ['50%'] }}
           >
-            <span className="text-xs text-grey-400 dark:text-slate-200">
+            <span className="text-xs text-grey-400 dark:text-slate-300">
               {remainingSupplyText}
             </span>
           </Skeleton>

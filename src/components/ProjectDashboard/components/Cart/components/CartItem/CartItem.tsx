e0981e6b16f1f21fcd7f06e1a07a9443ede07f0e
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-06-18 08:29:22 +1000

    feat: mobile cart items (#3740)
    ## What does this PR do and why?
    
    _Provide a description of what this PR does. Link to any relevant GitHub issues, Notion tasks or Discord discussions._
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] (if relevant) I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] (if relevant) I have tested this PR in dark mode and light mode (if applicable).

--- src/components/ProjectDashboard/components/Cart/components/CartItem/CartItem.tsx
+++ src/components/ProjectDashboard/components/Cart/components/CartItem/CartItem.tsx
@@ -1,4 +1,5 @@
 import { MinusIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline'
+import useMobile from 'hooks/useMobile'
 import { V2V3CurrencyOption } from 'models/v2v3/currencyOption'
 import { ReactNode, useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -27,12 +28,40 @@ export const CartItem: React.FC<CartItemProps> = ({
   onRemove,
   ...quantityProps
 }) => {
+  const isMobile = useMobile()
+
   const priceText = useMemo(() => {
     if (price === null) {
       return '-'
     }
     return formatCurrencyAmount(price)
   }, [price])
+
+  if (isMobile) {
+    return (
+      <div
+        className={twMerge(
+          'flex justify-between gap-3 border-b border-grey-200 py-6 dark:border-slate-500',
+          className,
+        )}
+      >
+        <div className="flex min-w-0 gap-3">
+          {icon}
+          <div className="flex min-w-0 flex-col gap-3">
+            <span className="mt-3">{title}</span>
+            {'quantity' in quantityProps && (
+              <QuantityControl {...quantityProps} />
+            )}
+          </div>
+        </div>
+
+        <div className="mt-4 flex gap-4">
+          <span className="mr-4 text-sm font-medium">{priceText}</span>
+          <RemoveIcon onClick={onRemove} />
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div
@@ -46,31 +75,37 @@ export const CartItem: React.FC<CartItemProps> = ({
         <span className="ml-3 dark:text-slate-50">{title}</span>
       </div>
       <div className="flex items-center">
-        {'quantity' in quantityProps && (
-          <span className="mr-8 flex gap-3 rounded-lg border border-grey-200 p-1 text-sm">
-            <button
-              data-testid="cart-item-decrease-button"
-              onClick={quantityProps.onDecrease}
-            >
-              <MinusIcon className="h-4 w-4 text-grey-500" />
-            </button>
-            {quantityProps.quantity}
-            <button
-              data-testid="cart-item-increase-button"
-              onClick={quantityProps.onIncrease}
-            >
-              <PlusIcon className="h-4 w-4 text-grey-500" />
-            </button>
-          </span>
-        )}
+        {'quantity' in quantityProps && <QuantityControl {...quantityProps} />}
         <span className="mr-4 text-sm font-medium">{priceText}</span>
-        <TrashIcon
-          data-testid="cart-item-remove-button"
-          role="button"
-          className="inline h-4 w-4 text-grey-400 dark:text-slate-300"
-          onClick={onRemove}
-        />
+        <RemoveIcon onClick={onRemove} />
       </div>
     </div>
+  )
+}
+
+const RemoveIcon: React.FC<{ onClick: () => void }> = ({ onClick }) => (
+  <TrashIcon
+    data-testid="cart-item-remove-button"
+    role="button"
+    className="inline h-6 w-6 text-grey-400 dark:text-slate-300 md:h-4 md:w-4"
+    onClick={onClick}
+  />
+)
+
+const QuantityControl: React.FC<{
+  quantity: number
+  onIncrease: () => void
+  onDecrease: () => void
+}> = ({ quantity, onIncrease, onDecrease }) => {
+  return (
+    <span className="mr-8 flex w-fit gap-3 rounded-lg border border-grey-200 p-1 text-sm">
+      <button data-testid="cart-item-decrease-button" onClick={onDecrease}>
+        <MinusIcon className="h-4 w-4 text-grey-500" />
+      </button>
+      {quantity}
+      <button data-testid="cart-item-increase-button" onClick={onIncrease}>
+        <PlusIcon className="h-4 w-4 text-grey-500" />
+      </button>
+    </span>
   )
 }

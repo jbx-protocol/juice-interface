50f8e55a56e3f73697604893ebe2280801c52477
Wraeth <wraeth.dao@gmail.com>
2023-06-20 17:55:04 +1000

    success pay mobile

--- src/components/ProjectDashboard/components/SuccessPayView/components/SuccessPayCard.tsx
+++ src/components/ProjectDashboard/components/SuccessPayView/components/SuccessPayCard.tsx
@@ -3,6 +3,7 @@ import EthereumAddress from 'components/EthereumAddress'
 import EtherscanLink from 'components/EtherscanLink'
 import { useProjectMetadata } from 'components/ProjectDashboard/hooks'
 import { useProjectPageQueries } from 'components/ProjectDashboard/hooks/useProjectPageQueries'
+import useMobile from 'hooks/useMobile'
 import moment from 'moment'
 import { useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -10,6 +11,7 @@ import { formatCurrencyAmount } from 'utils/formatCurrencyAmount'
 import { ProjectHeaderLogo } from '../../ProjectHeader/components/ProjectHeaderLogo'
 
 export const SuccessPayCard = ({ className }: { className?: string }) => {
+  const isMobile = useMobile()
   const { name } = useProjectMetadata().projectMetadata ?? {}
   const { projectPayReceipt } = useProjectPageQueries()
 
@@ -24,43 +26,56 @@ export const SuccessPayCard = ({ className }: { className?: string }) => {
   return (
     <div
       className={twMerge(
-        'mx-auto flex w-full max-w-xl justify-between gap-3 rounded-lg border border-grey-200 p-6 shadow dark:border-slate-500 dark:bg-slate-700',
+        'mx-auto flex w-full max-w-xl flex-col justify-between divide-y divide-grey-200 rounded-lg border border-grey-200 p-6 shadow dark:divide-slate-500 dark:border-slate-500 dark:bg-slate-700 md:flex-row',
         className,
       )}
     >
-      <div className="flex gap-3 text-start">
-        <ProjectHeaderLogo className="h-20 w-20 bg-white" />
-        <div className="flex flex-col gap-1">
-          <span className="text-xs text-grey-500 dark:text-slate-200">
-            <Trans>Paid</Trans>
-          </span>
-          <span className="truncate text-sm font-medium">{name}</span>
-          <span className="font-heading text-2xl font-medium">
-            {formatCurrencyAmount({
-              ...projectPayReceipt.totalAmount,
-              withScale: true,
-            })}
-          </span>
+      <div className="flex w-full justify-between gap-3 pb-6 md:pb-0">
+        <div className="flex gap-3 text-start">
+          <ProjectHeaderLogo className="h-20 w-20 bg-white" />
+          <div className="flex flex-col gap-1">
+            <span className="text-xs text-grey-500 dark:text-slate-200">
+              <Trans>Paid</Trans>
+            </span>
+            <span className="truncate text-sm font-medium">{name}</span>
+            <span className="font-heading text-2xl font-medium">
+              {formatCurrencyAmount({
+                ...projectPayReceipt.totalAmount,
+                withScale: true,
+              })}
+            </span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-end">
+          <div className="flex items-center gap-2 ">
+            <span className="text-xs text-grey-500 dark:text-slate-200">
+              {transactionDateString}
+            </span>
+            <EtherscanLink
+              linkClassName="text-black dark:text-slate-50"
+              type="tx"
+              value={projectPayReceipt.transactionHash}
+            />
+          </div>
+          {!isMobile && (
+            <EthereumAddress
+              tooltipDisabled
+              address={projectPayReceipt.fromAddress}
+              withEnsAvatar
+            />
+          )}
         </div>
       </div>
-
-      <div className="flex flex-col items-end">
-        <div className="flex items-center gap-2 ">
-          <span className="text-xs text-grey-500 dark:text-slate-200">
-            {transactionDateString}
-          </span>
-          <EtherscanLink
-            linkClassName="text-black dark:text-slate-50"
-            type="tx"
-            value={projectPayReceipt.transactionHash}
+      {isMobile && (
+        <div className="flex justify-between pt-6">
+          <EthereumAddress
+            tooltipDisabled
+            address={projectPayReceipt.fromAddress}
+            withEnsAvatar
           />
         </div>
-        <EthereumAddress
-          tooltipDisabled
-          address={projectPayReceipt.fromAddress}
-          withEnsAvatar
-        />
-      </div>
+      )}
     </div>
   )
 }

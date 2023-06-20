d33d04f9c213aef97fe5e9731b12d0ce75efcd0a
Wraeth <wraeth.dao@gmail.com>
2023-06-20 17:18:34 +1000

    fix: a little min height on panels never hurt anyone

--- src/components/ProjectDashboard/components/ActivityPanel/ActivityPanel.tsx
+++ src/components/ProjectDashboard/components/ActivityPanel/ActivityPanel.tsx
@@ -2,7 +2,7 @@ import { V2V3ProjectActivity } from 'components/v2v3/V2V3Project/ProjectActivity
 
 export const ActivityPanel = () => {
   return (
-    <div className="w-full max-w-[596px]">
+    <div className="min-h-[384px] w-full max-w-[596px]">
       <V2V3ProjectActivity />
     </div>
   )

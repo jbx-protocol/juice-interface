b023aa04446ccdaf008529d874ec688ccab20de5
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-22 10:09:46 +0930

    feat: update juicy picks

--- src/pages/home/JuicyPicksSection/constants.ts
+++ src/pages/home/JuicyPicksSection/constants.ts
@@ -1,10 +1,10 @@
 // project that gets more real estate on the home page
-const SPOTLIGHT_PROJECT_ID = 471 // pinnable
+const SPOTLIGHT_PROJECT_ID = 490 // blunt
 
 export const JUICY_PICKS_PROJECT_IDS = [
   SPOTLIGHT_PROJECT_ID,
-  464, // purple
-  482, // it gets worse
-  488, // bananapus
-  477, // nance
+  501, // cryo
+  507, // thirsty thirsty
+  471, // pinnable
+  508, // cryo
 ]

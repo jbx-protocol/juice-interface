1c8ab6afda44ed9f5937dee48a168299a208c213
Wraeth <wraeth.dao@gmail.com>
2023-06-24 09:37:58 +1000

    JB-482 - Link ‘payments N’ to activity tab

--- src/components/ProjectDashboard/components/ProjectHeader/components/ProjectHeaderStats.tsx
+++ src/components/ProjectDashboard/components/ProjectHeader/components/ProjectHeaderStats.tsx
@@ -1,15 +1,25 @@
 import { t, Trans } from '@lingui/macro'
 import ETHAmount from 'components/currency/ETHAmount'
 import { useProjectHeader } from 'components/ProjectDashboard/hooks'
+import { useProjectPageQueries } from 'components/ProjectDashboard/hooks/useProjectPageQueries'
 import { TRENDING_WINDOW_DAYS } from 'pages/projects/RankingExplanation'
+import { useCallback } from 'react'
 import { ProjectHeaderStat } from './ProjectHeaderStat'
 
 export function ProjectHeaderStats() {
   const { payments, totalVolume, last7DaysPercent } = useProjectHeader()
+  const { setProjectPageTab } = useProjectPageQueries()
+
+  const openActivityTab = useCallback(
+    () => setProjectPageTab('activity'),
+    [setProjectPageTab],
+  )
 
   return (
     <div className="flex shrink-0 gap-12">
-      <ProjectHeaderStat label={t`Payments`} stat={payments} />
+      <a role="button" onClick={openActivityTab}>
+        <ProjectHeaderStat label={t`Payments`} stat={payments} />
+      </a>
       <ProjectHeaderStat
         label={t`Total volume`}
         stat={<ETHAmount amount={totalVolume} precision={2} />}

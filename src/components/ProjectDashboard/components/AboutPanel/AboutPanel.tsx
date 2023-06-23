8d5b87a093ae005f2a7fef0c3445be1f55a86483
Wraeth <wraeth.dao@gmail.com>
2023-06-23 17:02:54 +1000

    JB-488 - Make social links text weight medium and wire them up to social accounts

--- src/components/ProjectDashboard/components/AboutPanel/AboutPanel.tsx
+++ src/components/ProjectDashboard/components/AboutPanel/AboutPanel.tsx
@@ -1,17 +1,25 @@
 import { Trans } from '@lingui/macro'
-import { useAboutPanel } from 'components/ProjectDashboard/hooks/useAboutPanel'
+import {
+  SocialLink,
+  useAboutPanel,
+} from 'components/ProjectDashboard/hooks/useAboutPanel'
 import RichNote from 'components/RichNote/RichNote'
 import { SocialLinkButton } from '../ui'
 
 export const AboutPanel = () => {
-  const { description } = useAboutPanel()
+  const { description, socialLinks } = useAboutPanel()
   return (
     <div className="flex min-h-[384px] flex-col gap-8 md:max-w-[596px]">
       <div className="flex flex-wrap gap-y-3 gap-x-10">
-        <SocialLinkButton type="twitter" href="#" />
-        <SocialLinkButton type="discord" href="#" />
-        <SocialLinkButton type="telegram" href="#" />
-        <SocialLinkButton type="website" href="#" />
+        {Object.entries(socialLinks)
+          .filter(([, href]) => !!href)
+          .map(([type, href]) => (
+            <SocialLinkButton
+              key={type}
+              type={type as SocialLink}
+              href={href ?? ''}
+            />
+          ))}
       </div>
       {description ? (
         <RichNote note={description} />

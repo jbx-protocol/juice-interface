fbf02f894109671ba5b2f3ffb357e4a9b12538df
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-23 14:55:45 +0930

    chore: nuke onchain gov in reconfig

--- src/components/v2v3/shared/FundingCycleConfigurationDrawers/NftDrawer/shared/NftCollectionDetailsFormItems.tsx
+++ src/components/v2v3/shared/FundingCycleConfigurationDrawers/NftDrawer/shared/NftCollectionDetailsFormItems.tsx
@@ -1,29 +1,11 @@
-import { Trans, t } from '@lingui/macro'
-import { Form, Input, Radio } from 'antd'
-import { Badge } from 'components/Badge'
-import { Callout } from 'components/Callout'
-import ExternalLink from 'components/ExternalLink'
-import { MinimalCollapse } from 'components/MinimalCollapse'
-import { RadioItem } from 'components/inputs/RadioItem'
-import { FEATURE_FLAGS } from 'constants/featureFlags'
-import { JB721GovernanceType } from 'models/nftRewards'
-import { useMemo } from 'react'
-import { featureFlagEnabled } from 'utils/featureFlags'
-import { helpPagePath } from 'utils/routes'
+import { t } from '@lingui/macro'
+import { Form, Input } from 'antd'
 
 export function NftCollectionDetailsFormItems({
   isReconfigure,
 }: {
   isReconfigure?: boolean
 }) {
-  const showOnChainGoverance = useMemo(() => {
-    // If not reconfiguring, always return true
-    if (!isReconfigure) return true
-    // Only show if feature flag is enabled
-    return featureFlagEnabled(
-      FEATURE_FLAGS.RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE,
-    )
-  }, [isReconfigure])
   return (
     <>
       <Form.Item name="collectionName" label={t`Collection name`} required>
@@ -41,61 +23,6 @@ export function NftCollectionDetailsFormItems({
       <Form.Item name="collectionDescription" label={t`Collection description`}>
         <Input type="string" autoComplete="off" />
       </Form.Item>
-
-      {/* TODO: Remove this one feature flag is removed */}
-      {showOnChainGoverance && (
-        <MinimalCollapse header={<Trans>On-chain Governance</Trans>}>
-          <div className="flex flex-col gap-6">
-            {isReconfigure && (
-              <Callout.Info
-                className="bg-smoke-100 dark:bg-slate-500"
-                transparent
-              >
-                Once deployed, on-chain governance cannot be changed
-              </Callout.Info>
-            )}
-            <Form.Item name="onChainGovernance">
-              <Radio.Group
-                className="flex flex-col gap-5"
-                disabled={isReconfigure}
-                defaultValue={JB721GovernanceType.NONE}
-              >
-                <RadioItem
-                  value={JB721GovernanceType.NONE}
-                  title={
-                    <div className="flex gap-2">
-                      <Trans>No on-chain governance</Trans>{' '}
-                      <span>
-                        <Badge variant="info" upperCase>
-                          <Trans>Default</Trans>
-                        </Badge>
-                      </span>
-                    </div>
-                  }
-                  description={t`Your project's NFTs will not have on-chain governance capabilities.`}
-                />
-                <RadioItem
-                  value={JB721GovernanceType.ONCHAIN}
-                  title={t`Standard on-chain governance`}
-                  description={
-                    <Trans>
-                      Track the historical voting weight of each token holder
-                      across all tiers of NFTs.{' '}
-                      <ExternalLink
-                        href={helpPagePath(
-                          '/user/configuration/#on-chain-governance',
-                        )}
-                      >
-                        Learn more.
-                      </ExternalLink>
-                    </Trans>
-                  }
-                />
-              </Radio.Group>
-            </Form.Item>
-          </div>
-        </MinimalCollapse>
-      )}
     </>
   )
 }
--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -1,10 +1,6 @@
-export type FeatureFlag =
-  | 'SIMULATE_TXS'
-  | 'RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE'
-  | 'NEW_PROJECT_PAGE'
+export type FeatureFlag = 'SIMULATE_TXS' | 'NEW_PROJECT_PAGE'
 
 export const FEATURE_FLAGS: { [k in FeatureFlag]: string } = {
   SIMULATE_TXS: 'simulateTxs',
-  RECONFIGURE_SHOW_NFT_GOVERNANCE_TYPE: 'reconfigureShowNftGovernanceType',
   NEW_PROJECT_PAGE: 'newProjectPage',
 }

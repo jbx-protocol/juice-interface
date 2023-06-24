0f56a5a9338b3790d8a48ef2bc3e5ac16ebaff27
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-06-24 10:49:21 +1000

    JB-490 - Add ‘About {project title} heading to this section (#3808)
    ## What does this PR do and why?
    
    Closes JB-490
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] (if relevant) I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] (if relevant) I have tested this PR in dark mode and light mode (if applicable).

--- src/components/ProjectDashboard/components/AboutPanel/AboutPanel.tsx
+++ src/components/ProjectDashboard/components/AboutPanel/AboutPanel.tsx
@@ -7,9 +7,9 @@ import RichNote from 'components/RichNote/RichNote'
 import { SocialLinkButton } from '../ui'
 
 export const AboutPanel = () => {
-  const { description, socialLinks } = useAboutPanel()
+  const { description, socialLinks, projectName } = useAboutPanel()
   return (
-    <div className="flex min-h-[384px] flex-col gap-8 md:max-w-[596px]">
+    <div className="flex min-h-[384px] flex-col gap-8 md:max-w-[596px] md:gap-10">
       <div className="flex flex-wrap gap-y-3 gap-x-10">
         {Object.entries(socialLinks)
           .filter(([, href]) => !!href)
@@ -21,13 +21,18 @@ export const AboutPanel = () => {
             />
           ))}
       </div>
-      {description ? (
-        <RichNote note={description} />
-      ) : (
-        <div className="text-grey-500 dark:text-slate-200">
-          <Trans>This project has no description.</Trans>
-        </div>
-      )}
+      <div className="flex flex-col gap-4">
+        <h3 className="mb-0 font-heading text-2xl font-medium">
+          <Trans>About {projectName}</Trans>
+        </h3>
+        {description ? (
+          <RichNote className="mt-0" note={description} />
+        ) : (
+          <div className="text-grey-500 dark:text-slate-200">
+            <Trans>This project has no description.</Trans>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
--- src/components/ProjectDashboard/hooks/useAboutPanel.ts
+++ src/components/ProjectDashboard/hooks/useAboutPanel.ts
@@ -27,6 +27,7 @@ export const useAboutPanel = () => {
   return {
     description,
     socialLinks,
+    projectName: projectMetadata?.name,
   }
 }
 

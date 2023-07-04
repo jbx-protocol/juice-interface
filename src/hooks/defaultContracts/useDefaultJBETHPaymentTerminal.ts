1e13cc3110f70fb862de01c4a1d1687f22bbceb6
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-07-05 07:46:22 +1000

    add some comments

--- src/hooks/defaultContracts/useDefaultJBETHPaymentTerminal.ts
+++ src/hooks/defaultContracts/useDefaultJBETHPaymentTerminal.ts
@@ -3,6 +3,9 @@ import { V2V3ContractsContext } from 'contexts/v2v3/Contracts/V2V3ContractsConte
 import { NetworkName } from 'models/networkName'
 import { useContext } from 'react'
 
+/**
+ * Return the payment terminal address that should be used to launch projects, funding cycles, etc.
+ */
 export function useDefaultJBETHPaymentTerminal() {
   const { contracts } = useContext(V2V3ContractsContext)
 

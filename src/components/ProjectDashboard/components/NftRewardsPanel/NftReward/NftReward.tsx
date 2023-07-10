d4c4eca10207d108cf05e4558532f43e2af7b336
Wraeth <wraeth.dao@gmail.com>
2023-07-10 16:09:29 +1000

    fix: hide nfts if payments paused and show tooltip

--- src/components/ProjectDashboard/components/NftRewardsPanel/NftReward/NftReward.tsx
+++ src/components/ProjectDashboard/components/NftRewardsPanel/NftReward/NftReward.tsx
@@ -1,6 +1,10 @@
 import { t } from '@lingui/macro'
+import { Tooltip } from 'antd'
 import { NftPreview } from 'components/NftRewards/NftPreview'
-import { useProjectCart } from 'components/ProjectDashboard/hooks'
+import {
+  useProjectCart,
+  useProjectContext,
+} from 'components/ProjectDashboard/hooks'
 import { DEFAULT_NFT_MAX_SUPPLY } from 'constants/nftRewards'
 import { useNftRewardsEnabledForPay } from 'hooks/JB721Delegate/useNftRewardsEnabledForPay'
 import { NftRewardTier } from 'models/nftRewards'
@@ -35,6 +39,7 @@ export function NftReward({
   const [previewVisible, setPreviewVisible] = useState<boolean>(false)
   const cart = useProjectCart()
   const nftsEnabledForPay = useNftRewardsEnabledForPay()
+  const { fundingCycleMetadata } = useProjectContext()
 
   const quantitySelected = useMemo(
     () => cart.nftRewards.find(nft => nft.id === rewardTier?.id)?.quantity ?? 0,
@@ -59,38 +64,56 @@ export function NftReward({
     ? t`Unlimited`
     : t`${rewardTier?.remainingSupply} remaining`
 
-  const disabled = !hasRemainingSupply || !nftsEnabledForPay
+  const disabled =
+    !hasRemainingSupply || !nftsEnabledForPay || fundingCycleMetadata?.pausePay
+
+  const disabledReason = useMemo(() => {
+    if (!hasRemainingSupply) return t`Sold out`
+    if (!nftsEnabledForPay) return t`NFTs are not enabled for pay`
+    if (fundingCycleMetadata?.pausePay) return t`Payments are paused`
+  }, [hasRemainingSupply, nftsEnabledForPay, fundingCycleMetadata?.pausePay])
 
   return (
     <>
-      <div
-        className={twMerge(
-          'relative flex h-full w-40 select-none flex-col rounded-[10px] border border-grey-200 dark:border-slate-500 md:w-[252px]',
-          'shadow-[0_4px_14px_rgba(0,0,0,0.0392156862745098)]', // box-shadow: 0px 4px 14px 0px #0000000A
-          isSelected ? 'border-2 border-bluebs-500 dark:border-bluebs-500' : '',
-          !disabled ? 'group cursor-pointer' : 'cursor-not-allowed opacity-50',
-          className,
-        )}
-        onClick={!disabled ? openPreview : undefined}
+      <Tooltip
+        title={disabledReason}
+        placement="top"
+        open={disabled ? undefined : false}
       >
-        <NftThumbnail
-          fileUrl={fileUrl}
-          isSelected={isSelected}
-          rewardTier={rewardTier}
-        />
-        <NftDetails
-          rewardTier={rewardTier}
-          loading={loading}
-          hideAttributes={hideAttributes}
-          remainingSupplyText={remainingSupplyText}
-        />
-        {!disabled &&
-          (isSelected ? (
-            <RemoveNftButton onClick={() => onDeselect()} />
-          ) : (
-            <AddNftButton onClick={() => onSelect(1)} />
-          ))}
-      </div>
+        <div
+          className={twMerge(
+            'relative flex h-full w-40 select-none flex-col rounded-[10px] border border-grey-200 dark:border-slate-500 md:w-[252px]',
+            'shadow-[0_4px_14px_rgba(0,0,0,0.0392156862745098)]', // box-shadow: 0px 4px 14px 0px #0000000A
+            isSelected
+              ? 'border-2 border-bluebs-500 dark:border-bluebs-500'
+              : '',
+            !disabled
+              ? 'group cursor-pointer'
+              : 'cursor-not-allowed opacity-50',
+            className,
+          )}
+          onClick={!disabled ? openPreview : undefined}
+        >
+          <NftThumbnail
+            fileUrl={fileUrl}
+            isSelected={isSelected}
+            rewardTier={rewardTier}
+          />
+          <NftDetails
+            rewardTier={rewardTier}
+            loading={loading}
+            hideAttributes={hideAttributes}
+            remainingSupplyText={remainingSupplyText}
+          />
+          {!disabled &&
+            (isSelected ? (
+              <RemoveNftButton onClick={() => onDeselect()} />
+            ) : (
+              <AddNftButton onClick={() => onSelect(1)} />
+            ))}
+        </div>
+      </Tooltip>
+
       {rewardTier && !previewDisabled && previewVisible ? (
         <NftPreview
           open={previewVisible}

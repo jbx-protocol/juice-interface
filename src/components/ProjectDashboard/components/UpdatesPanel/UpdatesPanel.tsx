7b60eda6c93631813a9c757a2cfe8d9c28c9604e
Wraeth <wraeth.dao@gmail.com>
2023-07-12 13:08:14 +1000

    html/css for project update form modal

--- src/components/ProjectDashboard/components/UpdatesPanel/UpdatesPanel.tsx
+++ src/components/ProjectDashboard/components/UpdatesPanel/UpdatesPanel.tsx
@@ -3,28 +3,37 @@ import { Trans, t } from '@lingui/macro'
 import { Button } from 'antd'
 import { useProjectContext } from 'components/ProjectDashboard/hooks'
 import { useIsUserAddress } from 'hooks/useIsUserAddress'
+import { useCallback, useState } from 'react'
+import { AddProjectUpdateModal } from '../AddProjectUpdateModal'
 import { EmptyScreen } from '../EmptyScreen'
 
 export const UpdatesPanel = () => {
+  const [open, setOpen] = useState(false)
   const { projectOwnerAddress } = useProjectContext()
   const isProjectOwner = useIsUserAddress(projectOwnerAddress)
+
+  const openModal = useCallback(() => setOpen(true), [setOpen])
 
   const emptyScreenMessage = isProjectOwner
     ? t`Your project has no updates`
     : t`This project has no updates`
 
   return (
-    <div className="flex flex-col items-center gap-6">
-      <EmptyScreen title={emptyScreenMessage} />
-      {isProjectOwner && (
-        <Button
-          className="flex w-fit items-center gap-2"
-          type="primary"
-          icon={<PlusIcon className="h-5 w-5" />}
-        >
-          <Trans>Add project update</Trans>
-        </Button>
-      )}
-    </div>
+    <>
+      <div className="flex flex-col items-center gap-6">
+        <EmptyScreen title={emptyScreenMessage} />
+        {isProjectOwner && (
+          <Button
+            className="flex w-fit items-center gap-2"
+            type="primary"
+            icon={<PlusIcon className="h-5 w-5" />}
+            onClick={openModal}
+          >
+            <Trans>Add project update</Trans>
+          </Button>
+        )}
+      </div>
+      <AddProjectUpdateModal open={open} setOpen={setOpen} />
+    </>
   )
 }

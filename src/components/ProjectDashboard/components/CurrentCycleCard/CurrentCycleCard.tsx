ddfd6d7a3bd4372ec66009235aaa1a494e58e1b8
Wraeth <wraeth.dao@gmail.com>
2023-07-13 16:56:27 +1000

    fix: fix broken cycle card

--- src/components/ProjectDashboard/components/CurrentCycleCard/CurrentCycleCard.tsx
+++ src/components/ProjectDashboard/components/CurrentCycleCard/CurrentCycleCard.tsx
@@ -35,11 +35,11 @@ export const CurrentCycleCard = ({ className }: { className?: string }) => {
       className={twMerge('min-w-0 cursor-pointer pr-9', className)}
       onClick={openCyclePayoutsTab}
     >
-      <Tooltip className="inline-flex items-center gap-1" title={cycleTooltip}>
+      <Tooltip className="flex min-w-0 items-center gap-1" title={cycleTooltip}>
         <span className="truncate whitespace-nowrap text-base font-medium">
           <Trans>Current Cycle: #{currentCycleNumber}</Trans>
         </span>
-        <QuestionMarkCircleIcon className="h-4 w-4 text-grey-500 dark:text-slate-200" />
+        <QuestionMarkCircleIcon className="h-4 w-4 flex-shrink-0 text-grey-500 dark:text-slate-200" />
       </Tooltip>
       <div className="mt-4 flex min-w-0 items-center gap-2">
         <TruncatedText className="text-2xl font-medium" text={text} />

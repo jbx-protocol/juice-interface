6095bccf8f9b5807f500691553277fe0a2ff18bd
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-07-13 14:54:40 +1000

    support deletion of project updates (#3888)

--- src/pages/api/projects/[projectId]/updates/index.page.ts
+++ src/pages/api/projects/[projectId]/updates/index.page.ts
@@ -1,10 +1,9 @@
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { sudoPublicDbClient } from 'lib/api/supabase/clients'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { authenticateUserApiCall } from 'server/auth'
 import { Database } from 'types/database.types'
-import { isEqualAddress } from 'utils/address'
 import * as Yup from 'yup'
+import { authenticateProjectUpdateOperation } from '../authenticateProjectUpdateOperation'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await sudoPublicDbClient.auth.getSession()
@@ -60,33 +59,8 @@ const POSTSchema = Yup.object().shape({
 })
 
 const POST = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await authenticateUserApiCall(req, res)
+  const session = await authenticateProjectUpdateOperation(req, res)
   if (!session) return
-
-  const projectResult = await sudoPublicDbClient
-    .from('projects')
-    .select()
-    .eq('id', req.query.projectId)
-    .single()
-
-  if (projectResult.error) {
-    console.error('Error occurred', projectResult.error)
-    return res.status(500).json({ message: 'Internal server error occurred.' })
-  }
-
-  const userResult = await sudoPublicDbClient
-    .from('users')
-    .select()
-    .eq('id', session.user.id)
-    .single()
-  if (userResult.error) {
-    console.error('Error occurred', userResult.error)
-    return res.status(500).json({ message: 'Internal server error occurred.' })
-  }
-
-  if (!isEqualAddress(userResult.data.wallet, projectResult.data.owner)) {
-    return res.status(401).json({ message: 'Unauthorized.' })
-  }
 
   const result = await POSTSchema.validate(req.body)
   const { title, message, imageUrl } = result

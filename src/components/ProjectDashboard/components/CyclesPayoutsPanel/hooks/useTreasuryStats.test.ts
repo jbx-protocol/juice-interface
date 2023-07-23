1b92eec463dc321e4e444f11632035ff96e44fcc
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-07-24 09:17:16 +1000

    fix: broken tests (#3913)
    ## What does this PR do and why?
    
    _Provide a description of what this PR does. Link to any relevant GitHub issues, Notion tasks or Discord discussions._
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] (if relevant) I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] (if relevant) I have tested this PR in dark mode and light mode (if applicable).

--- src/components/ProjectDashboard/components/CyclesPayoutsPanel/hooks/useTreasuryStats.test.ts
+++ src/components/ProjectDashboard/components/CyclesPayoutsPanel/hooks/useTreasuryStats.test.ts
@@ -18,6 +18,7 @@ describe('useTreasuryStats', () => {
     distributionLimit: parseWad('100'),
     distributionLimitCurrency: BigNumber.from(V2V3_CURRENCY_ETH),
     balanceInDistributionLimitCurrency: parseWad('100'),
+    primaryTerminalCurrentOverflow: parseWad('0'),
   }
   const DefaultUseDistributableAmount = {
     distributableAmount: parseWad('100'),
@@ -61,30 +62,29 @@ describe('useTreasuryStats', () => {
     expect(result.current.overflow).toEqual('No overflow')
   })
 
-  it('returns undefined overflow if balanceInDistributionLimitCurrency is undefined', () => {
-    ;(useProjectContext as jest.Mock).mockReturnValue({
-      ...DefaultProjectContext,
-      balanceInDistributionLimitCurrency: undefined,
-    })
-    const { result } = renderHook(useTreasuryStats)
-    expect(result.current.overflow).toBeUndefined()
-  })
-
   it.each`
-    distributionLimit | balanceInDistributionLimitCurrency | expected
-    ${0}              | ${0}                               | ${'Ξ0'}
-    ${10}             | ${100}                             | ${'Ξ90'}
-    ${10}             | ${0}                               | ${'Ξ0'}
-    ${10}             | ${10}                              | ${'Ξ0'}
-    ${10}             | ${11}                              | ${'Ξ1'}
+    distributionLimit | balanceInDistributionLimitCurrency | primaryTerminalCurrentOverflow | expected
+    ${0}              | ${0}                               | ${0}                           | ${'Ξ0'}
+    ${10}             | ${100}                             | ${90}                          | ${'Ξ90'}
+    ${10}             | ${0}                               | ${0}                           | ${'Ξ0'}
+    ${10}             | ${10}                              | ${0}                           | ${'Ξ0'}
+    ${10}             | ${11}                              | ${1}                           | ${'Ξ1'}
   `(
     'returns overflow = $expected if distributionLimit is $distributionLimit and balanceInDistributionLimitCurrency is $balanceInDistributionLimitCurrency',
-    ({ distributionLimit, balanceInDistributionLimitCurrency, expected }) => {
+    ({
+      distributionLimit,
+      balanceInDistributionLimitCurrency,
+      primaryTerminalCurrentOverflow,
+      expected,
+    }) => {
       ;(useProjectContext as jest.Mock).mockReturnValue({
         ...DefaultProjectContext,
         distributionLimit: parseWad(distributionLimit),
         balanceInDistributionLimitCurrency: parseWad(
           balanceInDistributionLimitCurrency,
+        ),
+        primaryTerminalCurrentOverflow: parseWad(
+          primaryTerminalCurrentOverflow,
         ),
       })
       const { result } = renderHook(useTreasuryStats)

df330f3b48611fa65d283fc790f95face591c725
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-07-25 10:17:48 +1000

    fix: remove about panel title completely and hide div of links if non-existent
    ## What does this PR do and why?
    
    _Provide a description of what this PR does. Link to any relevant GitHub issues, Notion tasks or Discord discussions._
    
    ## Screenshots or screen recordings
    
    _If applicable, provide screenshots or screen recordings to demonstrate the changes._
    
    ## Acceptance checklist
    
    - [ ] I have evaluated the [Approval Guidelines](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#approval-guidelines) for this PR.
    - [ ] (if relevant) I have tested this PR in [all supported browsers](https://github.com/jbx-protocol/juice-interface/blob/main/CONTRIBUTING.md#supported-browsers).
    - [ ] (if relevant) I have tested this PR in dark mode and light mode (if applicable).

--- src/components/ProjectDashboard/components/AboutPanel/AboutPanel.tsx
+++ src/components/ProjectDashboard/components/AboutPanel/AboutPanel.tsx
@@ -1,4 +1,4 @@
-import { Trans, t } from '@lingui/macro'
+import { t } from '@lingui/macro'
 import {
   SocialLink,
   useAboutPanel,
@@ -8,28 +8,27 @@ import { EmptyScreen } from '../EmptyScreen'
 import { SocialLinkButton } from '../ui'
 
 export const AboutPanel = () => {
-  const { description, socialLinks, projectName } = useAboutPanel()
+  const { description, socialLinks } = useAboutPanel()
   return (
     <div className="flex min-h-[384px] w-full flex-col gap-8 md:max-w-[596px] md:gap-10">
-      <div className="flex flex-wrap gap-y-3 gap-x-10">
-        {Object.entries(socialLinks)
-          .filter(([, href]) => !!href)
-          .map(([type, href]) => (
-            <SocialLinkButton
-              key={type}
-              type={type as SocialLink}
-              href={href ?? ''}
-            />
-          ))}
-      </div>
+      {Object.keys(socialLinks).length > 0 && (
+        <div className="flex flex-wrap gap-y-3 gap-x-10">
+          {Object.entries(socialLinks)
+            .filter(([, href]) => !!href)
+            .map(([type, href]) => (
+              <SocialLinkButton
+                key={type}
+                type={type as SocialLink}
+                href={href ?? ''}
+              />
+            ))}
+        </div>
+      )}
       <div className="flex flex-col gap-4">
         {description ? (
           <RichPreview source={description} />
         ) : (
           <>
-            <h3 className="mb-0 font-heading text-2xl font-medium">
-              <Trans>About {projectName}</Trans>
-            </h3>
             <EmptyScreen subtitle={t`This project has no description`} />
           </>
         )}

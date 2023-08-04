8ed64cf583d965a7bdabc3cacfd5b0338b57d46c
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-08-04 14:19:01 +1000

    fix: FormattedNumberInput has lost y padding (#3937)

--- src/components/inputs/JuiceInputNumber.tsx
+++ src/components/inputs/JuiceInputNumber.tsx
@@ -1,11 +1,11 @@
 import { InputNumber, InputNumberProps } from 'antd'
-import { classNames } from 'utils/classNames'
+import { twMerge } from 'tailwind-merge'
 
 export const JuiceInputNumber = (props: InputNumberProps) => {
   return (
     <InputNumber
       {...props}
-      className={classNames(
+      className={twMerge(
         'border-smoke-300 bg-smoke-50 text-black dark:border-slate-300 dark:bg-slate-600 dark:text-slate-100 dark:placeholder:text-slate-300',
         props.className,
       )}

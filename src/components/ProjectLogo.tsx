19b2c829458f9b479f5c4a0284e6b92d2bd065d7
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-08-04 16:10:11 +0200

    [PREVIEW] feat: add project logo api, use in payouts

--- src/components/ProjectLogo.tsx
+++ src/components/ProjectLogo.tsx
@@ -13,21 +13,26 @@ export default function ProjectLogo({
   name,
   projectId,
   lazyLoad,
+  fallback = '🧃',
 }: {
-  className?: string
-  uri: string | undefined
   name: string | undefined
+  className?: string
+  uri?: string | undefined
   projectId?: number | undefined
   lazyLoad?: boolean
+  fallback?: string | JSX.Element | null
 }) {
   const [srcLoadError, setSrcLoadError] = useState(false)
-  const validImg = uri && !srcLoadError
+  const [loading, setLoading] = useState(true)
 
   const imageSrc = useMemo(() => {
-    if (projectId && IMAGE_URI_OVERRIDES[projectId]) {
+    if (!projectId) return undefined
+
+    if (IMAGE_URI_OVERRIDES[projectId]) {
       return IMAGE_URI_OVERRIDES[projectId]
     }
-    if (!uri) return undefined
+
+    if (!uri) return `/api/juicebox/projectLogo/${projectId}`
 
     // Some older JB projects have a logo URI hardcoded to use Pinata.
     // JBM no longer uses Pinata.
@@ -40,11 +45,14 @@ export default function ProjectLogo({
     return ipfsUriToGatewayUrl(uri)
   }, [uri, projectId])
 
+  const validImg = imageSrc && !srcLoadError
+
   return (
     <div
       className={twMerge(
-        'flex h-20 w-20 items-center justify-center overflow-hidden rounded-lg',
+        'flex h-20 w-20 items-center justify-center overflow-hidden rounded-lg text-4xl',
         'bg-smoke-100 dark:bg-slate-700',
+        loading ? 'animate-pulse' : undefined,
         className,
       )}
     >
@@ -54,15 +62,14 @@ export default function ProjectLogo({
           src={imageSrc}
           alt={name + ' logo'}
           onError={() => setSrcLoadError(true)}
+          onLoad={() => setLoading(false)}
           loading={lazyLoad ? 'lazy' : undefined}
           crossOrigin="anonymous"
           title={name}
         />
-      ) : (
-        <div className="text-4xl" title={name}>
-          🧃
-        </div>
-      )}
+      ) : null}
+
+      {!validImg ? <span title={name}>{fallback}</span> : null}
     </div>
   )
 }
---
+++ src/pages/api/juicebox/projectLogo/[projectId].page.ts
@@ -0,0 +1,93 @@
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import axios from 'axios'
+import { getLogger } from 'lib/logger'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Database } from 'types/database.types'
+import {
+  cidFromUrl,
+  ipfsGatewayUrl,
+  ipfsUriToGatewayUrl,
+  isIpfsUri,
+} from 'utils/ipfs'
+import { getProjectMetadata } from 'utils/server/metadata'
+
+const logger = getLogger('api/juicebox/projectLogo/[projectId]')
+
+async function findLogoFromDb(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  projectId: string,
+) {
+  const supabase = createServerSupabaseClient<Database>({ req, res })
+
+  const query = await supabase
+    .from('projects')
+    .select('logo_uri')
+    .eq('project_id', projectId)
+
+  if (query.error) throw query.error
+
+  return query.data?.[0]?.logo_uri as string | undefined
+}
+
+async function findLogoOnChain(projectId: string) {
+  const { logoUri } = (await getProjectMetadata(projectId as string)) ?? {}
+  return logoUri
+}
+
+/**
+ *
+ * @dev 2 network requests happen here:
+ * 1. fetch the project metadata CID (DB fetch is prioritized)
+ * 2. fetch the project logo image data
+ *
+ * Note, if DB request fails, we still try to fetch the logo from chain. This will be 2 more requests.
+ *
+ * Thus this endpoint should be cached.
+ * However, the ideal duration is TBD.
+ * Please consider changing (lowering) if it causes issues with users.
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'GET') {
+    return res.status(404)
+  }
+
+  try {
+    const { projectId } = req.query
+    if (!projectId) {
+      return res.status(400).json({ error: 'projectId is required' })
+    }
+
+    // try getting logo from db. if for some reason its not there, try getting it from chain.
+    const logoUri =
+      (await findLogoFromDb(req, res, projectId as string)) ??
+      (await findLogoOnChain(projectId as string))
+    if (!logoUri) {
+      return res.status(404).json({ error: 'project metadata not found' })
+    }
+
+    const imageUrl = isIpfsUri(logoUri)
+      ? ipfsUriToGatewayUrl(logoUri)
+      : // Some older JB projects have a logo URI hardcoded to use Pinata.
+        // JBM no longer uses Pinata.
+        // This rewrites those URLs to use the Infura gateway.
+        ipfsGatewayUrl(cidFromUrl(logoUri))
+
+    const logoImageRes = await axios.get(imageUrl, {
+      responseType: 'arraybuffer',
+    })
+
+    const contentType = logoImageRes.headers['content-type'] ?? 'image/png'
+    res.setHeader('Content-Type', contentType)
+    res.setHeader('Content-Length', logoImageRes.data.length)
+    // cache for 5 minutes
+    res.setHeader('Cache-Control', `s-maxage=${60 * 5}, stale-while-revalidate`)
+
+    return res.end(logoImageRes.data, 'binary')
+  } catch (err) {
+    logger.error({ error: err })
+    return res.status(500).json({ error: 'failed to resolve project logo' })
+  }
+}
+
+export default handler

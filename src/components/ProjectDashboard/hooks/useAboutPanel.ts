0692b0109ab5ac91752496fdf4ea16dc07e23ec1
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-08-06 05:16:21 +1000

    fix: wrap non anchor links in anchor (#3941)

--- src/components/ProjectDashboard/hooks/useAboutPanel.ts
+++ src/components/ProjectDashboard/hooks/useAboutPanel.ts
@@ -23,12 +23,28 @@ export const useAboutPanel = () => {
     projectMetadata?.telegram,
     projectMetadata?.twitter,
   ])
-  const description = projectMetadata?.description
+  const description = useMemo(
+    () =>
+      projectMetadata?.description
+        ? wrapNonAnchorsInAnchor(projectMetadata?.description)
+        : undefined,
+    [projectMetadata?.description],
+  )
+
   return {
     description,
     socialLinks,
     projectName: projectMetadata?.name,
   }
+}
+
+const wrapNonAnchorsInAnchor = (text: string) => {
+  const urlRegex =
+    /\b((http|https):\/\/[a-zA-Z0-9-._~:/?#@\\[\]!$&'()*+,;=%]+)/g
+
+  return text.replace(urlRegex, url => {
+    return '<a href="' + url + '">' + url + '</a>'
+  })
 }
 
 const linkOrUndefined = (link: string | undefined) => {

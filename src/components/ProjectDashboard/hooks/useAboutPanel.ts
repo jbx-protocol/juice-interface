45b49606c931dd592cf547ce729ec6db62bf1bc3
Filip V <f@filip.world>
2023-08-05 16:16:11 -0400

    fix: small URL regex correction (#3946)

--- src/components/ProjectDashboard/hooks/useAboutPanel.ts
+++ src/components/ProjectDashboard/hooks/useAboutPanel.ts
@@ -40,7 +40,7 @@ export const useAboutPanel = () => {
 
 const wrapNonAnchorsInAnchor = (text: string) => {
   const urlRegex =
-    /\b((http|https):\/\/[a-zA-Z0-9-._~:/?#@\\[\]!$&'()*+,;=%]+)/g
+    /\b((http|https):\/\/[a-zA-Z0-9-._~:/?#@\\[\]!$&'()*+,;=%]+)\b/g
 
   return text.replace(urlRegex, url => {
     return '<a href="' + url + '">' + url + '</a>'

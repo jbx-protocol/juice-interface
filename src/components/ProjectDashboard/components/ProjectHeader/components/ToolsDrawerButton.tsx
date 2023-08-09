8144b02f51874905c4b292e558778767835c2c4c
peripheralist <peripheralist@gmail.com>
2023-08-09 13:27:45 -0600

    add tools drawer to new v2v3 dashboard

---
+++ src/components/ProjectDashboard/components/ProjectHeader/components/ToolsDrawerButton.tsx
@@ -0,0 +1,21 @@
+import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
+import { Tooltip } from 'antd'
+import { V2V3ProjectToolsDrawer } from 'components/v2v3/V2V3Project/V2V3ProjectToolsDrawer'
+import { useState } from 'react'
+
+export default function ToolsDrawerButton() {
+  const [open, setOpen] = useState<boolean>()
+
+  return (
+    <>
+      <Tooltip title={<span>Advanced tools</span>}>
+        <EllipsisVerticalIcon
+          className="h-6 w-6 cursor-pointer text-bluebs-600"
+          onClick={() => setOpen(true)}
+        />
+      </Tooltip>
+
+      <V2V3ProjectToolsDrawer open={open} onClose={() => setOpen(false)} />
+    </>
+  )
+}

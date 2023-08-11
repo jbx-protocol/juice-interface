fc5a3881b673110b3a17c3455f4695e94a724d68
peripheralist <peripheralist@gmail.com>
2023-08-11 13:55:23 -0600

    support tools drawer on mobile

--- src/components/ProjectDashboard/components/ProjectHeader/components/ProjectHeaderPopupMenu.tsx
+++ src/components/ProjectDashboard/components/ProjectHeader/components/ProjectHeaderPopupMenu.tsx
@@ -1,12 +1,17 @@
+import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
 import { Trans } from '@lingui/macro'
 import { BookmarkButtonIcon } from 'components/BookmarkButton/BookmarkButtonIcon'
 import { useBookmarkButton } from 'components/BookmarkButton/hooks/useBookmarkButton'
 import { SubscribeButtonIcon } from 'components/SubscribeButton/SubscribeButtonIcon'
 import { useSubscribeButton } from 'components/SubscribeButton/hooks/useSubscribeButton'
+import { V2V3ProjectToolsDrawer } from 'components/v2v3/V2V3Project/V2V3ProjectToolsDrawer'
 import { PV_V2 } from 'constants/pv'
+import { useState } from 'react'
 import { PopupMenu } from '../../../../ui/PopupMenu'
 
 export function ProjectHeaderPopupMenu({ projectId }: { projectId: number }) {
+  const [toolsIsOpen, setToolsIsOpen] = useState<boolean>()
+
   const { isBookmarked, onBookmarkButtonClicked } = useBookmarkButton({
     projectId,
     pv: PV_V2,
@@ -16,46 +21,71 @@ export function ProjectHeaderPopupMenu({ projectId }: { projectId: number }) {
   })
 
   return (
-    <PopupMenu
-      menuButtonIconClassName="h-8 w-8"
-      items={[
-        {
-          id: 'bookmark',
-          label: (
-            <>
-              <BookmarkButtonIcon
-                isBookmarked={isBookmarked}
-                className="h-5 w-5"
-              />
-              <span className="whitespace-nowrap text-sm font-medium">
-                <Trans>Bookmark project</Trans>
-              </span>
-            </>
-          ),
-          onClick(ev) {
-            ev.preventDefault()
-            ev.stopPropagation()
+    <>
+      <PopupMenu
+        menuButtonIconClassName="h-8 w-8"
+        items={[
+          {
+            id: 'bookmark',
+            label: (
+              <>
+                <BookmarkButtonIcon
+                  isBookmarked={isBookmarked}
+                  className="h-5 w-5"
+                />
+                <span className="whitespace-nowrap text-sm font-medium">
+                  <Trans>Bookmark project</Trans>
+                </span>
+              </>
+            ),
+            onClick(ev) {
+              ev.preventDefault()
+              ev.stopPropagation()
 
-            onBookmarkButtonClicked()
+              onBookmarkButtonClicked()
+            },
           },
-        },
-        {
-          id: 'subscribe',
-          label: (
-            <>
-              <SubscribeButtonIcon
-                isSubscribed={isSubscribed}
-                className="h-5 w-5"
-              />
+          {
+            id: 'subscribe',
+            label: (
+              <>
+                <SubscribeButtonIcon
+                  isSubscribed={isSubscribed}
+                  className="h-5 w-5"
+                />
 
-              <span className="whitespace-nowrap text-sm font-medium">
-                <Trans>Subscribe to updates</Trans>
-              </span>
-            </>
-          ),
-          onClick: onSubscribeButtonClicked,
-        },
-      ]}
-    />
+                <span className="whitespace-nowrap text-sm font-medium">
+                  <Trans>Subscribe to updates</Trans>
+                </span>
+              </>
+            ),
+            onClick: onSubscribeButtonClicked,
+          },
+          {
+            id: 'tools',
+            label: (
+              <>
+                <EllipsisVerticalIcon className="h-5 w-5" />
+
+                <span className="whitespace-nowrap text-sm font-medium">
+                  <Trans>Advanced tools</Trans>
+                </span>
+              </>
+            ),
+            onClick: ev => {
+              ev.preventDefault()
+              ev.stopPropagation()
+
+              setToolsIsOpen(true)
+            },
+          },
+        ]}
+      />
+
+      <V2V3ProjectToolsDrawer
+        open={toolsIsOpen}
+        onClose={() => setToolsIsOpen(false)}
+      />
+    </>
   )
 }
--- src/components/ProjectDashboard/components/ProjectHeader/components/ToolsDrawerButton.tsx
+++ src/components/ProjectDashboard/components/ProjectHeader/components/ToolsDrawerButton.tsx
@@ -1,4 +1,5 @@
 import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
+import { t } from '@lingui/macro'
 import { Tooltip } from 'antd'
 import { V2V3ProjectToolsDrawer } from 'components/v2v3/V2V3Project/V2V3ProjectToolsDrawer'
 import { useState } from 'react'
@@ -8,7 +9,7 @@ export default function ToolsDrawerButton() {
 
   return (
     <>
-      <Tooltip title={<span>Advanced tools</span>}>
+      <Tooltip title={t`Advanced tools`}>
         <EllipsisVerticalIcon
           className="h-6 w-6 cursor-pointer text-bluebs-600 dark:text-white"
           onClick={() => setOpen(true)}

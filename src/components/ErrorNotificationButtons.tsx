8e16fd8827947f549711a417d6f916b7314b96f8
Wraeth <wraeth.dao@gmail.com>
2023-08-30 10:04:25 +1000

    reapply language provide to error notification buttons

--- src/components/ErrorNotificationButtons.tsx
+++ src/components/ErrorNotificationButtons.tsx
@@ -1,5 +1,7 @@
 import { WarningOutlined } from '@ant-design/icons'
+import { Trans } from '@lingui/macro'
 import { Button } from 'antd'
+import { LanguageProvider } from 'contexts/Language/LanguageProvider'
 import { helpPagePath } from 'utils/routes'
 import ExternalLink from './ExternalLink'
 
@@ -12,16 +14,20 @@ const resetSite = () => {
 
 export default function ErrorNotificationButtons() {
   return (
-    <div className="flex items-center gap-5">
-      <ExternalLink
-        className="text-grey-900 hover:underline dark:text-slate-100"
-        href={helpPagePath('/user/resources/troubleshoot/')}
-      >
-        Troubleshoot
-      </ExternalLink>
-      <Button icon={<WarningOutlined />} size="small" onClick={resetSite}>
-        <span>Reset website</span>
-      </Button>
-    </div>
+    <LanguageProvider>
+      <div className="flex items-center gap-5">
+        <ExternalLink
+          className="text-grey-900 hover:underline dark:text-slate-100"
+          href={helpPagePath('/user/resources/troubleshoot/')}
+        >
+          <Trans>Troubleshoot</Trans>
+        </ExternalLink>
+        <Button icon={<WarningOutlined />} size="small" onClick={resetSite}>
+          <span>
+            <Trans>Reset website</Trans>
+          </span>
+        </Button>
+      </div>
+    </LanguageProvider>
   )
 }

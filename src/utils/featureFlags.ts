6ad495beb0e618b7abb2c620d1c8075f23b0f443
Johnny D <johnnyd.eth@protonmail.com>
2023-09-14 22:13:19 +1000

    Flip settings feature flags (new edit cycle and nft pages)

--- src/utils/featureFlags.ts
+++ src/utils/featureFlags.ts
@@ -18,6 +18,16 @@ const FEATURE_FLAG_DEFAULTS: {
     goerli: true,
     localhost: true,
   },
+  [FEATURE_FLAGS.NEW_CYCLE_CONFIG_PAGE]: {
+    mainnet: true,
+    goerli: true,
+    localhost: true,
+  },
+  [FEATURE_FLAGS.NEW_EDIT_NFTS]: {
+    mainnet: true,
+    goerli: true,
+    localhost: true,
+  },
 }
 
 const featureFlagKey = (baseKey: string) => {

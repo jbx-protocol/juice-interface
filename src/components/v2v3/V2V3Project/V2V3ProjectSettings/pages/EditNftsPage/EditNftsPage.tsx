70b303e08cf138ce1850f20ccbae00dd1f1efa3b
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-09-20 16:43:06 +0200

    remove old unused project page code

--- src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/EditNftsPage/EditNftsPage.tsx
+++ src/components/v2v3/V2V3Project/V2V3ProjectSettings/pages/EditNftsPage/EditNftsPage.tsx
@@ -3,9 +3,9 @@ import { ProjectMetadataContext } from 'contexts/shared/ProjectMetadataContext'
 import { V2V3ProjectContext } from 'contexts/v2v3/Project/V2V3ProjectContext'
 import { useNftDeployerCanReconfigure } from 'hooks/JB721Delegate/contractReader/useNftDeployerCanReconfigure'
 import { useContext } from 'react'
+import { LaunchNftsPage } from './LaunchNftCollection'
 import { EnableNftsCard } from './LaunchNftCollection/EnableNftsCard'
-import { LaunchNftsPage } from './LaunchNftCollection/LaunchNftsCollection'
-import { UpdateNftsPage } from './UpdateNftsPage/UpdateNftsPage'
+import { UpdateNftsPage } from './UpdateNftsPage'
 
 export function EditNftsPage() {
   const { projectId } = useContext(ProjectMetadataContext)

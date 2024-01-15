c2ea34388492d1109f133008b705ef02895df4a6
Filip <f@filip.world>
2024-01-15 13:26:19 -0500

    fix: use primary ETH payment terminal for held fees txs

--- src/hooks/v2v3/contractReader/useHeldFeesOf.ts
+++ src/hooks/v2v3/contractReader/useHeldFeesOf.ts
@@ -1,25 +1,24 @@
 import { ProjectMetadataContext } from 'contexts/shared/ProjectMetadataContext'
-import { V2V3ContractsContext } from 'contexts/v2v3/Contracts/V2V3ContractsContext'
 import { useContext, useEffect, useState } from 'react'
 import { sumHeldFees } from 'utils/v2v3/math'
+import { useProjectPrimaryEthTerminal } from '../V2V3ProjectContracts/projectContractLoaders/useProjectPrimaryEthTerminal'
 
 export function useHeldFeesOf() {
   const [heldFees, setHeldFees] = useState<number>()
 
   const { projectId } = useContext(ProjectMetadataContext)
-  const { contracts } = useContext(V2V3ContractsContext)
+  if (!projectId) return
+  const { JBETHPaymentTerminal } = useProjectPrimaryEthTerminal({ projectId })
 
   useEffect(() => {
     async function fetchData() {
-      const res = await contracts?.JBETHPaymentTerminal.functions.heldFeesOf(
-        projectId,
-      )
+      const res = await JBETHPaymentTerminal?.functions.heldFeesOf(projectId)
       if (!res) return
       const _heldFees = sumHeldFees(res[0])
       setHeldFees(_heldFees)
     }
     fetchData()
-  }, [contracts, projectId])
+  }, [projectId, JBETHPaymentTerminal])
 
   return heldFees
 }

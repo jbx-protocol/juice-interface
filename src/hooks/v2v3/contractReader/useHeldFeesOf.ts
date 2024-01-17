380c104fd170ded642699fb0093104913cd2c9d9
Filip <f@filip.world>
2024-01-17 17:17:39 -0500

    simplify useHeldFeesOf

--- src/hooks/v2v3/contractReader/useHeldFeesOf.ts
+++ src/hooks/v2v3/contractReader/useHeldFeesOf.ts
@@ -1,24 +1,23 @@
 import { ProjectMetadataContext } from 'contexts/shared/ProjectMetadataContext'
+import { V2V3ProjectContractsContext } from 'contexts/v2v3/ProjectContracts/V2V3ProjectContractsContext'
 import { useContext, useEffect, useState } from 'react'
 import { sumHeldFees } from 'utils/v2v3/math'
-import { useProjectPrimaryEthTerminal } from '../V2V3ProjectContracts/projectContractLoaders/useProjectPrimaryEthTerminal'
 
 export function useHeldFeesOf() {
   const [heldFees, setHeldFees] = useState<number>()
-
   const { projectId } = useContext(ProjectMetadataContext)
-  if (!projectId) return
-  const { JBETHPaymentTerminal } = useProjectPrimaryEthTerminal({ projectId })
+  const { contracts } = useContext(V2V3ProjectContractsContext)
 
   useEffect(() => {
     async function fetchData() {
-      const res = await JBETHPaymentTerminal?.functions.heldFeesOf(projectId)
+      const res = await contracts.JBETHPaymentTerminal?.functions.heldFeesOf(
+        projectId,
+      )
       if (!res) return
       const _heldFees = sumHeldFees(res[0])
       setHeldFees(_heldFees)
     }
     fetchData()
-  }, [projectId, JBETHPaymentTerminal])
-
+  }, [projectId, contracts])
   return heldFees
 }

7a58c8056d6611c362fe09271b0256c76929175a
Johnny D <johnnyD.eth@protonmail.com>
2024-01-22 12:35:09 +0800

    fix: break words in project description (#4226)

--- src/components/RichPreview/RichPreview.tsx
+++ src/components/RichPreview/RichPreview.tsx
@@ -6,6 +6,7 @@ export const RichPreview = ({ source }: { source: string }) => {
   return (
     <div
       id="rich-text"
+      className="break-words"
       dangerouslySetInnerHTML={{
         __html: purified,
       }}

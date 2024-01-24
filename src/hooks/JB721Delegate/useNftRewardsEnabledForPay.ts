87832f101524ea713954ddf7e4cd6e51bd7bf11d
Wraeth <wraeth.dao@gmail.com>
2024-01-24 11:33:35 +0900

    cache Roman storm project nfts

--- src/hooks/JB721Delegate/useNftRewardsEnabledForPay.ts
+++ src/hooks/JB721Delegate/useNftRewardsEnabledForPay.ts
@@ -1,11 +1,14 @@
+import { NftRewardsContext } from 'contexts/NftRewards/NftRewardsContext'
 import { V2V3ProjectContext } from 'contexts/v2v3/Project/V2V3ProjectContext'
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { hasDataSourceForPay } from 'utils/v2v3/fundingCycle'
-import { useHasNftRewards } from './useHasNftRewards'
 
 export function useNftRewardsEnabledForPay() {
   const { fundingCycleMetadata } = useContext(V2V3ProjectContext)
-  const { value: hasNftRewards } = useHasNftRewards()
+  const {
+    nftRewards: { rewardTiers },
+  } = useContext(NftRewardsContext)
+  const hasNftRewards = useMemo(() => rewardTiers?.length !== 0, [rewardTiers])
 
   return hasNftRewards && hasDataSourceForPay(fundingCycleMetadata)
 }

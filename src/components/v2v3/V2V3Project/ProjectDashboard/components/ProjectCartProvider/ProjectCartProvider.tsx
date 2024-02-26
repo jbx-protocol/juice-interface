4a7d15991a21d4680a21a92abd7cbd8b9365f74c
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2024-02-27 07:09:57 +1000

    fix: nft credit when credit > selected (#4264)

--- src/components/v2v3/V2V3Project/ProjectDashboard/components/ProjectCartProvider/ProjectCartProvider.tsx
+++ src/components/v2v3/V2V3Project/ProjectDashboard/components/ProjectCartProvider/ProjectCartProvider.tsx
@@ -44,17 +44,51 @@ export const ProjectCartContext = createContext<ProjectCartContextType>({
   payModalOpen: false,
 })
 
+/**
+ * Return the sum total of selected NFTs to mint, minus any credits the user has.
+ */
+function useNftRewardsTotal({
+  selectedNftRewards,
+  payAmountCurrency,
+  userNftCredits,
+}: {
+  selectedNftRewards: ProjectCartNftReward[]
+  payAmountCurrency: V2V3CurrencyOption | undefined
+  userNftCredits: ReturnType<typeof useNftCredits>
+}) {
+  const converter = useCurrencyConverter()
+  const nftRewards = useContext(NftRewardsContext).nftRewards
+
+  const rewardTiers = nftRewards.rewardTiers ?? []
+  const userNftCreditsNumber = parseFloat(
+    formatEther(userNftCredits.data ?? BigNumber.from(0)),
+  )
+
+  let nftRewardsTotal = selectedNftRewards.reduce(
+    (acc, nft) =>
+      acc +
+      Number(rewardTiers.find(n => n.id === nft.id)?.contributionFloor ?? 0) *
+        nft.quantity,
+    0,
+  )
+
+  if (nftRewardsTotal > 0) {
+    nftRewardsTotal = Math.max(0, nftRewardsTotal - userNftCreditsNumber)
+  }
+
+  if (payAmountCurrency === V2V3_CURRENCY_USD) {
+    nftRewardsTotal =
+      converter.weiToUsd(parseWad(nftRewardsTotal))?.toNumber() ?? 0
+  }
+
+  return nftRewardsTotal
+}
+
 export const ProjectCartProvider = ({
   children,
 }: {
   children: React.ReactNode
 }) => {
-  const { userAddress } = useWallet()
-  const userNftCredits = useNftCredits(userAddress)
-  const userNftCreditsNumber = parseFloat(
-    formatEther(userNftCredits.data ?? BigNumber.from(0)),
-  )
-
   const [state, dispatch] = useReducer(projectCartReducer, {
     payAmount: undefined,
     nftRewards: [],
@@ -62,48 +96,29 @@ export const ProjectCartProvider = ({
     expanded: false,
     payModalOpen: false,
   })
-  const converter = useCurrencyConverter()
-  const nftRewards = useContext(NftRewardsContext).nftRewards
-  const rewardTiers = useMemo(
-    () => nftRewards.rewardTiers ?? [],
-    [nftRewards.rewardTiers],
-  )
+
+  const { userAddress } = useWallet()
+  const userNftCredits = useNftCredits(userAddress)
 
   const visible = useMemo(
     () => (state?.payAmount?.amount ?? 0) > 0 || state?.nftRewards?.length > 0,
     [state?.nftRewards?.length, state?.payAmount?.amount],
   )
 
-  const totalAmount = useMemo(() => {
-    let nftRewardsTotal = state.nftRewards.reduce(
-      (acc, nft) =>
-        acc +
-        Number(rewardTiers.find(n => n.id === nft.id)?.contributionFloor ?? 0) *
-          nft.quantity,
-      0,
-    )
-    if (nftRewardsTotal > 0) {
-      nftRewardsTotal -= userNftCreditsNumber
-    }
-    if (state.payAmount?.currency === V2V3_CURRENCY_USD) {
-      nftRewardsTotal =
-        converter.weiToUsd(parseWad(nftRewardsTotal))?.toNumber() ?? 0
-    }
+  const nftRewardsTotal = useNftRewardsTotal({
+    selectedNftRewards: state.nftRewards,
+    payAmountCurrency: state.payAmount?.currency,
+    userNftCredits,
+  })
 
+  const totalAmount = useMemo(() => {
     const payAmount = state.payAmount?.amount ?? 0
 
     return {
       amount: payAmount + nftRewardsTotal,
       currency: state.payAmount?.currency ?? V2V3_CURRENCY_ETH,
     }
-  }, [
-    userNftCreditsNumber,
-    converter,
-    rewardTiers,
-    state.nftRewards,
-    state.payAmount?.amount,
-    state.payAmount?.currency,
-  ])
+  }, [state.payAmount?.amount, state.payAmount?.currency, nftRewardsTotal])
 
   const value = {
     dispatch,

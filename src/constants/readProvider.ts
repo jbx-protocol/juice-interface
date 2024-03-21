f0895534dd466ca93dad21bfd4d53d517b21c9e0
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-03-21 10:14:51 +1000

    fix: nuke batch provider (#4289)

--- src/constants/readProvider.ts
+++ src/constants/readProvider.ts
@@ -1,7 +1,7 @@
 import { providers } from 'ethers'
 import { readNetwork } from './networks'
 
-export const readProvider = new providers.JsonRpcBatchProvider(
+export const readProvider = new providers.StaticJsonRpcProvider(
   readNetwork.rpcUrl,
   readNetwork.chainId,
 )

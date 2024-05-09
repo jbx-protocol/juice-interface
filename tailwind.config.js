15a6ed6f9a9cbac11b9586847bacb274540d1c1e
Wraeth <wraeth.dao@gmail.com>
2024-05-09 15:39:02 +1000

    shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   darkMode: 'class', // Use class for now, switch to media later
   important: true,
+  prefix: '',
   theme: {
     fontFamily: {
       display: ['Agrandir-Wide', ...defaultTheme.fontFamily.sans],
@@ -23,6 +26,13 @@ module.exports = {
         'Courier New',
         'monospace',
       ],
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
     },
     extend: {
       gridTemplateColumns: {
@@ -50,8 +60,18 @@ module.exports = {
           '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
           '100%': { transform: 'translate(50%, 100%)', opacity: '0' },
         },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
       },
       animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
         rise: 'rise 0.6s ease-in-out forwards',
         set: 'set 0.6s ease-in-out forwards',
       },
@@ -272,5 +292,6 @@ module.exports = {
       }
       addUtilities(newUtilities)
     },
+    require('tailwindcss-animate'),
   ],
 }

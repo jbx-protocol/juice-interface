66e9710b8c315e956eecbc36614fc2c7bdb7b420
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2024-05-18 12:45:18 +1000

    fix: fix bad base url (#4334)

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,7 +1,8 @@
-export const SiteBaseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
-    : undefined
+export const SiteBaseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  ? process.env.NEXT_PUBLIC_BASE_URL
+  : process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
+  : undefined
 
 // Fixes File '/Users/wraeth/projects/juice-interface/src/constants/url.ts' is not a module.
 export {}

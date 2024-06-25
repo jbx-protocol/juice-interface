8957c46a0a944dcdf5335d4ab5a5481b49978d9f
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-06-25 21:25:26 +1000

    project payer

--- src/constants/transactionDefaults.ts
+++ src/constants/transactionDefaults.ts
@@ -1,5 +1,7 @@
+import { toUtf8Bytes } from 'ethers'
+
 export const DEFAULT_MEMO = ''
-export const DEFAULT_METADATA = 0
+export const DEFAULT_METADATA = toUtf8Bytes('0')
 export const DEFAULT_MIN_RETURNED_TOKENS = 0
 export const DEFAULT_ALLOW_OVERSPENDING = true
 export const DEFAULT_JB_721_TIER_CATEGORY = 1

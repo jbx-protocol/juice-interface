2947d551966a3471207af2b962f493470ce4deef
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-07-06 12:24:24 +1000

    feat: init subgraph, activity feed

---
+++ src/packages/v4/graphql/useSubgraphQuery.ts
@@ -0,0 +1,24 @@
+import { type TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { useQuery, type UseQueryResult } from "@tanstack/react-query";
+import request from "graphql-request";
+
+export function useSubgraphQuery<TResult, TVariables>(
+  document: TypedDocumentNode<TResult, TVariables>,
+  ...[variables]: TVariables extends Record<string, never> ? [] : [TVariables]
+): UseQueryResult<TResult> {
+  return useQuery({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    queryKey: [(document.definitions[0] as any).name.value, variables],
+    queryFn: async ({ queryKey }) => {
+      if (!process.env.NEXT_PUBLIC_V4_SUBGRAPH_URL) {
+        throw new Error("NEXT_PUBLIC_V4_SUBGRAPH_URL is not set");
+      }
+
+      return request(
+        process.env.NEXT_PUBLIC_V4_SUBGRAPH_URL,
+        document,
+        queryKey[1] ? queryKey[1] : undefined
+      );
+    },
+  });
+}
--- src/packages/v4/views/V4ProjectDashboard/V4ProjectDashboard.tsx
+++ src/packages/v4/views/V4ProjectDashboard/V4ProjectDashboard.tsx
@@ -1,4 +1,5 @@
 import { useJBContractContext } from 'juice-sdk-react'
+import { ActivityList } from 'packages/v4/components/ActivityList/ActivitiyList'
 import { NativeTokenValue } from 'packages/v4/components/NativeTokenValue'
 import { useNativeTokenSurplus } from 'packages/v4/hooks/useNativeTokenSurplus'
 
@@ -7,13 +8,28 @@ export function V4ProjectDashboard() {
   const { data: nativeTokenSurplus } = useNativeTokenSurplus()
 
   return (
-    <div>
-      <h1>Project #{projectId.toString()}</h1>
-      <div>
-        Surplus (overflow):{' '}
-        {nativeTokenSurplus ? (
-          <NativeTokenValue wei={nativeTokenSurplus} />
-        ) : null}
+    <div className="mx-auto max-w-5xl p-6">
+      <header>
+        <h1>Project #{projectId.toString()}</h1>
+      </header>
+      <div className="flex justify-between gap-2">
+        <div>
+          <div>
+            Surplus (overflow):{' '}
+            {nativeTokenSurplus ? (
+              <NativeTokenValue wei={nativeTokenSurplus} />
+            ) : null}
+          </div>
+
+          <div className="flex max-h-52 overflow-auto">
+            <ActivityList />
+          </div>
+        </div>
+
+        <div>
+          Pay me
+          <div>Input</div>
+        </div>
       </div>
     </div>
   )

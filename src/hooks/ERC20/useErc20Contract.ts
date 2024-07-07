4247261cd289b7bcd1dec4c6a60f53aa6147cc05
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-07-08 09:03:27 +1000

    chore: remove unused deps

--- src/hooks/ERC20/useErc20Contract.ts
+++ src/hooks/ERC20/useErc20Contract.ts
@@ -1,6 +1,5 @@
-import erc20Abi from 'erc-20-abi'
-
 import { useLoadContractFromAddress } from '../useLoadContractFromAddress'
+import erc20Abi from './erc20Abi'
 
 export function useErc20Contract(address: string | undefined) {
   return useLoadContractFromAddress({ address, abi: erc20Abi })

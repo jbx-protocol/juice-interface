99cb0f01170825511d1efe9b086efd84e668d3c0
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-07-18 21:08:14 +1000

    fix useV4DistributableAmount

--- src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/CyclesPayoutsPanel/hooks/useV4DistributableAmount.ts
+++ src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/CyclesPayoutsPanel/hooks/useV4DistributableAmount.ts
@@ -1,25 +1,54 @@
-import { useReadJbProjectsBalanceOf, useReadJbTerminalStoreUsedPayoutLimitOf } from 'juice-sdk-react'
+import { NATIVE_CURRENCY_ID, NATIVE_TOKEN } from 'juice-sdk-core'
+import {
+  useJBContractContext,
+  useJBRulesetContext,
+  useJBTerminalContext,
+  useReadJbTerminalStoreBalanceOf,
+  useReadJbTerminalStoreUsedPayoutLimitOf,
+} from 'juice-sdk-react'
 import { usePayoutLimits } from 'packages/v4/hooks/usePayoutLimits'
+import { zeroAddress } from 'viem'
 
 export const useV4DistributableAmount = () => {
-  const { data: usedPayoutLimit } = useReadJbTerminalStoreUsedPayoutLimitOf()
+  const { store } = useJBTerminalContext()
+  const { projectId, contracts } = useJBContractContext()
+  const { ruleset } = useJBRulesetContext()
+  const { data: usedPayoutLimit } = useReadJbTerminalStoreUsedPayoutLimitOf({
+    address: store.data ?? undefined,
+    args: [
+      contracts.primaryNativeTerminal.data ?? zeroAddress,
+      projectId,
+      NATIVE_TOKEN,
+      ruleset.data?.cycleNumber ?? 0n,
+      NATIVE_CURRENCY_ID,
+    ],
+  })
 
-  const { data: _balanceInPayoutLimitCurrency } = useReadJbProjectsBalanceOf()
+  const { data: _balanceInPayoutLimitCurrency } =
+    useReadJbTerminalStoreBalanceOf({
+      address: store.data ?? undefined,
+      args: [
+        contracts.primaryNativeTerminal.data ?? zeroAddress,
+        projectId,
+        NATIVE_TOKEN,
+      ],
+    })
 
   const { data: payoutLimit } = usePayoutLimits()
 
   const effectiveDistributionLimit = payoutLimit?.amount ?? 0n
   const distributedAmount = usedPayoutLimit ?? 0n
-  const balanceInDistributionLimit =
-    _balanceInPayoutLimitCurrency ?? 0n
+  const balanceInDistributionLimit = _balanceInPayoutLimitCurrency ?? 0n
 
-  const distributable = effectiveDistributionLimit === 0n
-    ? effectiveDistributionLimit
-    : effectiveDistributionLimit - distributedAmount
+  const distributable =
+    effectiveDistributionLimit === 0n
+      ? effectiveDistributionLimit
+      : effectiveDistributionLimit - distributedAmount
 
-  const distributableAmount = balanceInDistributionLimit > distributable
-    ? distributable
-    : balanceInDistributionLimit
+  const distributableAmount =
+    balanceInDistributionLimit > distributable
+      ? distributable
+      : balanceInDistributionLimit
 
   return {
     distributableAmount,

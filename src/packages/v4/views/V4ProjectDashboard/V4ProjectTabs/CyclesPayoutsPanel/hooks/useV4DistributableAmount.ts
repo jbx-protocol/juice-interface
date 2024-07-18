13d8b0b28f01d7b672835551d68f14568ff25561
johnnyd-eth <johnnyd.dev@gmail.com>
2024-07-18 16:21:26 +1000

    _

--- src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/CyclesPayoutsPanel/hooks/useV4DistributableAmount.ts
+++ src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/CyclesPayoutsPanel/hooks/useV4DistributableAmount.ts
@@ -1,25 +1,35 @@
-import { useReadJbProjectsBalanceOf, useReadJbTerminalStoreUsedPayoutLimitOf } from 'juice-sdk-react'
+import * as constants from '@ethersproject/constants'
+import { NATIVE_TOKEN } from 'juice-sdk-core'
+import { useJBContractContext, useReadJbTerminalStoreBalanceOf, useReadJbTerminalStoreUsedPayoutLimitOf } from 'juice-sdk-react'
 import { usePayoutLimits } from 'packages/v4/hooks/usePayoutLimits'
 
 export const useV4DistributableAmount = () => {
+  const { projectId, contracts: { primaryNativeTerminal }} = useJBContractContext()
   const { data: usedPayoutLimit } = useReadJbTerminalStoreUsedPayoutLimitOf()
 
-  const { data: _balanceInPayoutLimitCurrency } = useReadJbProjectsBalanceOf()
+  const { data: _treasuryBalance } = useReadJbTerminalStoreBalanceOf({
+    address: primaryNativeTerminal.data ?? undefined, // TODO: this must be wrong, find terminalStore address
+    args: [
+        primaryNativeTerminal.data ?? constants.AddressZero,
+        projectId,
+        NATIVE_TOKEN
+    ]
+  })
 
   const { data: payoutLimit } = usePayoutLimits()
 
   const effectiveDistributionLimit = payoutLimit?.amount ?? 0n
   const distributedAmount = usedPayoutLimit ?? 0n
-  const balanceInDistributionLimit =
-    _balanceInPayoutLimitCurrency ?? 0n
+  const treasuryBalance =
+    _treasuryBalance ?? 0n
 
   const distributable = effectiveDistributionLimit === 0n
     ? effectiveDistributionLimit
     : effectiveDistributionLimit - distributedAmount
 
-  const distributableAmount = balanceInDistributionLimit > distributable
+  const distributableAmount = treasuryBalance > distributable
     ? distributable
-    : balanceInDistributionLimit
+    : treasuryBalance
 
   return {
     distributableAmount,

98c4295afdc05089fa6264cc386a3cf70446284d
johnnyd-eth <johnnyd.dev@gmail.com>
2024-08-07 10:28:06 +1000

    v4 about tab

--- src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/V4AboutPanel/V4AboutPanel.tsx
+++ src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/V4AboutPanel/V4AboutPanel.tsx
@@ -1,10 +1,10 @@
-import { t } from '@lingui/macro'
-import { EmptyScreen } from 'components/Project/ProjectTabs/EmptyScreen'
-import { RichPreview } from 'components/RichPreview/RichPreview'
-import { useAboutPanel } from 'packages/v2v3/components/V2V3Project/ProjectDashboard/hooks/useAboutPanel'
+import { t } from "@lingui/macro"
+import { EmptyScreen } from "components/Project/ProjectTabs/EmptyScreen"
+import { RichPreview } from "components/RichPreview/RichPreview"
+import { useV4AboutPanel } from "./hooks/useV4AboutPanel"
 
-export const AboutPanel = () => {
-  const { description } = useAboutPanel()
+export const V4AboutPanel = () => {
+  const { description } = useV4AboutPanel()
   return (
     <div className="flex min-h-[384px] w-full flex-col gap-8 md:gap-10">
       <div className="flex flex-col gap-4 whitespace-pre-wrap">

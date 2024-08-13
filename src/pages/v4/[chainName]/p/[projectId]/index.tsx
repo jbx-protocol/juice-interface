0bc8422bae0197750d2595a4888b0d67c86a96b9
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-08-13 21:02:44 +1000

    revert any i18n ting v4

--- src/pages/v4/[chainName]/p/[projectId]/index.tsx
+++ src/pages/v4/[chainName]/p/[projectId]/index.tsx
@@ -12,7 +12,6 @@ import { wagmiConfig } from 'packages/v4/wagmiConfig'
 import React, { PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
 import { featureFlagEnabled } from 'utils/featureFlags'
-import globalGetServerSideProps from 'utils/next-server/globalGetServerSideProps'
 import { WagmiProvider } from 'wagmi'
 
 // This is a hack to avoid SSR for now. At the moment when this is not applied to this page, you will see a rehydration error.
@@ -37,25 +36,6 @@ const _Wrapper: React.FC<PropsWithChildren> = ({ children }) => {
   return <>{children}</>
 }
 
-export const getServerSideProps = globalGetServerSideProps
-
-export default function V4ProjectPage() {
-  const router = useRouter()
-  const { chainName, projectId } = router.query
-  if (!chainName || !projectId) {
-    return <div>Invalid URL</div>
-  }
-  const chainId = chainNameMap[chainName as string]
-
-  return (
-    <_Wrapper>
-      <Providers chainId={chainId} projectId={BigInt(projectId as string)}>
-        <V4ProjectDashboard />
-      </Providers>
-    </_Wrapper>
-  )
-}
-
 const Providers: React.FC<
   PropsWithChildren & { chainId: JBChainId; projectId: bigint }
 > = ({ chainId, projectId, children }) => {
@@ -77,5 +57,22 @@ const Providers: React.FC<
         </JBProjectProvider>
       </WagmiProvider>
     </AppWrapper>
+  )
+}
+
+export default function V4ProjectPage() {
+  const router = useRouter()
+  const { chainName, projectId } = router.query
+  if (!chainName || !projectId) {
+    return <div>Invalid URL</div>
+  }
+  const chainId = chainNameMap[chainName as string]
+
+  return (
+    <_Wrapper>
+      <Providers chainId={chainId} projectId={BigInt(projectId as string)}>
+        <V4ProjectDashboard />
+      </Providers>
+    </_Wrapper>
   )
 }

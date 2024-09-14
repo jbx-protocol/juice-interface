f809ddfb0b39476a0898c1b4863a4b2b38caa3cb
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-09-15 06:30:36 +1000

    feat: add chain_id col to projects

--- supabase/migrations/20240913212503_project_pv_v4.sql
+++ supabase/migrations/20240913212503_project_pv_v4.sql
@@ -4,3 +4,6 @@ DROP CONSTRAINT projects_pv_check;
 ALTER TABLE public.projects 
 ADD CONSTRAINT projects_pv_check
 CHECK (pv IN ('1', '2', '4'));
+
+ALTER TABLE public.projects
+add COLUMN "chain_id" int

2fdbedfa1e1df23df8cb2bfcda7b81f06d977541
Johnny D <johnnyD.eth@protonmail.com>
2024-09-30 12:30:59 +1000

    feat: v4 send reserved tokens (#4477)

--- src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/V4TokensPanel/V4DistributeReservedTokensModal.tsx
+++ src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/V4TokensPanel/V4DistributeReservedTokensModal.tsx
@@ -1,11 +1,15 @@
 import { t, Trans } from '@lingui/macro'
+import { waitForTransactionReceipt } from '@wagmi/core'
 import TransactionModal from 'components/modals/TransactionModal'
+import { TxHistoryContext } from 'contexts/Transaction/TxHistoryContext'
 import useNameOfERC20 from 'hooks/ERC20/useNameOfERC20'
-import { useJBContractContext, useReadJbTokensTokenOf } from 'juice-sdk-react'
+import { useJBContractContext, useReadJbTokensTokenOf, useWriteJbControllerSendReservedTokensToSplitsOf } from 'juice-sdk-react'
 import SplitList from 'packages/v4/components/SplitList/SplitList'
 import useProjectOwnerOf from 'packages/v4/hooks/useV4ProjectOwnerOf'
 import { useV4ReservedSplits } from 'packages/v4/hooks/useV4ReservedSplits'
-import { useState } from 'react'
+import { wagmiConfig } from 'packages/v4/wagmiConfig'
+import { useContext, useState } from 'react'
+import { emitErrorNotification } from 'utils/notifications'
 import { tokenSymbolText } from 'utils/tokenSymbolText'
 import { useV4ReservedTokensSubPanel } from './hooks/useV4ReservedTokensSubPanel'
 
@@ -18,7 +22,9 @@ export default function V4DistributeReservedTokensModal({
   onCancel?: VoidFunction
   onConfirmed?: VoidFunction
 }) {
-  const { projectId } = useJBContractContext()
+  const { addTransaction } = useContext(TxHistoryContext)
+
+  const { projectId, contracts } = useJBContractContext()
   const { splits: reservedTokensSplits } = useV4ReservedSplits()
   const { data: projectOwnerAddress } = useProjectOwnerOf()
   const { data: tokenAddress } = useReadJbTokensTokenOf()
@@ -28,30 +34,47 @@ export default function V4DistributeReservedTokensModal({
   const [transactionPending, setTransactionPending] = useState<boolean>()
 
   // const distributeReservedTokensTx = useDistributeReservedTokens()
+  const { writeContractAsync: writeSendReservedTokens, data } =
+    useWriteJbControllerSendReservedTokensToSplitsOf()
+
   const { pendingReservedTokens, pendingReservedTokensFormatted } =
     useV4ReservedTokensSubPanel()
 
-  async function distributeReservedTokens() {
+  async function sendReservedTokens() {
+    if (
+      // !payoutLimitAmountCurrency ||
+      // !distributionAmount ||
+      !contracts.controller.data ||
+      !projectId
+    )
+      return
+
     setLoading(true)
 
-    // const txSuccessful = await distributeReservedTokensTx(
-    //   {},
-    //   {
-    //     onDone: () => {
-    //       setTransactionPending(true)
-    //     },
-    //     onConfirmed: () => {
-    //       setLoading(false)
-    //       setTransactionPending(false)
-    //       onConfirmed?.()
-    //     },
-    //   },
-    // )
+    const args = [
+      BigInt(projectId)
+    ] as const
 
-    // if (!txSuccessful) {
-    setLoading(false)
-    setTransactionPending(false)
-    // }
+    try {
+      const hash = await writeSendReservedTokens({
+        address: contracts.controller.data,
+        args,
+      })
+      setTransactionPending(true)
+
+      addTransaction?.('Send reserved tokens', { hash })
+      await waitForTransactionReceipt(wagmiConfig, {
+        hash,
+      })
+
+      setLoading(false)
+      setTransactionPending(false)
+      onConfirmed?.()
+    } catch (e) {
+      setLoading(false)
+
+      emitErrorNotification((e as unknown as Error).message)
+    }
   }
 
   const tokenTextPlural = tokenSymbolText({
@@ -70,7 +93,7 @@ export default function V4DistributeReservedTokensModal({
     <TransactionModal
       title={<Trans>Send reserved {tokenTextPlural}</Trans>}
       open={open}
-      onOk={() => distributeReservedTokens()}
+      onOk={() => sendReservedTokens()}
       okText={t`Send ${tokenTextPlural}`}
       connectWalletText={t`Connect wallet to send reserved ${tokenTextPlural}`}
       confirmLoading={loading}

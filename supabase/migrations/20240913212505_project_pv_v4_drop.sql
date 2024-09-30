e154a28f70e4fb2172b6470a5c55489e4880d1e0
johnnyd-eth <johnnyd.dev@gmail.com>
2024-09-30 11:19:32 +1000

    fix: conditionally add chain_id col to supabase

--- supabase/migrations/20240913212505_project_pv_v4_drop.sql
+++ supabase/migrations/20240913212505_project_pv_v4_drop.sql
@@ -1,14 +1,3 @@
-DO $
-$
-BEGIN
-  IF NOT EXISTS (
-        SELECT 1
-  FROM pg_catalog.pg_attribute
-  WHERE attrelid = 'public.projects'::regclass
-        AND attname = 'chain_id'
-        AND NOT attisdropped
-    ) THEN
-  ALTER TABLE public.projects ADD COLUMN "chain_id" int;
-END
-IF;
-END $$;
+ALTER TABLE public.projects
+ADD COLUMN
+IF NOT EXISTS "chain_id" int;

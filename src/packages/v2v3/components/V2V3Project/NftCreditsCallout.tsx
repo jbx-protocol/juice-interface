797ff63c4a188beaa81434579476faa2472504ca
Wraeth <wraeth.dao@gmail.com>
2025-01-03 08:46:33 +1000

    add spacing for nft callout credit

--- src/packages/v2v3/components/V2V3Project/NftCreditsCallout.tsx
+++ src/packages/v2v3/components/V2V3Project/NftCreditsCallout.tsx
@@ -1,10 +1,10 @@
-import { CubeIcon } from "@heroicons/react/24/outline"
-import { Trans } from "@lingui/macro"
-import { Button } from "antd"
-import { useWallet } from "hooks/Wallet"
-import { useNftCredits } from "packages/v2v3/hooks/JB721Delegate/useNftCredits"
-import { fromWad } from "utils/format/formatNumber"
-import { useProjectPageQueries } from "./ProjectDashboard/hooks/useProjectPageQueries"
+import { CubeIcon } from '@heroicons/react/24/outline'
+import { Trans } from '@lingui/macro'
+import { Button } from 'antd'
+import { useWallet } from 'hooks/Wallet'
+import { useNftCredits } from 'packages/v2v3/hooks/JB721Delegate/useNftCredits'
+import { fromWad } from 'utils/format/formatNumber'
+import { useProjectPageQueries } from './ProjectDashboard/hooks/useProjectPageQueries'
 
 export function NftCreditsCallout() {
   const { setProjectPageTab } = useProjectPageQueries()
@@ -13,19 +13,23 @@ export function NftCreditsCallout() {
   if (nftCredits && nftCredits.gt(0)) {
     return (
       <div
-        className={'flex flex-col gap-5 rounded-lg border border-grey-200 bg-white p-5 pb-6 shadow-[0_6px_16px_0_rgba(0,_0,_0,_0.04)] dark:border-slate-600 dark:bg-slate-700'}
+        className={
+          'mt-5 flex flex-col gap-5 rounded-lg border border-grey-200 bg-white p-5 pb-6 shadow-[0_6px_16px_0_rgba(0,_0,_0,_0.04)] dark:border-slate-600 dark:bg-slate-700'
+        }
       >
-        <div className='flex flex-start gap-1.5 items-center'>
-          <div className='bg-bluebs-50 dark:bg-bluebs-500 rounded-full flex items-center justify-center p-1'>
-            <CubeIcon className="h-5 w-5 text-gray-500 text-bluebs-600 dark:text-black stroke-2" />
+        <div className="flex-start flex items-center gap-1.5">
+          <div className="flex items-center justify-center rounded-full bg-bluebs-50 p-1 dark:bg-bluebs-500">
+            <CubeIcon className="text-gray-500 h-5 w-5 stroke-2 text-bluebs-600 dark:text-black" />
           </div>
           <Trans>
-            You have <strong className="font-bold">{fromWad(nftCredits)} ETH</strong> of unclaimed NFT credits
+            You have{' '}
+            <strong className="font-bold">{fromWad(nftCredits)} ETH</strong> of
+            unclaimed NFT credits
           </Trans>
         </div>
-        <Button 
-          type="default" 
-          className="bg-bluebs-50 dark:bg-bluebs-500 border-none font-medium dark:text-black"
+        <Button
+          type="default"
+          className="border-none bg-bluebs-50 font-medium dark:bg-bluebs-500 dark:text-black"
           onClick={() => setProjectPageTab('nft_rewards')}
         >
           Browse NFTs

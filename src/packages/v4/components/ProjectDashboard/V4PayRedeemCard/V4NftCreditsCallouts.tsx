1cd2592ea7f0452142221bc2215b40776cfa63c4
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2025-01-03 12:35:47 +1100

    add spacing for nft callout credit (#4569)

--- src/packages/v4/components/ProjectDashboard/V4PayRedeemCard/V4NftCreditsCallouts.tsx
+++ src/packages/v4/components/ProjectDashboard/V4PayRedeemCard/V4NftCreditsCallouts.tsx
@@ -16,7 +16,7 @@ export function V4NftCreditsCallouts() {
   return (
     <div
       className={
-        'flex flex-col gap-5 rounded-lg border border-grey-200 bg-white p-5 pb-6 shadow-[0_6px_16px_0_rgba(0,_0,_0,_0.04)] dark:border-slate-600 dark:bg-slate-700'
+        'mt-5 flex flex-col gap-5 rounded-lg border border-grey-200 bg-white p-5 pb-6 shadow-[0_6px_16px_0_rgba(0,_0,_0,_0.04)] dark:border-slate-600 dark:bg-slate-700'
       }
     >
       <div className="flex-start flex items-center gap-1.5">

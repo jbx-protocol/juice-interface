2b467100095a19f8c870934b7467f2d134c5e173
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-01-03 14:58:21 +1000

    Update v4 wagmi transports

--- src/packages/v4/wagmiConfig.ts
+++ src/packages/v4/wagmiConfig.ts
@@ -13,13 +13,13 @@ export const wagmiConfig = createConfig({
       `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
     ),
     [optimismSepolia.id]: http(
-      'https://sepolia.optimism.io'
+      `https://optimism-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
     ),
     [baseSepolia.id]: http(
-      `https://api.developer.coinbase.com/rpc/v1/base-sepolia/${process.env.NEXT_PUBLIC_BASE_ID}`,
+      `https://base-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
     ),
     [arbitrumSepolia.id]: http(
-      `https://sepolia-rollup.arbitrum.io/rpc`,
+      `https://arbitrum-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
     ),
   },
 })

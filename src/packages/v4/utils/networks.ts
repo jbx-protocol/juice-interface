3ec1efa09aef96898363196bd8c50792844429be
Wraeth <wraeth.dao@gmail.com>
2025-01-06 10:50:36 +1000

    Fix: ensure chain slug is used over name for pathing

--- src/packages/v4/utils/networks.ts
+++ src/packages/v4/utils/networks.ts
@@ -5,6 +5,10 @@ export function getChainName(chainId: number) {
   return JB_CHAINS[chainId as JBChainId].name
 }
 
+export function getChainSlug(chainId: number) {
+  return JB_CHAINS[chainId as JBChainId].slug
+}
+
 export function isTestnet(chainId: number) {
   return TESTNET_IDS.has(chainId)
 }

5f4b5404837b79238c64241c68c1364772389c6c
Wraeth <wraeth.dao@gmail.com>
2025-01-08 08:39:06 +1000

    v4 feature: filter omnichain activity list results

--- src/lib/apollo/subgraphUri.ts
+++ src/lib/apollo/subgraphUri.ts
@@ -1,5 +1,11 @@
 import { isBrowser } from 'utils/isBrowser'
-import { sepolia } from 'viem/chains'
+import {
+  arbitrumSepolia,
+  baseSepolia,
+  optimism,
+  optimismSepolia,
+  sepolia,
+} from 'viem/chains'
 
 export const subgraphUri = () => {
   let uri: string | undefined
@@ -29,9 +35,26 @@ export const v4SubgraphUri = (chainId: number) => {
   let uri: string | undefined
 
   const env = {
+    [optimism.id]: {
+      browserUrl: process.env.NEXT_PUBLIC_V4_OPTIMISM_SUBGRAPH_URL,
+      serverUrl: process.env.V4_OPTIMISM_SUBGRAPH_URL,
+    },
+    // Test nets
     [sepolia.id]: {
       browserUrl: process.env.NEXT_PUBLIC_V4_SEPOLIA_SUBGRAPH_URL,
       serverUrl: process.env.V4_SEPOLIA_SUBGRAPH_URL,
+    },
+    [optimismSepolia.id]: {
+      browserUrl: process.env.NEXT_PUBLIC_V4_OPTIMISM_SEPOLIA_SUBGRAPH_URL,
+      serverUrl: process.env.V4_OPTIMISM_SEPOLIA_SUBGRAPH_URL,
+    },
+    [baseSepolia.id]: {
+      browserUrl: process.env.NEXT_PUBLIC_V4_BASE_SEPOLIA_SUBGRAPH_URL,
+      serverUrl: process.env.V4_BASE_SEPOLIA_SUBGRAPH_URL,
+    },
+    [arbitrumSepolia.id]: {
+      browserUrl: process.env.NEXT_PUBLIC_V4_ARBITRUM_SEPOLIA_SUBGRAPH_URL,
+      serverUrl: process.env.V4_ARBITRUM_SEPOLIA_SUBGRAPH_URL,
     },
   } as Record<number, { browserUrl?: string; serverUrl?: string }>
 

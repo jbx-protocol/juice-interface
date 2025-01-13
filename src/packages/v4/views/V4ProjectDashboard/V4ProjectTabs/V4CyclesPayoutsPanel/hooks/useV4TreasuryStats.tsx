6a21f1284c50de27dcb616042ef165dd5c560cd7
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-01-14 07:08:38 +1000

    Aggregate suckers balance

--- src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/V4CyclesPayoutsPanel/hooks/useV4TreasuryStats.tsx
+++ src/packages/v4/views/V4ProjectDashboard/V4ProjectTabs/V4CyclesPayoutsPanel/hooks/useV4TreasuryStats.tsx
@@ -1,14 +1,13 @@
+import { Tooltip } from 'antd'
+import { NETWORKS } from 'constants/networks'
 import {
   JBChainId,
   NativeTokenValue,
   useJBRulesetMetadata,
-  useSuckersNativeTokenSurplus
+  useSuckersNativeTokenBalance,
+  useSuckersNativeTokenSurplus,
 } from 'juice-sdk-react'
-
-import { Tooltip } from 'antd'
-import { NETWORKS } from 'constants/networks'
 import { ChainLogo } from 'packages/v4/components/ChainLogo'
-import { useV4BalanceOfNativeTerminal } from 'packages/v4/hooks/useV4BalanceOfNativeTerminal'
 import { useMemo } from 'react'
 import { useV4DistributableAmount } from './useV4DistributableAmount'
 
@@ -16,21 +15,64 @@ export const useV4TreasuryStats = () => {
   const { data: rulesetMetadata } = useJBRulesetMetadata()
   const { distributableAmount } = useV4DistributableAmount()
 
-  const { data: _treasuryBalance } = useV4BalanceOfNativeTerminal()
+  const { data: suckersBalance } = useSuckersNativeTokenBalance()
+  const totalBalance =
+    suckersBalance?.reduce((acc, curr) => {
+      return acc + curr.balance
+    }, 0n) ?? 0n
 
-  const surplusQuery = useSuckersNativeTokenSurplus();
+  const surplusQuery = useSuckersNativeTokenSurplus()
 
   // NOTE: if loading state or conversion to USD necessary
   // const { data: ethPrice, isLoading: isEthLoading } = useEtherPrice();
   // const loading = isEthLoading || surplusQuery.isLoading;
 
-  const ethSurplusByChain = surplusQuery?.data 
+  const ethSurplusByChain = surplusQuery?.data
   const totalEthSurplus =
     ethSurplusByChain?.reduce((acc, curr) => {
-      return acc + curr.surplus;
-    }, 0n) ?? 0n;
+      return acc + curr.surplus
+    }, 0n) ?? 0n
 
-  const treasuryBalance = <NativeTokenValue wei={_treasuryBalance ?? 0n} />
+  const treasuryBalance = useMemo(() => {
+    // NOTE: Don't think we need this since other chains payouts limits may be different?
+    // if (payoutLimit && payoutLimit.amount === MAX_PAYOUT_LIMIT)
+    //   return t`No surplus`
+    return (
+      <Tooltip
+        title={
+          suckersBalance?.length && suckersBalance.length > 0 ? (
+            <div className="flex flex-col gap-2">
+              {suckersBalance?.map((balance, index) => (
+                <div
+                  className="flex items-center justify-between gap-4"
+                  key={suckersBalance[index].chainId}
+                >
+                  <div
+                    key={suckersBalance[index].chainId}
+                    className="flex items-center gap-2"
+                  >
+                    <ChainLogo
+                      chainId={suckersBalance[index].chainId as JBChainId}
+                    />
+                    <span>{NETWORKS[balance.chainId].label}</span>
+                  </div>
+                  {/* (NOTE: Following comment copied from Revnet: 
+                  "TODO maybe show USD-converted value here instead?" */}
+                  <span className="whitespace-nowrap font-medium">
+                    <NativeTokenValue wei={balance.balance ?? 0n} />
+                  </span>
+                </div>
+              ))}
+            </div>
+          ) : undefined
+        }
+      >
+        <span>
+          <NativeTokenValue wei={totalBalance} />
+        </span>
+      </Tooltip>
+    )
+  }, [totalBalance, suckersBalance])
 
   const surplusElement = useMemo(() => {
     // NOTE: Don't think we need this since other chains payouts limits may be different?
@@ -38,31 +80,43 @@ export const useV4TreasuryStats = () => {
     //   return t`No surplus`
     return (
       <Tooltip
-        title={ethSurplusByChain?.length && ethSurplusByChain.length > 0 ?
-          <div className="flex flex-col gap-2">
-            {ethSurplusByChain?.map((surplus, index) => (
-              <div className="flex justify-between gap-4 items-center" key={ethSurplusByChain[index].chainId}>
-                <div key={ethSurplusByChain[index].chainId} className="flex items-center gap-2">
-                  <ChainLogo chainId={ethSurplusByChain[index].chainId as JBChainId} />
-                  <span>{NETWORKS[surplus.chainId].label}</span>
-                </div>
-                {/* (NOTE: Following comment copied from Revnet: 
+        title={
+          ethSurplusByChain?.length && ethSurplusByChain.length > 0 ? (
+            <div className="flex flex-col gap-2">
+              {ethSurplusByChain?.map((surplus, index) => (
+                <div
+                  className="flex items-center justify-between gap-4"
+                  key={ethSurplusByChain[index].chainId}
+                >
+                  <div
+                    key={ethSurplusByChain[index].chainId}
+                    className="flex items-center gap-2"
+                  >
+                    <ChainLogo
+                      chainId={ethSurplusByChain[index].chainId as JBChainId}
+                    />
+                    <span>{NETWORKS[surplus.chainId].label}</span>
+                  </div>
+                  {/* (NOTE: Following comment copied from Revnet: 
                   "TODO maybe show USD-converted value here instead?" */}
-                <span className="font-medium whitespace-nowrap">
-                  <NativeTokenValue wei={surplus.surplus ?? 0n} />
-                </span>
-              </div>
-            ))}
-            
-          </div>
-        : undefined}
+                  <span className="whitespace-nowrap font-medium">
+                    <NativeTokenValue wei={surplus.surplus ?? 0n} />
+                  </span>
+                </div>
+              ))}
+            </div>
+          ) : undefined
+        }
       >
-        <span><NativeTokenValue wei={totalEthSurplus ?? 0n} /></span>
+        <span>
+          <NativeTokenValue wei={totalEthSurplus} />
+        </span>
       </Tooltip>
     )
   }, [totalEthSurplus, ethSurplusByChain])
 
   const availableToPayout = <NativeTokenValue wei={distributableAmount.value} />
+
   return {
     treasuryBalance,
     availableToPayout,

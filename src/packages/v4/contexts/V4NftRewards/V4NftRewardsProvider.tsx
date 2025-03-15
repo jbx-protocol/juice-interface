34e956cafc7f6eae37e24f9a32afb24f3699b049
johnnyd-eth <johnnyd.dev@gmail.com>
2025-03-15 12:32:05 +1100

    feat: overspending specific nft flow for v4

--- src/packages/v4/contexts/V4NftRewards/V4NftRewardsProvider.tsx
+++ src/packages/v4/contexts/V4NftRewards/V4NftRewardsProvider.tsx
@@ -1,26 +1,21 @@
 import {
   jb721TiersHookStoreAbi,
   useJBRulesetContext,
+  useReadJb721TiersHookContractUri,
   useReadJb721TiersHookPricingContext,
   useReadJb721TiersHookStoreAddress,
   useReadJb721TiersHookStoreFlagsOf,
   useReadJb721TiersHookStoreTiersOf,
 } from 'juice-sdk-react'
+import React, { createContext } from 'react'
+import { DEFAULT_NFT_FLAGS_V4, DEFAULT_NFT_PRICING, EMPTY_NFT_COLLECTION_METADATA } from 'redux/slices/v2v3/editingV2Project'
+
 import { JB721GovernanceType } from 'models/nftRewards'
 import { V2V3CurrencyOption } from 'packages/v2v3/models/currencyOption'
-import React, { createContext } from 'react'
-import { DEFAULT_NFT_PRICING } from 'redux/slices/v2v3/editingV2Project'
-import { NftRewardsData } from 'redux/slices/v2v3/shared/v2ProjectTypes'
+import { V4NftRewardsData } from 'packages/v4/models/nfts'
 import { CIDsOfNftRewardTiersResponse } from 'utils/nftRewards'
 import { ContractFunctionReturnType } from 'viem'
 import { useNftRewards } from './useNftRewards'
-
-const DEFAULT_NFT_FLAGS = {
-  noNewTiersWithReserves: false,
-  noNewTiersWithVotes: false,
-  noNewTiersWithOwnerMinting: false,
-  preventOverspending: false,
-}
 
 // TODO: This should be imported from the SDK
 export type JB721TierV4 = ContractFunctionReturnType<
@@ -31,10 +26,7 @@ export type JB721TierV4 = ContractFunctionReturnType<
 
 type NftRewardsContextType = {
   // nftRewards: is useReadJb721TiersHookStoreTiersOf.data returned
-  nftRewards: Omit<
-    NftRewardsData,
-    'flags' | 'collectionMetadata' | 'postPayModal'
-  >
+  nftRewards: V4NftRewardsData
   loading: boolean | undefined
 }
 
@@ -42,9 +34,8 @@ export const V4NftRewardsContext = createContext<NftRewardsContextType>({
   nftRewards: {
     CIDs: undefined,
     rewardTiers: undefined,
-    // postPayModal: undefined,
-    // collectionMetadata: EMPTY_NFT_COLLECTION_METADATA,
-    // flags: DEFAULT_NFT_FLAGS,
+    collectionMetadata: EMPTY_NFT_COLLECTION_METADATA,
+    flags: DEFAULT_NFT_FLAGS_V4,
     governanceType: JB721GovernanceType.NONE,
     pricing: DEFAULT_NFT_PRICING,
   },
@@ -64,7 +55,7 @@ export const V4NftRewardsProvider: React.FC<
   const tiersOf = useReadJb721TiersHookStoreTiersOf({
     address: storeAddress.data,
     args: [
-      dataHookAddress ?? `0x${'0'.repeat(40)}`,
+      dataHookAddress ?? zeroAddress,
       [], // _categories
       false, // _includeResolvedUri, return in each tier a result from a tokenUriResolver if one is included in the delegate
       0n, // _startingId
@@ -83,6 +74,9 @@ export const V4NftRewardsProvider: React.FC<
   const flags = useReadJb721TiersHookStoreFlagsOf({
     address: '0x7b1F4Ba6312A104E645B06Ab97e4CaA1ef0F773f',
   })
+
+  const { data: collectionMetadataUri } =
+      useReadJb721TiersHookContractUri({ address: dataHookAddress})
 
   const loading = React.useMemo(
     () =>
@@ -106,12 +100,11 @@ export const V4NftRewardsProvider: React.FC<
       rewardTiers: loadedRewardTiers,
       pricing: { currency },
       governanceType: JB721GovernanceType.NONE,
-      // collectionMetadata: {
-      //   ...EMPTY_NFT_COLLECTION_METADATA,
-      //   uri: collection
-      // }
-      // postPayModal: projectMetadata?.nftPaymentSuccessModal,
-      // flags: flags.data ?? DEFAULT_NFT_FLAGS,
+      collectionMetadata: {
+        ...EMPTY_NFT_COLLECTION_METADATA,
+        uri: collectionMetadataUri
+      },
+      flags: flags.data ?? DEFAULT_NFT_FLAGS_V4,
     },
     loading,
   }

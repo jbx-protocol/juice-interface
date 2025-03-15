e55c6adec64cfaee9fcdc9b162287ad2554d3049
johnnyd-eth <johnnyd.dev@gmail.com>
2025-03-15 12:41:18 +1100

    feat: move pause payments out of advanced in settings (v2/v3 and v4)

--- src/packages/v4/views/V4ProjectSettings/EditCyclePage/DetailsSection/DetailsSectionAdvanced.tsx
+++ src/packages/v4/views/V4ProjectSettings/EditCyclePage/DetailsSection/DetailsSectionAdvanced.tsx
@@ -1,20 +1,18 @@
-import { Trans } from '@lingui/macro'
-import { Form } from 'antd'
-import { AdvancedDropdown } from 'components/Project/ProjectSettings/AdvancedDropdown'
-import TooltipLabel from 'components/TooltipLabel'
-import { JuiceSwitch } from 'components/inputs/JuiceSwitch'
 import {
   CONTROLLER_CONFIG_EXPLANATION,
   TERMINAL_CONFIG_EXPLANATION,
   TERMINAL_MIGRATION_EXPLANATION
 } from 'components/strings'
 
+import { Trans } from '@lingui/macro'
+import { Form } from 'antd'
+import { JuiceSwitch } from 'components/inputs/JuiceSwitch'
+import { AdvancedDropdown } from 'components/Project/ProjectSettings/AdvancedDropdown'
+import TooltipLabel from 'components/TooltipLabel'
+
 export function DetailsSectionAdvanced() {
   return (
     <AdvancedDropdown>
-      <Form.Item name="pausePay">
-        <JuiceSwitch label={<Trans>Disable payments to this project</Trans>} />
-      </Form.Item>
       <Form.Item name="allowSetTerminals">
         <JuiceSwitch
           label={

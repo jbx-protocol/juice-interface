fa85325a0bdea9a22625b021ff622b524af8f95d
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-04-08 21:54:34 +1000

    Fix: inifite loop

--- src/packages/v4/views/V4ProjectSettings/EditCyclePage/hooks/useLoadEditCycleData.tsx
+++ src/packages/v4/views/V4ProjectSettings/EditCyclePage/hooks/useLoadEditCycleData.tsx
@@ -1,10 +1,5 @@
 import { useJBProjectId, useJBRuleset } from 'juice-sdk-react'
 import { useEffect, useState } from 'react'
-import {
-  deriveDurationOption,
-  deriveDurationUnit,
-  secondsToOtherUnit,
-} from 'utils/format/formatTime'
 
 import { Form } from 'antd'
 import { Ether } from 'juice-sdk-core'
@@ -13,6 +8,11 @@ import { usePayoutLimit } from 'packages/v4/hooks/usePayoutLimit'
 import { useV4CurrentPayoutSplits } from 'packages/v4/hooks/useV4CurrentPayoutSplits'
 import { useV4ReservedSplits } from 'packages/v4/hooks/useV4ReservedSplits'
 import { V4CurrencyName } from 'packages/v4/utils/currency'
+import {
+  deriveDurationOption,
+  deriveDurationUnit,
+  secondsToOtherUnit,
+} from 'utils/format/formatTime'
 import { EditCycleFormFields } from '../EditCycleFormFields'
 
 /** Loads project FC data directly into an AntD form instance */
@@ -34,59 +34,71 @@ export const useLoadEditCycleData = () => {
 
   const [editCycleForm] = Form.useForm<EditCycleFormFields>()
 
-  const payoutLimitAmount = new Ether(payoutLimit.amount).toFloat()
-
   useEffect(() => {
-    if (ruleset && rulesetMetadata) {
-      const duration = Number(ruleset.duration)
-
-      const issuanceRate = upcomingRuleset?.weight.toFloat() ?? 0
-      const reservedPercent = rulesetMetadata.reservedPercent.formatPercentage()
-      // : DefaultTokenSettings.reservedTokensPercentage
-
-      const weightCutPercent = ruleset.weightCutPercent.formatPercentage()
-      // : DefaultTokenSettings.discountRate
-
-      const cashOutTaxRate = rulesetMetadata.cashOutTaxRate.formatPercentage()
-      // : DefaultTokenSettings.cashOutTaxRate
-
-      const allowOwnerMinting = rulesetMetadata.allowOwnerMinting
-      // : DefaultTokenSettings.tokenMinting
-
-      const tokenTransfers = !rulesetMetadata.pauseCreditTransfers
-      // : DefaultTokenSettings.pauseTransfers
-
-      const formData: EditCycleFormFields = {
-        duration: secondsToOtherUnit({
-          duration,
-          unit: deriveDurationUnit(duration),
-        }),
-        durationUnit: deriveDurationOption(duration),
-        approvalHook: ruleset.approvalHook,
-        allowSetTerminals: rulesetMetadata.allowSetTerminals,
-        allowSetController: rulesetMetadata.allowSetController,
-        allowTerminalMigration: rulesetMetadata.allowTerminalMigration,
-        pausePay: rulesetMetadata.pausePay,
-        payoutSplits: payoutSplits ?? [],
-        payoutLimit: payoutLimitAmount,
-        payoutLimitCurrency: V4CurrencyName(payoutLimit?.currency) ?? 'ETH',
-        holdFees: rulesetMetadata?.holdFees,
-        issuanceRate,
-        reservedPercent,
-        reservedTokensSplits,
-        weightCutPercent,
-        cashOutTaxRate,
-        allowOwnerMinting,
-        tokenTransfers,
-        // nftRewards: currentProjectData.nftRewards,
-        // useDataSourceForRedeem:
-        //   rulesetMetadata.useDataSourceForRedeem,
-        memo: '',
-      }
-      setInitialFormData(formData)
-      editCycleForm.setFieldsValue(formData)
+    if (!ruleset || !rulesetMetadata) {
+      return
     }
-  }, [ruleset, rulesetMetadata, payoutSplits])
+    const payoutLimitAmount = new Ether(payoutLimit.amount).toFloat()
+
+    const duration = Number(ruleset.duration)
+
+    const issuanceRate = upcomingRuleset?.weight.toFloat() ?? 0
+    const reservedPercent = rulesetMetadata.reservedPercent.formatPercentage()
+    // : DefaultTokenSettings.reservedTokensPercentage
+
+    const weightCutPercent = ruleset.weightCutPercent.formatPercentage()
+    // : DefaultTokenSettings.discountRate
+
+    const cashOutTaxRate = rulesetMetadata.cashOutTaxRate.formatPercentage()
+    // : DefaultTokenSettings.cashOutTaxRate
+
+    const allowOwnerMinting = rulesetMetadata.allowOwnerMinting
+    // : DefaultTokenSettings.tokenMinting
+
+    const tokenTransfers = !rulesetMetadata.pauseCreditTransfers
+    // : DefaultTokenSettings.pauseTransfers
+
+    const formData: EditCycleFormFields = {
+      duration: secondsToOtherUnit({
+        duration,
+        unit: deriveDurationUnit(duration),
+      }),
+      durationUnit: deriveDurationOption(duration),
+      approvalHook: ruleset.approvalHook,
+      allowSetTerminals: rulesetMetadata.allowSetTerminals,
+      allowSetController: rulesetMetadata.allowSetController,
+      allowTerminalMigration: rulesetMetadata.allowTerminalMigration,
+      pausePay: rulesetMetadata.pausePay,
+      payoutSplits: payoutSplits ?? [],
+      payoutLimit: payoutLimitAmount,
+      payoutLimitCurrency: V4CurrencyName(payoutLimit?.currency) ?? 'ETH',
+      holdFees: rulesetMetadata?.holdFees,
+      issuanceRate,
+      reservedPercent,
+      reservedTokensSplits,
+      weightCutPercent,
+      cashOutTaxRate,
+      allowOwnerMinting,
+      tokenTransfers,
+      // nftRewards: currentProjectData.nftRewards,
+      // useDataSourceForRedeem:
+      //   rulesetMetadata.useDataSourceForRedeem,
+      memo: '',
+    }
+    if (!initialFormData) {
+      setInitialFormData(formData)
+    }
+    editCycleForm.setFieldsValue(formData)
+  }, [
+    initialFormData,
+    editCycleForm,
+    ruleset,
+    rulesetMetadata,
+    payoutSplits,
+    payoutLimit,
+    reservedTokensSplits,
+    upcomingRuleset?.weight,
+  ])
 
   return {
     initialFormData,

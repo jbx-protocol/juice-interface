c14ce95085f705444d43969488ba85bb01cf5f4b
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-04-13 09:00:34 +1000

    feat: omnichain stats

--- src/packages/v4/views/V4ProjectDashboard/hooks/useV4ProjectHeader.ts
+++ src/packages/v4/views/V4ProjectDashboard/hooks/useV4ProjectHeader.ts
@@ -2,18 +2,16 @@ import { SubtitleType, useSubtitle } from 'hooks/useSubtitle'
 import {
   useJBContractContext,
   useJBProjectMetadataContext,
-  useSuckers,
 } from 'juice-sdk-react'
 
 import { BigNumber } from '@ethersproject/bignumber'
-import { useQuery } from '@tanstack/react-query'
 import { useGnosisSafe } from 'hooks/safe/useGnosisSafe'
 import { useProjectTrendingPercentageIncrease } from 'hooks/useProjectTrendingPercentageIncrease'
 import { GnosisSafe } from 'models/safe'
 import { ProjectsDocument } from 'packages/v4/graphql/client/graphql'
+import { useOmnichainSubgraphQuery } from 'packages/v4/graphql/useOmnichainSubgraphQuery'
 import { useSubgraphQuery } from 'packages/v4/graphql/useSubgraphQuery'
 import useV4ProjectOwnerOf from 'packages/v4/hooks/useV4ProjectOwnerOf'
-import { fetchProjectsBySuckers } from 'packages/v4/utils/fetchProjectsBySuckers'
 import React from 'react'
 
 export interface ProjectHeaderData {
@@ -48,58 +46,53 @@ export const useV4ProjectHeader = (): ProjectHeaderData => {
   })
   const projectStatsData = data?.projects?.[0]
 
-  const suckers = useSuckers()
-
-  const omnichainProjects = useQuery({
-    enabled: !suckers.isLoading,
-    queryKey: ['projectData', suckers],
-    queryFn: async () => {
-      if (!suckers.data) {
-        return null
-      }
-      return await fetchProjectsBySuckers(suckers.data)
+  const omnichainProjects = useOmnichainSubgraphQuery(ProjectsDocument, {
+    where: {
+      projectId: projectIdNum,
     },
   })
 
   const paymentsCount = React.useMemo(() => {
-    if (omnichainProjects.isLoading || !omnichainProjects.data) {
+    if (omnichainProjects?.isLoading || !omnichainProjects?.data) {
       // Fallback to the local chain as subgraph omnichain call can take a while
       return projectStatsData?.paymentsCount ?? 0
     }
-    return omnichainProjects.data.reduce((acc, result) => {
-      return acc + (result.data?.paymentsCount ?? 0)
+    return omnichainProjects?.data.reduce((acc, result) => {
+      return acc + (result.value.response.projects[0]?.paymentsCount ?? 0)
     }, 0)
   }, [
-    omnichainProjects.data,
-    omnichainProjects.isLoading,
+    omnichainProjects?.data,
+    omnichainProjects?.isLoading,
     projectStatsData?.paymentsCount,
   ])
 
   const createdAt = projectStatsData?.createdAt
 
   const totalVolume = React.useMemo(() => {
-    if (omnichainProjects.isLoading || !omnichainProjects.data) {
+    if (omnichainProjects?.isLoading || !omnichainProjects?.data) {
       return projectStatsData?.volume ?? 0n
     }
-    return omnichainProjects.data.reduce((acc, result) => {
-      return acc + BigInt(result.data?.volume ?? 0)
+    return omnichainProjects?.data.reduce((acc, result) => {
+      return acc + BigInt(result.value.response.projects[0]?.volume ?? 0)
     }, 0n)
   }, [
-    omnichainProjects.data,
-    omnichainProjects.isLoading,
+    omnichainProjects?.data,
+    omnichainProjects?.isLoading,
     projectStatsData?.volume,
   ])
 
   const trendingVolume = React.useMemo(() => {
-    if (omnichainProjects.isLoading || !omnichainProjects.data) {
+    if (omnichainProjects?.isLoading || !omnichainProjects?.data) {
       return projectStatsData?.trendingVolume ?? 0n
     }
-    return omnichainProjects.data.reduce((acc, result) => {
-      return acc + BigInt(result.data?.trendingVolume ?? 0)
+    return omnichainProjects?.data.reduce((acc, result) => {
+      return (
+        acc + BigInt(result.value.response.projects[0]?.trendingVolume ?? 0)
+      )
     }, 0n)
   }, [
-    omnichainProjects.data,
-    omnichainProjects.isLoading,
+    omnichainProjects?.data,
+    omnichainProjects?.isLoading,
     projectStatsData?.trendingVolume,
   ])
 

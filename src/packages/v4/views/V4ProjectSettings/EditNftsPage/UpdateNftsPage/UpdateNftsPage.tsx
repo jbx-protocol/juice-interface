1c9ef9ff28b00439e7958f35a81091b7d4d14866
johnnyd-eth <johnnyd.dev@gmail.com>
2025-04-13 21:20:06 +1000

    remove edit collection

--- src/packages/v4/views/V4ProjectSettings/EditNftsPage/UpdateNftsPage/UpdateNftsPage.tsx
+++ src/packages/v4/views/V4ProjectSettings/EditNftsPage/UpdateNftsPage/UpdateNftsPage.tsx
@@ -1,16 +1,15 @@
 import { t } from '@lingui/macro'
 import { Tabs } from 'antd'
-import { EditCollectionDetailsSection } from './EditCollectionDetailsSection'
 import { EditNftsSection } from './EditNftsSection'
 
 export function UpdateNftsPage() {
   const items = [
     { label: t`NFTs`, key: 'nfts', children: <EditNftsSection /> },
-    {
-      label: t`Collection details`,
-      key: 'collection',
-      children: <EditCollectionDetailsSection />,
-    },
+    // {
+    //   label: t`Collection details`,
+    //   key: 'collection',
+    //   children: <EditCollectionDetailsSection />,
+    // },
   ]
 
   return <Tabs items={items} />

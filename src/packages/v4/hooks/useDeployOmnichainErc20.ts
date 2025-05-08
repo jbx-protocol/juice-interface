dae6aa6d6486edc3f4af3318c4c192e64da1640e
johnnyd-eth <johnnyd.dev@gmail.com>
2025-05-08 22:22:12 +1000

    feat: multichain erc20 llaunch v4

---
+++ src/packages/v4/hooks/useDeployOmnichainErc20.ts
@@ -0,0 +1,47 @@
+import { JBChainId, createSalt, jbProjectDeploymentAddresses } from 'juice-sdk-core'
+import { jbControllerAbi, useGetRelayrTxQuote } from 'juice-sdk-react'
+import { ContractFunctionArgs, encodeFunctionData } from 'viem'
+
+import { useWallet } from 'hooks/Wallet'
+import { Address } from 'viem'
+
+export function useDeployOmnichainErc20() {
+  const { userAddress } = useWallet()
+  const { getRelayrTxQuote } = useGetRelayrTxQuote()
+
+  async function deployOmnichainErc20(
+    deployData: {
+      [k in JBChainId]?: ContractFunctionArgs<typeof jbControllerAbi, 'nonpayable', 'deployERC20For'>
+    },
+    chainIds: JBChainId[],
+  ) {
+    if (!userAddress) return
+    const salt = createSalt()
+
+    const relayrTransactions = chainIds.map(chainId => {
+      const args = deployData[chainId]
+      if (!args) throw new Error('No deploy data for chain ' + chainId)
+      const encoded = encodeFunctionData({
+        abi: jbControllerAbi,
+        functionName: 'deployERC20For',
+        args,
+      })
+      // use controller address for each chain
+      const to = jbProjectDeploymentAddresses.JBController[chainId] as Address
+      return {
+        data: {
+          from: userAddress,
+          to,
+          value: 0n,
+          gas: 300_000n * BigInt(chainIds.length),
+          data: encoded,
+        },
+        chainId,
+      }
+    })
+
+    return getRelayrTxQuote(relayrTransactions)
+  }
+
+  return { deployOmnichainErc20 }
+}

0fb16596d9eef7504ca4644bce0a1a44f55e4297
peripheralist <79433522+peripheralist@users.noreply.github.com>
2025-05-18 03:47:59 -0400

    Implement bendystraw (#4614)

--- src/packages/v4/views/V4ProjectDashboard/hooks/useV4ProjectHeader.ts
+++ src/packages/v4/views/V4ProjectDashboard/hooks/useV4ProjectHeader.ts
@@ -1,18 +1,17 @@
 import { SubtitleType, useSubtitle } from 'hooks/useSubtitle'
 import {
+  useJBChainId,
   useJBContractContext,
   useJBProjectMetadataContext,
 } from 'juice-sdk-react'
 
 import { BigNumber } from '@ethersproject/bignumber'
+import { useProjectQuery, useSuckerGroupQuery } from 'generated/v4/graphql'
 import { useGnosisSafe } from 'hooks/safe/useGnosisSafe'
 import { useProjectTrendingPercentageIncrease } from 'hooks/useProjectTrendingPercentageIncrease'
+import { bendystrawClient } from 'lib/apollo/bendystrawClient'
 import { GnosisSafe } from 'models/safe'
-import { ProjectsDocument } from 'packages/v4/graphql/client/graphql'
-import { useOmnichainSubgraphQuery } from 'packages/v4/graphql/useOmnichainSubgraphQuery'
-import { useSubgraphQuery } from 'packages/v4/graphql/useSubgraphQuery'
 import useV4ProjectOwnerOf from 'packages/v4/hooks/useV4ProjectOwnerOf'
-import React from 'react'
 
 export interface ProjectHeaderData {
   title: string | undefined
@@ -30,75 +29,35 @@ export interface ProjectHeaderData {
 export const useV4ProjectHeader = (): ProjectHeaderData => {
   const { projectId } = useJBContractContext()
   const { metadata } = useJBProjectMetadataContext()
+  const chainId = useJBChainId()
   const projectMetadata = metadata?.data
 
   const { data: projectOwnerAddress } = useV4ProjectOwnerOf()
 
   const projectIdNum = parseInt(projectId.toString())
 
-  const { data } = useSubgraphQuery({
-    document: ProjectsDocument,
+  const { data: project } = useProjectQuery({
+    client: bendystrawClient,
     variables: {
-      where: {
-        projectId: projectIdNum,
-      },
-    },
-  })
-  const projectStatsData = data?.projects?.[0]
-
-  const omnichainProjects = useOmnichainSubgraphQuery(ProjectsDocument, {
-    where: {
       projectId: projectIdNum,
+      chainId: Number(chainId),
     },
+    skip: !projectId || !chainId,
   })
 
-  const paymentsCount = React.useMemo(() => {
-    if (omnichainProjects?.isLoading || !omnichainProjects?.data) {
-      // Fallback to the local chain as subgraph omnichain call can take a while
-      return projectStatsData?.paymentsCount ?? 0
-    }
-    return omnichainProjects?.data.reduce((acc, result) => {
-      return acc + (result.value.response.projects[0]?.paymentsCount ?? 0)
-    }, 0)
-  }, [
-    omnichainProjects?.data,
-    omnichainProjects?.isLoading,
-    projectStatsData?.paymentsCount,
-  ])
+  const { data: suckerGroup } = useSuckerGroupQuery({
+    client: bendystrawClient,
+    variables: {
+      id: project?.project?.suckerGroupId || '',
+    },
+    skip: !project?.project?.suckerGroupId,
+  })
 
-  const createdAt = projectStatsData?.createdAt
-
-  const totalVolume = React.useMemo(() => {
-    if (omnichainProjects?.isLoading || !omnichainProjects?.data) {
-      return projectStatsData?.volume ?? 0n
-    }
-    return omnichainProjects?.data.reduce((acc, result) => {
-      return acc + BigInt(result.value.response.projects[0]?.volume ?? 0)
-    }, 0n)
-  }, [
-    omnichainProjects?.data,
-    omnichainProjects?.isLoading,
-    projectStatsData?.volume,
-  ])
-
-  const trendingVolume = React.useMemo(() => {
-    if (omnichainProjects?.isLoading || !omnichainProjects?.data) {
-      return projectStatsData?.trendingVolume ?? 0n
-    }
-    return omnichainProjects?.data.reduce((acc, result) => {
-      return (
-        acc + BigInt(result.value.response.projects[0]?.trendingVolume ?? 0)
-      )
-    }, 0n)
-  }, [
-    omnichainProjects?.data,
-    omnichainProjects?.isLoading,
-    projectStatsData?.trendingVolume,
-  ])
+  const sg = suckerGroup?.suckerGroup
 
   const last7DaysPercent = useProjectTrendingPercentageIncrease({
-    totalVolume: BigNumber.from(totalVolume ?? 0),
-    trendingVolume: BigNumber.from(trendingVolume ?? 0),
+    totalVolume: BigNumber.from(sg?.volume ?? 0),
+    trendingVolume: BigNumber.from(sg?.trendingVolume ?? 0),
   })
 
   const { data: gnosisSafe } = useGnosisSafe(projectOwnerAddress)
@@ -110,11 +69,11 @@ export const useV4ProjectHeader = (): ProjectHeaderData => {
     subtitle,
     projectId: projectIdNum,
     owner: projectOwnerAddress,
-    payments: paymentsCount,
-    totalVolume,
+    payments: sg?.paymentsCount,
+    totalVolume: sg?.volume,
     last7DaysPercent,
     gnosisSafe,
     archived: projectMetadata?.archived,
-    createdAtSeconds: createdAt,
+    createdAtSeconds: sg?.createdAt,
   }
 }

b691a429db9e2f0af519d0cf1a128186491f7bee
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-05-18 20:28:07 +1000

    fix: wallets n stuff

--- src/hooks/Wallet/hooks/useChangeNetworks.ts
+++ src/hooks/Wallet/hooks/useChangeNetworks.ts
@@ -1,6 +1,8 @@
 import { useSetChain } from '@web3-onboard/react'
 import { readNetwork } from 'constants/networks'
 import { useCallback } from 'react'
+import { useSwitchChain } from 'wagmi'
+import { useChain } from './useChain'
 
 /**
  * Attempts to sync the user wallet's chain with the readNetwork (hard-coded per environment)
@@ -8,19 +10,31 @@ import { useCallback } from 'react'
  */
 export function useChangeNetworks() {
   const [{ chains }, setChain] = useSetChain()
+  const { switchChain } = useSwitchChain()
+  const x = useChain()
+  // console.log(x)
 
-  const changeNetworks = useCallback(async (chainId?: number) => { // should be JBChainId
-    const chain = chains.find(c => Number(c.id) === (chainId ?? readNetwork.chainId))
-    if (!chain) {
-      console.error('FATAL: Chain not found')
-      throw new Error('FATAL: Chain not found')
-    }
+  const changeNetworks = useCallback(
+    async (chainId?: number) => {
+      // should be JBChainId
+      const chain = chains.find(
+        c => Number(c.id) === (chainId ?? readNetwork.chainId),
+      )
+      if (!chain) {
+        console.error('FATAL: Chain not found')
+        throw new Error('FATAL: Chain not found')
+      }
 
-    return await setChain({
-      chainId: chain.id,
-      chainNamespace: chain.namespace,
-    })
-  }, [chains, setChain])
+      await setChain({
+        chainId: chain.id,
+        chainNamespace: chain.namespace,
+      })
+
+      // switch network on wagmi too. this hopefully ensures wagmi's internal state is in sync with the wallet
+      await switchChain?.({ chainId: parseInt(chain.id) })
+    },
+    [chains, setChain, switchChain],
+  )
 
   return changeNetworks
 }
--- src/hooks/Wallet/hooks/useConnect.ts
+++ src/hooks/Wallet/hooks/useConnect.ts
@@ -1,21 +1,37 @@
 import { ConnectOptions } from '@web3-onboard/core'
 import { useConnectWallet } from '@web3-onboard/react'
 import { useCallback } from 'react'
-import { useConnect as useConnectWagmi } from 'wagmi'
+import { useConnectors, useConnect as useConnectWagmi } from 'wagmi'
 
 export function useConnect() {
   const [, connect] = useConnectWallet()
   const { connect: connectWagmi } = useConnectWagmi()
+  const connectors = useConnectors()
 
+  /**
+   * A wrapper on web3-onboard's connect function to ensure we sync the connection state to wagmi too.
+   */
   const _connect = useCallback(
     async (opts?: ConnectOptions) => {
       const walletState = await connect(opts)
-      const wagmiConnector = walletState[0]?.wagmiConnector
-      if (wagmiConnector) {
-        await connectWagmi({ connector: wagmiConnector })
+
+      const connectedWalletLabel = walletState?.[0]?.label
+      console.info('🧃 [web3-onboard] connected to', walletState)
+
+      /**
+       * Find the wagmi connector for the web3-onboard-connected wallet.
+       *
+       * This may be flaky, idk.
+       */
+      const connector = connectors.find(
+        c => c.name.toLowerCase() === connectedWalletLabel.toLowerCase(),
+      )
+      if (connector) {
+        await connectWagmi({ connector })
+        console.info('🧃 [wagmi] connected to', walletState)
       }
     },
-    [connect, connectWagmi],
+    [connect, connectWagmi, connectors],
   )
 
   return _connect

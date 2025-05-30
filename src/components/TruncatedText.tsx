0748b9099107ea046cbb415a51047c95a0259c72
johnnyd-eth <johnnyd.dev@gmail.com>
2025-05-31 05:59:39 +1000

    feat: truncate project tagline to 3 lines

--- src/components/TruncatedText.tsx
+++ src/components/TruncatedText.tsx
@@ -4,14 +4,17 @@ import { twMerge } from 'tailwind-merge'
 interface TruncatedTextProps {
   text: ReactNode
   className?: string
+  lines?: number
 }
 
 export const TruncatedText: React.FC<
   React.PropsWithChildren<TruncatedTextProps>
-> = ({ text, className }) => {
+> = ({ text, className, lines = 1 }) => {
+  const lineClampClass = lines === 1 ? 'truncate' : `line-clamp-${lines}`
+  
   return (
     <div
-      className={twMerge('truncate', className)}
+      className={twMerge(lineClampClass, className)}
       title={typeof text === 'string' ? text : undefined}
     >
       {text}

0b2ebd1d6aaef59bbb87d904e0f3f5632c01ba9a
johnnyd-eth <johnnyd.dev@gmail.com>
2025-06-04 18:37:55 +1000

    fix: upcoming first cycle nfts disabled

--- src/packages/v4/hooks/useNftRewardsEnabledForPay.ts
+++ src/packages/v4/hooks/useNftRewardsEnabledForPay.ts
@@ -1,12 +1,17 @@
-import { JBRulesetContext, useJBRulesetContext } from 'juice-sdk-react'
+import { JBRulesetContext, useJBProjectId, useJBRulesetContext, useJBUpcomingRuleset } from 'juice-sdk-react'
+
 import React from 'react'
-import { zeroAddress } from 'viem'
 import { useV4NftRewards } from '../contexts/V4NftRewards/V4NftRewardsProvider'
+import { zeroAddress } from 'viem'
 
 type RulesetMetadata = JBRulesetContext['rulesetMetadata']['data']
 
 export function useNftRewardsEnabledForPay() {
   const jbRuleset = useJBRulesetContext()
+  const { projectId, chainId } = useJBProjectId()
+  const { rulesetMetadata: upcomingRulesetMetadata } = useJBUpcomingRuleset({
+    projectId, chainId
+  })
   const { nftRewards } = useV4NftRewards()
 
   const hasNftRewards = React.useMemo(
@@ -14,7 +19,13 @@ export function useNftRewardsEnabledForPay() {
     [nftRewards.rewardTiers],
   )
 
-  return hasNftRewards && hasDataSourceForPay(jbRuleset.rulesetMetadata.data)
+  let _effectiveRulesetMetadata: RulesetMetadata = jbRuleset.rulesetMetadata.data
+
+  if (jbRuleset.ruleset?.data?.cycleNumber === 0) {
+    _effectiveRulesetMetadata = upcomingRulesetMetadata
+  }
+
+  return hasNftRewards && hasDataSourceForPay(_effectiveRulesetMetadata)
 }
 
 const hasDataSourceForPay = (rulesetMetadata: RulesetMetadata) => {

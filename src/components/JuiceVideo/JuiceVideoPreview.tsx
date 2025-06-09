58896de2b573340695e9830dd338c3314aec8df0
johnnyd-eth <johnnyd.dev@gmail.com>
2025-06-09 12:11:23 +1000

    feat: video autoload

--- src/components/JuiceVideo/JuiceVideoPreview.tsx
+++ src/components/JuiceVideo/JuiceVideoPreview.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 import { twJoin } from 'tailwind-merge'
+import { useState } from 'react'
 
 export function JuiceVideoPreview({
   src,
@@ -13,16 +13,18 @@ export function JuiceVideoPreview({
   return (
     <video
       controls
+      muted
+      preload="auto"
+      loop
+      autoPlay
       className={twJoin(
         'max-h-[50vh] max-w-[458px] md:max-h-[60vh]',
         loading ? 'h-[50vh] w-96' : '',
         widthClass,
       )}
-      autoPlay
-      preload="none"
       onLoadedData={() => setLoading(false)}
     >
-      <source src={src} type="video/mp4" />
+      <source src={src} />
       Your browser does not support the video tag.
     </video>
   )

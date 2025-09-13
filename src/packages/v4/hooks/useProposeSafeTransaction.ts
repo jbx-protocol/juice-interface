c39576345dcc4a18ba2aa8bbc029fc23d6f1dd12
jango <77952627+mejango@users.noreply.github.com>
2025-09-13 01:33:34 -0300

    Update useProposeSafeTransaction.ts (#4644)
    Signed-off-by: jango <77952627+mejango@users.noreply.github.com>

--- src/packages/v4/hooks/useProposeSafeTransaction.ts
+++ src/packages/v4/hooks/useProposeSafeTransaction.ts
@@ -94,13 +94,15 @@ export function useProposeSafeTransaction({ safeAddress }: ProposeSafeTransactio
         options,
         onlyCalls: true,
       })
-      const safeTxHash = await protocolKit.getTransactionHash(safeTx)
-      const signature = await protocolKit.signTransaction(safeTx)
 
+      const signedSafeTx = await protocolKit.signTransaction(safeTx)
+      const safeTxHash = await protocolKit.getTransactionHash(signedSafeTx)
+      const signature = signedSafeTx.getSignature(userAddress)
+      
       // propose transaction to service
       await apiKit.proposeTransaction({
         safeAddress: checksumSafeAddress,
-        safeTransactionData: safeTx.data,
+        safeTransactionData: signedSafeTx.data,
         safeTxHash,
         senderAddress: checksumUserAddress,
         senderSignature: signature.data,
@@ -108,7 +110,7 @@ export function useProposeSafeTransaction({ safeAddress }: ProposeSafeTransactio
 
       return {
         safeTxHash,
-        nonce: safeTx.data.nonce.toString(),
+        nonce: signedSafeTx.data.nonce.toString(),
       }
     } catch (error: unknown) {
       const message = error instanceof Error ? error.message : 'An unknown error occurred.'

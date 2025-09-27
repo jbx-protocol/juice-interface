6ec8cb5c2090c9bd60856679153eb51aa7272483
Jordy McNab <jordy@usecapsule.com>
2025-09-27 13:03:49 -0500

    feat: update route utilities and ProjectCard to support v5 projects

--- src/packages/v4v5/utils/routes.ts
+++ src/packages/v4v5/utils/routes.ts
@@ -12,18 +12,45 @@ export const v4ProjectRoute = ({
   return `/v4/${chainSlug}:${projectId?.toString()}`
 }
 
+export const v5ProjectRoute = ({
+  chainId,
+  projectId,
+}: {
+  chainId: number
+  projectId?: number
+}) => {
+  const chainSlug = getChainSlug(chainId)
+  return `/v5/${chainSlug}:${projectId?.toString()}`
+}
+
+export const v4v5ProjectRoute = ({
+  chainId,
+  projectId,
+  version,
+}: {
+  chainId: number
+  projectId?: number
+  version: 4 | 5
+}) => {
+  const chainSlug = getChainSlug(chainId)
+  return `/v${version}/${chainSlug}:${projectId?.toString()}`
+}
+
 export const settingsPagePath = (
   {
     projectId,
     chainId,
+    version = 4,
   }: {
     projectId: number
     chainId: number
+    version?: 4 | 5
   },
   settingsPage?: SettingsPageKey,
 ) => {
-  return `${v4ProjectRoute({
+  return `${v4v5ProjectRoute({
     chainId,
     projectId,
+    version,
   })}/settings/${settingsPage ?? ''}`
 }
